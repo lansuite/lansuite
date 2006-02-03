d39b4f33dd53fbe4f2f03f98101f97582e8128e9
jochenjung <jochenjung@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2006-02-03 16:58:15 +0000

    Doku erweitert

--- trunk/modules/board/docu/de_help.php
+++ trunk/modules/board/docu/de_help.php
@@ -10,5 +10,9 @@ $helplet['key'][2] = 'Lesezeichen';
 $helplet['value'][2] = 'Um das wiederauffinden eines Beitrages zu erleichtern, gibt es am Ende eines jeden Threads die Option "Diesen Beitrag in meine Lesezeichen aufnehmen". Aktiviert man dieses Häkchen, so kann man über den Link "Lesezeichen" wieder direkt zu diesem Thread springen';
 $helplet['key'][3] = 'Thread abonnieren';
 $helplet['value'][3] = 'Damit man auf Änderungen in einem interessanten Thread aufmerksam gemacht werden kann, lässt sich am Ende eines jeden Threads wahlweiße die Abonnierung als System-Mail, oder als Internet-Mail aktivieren';
+$helplet['key'][4] = 'Nich gelesene Threads';
+$helplet['value'][4] = 'Sofern man eingeloggt ist, werden Threads mit noch nicht gelesenen Posts in der Übersicht mit dem Wort "Neu" markiert. Alle anderen, sowie Nachrichten, die älter als eine Woche sind, werden mit "Alt" markiert. Somit ist immer leicht zu erkennen, welcher Thread seid dem letzten Post darin nicht mehr gelesen wurde.';
+$helplet['key'][5] = 'Spam-bekämpfung';
+$helplet['value'][5] = 'Als Admin hat man mit der Admin-Suche die Möglichkeit nach einem bestimmten Titel, oder einer bestimmten Person zu suchen und anschließend mit einem Klick alle auf die Suche zutreffenden Posts zu löschen. Somit geht das entfernen von Spam wesentlich einfacher, als das hinzufügen.';
 
 ?>
