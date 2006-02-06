0af52f7d617eab5956c5ae3e3d7424639e537475
genesis_81 <genesis_81@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2006-02-06 19:47:55 +0000

    Feature 292 Nun k�nnen auch Archive in die Bildergallery geladen werden.

--- modules/picgallery/download.php
+++ modules/picgallery/download.php
@@ -18,9 +18,15 @@
 //
 // Get pic data
 //
+$_GET['picurl'] = str_replace("..","",$_GET['picurl']);
 $picinfo = @GetImageSize("ext_inc/picgallery/$_GET[picurl]");
 if($picinfo['2'] == "1" OR $picinfo['2'] == "2" OR $picinfo['2'] == "3")
 {
+header("content-type: application/octet-stream");
+header("content-disposition: attachment; filename=$_GET[picurl]");
+
+readfile("ext_inc/picgallery/$_GET[picurl]");
+}else{
 header("content-type: application/octet-stream");
 header("content-disposition: attachment; filename=$_GET[picurl]");
 
