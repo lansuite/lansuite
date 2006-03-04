60c153010a0e6d294321731a286a84384d34975c
genesis_81 <genesis_81@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2006-03-04 12:54:15 +0000

    Bugfix 346: Serchfunktion zeigte auch daten aus Internen Foren an

--- trunk/modules/board/search.php
+++ trunk/modules/board/search.php
@@ -1,5 +1,5 @@
 <?php
-$mastersearch = new MasterSearch($vars, "index.php?mod=board&action=search", "index.php?mod=board&action=thread&tid=", "");
+$mastersearch = new MasterSearch($vars, "index.php?mod=board&action=search", "index.php?mod=board&action=thread&tid=", " AND b.need_type <= " . $auth['type']);
 $mastersearch->LoadConfig("thread", $lang['board']['search_thread'], $lang['board']['search_result']);
 $mastersearch->PrintForm();
 $mastersearch->Search();
