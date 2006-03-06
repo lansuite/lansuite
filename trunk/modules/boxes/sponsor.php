7224f184cb06985008017e59b8df112b746c34e6
jochenjung <jochenjung@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2006-03-06 18:07:45 +0000

    - Bugfix: #332 Komplette �berarbeitung des Sponsorenmoduls. Sollte unter anderem die im Bugtracker genannten Bugs fixen (KnoX 06.03.2006)

--- trunk/modules/boxes/sponsor.php
+++ trunk/modules/boxes/sponsor.php
@@ -10,27 +10,27 @@ $sponsoren = $db->query("SELECT * FROM {$config['tables']['sponsor']}
 
 while ($sponsor = $db->fetch_array($sponsoren)){
 	$out = '';
-
+/*
 	// If entry is HTML-Code
 	if (substr($sponsor['pic_path'], 0, 12) == 'html-code://') {
 		$out = substr($sponsor["pic_path"], 12, strlen($sponsor["pic_path"]) - 12);
 
 	// Else add Image-Tag
 	} else {
-		$org_file_name = substr($sponsor["pic_path"], 0, strrpos($sponsor["pic_path"], "."));
-		$org_ending = substr($sponsor["pic_path"], strrpos($sponsor["pic_path"], "."), 5);
-		if (file_exists($org_file_name . "_button" . $org_ending)) {
-			$sponsor["pic_path"] = $org_file_name . "_button" . $org_ending;
+*/	
+    $file_name = 'ext_inc/banner/banner_'. substr($sponsor['pic_path'], strrpos($sponsor["pic_path"], 'ext_inc/banner/') + 15, strlen($sponsor['pic_path']));
+		if (file_exists($file_name)) {
+			$sponsor["pic_path"] = $file_name;
 
-			$ImgSize = @GetImageSize($sponsor["pic_path"]);
+			$ImgSize = @GetImageSize($file_name);
 			if (!$ImgSize[0]) $ImgSize[0] = 60;
 			if ($ImgSize[0] > $cfg["sponsor_picwidth_small"]) $ImgSize[0] = $cfg["sponsor_picwidth_small"];
 
-			$out = "<img src=\"{$sponsor["pic_path"]}\" width=\"{$ImgSize[0]}\" border=\"0\" title=\"{$sponsor["name"]}\">";
+			$out = "<img src=\"$file_name\" width=\"{$ImgSize[0]}\" border=\"0\" title=\"{$sponsor["name"]}\">";
 		} elseif ($sponsor["name"] != '') $out = "<b>{$sponsor["name"]}</b>";
 
 		if ($out and $sponsor["url"] != '' and $sponsor["url"] != "http://") $out = "<a href=\"base.php?mod=bannerclick&sponsorid={$sponsor["sponsorid"]}\" target=\"_blank\">$out</a>";
-	}
+//	}
 
 	if ($out != '') $templ['box']['rows'] .= $out . "<br />";
 }
--- trunk/modules/sponsor/banner.php
+++ trunk/modules/sponsor/banner.php
@@ -1,22 +1,22 @@
 <?php
-$banner = $db->query_first("SELECT sponsorid, pic_path, url FROM {$config['tables']['sponsor']} WHERE rotation AND pic_path != '' AND pic_path != 'http://' ORDER BY RAND()");
+$banner = $db->query_first("SELECT sponsorid, pic_path, url
+  FROM {$config['tables']['sponsor']}
+  WHERE rotation AND pic_path != '' AND pic_path != 'http://' AND !(pic_path LIKE 'http://%')
+  ORDER BY RAND()");
+
+$file_name = 'ext_inc/banner/banner_'. substr($banner['pic_path'], strrpos($banner["pic_path"], 'ext_inc/banner/') + 15, strlen($banner['pic_path']));
 
 // If entry is HTML-Code
 if (substr($banner['pic_path'], 0, 12) == 'html-code://') {
 	$templ['index']['banner_code'] = substr($banner["pic_path"], 12, strlen($banner["pic_path"]) - 12);
 
 // Else add Image-Tag
-} else {
-	$org_file_name = substr($banner["pic_path"], 0, strrpos($banner["pic_path"], "."));
-	$org_ending = substr($banner["pic_path"], strrpos($banner["pic_path"], "."), 5);
-	if (file_exists($org_file_name . "_banner" . $org_ending)) {
-		$banner["pic_path"] = $org_file_name . "_banner" . $org_ending;
-		$templ['index']['banner_code'] = '<img src="'. $banner["pic_path"] .'" border="1" width="468" height="60" class="img_border" alt="top"/>';
-
-	// If no Banner-Thumb was found, take lansuite-banner
-	} else $templ['index']['banner_code'] = '<img src="ext_inc/banner/one_network_banner.jpg" border="1" width="468" height="60" class="img_border" alt="top"/>';
+} elseif (file_exists($file_name)) {
+	$templ['index']['banner_code'] = '<img src="'. $file_name .'" border="1" width="468" height="60" class="img_border" alt="top"/>';
 
 	// Link banner, if in online mode
 	if ($cfg["sys_internet"]) $templ['index']['banner_code'] = '<a href="base.php?mod=bannerclick&sponsorid='. $banner["sponsorid"] .'" target="_blank">'. $templ['index']['banner_code'] .'</a>';
-}
+
+// If no Banner-Thumb was found, take lansuite-banner
+} else $templ['index']['banner_code'] = '<img src="ext_inc/banner/one_network_banner.jpg" border="1" width="468" height="60" class="img_border" alt="top"/>';
 ?>
