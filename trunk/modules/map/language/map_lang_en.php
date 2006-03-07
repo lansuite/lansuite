9b23ccb048d5f74f1b5218d867f22d10afbe29df
jochenjung <jochenjung@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2006-03-07 16:54:40 +0000

    - Change: Games und Map Modul vom Addon ins Main-Package verschoben und auf Englisch �bersetzt (KnoX 07.03.2006)

---
+++ trunk/modules/map/language/map_lang_en.php
@@ -0,0 +1,18 @@
+<?php
+
+ $lang['map']['language_name'] = "English";
+ $lang['map']['version'] = 1;					// Integer !, Increment this Counter after you add, delete or rename of tags
+ $lang['map']['lastchange'] = "7. March 2006";	// String, must not have a strict format
+ $lang['map']['translator'] = "Jochen";			// String, the Realname of the translator
+
+
+ $lang['map']['headline']	= "Route";
+ $lang['map']['subheadline'] = "Here you can calculate the route to the lan party using Map24.de";
+ $lang["map"]["single_row"]	= "The route opens in a new window. If not, check, weather you have a PopUp-Blocker installed.";
+ $lang["map"]["name"]	= "Textinput";
+ $lang["map"]["user_insg"]	= "Overall users";
+ $lang['map']['street']	= "Street: ";
+ $lang['map']['plz']	= "Postal Code: ";
+ $lang['map']['city']	= "City: ";
+
+?>
