b2eac08fc1e88ea0426311615be81a6900e9e0aa
jochenjung <jochen.jung@gmail.com>
2006-03-07 17:38:55 +0000

    #314 " in ' ge�ndert

--- modules/faq/language/faq_lang_en.php
+++ modules/faq/language/faq_lang_en.php
@@ -1,55 +1,55 @@
 <?php
 
- $lang['faq']['language_name']        = "English";
+ $lang['faq']['language_name']        = 'English';
  $lang['faq']['version']              = 1;
- $lang['faq']['lastchange']           = "17. Mai 2005";
- $lang['faq']['translator']           = "Cewigo";
+ $lang['faq']['lastchange']           = '17. Mai 2005';
+ $lang['faq']['translator']           = 'Cewigo';
 
- $lang['faq']['show_caption']		 = "FAQ";
- $lang['faq']['show_longcaption']	 = "<b>F</b>requently <b>A</b>sked <b>Q</b>uestions";
- $lang['faq']['show_subcaption']		 = "On this page you can find frequently asked questions and there anwers.
+ $lang['faq']['show_caption']		 = 'FAQ';
+ $lang['faq']['show_longcaption']	 = '<b>F</b>requently <b>A</b>sked <b>Q</b>uestions';
+ $lang['faq']['show_subcaption']		 = 'On this page you can find frequently asked questions and there anwers.
                                             The questions are summarized in some categories. This categories can be expanded by
-                                            using the +-symbol.";
+                                            using the +-symbol.';
 
- $lang['faq']['add_quest_caption']    = "Add a question";
- $lang['faq']['add_quest_subcaption'] = " Please fill out the Form completely to add a question. The maximum of chars for caption are 30, the question can contain about 5000 chars. In the category-field you can define a category in which the question should apears.";
+ $lang['faq']['add_quest_caption']    = 'Add a question';
+ $lang['faq']['add_quest_subcaption'] = ' Please fill out the Form completely to add a question. The maximum of chars for caption are 30, the question can contain about 5000 chars. In the category-field you can define a category in which the question should apears.';
 
- $lang['faq']['add_quest']			 = "Question / Headline";
- $lang['faq']['add_text']			 = "Text";
- $lang['faq']['choise_cat']			 = "Existing category";
- $lang['faq']['new_cat']				 = "New category";
+ $lang['faq']['add_quest']			 = 'Question / Headline';
+ $lang['faq']['add_text']			 = 'Text';
+ $lang['faq']['choise_cat']			 = 'Existing category';
+ $lang['faq']['new_cat']				 = 'New category';
 
- $lang['faq']['add_cat_caption']		 = "Add a category";
- $lang['faq']['add_cat_subcaption']	 = " Please fill out the form completely to add a category. The maximum of chars for the categoryname are about 30 chars.";
+ $lang['faq']['add_cat_caption']		 = 'Add a category';
+ $lang['faq']['add_cat_subcaption']	 = ' Please fill out the form completely to add a category. The maximum of chars for the categoryname are about 30 chars.';
 
- $lang['faq']['add_quest_ok']		 = "The question and the category were successfully added";
- $lang['faq']['add_onlyquest_ok']	 = "The question were added successfully";
- $lang['faq']['add_cat_ok']			 = "The category were added successfully";
- $lang['faq']['change_quest_ok']	     = "Question and category were changed successfully";
- $lang['faq']['change_onlyquest_ok']	 = "The question was changed successfully";
- $lang['faq']['change_cat_ok']		 = "The category was changed successfully";
- $lang['faq']['change_cat_caption']	 = "Change category";
- $lang['faq']['change_caption']		 = "Change FAQ";
- $lang['faq']['change_cat_caption']	 = "Change question";
+ $lang['faq']['add_quest_ok']		 = 'The question and the category were successfully added';
+ $lang['faq']['add_onlyquest_ok']	 = 'The question were added successfully';
+ $lang['faq']['add_cat_ok']			 = 'The category were added successfully';
+ $lang['faq']['change_quest_ok']	     = 'Question and category were changed successfully';
+ $lang['faq']['change_onlyquest_ok']	 = 'The question was changed successfully';
+ $lang['faq']['change_cat_ok']		 = 'The category was changed successfully';
+ $lang['faq']['change_cat_caption']	 = 'Change category';
+ $lang['faq']['change_caption']		 = 'Change FAQ';
+ $lang['faq']['change_cat_caption']	 = 'Change question';
 
- $lang['faq']['del_caption'] 		 = "Delete FAQ";
- $lang['faq']['del_subcaption'] 		 = "On this page you can find frequently asked questions and there anwers.
+ $lang['faq']['del_caption'] 		 = 'Delete FAQ';
+ $lang['faq']['del_subcaption'] 		 = 'On this page you can find frequently asked questions and there anwers.
                                             The questions are summarized in some categories. This categories can be expanded by
-                                            using the +-symbol. With the delete-button you can delete the appropriate category and/or question.";
- $lang['faq']['del_cat_quest'] 		 = "Are you really sure that you want to delete the category<b> %CATNAME% </b> and the questions contained in it?";
- $lang['faq']['del_cat_ok']	 		 = "The categories were deleted successfully.";
- $lang['faq']['del_item_quest'] 		 = "Are you sure that you want to delete the question<b> %ITEMNAME% </b> ?";
- $lang['faq']['del_item_ok']			 = "The question were deleted successfully.";
- $lang['faq']['no_itenm']			 = "No entries available";
- $lang['faq']['cat_exists']			 = "This category name already exists";
- $lang['faq']['cat_not_exists']		 = "This category does not exist";
- $lang['faq']['quest_not_exists']     = "This question does not exist";
- $lang['faq']['text_to_long']		 = "The answer may not contain more than 5000 chars";
- $lang['faq']['no_cat_name']			 = "Please enter a name for the new category";
- $lang['faq']['no_quest_name']		 = "Please enter a question";
- $lang['faq']['no_quest_text']		 = "Please enter a text";
- $lang['faq']['new_cat_error']		 = "Please select a category or create a new one";
- $lang['faq']['choise_cat_error']	 = "Please select a category <b> or </b> create a new one";
+                                            using the +-symbol. With the delete-button you can delete the appropriate category and/or question.';
+ $lang['faq']['del_cat_quest'] 		 = 'Are you really sure that you want to delete the category<b> %CATNAME% </b> and the questions contained in it?';
+ $lang['faq']['del_cat_ok']	 		 = 'The categories were deleted successfully.';
+ $lang['faq']['del_item_quest'] 		 = 'Are you sure that you want to delete the question<b> %ITEMNAME% </b> ?';
+ $lang['faq']['del_item_ok']			 = 'The question were deleted successfully.';
+ $lang['faq']['no_itenm']			 = 'No entries available';
+ $lang['faq']['cat_exists']			 = 'This category name already exists';
+ $lang['faq']['cat_not_exists']		 = 'This category does not exist';
+ $lang['faq']['quest_not_exists']     = 'This question does not exist';
+ $lang['faq']['text_to_long']		 = 'The answer may not contain more than 5000 chars';
+ $lang['faq']['no_cat_name']			 = 'Please enter a name for the new category';
+ $lang['faq']['no_quest_name']		 = 'Please enter a question';
+ $lang['faq']['no_quest_text']		 = 'Please enter a text';
+ $lang['faq']['new_cat_error']		 = 'Please select a category or create a new one';
+ $lang['faq']['choise_cat_error']	 = 'Please select a category <b> or </b> create a new one';
 
- $lang['faq']['choose_cat']           = "choose category";
+ $lang['faq']['choose_cat']           = 'choose category';
 ?>
--- modules/guestlist/language/guestlist_lang_en.php
+++ modules/guestlist/language/guestlist_lang_en.php
@@ -1,21 +1,21 @@
 <?php
 
-$lang['guestlist']['structure_version']	= "1";
-$lang['guestlist']['content_version']	= "1";
-$lang['guestlist']['language_name'] = "English";
-$lang['guestlist']['lastchange'] = "14. Juni 2005";
-$lang['guestlist']['translator'] = "Jochen";
+$lang['guestlist']['structure_version']	= '1';
+$lang['guestlist']['content_version']	= '1';
+$lang['guestlist']['language_name'] = 'English';
+$lang['guestlist']['lastchange'] = '14. Juni 2005';
+$lang['guestlist']['translator'] = 'Jochen';
 
-$lang["guestlist"]["list_search"]	= "Search user"; 
-$lang["guestlist"]["list_for_party"]	= "Guestlist of this party"; 
-$lang["guestlist"]["list_paid"]	= "paid"; 
-$lang["guestlist"]["list_not_paid"]	= "not paid"; 
-$lang["guestlist"]["list_user"]	= "User"; 
-$lang["guestlist"]["map_caption"]	= "Usermap";
-$lang["guestlist"]["map_subcaption"]	= "This is where you can have a look at where your cusomers come from";
-$lang["guestlist"]["map_err_noplz"]	= "Were sorry, none of your customers has entered his / her postalcode. That why localization does not work.";
-$lang["guestlist"]["map_city"]	= "City"; 
-$lang["guestlist"]["map_user"]	= "User";
-$lang["guestlist"]["map_name"]	= "Name";
+$lang['guestlist']['list_search']	= 'Search user'; 
+$lang['guestlist']['list_for_party']	= 'Guestlist of this party'; 
+$lang['guestlist']['list_paid']	= 'paid'; 
+$lang['guestlist']['list_not_paid']	= 'not paid'; 
+$lang['guestlist']['list_user']	= 'User'; 
+$lang['guestlist']['map_caption']	= 'Usermap';
+$lang['guestlist']['map_subcaption']	= 'This is where you can have a look at where your cusomers come from';
+$lang['guestlist']['map_err_noplz']	= 'Were sorry, none of your customers has entered his / her postalcode. That why localization does not work.';
+$lang['guestlist']['map_city']	= 'City'; 
+$lang['guestlist']['map_user']	= 'User';
+$lang['guestlist']['map_name']	= 'Name';
 
 ?>
