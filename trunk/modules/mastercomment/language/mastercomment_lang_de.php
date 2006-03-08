5d09e63fe7a2acad35fe0d91c49ea2b8fbb48a7b
jochenjung <jochenjung@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2006-03-08 16:55:16 +0000

    #314 " in ' ge�ndert

--- trunk/modules/mastercomment/language/mastercomment_lang_de.php
+++ trunk/modules/mastercomment/language/mastercomment_lang_de.php
@@ -1,23 +1,23 @@
 <?php
 
 // Text
-$lang['mastercomment']['comment_caption']	= "Kommentar zu %MODULE% <b>\"%ITEM%\"</b>";
-$lang['mastercomment']['add_comment']		= "Kommentar abgeben";
-$lang['mastercomment']['comment']			= "Kommentar";
-$lang['mastercomment']['no_comments']		= "Keine Kommentare vorhanden.";
-$lang['mastercomment']['count_comments']	= "Anzahl Kommentare:";
+$lang['mastercomment']['comment_caption']	= 'Kommentar zu %MODULE% <b>\'%ITEM%\'</b>';
+$lang['mastercomment']['add_comment']		= 'Kommentar abgeben';
+$lang['mastercomment']['comment']			= 'Kommentar';
+$lang['mastercomment']['no_comments']		= 'Keine Kommentare vorhanden.';
+$lang['mastercomment']['count_comments']	= 'Anzahl Kommentare:';
 
-$lang['mastercomment']['add_comment_capt']	= "\"%ITEM%\" kommentieren";
+$lang['mastercomment']['add_comment_capt']	= '\'%ITEM%\' kommentieren';
 
-$lang['mastercomment']['del_comment_quest']	= "Wollen Sie diesen Kommentar wirklich l&ouml;schen?";
+$lang['mastercomment']['del_comment_quest']	= 'Wollen Sie diesen Kommentar wirklich l&ouml;schen?';
 
-$lang['mastercomment']['add_comment_ok']	= "Ihr Kommentar wurde erfolgreich eintragen";
-$lang['mastercomment']['change_comment_ok']	= "Der Kommentar wurde erfolgreich abge&auml;ndert";
-$lang['mastercomment']['add_comment_del']	= "Der Kommentar wurde erfolgreich gel&ouml;scht";
+$lang['mastercomment']['add_comment_ok']	= 'Ihr Kommentar wurde erfolgreich eintragen';
+$lang['mastercomment']['change_comment_ok']	= 'Der Kommentar wurde erfolgreich abge&auml;ndert';
+$lang['mastercomment']['add_comment_del']	= 'Der Kommentar wurde erfolgreich gel&ouml;scht';
 // Error
-$lang['mastercomment']['error_text'] 		= "Bitte geben Sie nicht mehr als 1500 Zeichen ein";
-$lang['mastercomment']['error_text2']		= "Bitte geben Sie einen Kommentar ein";
-$lang['mastercomment']['comment_not_exist']	= "Der gew&auml;hlte Kommentar existiert nicht";
+$lang['mastercomment']['error_text'] 		= 'Bitte geben Sie nicht mehr als 1500 Zeichen ein';
+$lang['mastercomment']['error_text2']		= 'Bitte geben Sie einen Kommentar ein';
+$lang['mastercomment']['comment_not_exist']	= 'Der gew&auml;hlte Kommentar existiert nicht';
 
 
 ?>
--- trunk/modules/misc/language/misc_lang_en.php
+++ trunk/modules/misc/language/misc_lang_en.php
@@ -1,46 +1,46 @@
 <?php
 
  //Default
- $lang['misc']['language_name'] = "English";
+ $lang['misc']['language_name'] = 'English';
  $lang['misc']['version']       = 1;
- $lang['misc']['lastchange']    = "16. October 2005";
- $lang['misc']['translator']    = "Cewigo (Jan)";
+ $lang['misc']['lastchange']    = '16. October 2005';
+ $lang['misc']['translator']    = 'Cewigo (Jan)';
 
  //Confirms
- $lang['misc']['cf_add_ips']    = "The IPs has been successfully added";
- $lang['misc']['cf_add_ip']     = "The ip-adresses has been sucessfully added.";
- $lang['misc']['print_paper']   = "print an ip-list for ...";
+ $lang['misc']['cf_add_ips']    = 'The IPs has been successfully added';
+ $lang['misc']['cf_add_ip']     = 'The ip-adresses has been sucessfully added.';
+ $lang['misc']['print_paper']   = 'print an ip-list for ...';
 
- $lang['misc']['ch_print_bl']   = "Here you can choose a block to print an ip-list for.";
- $lang['misc']['ch_print_se']   = "Here you can choose a block. In the next step you can choose the seat to print an ip-sheet for.";
- $lang['misc']['ch_print_usr']  = "Here you can choose an user you whish to print an ip-sheet for. Please note that only User will be shown who has actually a seat.";
+ $lang['misc']['ch_print_bl']   = 'Here you can choose a block to print an ip-list for.';
+ $lang['misc']['ch_print_se']   = 'Here you can choose a block. In the next step you can choose the seat to print an ip-sheet for.';
+ $lang['misc']['ch_print_usr']  = 'Here you can choose an user you whish to print an ip-sheet for. Please note that only User will be shown who has actually a seat.';
 
  //Printing...
- $lang['misc']['print_block']   = "According to block";
- $lang['misc']['print_seat']    = "According to seat";
- $lang['misc']['print_user']    = "According to user";
- $lang['misc']['print_popup']   = "Here you can open the new windows where you can print the Sheet";
- $lang['misc']['ipprint_noentr'] = "There are no entrys";
- $lang['misc']['ipprint_nofunc'] = "No function choosen";
+ $lang['misc']['print_block']   = 'According to block';
+ $lang['misc']['print_seat']    = 'According to seat';
+ $lang['misc']['print_user']    = 'According to user';
+ $lang['misc']['print_popup']   = 'Here you can open the new windows where you can print the Sheet';
+ $lang['misc']['ipprint_noentr'] = 'There are no entrys';
+ $lang['misc']['ipprint_nofunc'] = 'No function choosen';
 
  //Questions
- $lang['misc']['qest_block']    = "... block";
- $lang['misc']['qest_seat']     = "... seat";
- $lang['misc']['qest_user']     = "... user";
+ $lang['misc']['qest_block']    = '... block';
+ $lang['misc']['qest_seat']     = '... seat';
+ $lang['misc']['qest_user']     = '... user';
 
  //Errors
- $lang['misc']['err_dbl']       = "tautologous";
- $lang['misc']['err_invalid']   = "invalid";
- $lang['misc']['err_no_block']  = "This block does not exist.";
- $lang['misc']['err_double_ip'] = "At least one of the ips has allready been assigned";
- $lang['misc']['err_ip_format'] = "The form of at least one ip ist invallid. Form: 192.168.123.12";
+ $lang['misc']['err_dbl']       = 'tautologous';
+ $lang['misc']['err_invalid']   = 'invalid';
+ $lang['misc']['err_no_block']  = 'This block does not exist.';
+ $lang['misc']['err_double_ip'] = 'At least one of the ips has allready been assigned';
+ $lang['misc']['err_ip_format'] = 'The form of at least one ip ist invallid. Form: 192.168.123.12';
 
  //Contend
- $lang['misc']['this_plan']     = "this plan";
- $lang['misc']['ip_gen']        = "IP-generation";
- $lang['misc']['occupied']      = "occupied";
- $lang['misc']['free']          = "free";
- $lang['misc']['log_unknown']   = "unknown";
- $lang['misc']['wu_no_changes'] = "no changes in the last 15 minutes.";
+ $lang['misc']['this_plan']     = 'this plan';
+ $lang['misc']['ip_gen']        = 'IP-generation';
+ $lang['misc']['occupied']      = 'occupied';
+ $lang['misc']['free']          = 'free';
+ $lang['misc']['log_unknown']   = 'unknown';
+ $lang['misc']['wu_no_changes'] = 'no changes in the last 15 minutes.';
  
 ?>
