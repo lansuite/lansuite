49ef382ec5f84df552fbb067201ae37926dd0bcd
jochenjung <jochen.jung@gmail.com>
2006-03-09 20:36:33 +0000

    #314 " in ' ge�ndert

--- modules/paypal/language/paypal_lang_de.php
+++ modules/paypal/language/paypal_lang_de.php
@@ -1,53 +1,53 @@
 <?php
 
-$lang['paypal']['language_name'] = "Deutsch";
+$lang['paypal']['language_name'] = 'Deutsch';
 $lang['paypal']['version'] = 1;					// Integer !, Increment this Counter after you add, delete or rename of tags
-$lang['paypal']['lastchange'] = "20.05.2005";	// String, must not have a strict format
-$lang['paypal']['translator'] = "Genesis";			// String, the Realname of the translator
+$lang['paypal']['lastchange'] = '20.05.2005';	// String, must not have a strict format
+$lang['paypal']['translator'] = 'Genesis';			// String, the Realname of the translator
 
 
 
 // show.php
-$lang['paypal']['error']			= "Sie k&ouml;nnen nichts einzahlen wenn Sie nicht eingeloggt sind.";
-$lang['paypal']['caption']			= "Einzahlen";
-$lang['paypal']['subcaption'] 		= "Hier sehen sie was f&uuml;r Betr&auml;ge noch ausstehend sind. W&auml;hlen sie was sie bezahlen m&ouml;chten.";
-$lang["paypal"]["no_notpaid_party"]= "Alle Eintrittspreise bezahlt.";
-$lang["paypal"]["catering"]			= "Einzahlung f&uuml;r Catering";
-$lang["paypal"]["donation"]			= "Spende f&uuml;r die Organisatoren";
+$lang['paypal']['error']			= 'Sie k&ouml;nnen nichts einzahlen wenn Sie nicht eingeloggt sind.';
+$lang['paypal']['caption']			= 'Einzahlen';
+$lang['paypal']['subcaption'] 		= 'Hier sehen sie was f&uuml;r Betr&auml;ge noch ausstehend sind. W&auml;hlen sie was sie bezahlen m&ouml;chten.';
+$lang['paypal']['no_notpaid_party']= 'Alle Eintrittspreise bezahlt.';
+$lang['paypal']['catering']			= 'Einzahlung f&uuml;r Catering';
+$lang['paypal']['donation']			= 'Spende f&uuml;r die Organisatoren';
 
 // paying.php
-$lang["paypal"]["data_caption"]		= "Daten eingeben";
-$lang["paypal"]["data_subcaption"]	= "Bitte geben sie ihre Zahlungsdaten ein.";
-$lang["paypal"]["firstname"]		= "Vorname";
-$lang["paypal"]["lastname"]			= "Nachname";
-$lang["paypal"]["street"]			= "Strasse";
-$lang["paypal"]["city"]				= "Stadt";
-$lang["paypal"]["zip"]				= "PLZ";
-$lang["paypal"]["email"]			= "E-Mail";
-$lang["paypal"]["price"]			= "Zu &uuml;berweisender Betrag";
-$lang["paypal"]["no_price_error"]	= "Kein Preis ausgew&auml;hlt";
-$lang["paypal"]["send_warning"]		= "Achtung mit dem klicken auf weiter wird die Zahlung durchgef&uuml;hrt";
-$lang['paypal']['error_firstname']	= "Bitte geben sie einen Vornamen ein";
-$lang['paypal']['error_lastname']	= "Bitte geben sie einen Namen ein";
-$lang['paypal']['error_street']		= "Bitte geben sie eine Strasse ein";
-$lang['paypal']['error_city']		= "Bitte geben sie eine Stadt ein";
-$lang['paypal']['error_zip']		= "Bitte geben sie einen PLZ ein";
-$lang['paypal']['error_email']		= "Bitte geben sie eine Email ein";
+$lang['paypal']['data_caption']		= 'Daten eingeben';
+$lang['paypal']['data_subcaption']	= 'Bitte geben sie ihre Zahlungsdaten ein.';
+$lang['paypal']['firstname']		= 'Vorname';
+$lang['paypal']['lastname']			= 'Nachname';
+$lang['paypal']['street']			= 'Strasse';
+$lang['paypal']['city']				= 'Stadt';
+$lang['paypal']['zip']				= 'PLZ';
+$lang['paypal']['email']			= 'E-Mail';
+$lang['paypal']['price']			= 'Zu &uuml;berweisender Betrag';
+$lang['paypal']['no_price_error']	= 'Kein Preis ausgew&auml;hlt';
+$lang['paypal']['send_warning']		= 'Achtung mit dem klicken auf weiter wird die Zahlung durchgef&uuml;hrt';
+$lang['paypal']['error_firstname']	= 'Bitte geben sie einen Vornamen ein';
+$lang['paypal']['error_lastname']	= 'Bitte geben sie einen Namen ein';
+$lang['paypal']['error_street']		= 'Bitte geben sie eine Strasse ein';
+$lang['paypal']['error_city']		= 'Bitte geben sie eine Stadt ein';
+$lang['paypal']['error_zip']		= 'Bitte geben sie einen PLZ ein';
+$lang['paypal']['error_email']		= 'Bitte geben sie eine Email ein';
 
-$lang["paypal"]["send_caption"]		= "Senden der Daten";
-$lang["paypal"]["send_subcaption"]	= "Die Daten werden gesendet einen Moment bitte";
-$lang["paypal"]["on_moment"]		= "Einen Moment Bitte die Daten werden gesendet";
+$lang['paypal']['send_caption']		= 'Senden der Daten';
+$lang['paypal']['send_subcaption']	= 'Die Daten werden gesendet einen Moment bitte';
+$lang['paypal']['on_moment']		= 'Einen Moment Bitte die Daten werden gesendet';
 
-$lang['paypal']['comment']			= "Kommentar";
-$lang["paypal"]["success_caption"]	= "Zahlung erfolgreich";
-$lang["paypal"]["success_text"]		= "Die Zahlung war erfolgreich. Wir danken f&uuml;r die Einzahlung.";
-$lang["paypal"]["ordernr"]			= "Zahlungsnummer";
-$lang["paypal"]["date"]				= "Zahlungsdatum";
+$lang['paypal']['comment']			= 'Kommentar';
+$lang['paypal']['success_caption']	= 'Zahlung erfolgreich';
+$lang['paypal']['success_text']		= 'Die Zahlung war erfolgreich. Wir danken f&uuml;r die Einzahlung.';
+$lang['paypal']['ordernr']			= 'Zahlungsnummer';
+$lang['paypal']['date']				= 'Zahlungsdatum';
 
-$lang["paypal"]["error_caption"]	= "Fehler";
-$lang["paypal"]["error_text"]  		= "Die Transaktion konnte nicht durchgef&uuml;hrt werden.";
+$lang['paypal']['error_caption']	= 'Fehler';
+$lang['paypal']['error_text']  		= 'Die Transaktion konnte nicht durchgef&uuml;hrt werden.';
 
-$lang["paypal"]["admin_caption"]	= "Transaktionsfehler oder unerlaubter Zugriff";
-$lang["paypal"]["admin_text"]  		= "Bitte melden sie sich beim einem Admin damit der die Zahlung pr&uuml;fen kann.";
+$lang['paypal']['admin_caption']	= 'Transaktionsfehler oder unerlaubter Zugriff';
+$lang['paypal']['admin_text']  		= 'Bitte melden sie sich beim einem Admin damit der die Zahlung pr&uuml;fen kann.';
 
 ?>
