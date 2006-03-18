90a18fe764f75f3033419838668bf86036186ab6
jochenjung <jochen.jung@gmail.com>
2006-03-18 15:00:32 +0000

    - Bugfix: #374 Board "Anzahl der angezeigten Forumeintr�ge" rausgenommen, da es eh nicht funktionierte (KnoX 18.03.2006)

--- modules/board/mod_settings/config.sql
+++ modules/board/mod_settings/config.sql
@@ -1,6 +1,5 @@
 ('board_ranking', '1', 'boolean', 'Board', 'Ranking anzeigen?');
 ('board_prefix', 'Gast_', 'string', 'Board', 'Gast Präfix');
-('board_max_forum', '10', 'integer', 'Board', 'Anzahl der angezeigten Forumeinträge');
 ('board_max_posts', '5', 'integer', 'Board', 'Anzahl der angezeigten Einträge');
 ('board_subscribe_subject', 'Es ist ein neuer Beitrag vorhanden', 'string', 'Board', 'Betreff der Hinweismail auf neue Beiträge');
 ('board_subscribe_text', 'Im Forum gibt es einen neuen Beitrag. Du kannst ihn dir unter %URL% ansehen.', 'text', 'Board', 'Text der Hinweismail auf neue Beiträge');
