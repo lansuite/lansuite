2d0829257b30cff0ae68dad8205241d692d7396e
jochenjung <jochenjung@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2006-04-14 09:46:45 +0000

    - Change: Als Zeichensatz wird nun immer UTF-8 im HTTP-Header mitgesendet (KnoX 13.04.2006)

--- trunk/inc/classes/class_db_mysql.php
+++ trunk/inc/classes/class_db_mysql.php
@@ -58,7 +58,7 @@ class db {
 			}
 		} else $GLOBALS['db_link_id'] = $this->link_id;
 
-		@mysql_query("/*!40101 SET NAMES latin1 */;", $GLOBALS['db_link_id']);
+		@mysql_query("/*!40101 SET NAMES utf8_general_ci */;", $GLOBALS['db_link_id']);
 
 		return true;
 	}
