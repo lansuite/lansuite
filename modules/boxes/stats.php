8cf2d14d2c3feb81e07cdbe73bff464c505c8ead
jochenjung <jochen.jung@gmail.com>
2006-04-14 20:10:07 +0000

    - Change: #381 Es werden nur noch die User der letzen 10min (nicht mehr 60min) als 'Aktuell Online' markiert (KnoX 14.04.2006)

--- modules/boxes/stats.php
+++ modules/boxes/stats.php
@@ -11,20 +11,19 @@ $box->DotRow($lang['boxes']['stats_hits'] .': '. $hits['insg']);
 $box->EmptyRow();
 
 // Numer of users currently online
-$visit_timeout = time() - 60*60;
+$visit_timeout = time() - 60*10;
 $online = $db->query_first("SELECT COUNT(*) AS insg FROM {$config['tables']['stats_auth']} WHERE (lasthit > $visit_timeout)");
 $box->DotRow($lang['boxes']['stats_user_online'] .': '. $online["insg"]);
 
 // Get list of users currently online
 $user_online = $db->query("SELECT user.username, user.userid
-		FROM {$config['tables']['stats_auth']} AS auth
-		LEFT JOIN {$config['tables']['user']} AS user ON user.userid = auth.userid
-		WHERE (auth.lasthit > $visit_timeout)
-		AND user.userid > 0
-		GROUP BY userid
-		ORDER BY auth.lasthit
-		LIMIT 5
-		");
+	FROM {$config['tables']['stats_auth']} AS auth
+	LEFT JOIN {$config['tables']['user']} AS user ON user.userid = auth.userid
+	WHERE (auth.lasthit > $visit_timeout) AND auth.login = '1' AND user.userid > 0
+	GROUP BY user.userid
+	ORDER BY auth.lasthit
+	LIMIT 5
+	");
 while ($user = $db->fetch_array($user_online)) $box->EngangedRow($user["username"] .' '. $dsp->FetchUserIcon($user["userid"]));
 $db->free_result($user_online);
 
