fcc72008b730f683f45b78692859c45015a183bc
jochenjung <jochen.jung@gmail.com>
2006-04-22 23:29:46 +0000

    Log auf MS2 umgestellt

--- modules/misc/log.php
+++ modules/misc/log.php
@@ -2,12 +2,28 @@
 
 switch($_GET["step"]){
 	default:
-		$mastersearch = new MasterSearch($vars, "index.php?mod=misc&action=log", "index.php?mod=misc&action=log&step=2&logid=", "");
-		$mastersearch->LoadConfig("log", '', '');
-		$mastersearch->PrintForm();
-		$mastersearch->Search();
-		$mastersearch->PrintResult();
-		$templ['index']['info']['content'] .= $mastersearch->GetReturn();	
+    include_once('modules/mastersearch2/class_mastersearch2.php');
+    $ms2 = new mastersearch2();
+
+    $ms2->query['from'] = "{$config["tables"]["log"]} AS l
+      LEFT JOIN {$config["tables"]["user"]} AS u ON u.userid = l.userid";
+    $ms2->query['default_order_by'] = 'l.date DESC';
+
+    $ms2->AddTextSearchField('Message', array('l.description' => 'like'));
+    $ms2->AddTextSearchField('Group', array('l.sort_tag' => 'like'));
+    $ms2->AddTextSearchField('User', array('l.userid' => 'exact', 'u.name' => 'like', 'u.firstname' => 'like'));
+
+    $ms2->AddTextSearchDropDown('Priority', 'l.type', array('' => 'Alle', '1' => 'Niedrig', '2' => 'Normal', '3' => 'Hoch'));
+
+    $ms2->AddSelect('u.userid');
+    $ms2->AddResultField('Message', 'l.description', '', 140);
+    $ms2->AddResultField('Group', 'l.sort_tag');
+    $ms2->AddResultField('Date', 'l.date', 'MS2GetDate');
+    $ms2->AddResultField('User', 'u.username', 'UserNameAndIcon');
+
+    $ms2->AddIconField('details', 'index.php?mod=misc&action=log&step=2&logid=', $lang['ms2']['details']);
+
+    $ms2->PrintSearch('index.php?mod=misc&action=log', 'l.logid');
 	break;
 
   case 2:
