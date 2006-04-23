0e517d3e0a52920f0c5c07899ddfd6f5ee4717d2
jochenjung <jochen.jung@gmail.com>
2006-04-23 07:08:09 +0000

    Templates korrigiert

--- inc/classes/class_auth.php
+++ inc/classes/class_auth.php
@@ -53,11 +53,11 @@ class auth {
 		if ($_GET['mod'] == "logout") $this->logout();
 		elseif (isset($_POST['login_x'])) $this->login("normal"); # Normal Login
 		elseif (isset($_POST['save_x'])) $this->login("save"); # Login + Save
-		elseif ($_COOKIE['auth']['email'] != "" and (!$this->auth['login'])) $this->login("cookie"); # Login via Coockie
-		elseif ($_COOKIE['auth']['email'] != "" and $this->auth['login']) { # Reset Coockie-Timeout
-			setcookie("auth[email]", $_COOKIE['auth']['email'], time() + (3600*24*365));
-			setcookie("auth[userpassword]", $_COOKIE['auth']['userpassword'], time() + (3600*24*365));
-		}
+#		elseif ($_COOKIE['auth']['email'] != "" and (!$this->auth['login'])) $this->login("cookie"); # Login via Coockie
+#		elseif ($_COOKIE['auth']['email'] != "" and $this->auth['login']) { # Reset Coockie-Timeout
+#			setcookie("auth[email]", $_COOKIE['auth']['email'], time() + (3600*24*365));
+#			setcookie("auth[userpassword]", $_COOKIE['auth']['userpassword'], time() + (3600*24*365));
+#		}
 
 		// If not logged in, delete the sessions userdata
 		if ($this->auth['login'] == "" or $this->auth['login'] == "0"){
@@ -107,19 +107,19 @@ class auth {
 	// When user logs in
 	function login($loginart) {
 		global $db, $func, $cfg, $config, $party, $lang, $auth, $board_config, $ActiveModules;
-		
+
 		$auth['design'] = $config['lansuite']['default_design'];
 		
 		$tmp_login_email = "";
 		$tmp_login_pass = "";
 
-		if ($loginart == "cookie"){
-			if ($_COOKIE['auth']['email'] != "") $tmp_login_email = $_COOKIE['auth']['email'];
-			if ($_COOKIE['auth']['userpassword'] != "") $tmp_login_pass = $_COOKIE['auth']['userpassword'];
-		} else {
+#		if ($loginart == "cookie"){
+#			if ($_COOKIE['auth']['email'] != "") $tmp_login_email = $_COOKIE['auth']['email'];
+#			if ($_COOKIE['auth']['userpassword'] != "") $tmp_login_pass = $_COOKIE['auth']['userpassword'];
+#		} else {
 			if ($_POST['email'] != "") $tmp_login_email = strtolower(htmlspecialchars(trim($_POST['email'])));
 			if ($_POST['password'] != "") $tmp_login_pass = md5($_POST['password']);
-		}
+#		}
 
 		if ($tmp_login_email == "") $func->information($lang['class_auth']['get_email_or_id'], "");
 		elseif ($tmp_login_pass == "") $func->information($lang['class_auth']['get_pw'], "");
