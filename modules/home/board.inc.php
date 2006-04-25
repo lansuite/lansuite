2d3bd30d8ddeb5711e0552804a50eeda8b8bd88c
jochenjung <jochen.jung@gmail.com>
2006-04-25 17:46:11 +0000

    - Bugfix: #423 Auf manchen DB-Systemen, konnte das Board nicht mehr angezeigt werden, statt dessen nur ein MySQL-Fehler (KnoX 25.04.2006)

--- modules/home/board.inc.php
+++ modules/home/board.inc.php
@@ -4,13 +4,13 @@ $templ['home']['show']['item']['info']['caption'] = $lang["home"]["board_caption
 $templ['home']['show']['item']['control']['row'] = "";
 
 $authtyp = $auth['type'] + 1;
-$query = $db->query("SELECT t.tid, p.pid, t.caption
+$query = $db->query("SELECT t.tid, p.pid, t.caption, MAX(p.date) AS LastPost
 	FROM {$config["tables"]["board_threads"]} AS t
 	LEFT JOIN {$config["tables"]["board_forums"]} AS f ON t.fid = f.fid
 	LEFT JOIN {$config["tables"]["board_posts"]} AS p ON p.tid = t.tid
 	WHERE (f.need_type <= '{$authtyp}')
 	GROUP BY t.tid
-	ORDER BY MAX(p.date) DESC
+	ORDER BY LastPost DESC
 	LIMIT 0,5");
 if ($db->num_rows($query) > 0) while($row = $db->fetch_array($query)) {
   $templ['home']['show']['row']['control']['link']	= "index.php?mod=board&action=thread&tid={$row['tid']}&pid={$row['pid']}";
