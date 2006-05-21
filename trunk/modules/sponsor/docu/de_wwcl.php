2a7965ce96fec01eaf3e86fe18e748a95af2f365
jochenjung <jochenjung@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2006-05-21 21:15:01 +0000

    - Add: Vorlagen bei Sponsoren zum Einf�gen von NGL, WWCL und Google-Adsense Anzeigen (KnoX 21.05.2006)

---
+++ trunk/modules/sponsor/docu/de_wwcl.php
@@ -0,0 +1,27 @@
+<?
+
+$helplet['modul'] = 'WWCL-Code';
+$helplet['action'] = 'Hilfe';
+$helplet['info'] = 'Informationen zum Einfügen des WWCl-Codes';
+
+$helplet['key'][1] = 'WWCL';
+$helplet['value'][1] = 'Falls Sie WWCL-Turniere auf Ihrer Party spielen, sollten Sie unten stehenden Banner-Code auf Ihrer Sponsoren-Seite hinzufügen. Das Einfügen eines Buttons ist nicht notwendig, da in LanSuite automatisch eine WWCL-Box mit dem Button auf allen Turnier-Seiten erscheint, die WWCL-Spiele enthalten';
+$helplet['key'][2] = 'Code';
+$helplet['value'][2] = '&lt;!-- WWCL Banner CODE v3.0 --&gt;<br>
+&lt;SCRIPT LANGUAGE="JavaScript"&gt;<br>
+&lt;!--<br>
+now = new Date();<br>
+random = now.getTime();<br>
+document.write(\'&lt;A HREF="http://advertise.planetlan.net/oc.php?s=1&c=83&w=468&h=60&cb=\' + random + \'" TARGET="_blank"&gt;\');<br>
+document.write(\'&lt;IMG SRC="http://advertise.planetlan.net/oi.php?s=1&c=83&w=468&h=60&l=1&cb=\' + random + \'"\');<br>
+document.write(\' WIDTH=468 HEIGHT=60 BORDER=0&gt;&lt;/A&gt;\');<br>
+//--><br>
+&lt;/SCRIPT&gt;<br>
+&lt;NOSCRIPT&gt;<br>
+&lt;A HREF="http://advertise.planetlan.net/oc.php?s=1&c=83&w=468&h=60" TARGET="_blank"&gt;&lt;IMG<br>
+SRC="http://advertise.planetlan.net/oi.php?s=1&c=83&w=468&h=60&l=1"<br>
+WIDTH=468 HEIGHT=60 BORDER=0&gt;&lt;/A&gt;<br>
+&lt;/NOSCRIPT&gt;<br>
+&lt;!-- END OF WWCL BANNER CODE --&gt;';
+
+?>
