179ae1672e56d9f5f6ebf0dcabff0c81b596e5bc
jochenjung <jochenjung@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2006-05-23 18:54:38 +0000

    - Change: SPAW durch FCK-Editor ersetzt, da SPAW beim Firefox Probleme bereitete, FCK-Editor weniger KB verbraucht und trotzdem mehr Funktionen bietet (KnoX 23.05.2006)

---
+++ trunk/ext_scripts/FCKeditor/editor/filemanager/upload/php/config.php
@@ -0,0 +1,38 @@
+<?php 
+/*
+ * FCKeditor - The text editor for internet
+ * Copyright (C) 2003-2006 Frederico Caldeira Knabben
+ * 
+ * Licensed under the terms of the GNU Lesser General Public License:
+ * 		http://www.opensource.org/licenses/lgpl-license.php
+ * 
+ * For further information visit:
+ * 		http://www.fckeditor.net/
+ * 
+ * "Support Open Source software. What about a donation today?"
+ * 
+ * File Name: config.php
+ * 	Configuration file for the PHP File Uploader.
+ * 
+ * File Authors:
+ * 		Frederico Caldeira Knabben (fredck@fckeditor.net)
+ */
+
+global $Config ;
+
+// SECURITY: You must explicitelly enable this "uploader". 
+$Config['Enabled'] = false ;
+
+// Path to uploaded files relative to the document root.
+$Config['UserFilesPath'] = '/UserFiles/' ;
+
+$Config['AllowedExtensions']['File']	= array() ;
+$Config['DeniedExtensions']['File']		= array('php','php2','php3','php4','php5','phtml','pwml','inc','asp','aspx','ascx','jsp','cfm','cfc','pl','bat','exe','com','dll','vbs','js','reg','cgi') ;
+
+$Config['AllowedExtensions']['Image']	= array('jpg','gif','jpeg','png') ;
+$Config['DeniedExtensions']['Image']	= array() ;
+
+$Config['AllowedExtensions']['Flash']	= array('swf','fla') ;
+$Config['DeniedExtensions']['Flash']	= array() ;
+
+?>
