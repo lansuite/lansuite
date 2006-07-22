1be9a972d8aa2f4ed2d03d236a22614991917491
jochenjung <jochen.jung@gmail.com>
2006-07-22 15:59:11 +0000

    - Bugfix: Es war beim Benutzereditieren nicht m�glich einen Clan wieder zu entfernen (KnoX 22.07.2006)

--- modules/usrmgr/add.php
+++ modules/usrmgr/add.php
@@ -274,7 +274,7 @@ if ($auth['type'] >= 2 or ($auth['userid'] == $_GET['userid'] and $cfg['user_sel
 
 
   if (!$quick_signon) {
-  	if ($_POST['clan'] == '') {
+  	if (!isset($_POST['clan'])) {
       $users_clan = $db->query_first("SELECT clanid FROM {$config["tables"]["user"]} WHERE userid = ". (int)$_GET['userid']);
       $_POST['clan'] = $users_clan['clanid'];
     }
@@ -332,76 +332,5 @@ if ($auth['type'] >= 2 or ($auth['userid'] == $_GET['userid'] and $cfg['user_sel
     }
   }
 }
-/*
 
-
-if (($auth["type"] >= 2) or (($auth["userid"] == $_GET["userid"]) && $cfg['user_self_details_change'])) {
-	include("modules/usrmgr/class_adduser.php");
-	$AddUser = new AddUser();
-
-	if ($_GET["step"] == "") $_GET["step"] = 1;
-
-	switch($_GET['step']) {
-		default:
-			$AddUser->GetDBData($_GET["action"]);
-		break;
-
-		case 2:
-			$AddUser->CheckErrors($_GET["action"]);
-		break;
-		
-		case 3:
-			$party->set_seatcontrol($_GET['userid'],$_GET['seatcontrol']);
-			$username = $_GET['username'];
-			$pw = $_GET['pw'];
-		break;
-	}
-
-
-	switch($_GET['step']) {
-		default:
-    	$dsp->NewContent($lang["usrmgr"]["add_caption"], $lang["usrmgr"]["add_subcaption"]);
-    	$dsp->SetForm("index.php?mod=usrmgr&action={$_GET["action"]}&umode={$action}&quick_signon={$_GET['quick_signon']}&step=". ($_GET["step"] + 1) ."&userid={$_GET["userid"]}", "signon", "", "multipart/form-data");
-			$AddUser->ShowForm($_GET["action"]);
-		break;
-
-		case 2:
-			$AddUser->WriteToDB($_GET["action"]);
-
-			$user = $db->query_first("SELECT username FROM {$config["tables"]["user"]} WHERE userid = {$_GET["userid"]}");
-
-			$username = urlencode($_POST['username']);
-			$pw = urlencode(base64_encode($_POST['password']));
-			
-			if(isset($_POST['price_id']) && $auth['type'] > 1){
-				$seatprice = $party->price_seatcontrol($_POST['price_id']);
-			
-				if($seatprice > 0 && $_POST['paid'] > 0 && $_POST['signon']){
-					$seat_paid = $party->get_seatcontrol($_GET['userid']);
-					$text = str_replace("%PRICE%",$seatprice . " " . $cfg['sys_currency'] ,$lang['usrmgr']['paid_seatcontrol_quest']) .HTML_NEWLINE;
-					if($seat_paid){
-						$text .= $lang['usrmgr']['paid_seatcontrol_paid'];
-					}else{
-						$text .= $lang['usrmgr']['paid_seatcontrol_not_paid'];
-					}
-					$func->question($text,"index.php?mod=usrmgr&action={$_GET["action"]}&umode={$action}&step=". ($_GET["step"] + 1) ."&userid={$_GET["userid"]}&seatcontrol=1&username=$username&priceid={$_POST['price_id']}&pw=$pw","index.php?mod=usrmgr&action={$_GET["action"]}&umode={$action}&step=". ($_GET["step"] + 1) ."&userid={$_GET["userid"]}&seatcontrol=0&username=$username&priceid={$_POST['price_id']}&pw=$pw");
-					break;
-				}
-			}
-		
-		
-		case 3:
-			
-		//	if(isset($_GET['username'])) $username = $_GET['username'];
-		//	if(isset($_GET['pw'])) $pw = $_GET['pw'];
-		
-			if ($_GET["action"] == "change") $func->confirmation(str_replace("%USER%", urldecode($username), $lang["usrmgr"]["add_editsuccess"]), "index.php?mod=usrmgr&action=details&userid={$_GET["userid"]}");
-			else {
-				(($cfg["signon_autopw"]) || ($cfg["signon_password_view"]))? $pw_text = HTML_NEWLINE . str_replace("%PASSWORD%", base64_decode(urldecode($pw)), $lang["usrmgr"]["add_pwshow"]) : $pw_text = "";
-				$func->confirmation(str_replace("%USER%", urldecode($username), $lang["usrmgr"]["add_success"]) . $pw_text, "index.php?mod=usrmgr&action=details&userid={$_GET["userid"]}");
-			}
-		break;
-	}
-} else $func->error("ACCESS_DENIED", "");
-*/
 ?>
