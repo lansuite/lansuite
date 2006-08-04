4eaa15a74a567eda15371ca798f91c1891558e7e
jochenjung <jochen.jung@gmail.com>
2006-08-04 21:10:03 +0000

    - Bugfix: #493 Korrekturen am Rotation Banner (KnoX 04.08.2006)

--- modules/sponsor/banner.php
+++ modules/sponsor/banner.php
@@ -1,30 +1,25 @@
 <?php
-$banner = $db->query_first("SELECT sponsorid, pic_path_banner, url
+$banner = $db->query_first("SELECT sponsorid, pic_path_banner, url, name
   FROM {$config['tables']['sponsor']}
-  WHERE rotation AND pic_path != '' AND pic_path != 'http://' AND !(pic_path LIKE 'http://%')
+  WHERE rotation AND ((pic_path != '' AND pic_path != 'http://') OR pic_path_banner != '')
   ORDER BY RAND()");
 
 $file_name = '';
 $old_file_name = 'ext_inc/banner/banner_'. substr($banner['pic_path'], strrpos($banner["pic_path"], 'ext_inc/banner/') + 15, strlen($banner['pic_path']));
 
-// If no Banner-Thumb was found, take lansuite-banner
-if ($banner['pic_path_banner'] == '') $templ['index']['banner_code'] = '<img src="ext_inc/banner/one_network_banner.jpg" border="1" width="468" height="60" class="img_border" alt="top"/>';
+// If no specific rotation banner is given, use the banner from the sponsor page
+if ($banner['pic_path_banner'] == '' and file_exists($old_file_name)) $file_name = $old_file_name;
+else $file_name = $banner['pic_path_banner'];
+
+// If no Banner-Thumb was found, use LanSuite default banner
+if ($file_name == '') $file_name = 'ext_inc/banner/one_network_banner.jpg';
 
 // If entry is HTML-Code
-elseif (substr($banner['pic_path_banner'], 0, 12) == 'html-code://') {
-	$templ['index']['banner_code'] = substr($banner["pic_path_banner"], 12, strlen($banner["pic_path_banner"]) - 12);
+if (substr($file_name, 0, 12) == 'html-code://') $templ['index']['banner_code'] = substr($file_name, 12, strlen($file_name) - 12);
+else {
+	$templ['index']['banner_code'] = '<img src="'. $file_name .'" border="1" width="468" height="60" class="img_border" title="'. $banner['name'] .'" alt="Sponsor Banner"/>';
 
-// Else add Image-Tag
-} else {
-  if (file_exists($banner['pic_path_banner'])) $file_name = $banner['pic_path_banner'];
-  elseif (file_exists($old_file_name)) $file_name = $old_file_name;
-  else  $file_name = $banner['pic_path_banner'];
-  
-  if ($file_name != '') {
-  	$templ['index']['banner_code'] = '<img src="'. $file_name .'" border="1" width="468" height="60" class="img_border" alt="top"/>';
-
-  	// Link banner, if in online mode
-  	if ($cfg["sys_internet"]) $templ['index']['banner_code'] = '<a href="index.php?mod=sponsor&action=bannerclick&design=base&sponsorid='. $banner["sponsorid"] .'" target="_blank">'. $templ['index']['banner_code'] .'</a>';
-  }
+	// Link banner, if in online mode
+	if ($cfg['sys_internet']) $templ['index']['banner_code'] = '<a href="index.php?mod=sponsor&action=bannerclick&design=base&sponsorid='. $banner["sponsorid"] .'" target="_blank">'. $templ['index']['banner_code'] .'</a>';
 }
 ?>
