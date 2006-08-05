61543f0a50eda0ebc213dd87621f9497c11aeb22
jochenjung <jochenjung@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2006-08-05 17:27:15 +0000

    - Change: #353 Sponsorenstatistiken werden nun aufgeschl�sselt in Hits/Visits auf Sponsorenseite, Banner und Box (KnoX 05.08.2006)

--- trunk/modules/sponsor/banner.php
+++ trunk/modules/sponsor/banner.php
@@ -7,6 +7,8 @@ $banner = $db->query_first("SELECT sponsorid, pic_path_banner, url, name
 $file_name = '';
 $old_file_name = 'ext_inc/banner/banner_'. substr($banner['pic_path'], strrpos($banner["pic_path"], 'ext_inc/banner/') + 15, strlen($banner['pic_path']));
 
+if ($banner['sponsorid']) $db->query("UPDATE {$config['tables']['sponsor']} SET views_banner = views_banner + 1 WHERE sponsorid = '{$banner['sponsorid']}'");
+
 // If no specific rotation banner is given, use the banner from the sponsor page
 if ($banner['pic_path_banner'] == '' and file_exists($old_file_name)) $file_name = $old_file_name;
 else $file_name = $banner['pic_path_banner'];
@@ -20,6 +22,6 @@ else {
 	$templ['index']['banner_code'] = '<img src="'. $file_name .'" border="1" width="468" height="60" class="img_border" title="'. $banner['name'] .'" alt="Sponsor Banner"/>';
 
 	// Link banner, if in online mode
-	if ($cfg['sys_internet']) $templ['index']['banner_code'] = '<a href="index.php?mod=sponsor&action=bannerclick&design=base&sponsorid='. $banner["sponsorid"] .'" target="_blank">'. $templ['index']['banner_code'] .'</a>';
+	if ($cfg['sys_internet']) $templ['index']['banner_code'] = '<a href="index.php?mod=sponsor&action=bannerclick&design=base&type=banner&sponsorid='. $banner["sponsorid"] .'" target="_blank">'. $templ['index']['banner_code'] .'</a>';
 }
 ?>
