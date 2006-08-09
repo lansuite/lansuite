8d08ae61ddb71f856b9ef302e64bcfc33895310c
genesis_81 <genesis_81@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2006-08-09 11:16:36 +0000

    Fehler beim eigenen Ticket ansehen behoben.

--- trunk/modules/usrmgr/myticket.php
+++ trunk/modules/usrmgr/myticket.php
@@ -4,7 +4,7 @@ $module = $db->query_first("SELECT * FROM {$config["tables"]["modules"]} WHERE n
 $pdf = $db->query_first("SELECT * FROM {$config["tables"]["pdf_list"]} WHERE name = '{$cfg['user_ticket_template']}' AND template_type='guestcards'");
 if ($module["active"] && $pdf['template_id']){
 	$dsp->NewContent($lang["usrmgr"]["myticket_caption"], $lang["usrmgr"]["myticket_subcaption"]);
-	$dsp->AddSingleRow("<a class=\"menu\" href=\"index.php?mod=pdf&action=guestcards&design=base&act=print&id={$pdf['template_id']}&userid={$auth['userid']}\">{$lang["usrmgr"]["myticket_show"]}</a>");
+	$dsp->AddSingleRow("<a class=\"menu\" href=\"index.php?mod=pdf&action=ticket&design=base&act=print&id={$pdf['template_id']}&userid={$auth['userid']}\">{$lang["usrmgr"]["myticket_show"]}</a>");
 	$dsp->AddBackButton("index.php", "usrmgr/myticket"); 
 	$dsp->AddContent();
 }else{
