5ed2630b52837d833dc4d58102d078454b1270b5
jochenjung <jochenjung@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2006-09-02 09:39:27 +0000

    - Bugfix: #503 Im Messenger konnten keine Nachrichten mehr verschickt werden (KnoX 02.09.2006)

--- trunk/modules/msgsys/query_console.php
+++ trunk/modules/msgsys/query_console.php
@@ -1,12 +1,28 @@
 <?php
 
-if($_GET[action] == "add" && $_POST[text] != "") {
+if ($_GET['step'] == 2 and $_POST['text'] != '') {
 	$time = time();
 
-	$insert = $db->query("INSERT INTO	{$config[tables][messages]} 
-    SET text='$_POST[text]', timestamp='$time', new='1', senderid='{$_SESSION[auth][userid]}', receiverid='$_GET[queryid]'
+	$insert = $db->query("INSERT INTO	{$config['tables']['messages']}
+    SET text='{$_POST['text']}', timestamp='$time', new='1', senderid='{$auth['userid']}', receiverid='{$_GET['queryid']}'
     ");
 }
+
+/*
+$dsp->SetForm("index.php?mod=msgsys&action=query_console&design=base&queryid={$_GET['queryid']}&step=2");
+$dsp->AddSingleRow('
+<script language="JavaScript">
+function loadwindow()
+{
+	top.location.href="index.php?mod=msgsys&action=query&design=base&queryid='. $_GET['queryid'] .'";
+}
+</script>
+');
+$dsp->AddTextAreaPlusRow('text', 'Eingabe', '', '');
+$dsp->AddFormSubmitRow('send', '', 'senden" onClick="loadwindow()');
+$dsp->AddContent();
+$index .= $templ['index']['info']['content'];
+*/
 
 $gd->CreateButton('bold');
 $gd->CreateButton('kursiv');
@@ -15,6 +31,7 @@ $gd->CreateButton('code');
 $gd->CreateButton('picture');
 $gd->CreateButton('send');
 $index .= $dsp->FetchTpl('design/templates/messenger_query_console.htm', $templ);
+
 $func->templ_output($index);
 	
 ?>
