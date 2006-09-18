d427653905a1e1a64eaabf48afcb5a53fecd2637
jochenjung <jochenjung@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2006-09-18 16:17:27 +0000

    - Change: #512 Kommentare kann man nun direkt unterhalb der bereits vorhandenen schreiben, ohne vorher eine neue Seite laden zu m�ssen (KnoX 18.09.2006)

--- trunk/modules/mastercomment/class_mastercomment.php
+++ trunk/modules/mastercomment/class_mastercomment.php
@@ -1,16 +1,13 @@
 <?php
 
 class Mastercomment{
-	
-	
 	var $config;
 	var $vars;
 	var $data;
 	var $class;
 	var $lang;
-	
-	
-	
+
+
 	// Konstruktor
 	function Mastercomment($vars, $working_link,$modul,$objectid,$objectname) {
 		global $language;
@@ -20,85 +17,72 @@ class Mastercomment{
 		$this->config['objectname']		= $objectname;
 		$this->vars                     = $vars;
 	
-		if(file_exists("modules/mastercomment/language/mastercomment_lang_$language.php")){
-			include("modules/mastercomment/language/mastercomment_lang_$language.php");
-		}else{
-			include("modules/mastercomment/language/mastercomment_lang_de.php");
-		}
-		
+		if (file_exists("modules/mastercomment/language/mastercomment_lang_$language.php"))
+      include("modules/mastercomment/language/mastercomment_lang_$language.php");
+		else include("modules/mastercomment/language/mastercomment_lang_de.php");
 		$this->lang = $lang;
-		
 	}
-	
-	
-	
+
+
 	function action(){
+    global $auth;
+    
 		switch ($this->vars['mcact']){
-			
 			default:
 				$this->show_comment();
 				$_SESSION['add_comment_blocker'] = 0;
+				if ($auth['login']) $this->add_comment("add");
 				return "show";
 			break;	
-		
+
 			case "add":
 				$this->add_comment("add");
 				return "add";
 			break;
-				
+
 			case "delete":
 				$this->delete_comment();
 				return "delete";
 			break;
-			
+
 			case "change":
 				$this->add_comment("change");
 				return "change";
 			break;
-			
-		
 		}
-	
-	
-	
-	
 	}
-	
-	
-	
+
+
 	function show_comment(){
 		global $func, $config, $dsp, $db, $templ, $auth;
-		
+
 		$found_entries = $db->query("SELECT commentid
-						FROM {$config["tables"]["comments"]} 
-						WHERE relatedto_id = '{$this->config['objectid']}'
-						AND relatedto_item = '{$this->config['modul']}'");	
-	
-		$count_entrys =  $db->num_rows($found_entries);	
-		
+			FROM {$config["tables"]["comments"]} 
+			WHERE relatedto_id = '{$this->config['objectid']}'
+			AND relatedto_item = '{$this->config['modul']}'");	
+
+		$count_entrys = $db->num_rows($found_entries);
 		$pages = $func->page_split($this->vars['comment_page'],$config["size"]["comments"],$count_entrys,$this->config['working_link'],"comment_page");
 
 		$get_comments = $db->query("SELECT A.commentid, A.text, A.date, A.creatorid, B.username, B.userid, C.avatar_path, C.signature
-							FROM {$config["tables"]["comments"]} AS A
-							LEFT JOIN {$config["tables"]["user"]} AS B ON A.creatorid = B.userid
-							LEFT JOIN {$config["tables"]["usersettings"]} AS C ON B.userid = C.userid
-							WHERE A.relatedto_id = '{$this->config['objectid']}'
-								AND A.relatedto_item = '{$this->config['modul']}'
-								{$pages["sql"]}
-								");
-		
-		$dsp->NewContent(str_replace("%MODULE%", $this->config['modul'], str_replace("%ITEM%", $this->config['objectname'], $this->lang['mastercomment']['comment_caption'])));
+  		FROM {$config["tables"]["comments"]} AS A
+  		LEFT JOIN {$config["tables"]["user"]} AS B ON A.creatorid = B.userid
+  		LEFT JOIN {$config["tables"]["usersettings"]} AS C ON B.userid = C.userid
+  		WHERE A.relatedto_id = '{$this->config['objectid']}'
+  			AND A.relatedto_item = '{$this->config['modul']}'
+  			{$pages["sql"]}
+  			");
 
+    $dsp->AddFieldsetStart(str_replace("%MODULE%", $this->config['modul'], str_replace("%ITEM%", $this->config['objectname'], $this->lang['mastercomment']['comment_caption'])));
 		if ($count_entrys > 0){
 			if ($pages["html"]) $dsp->AddSingleRow($pages["html"] ,"align=\"right\"");
-		
+
 			$i = 1;
 			while ($row = $db->fetch_array($get_comments)){
-
 				$templ['mastercomment']['show']['row']['info']['text'] = $func->text2html($row["text"]);
 
-				if($row["signature"] != "")
-				$templ['mastercomment']['show']['row']['info']['text'] .= HTML_NEWLINE . HTML_NEWLINE . HTML_NEWLINE . $func->text2html($row["signature"]);
+				if ($row["signature"] != "")
+				  $templ['mastercomment']['show']['row']['info']['text'] .= HTML_NEWLINE . HTML_NEWLINE . HTML_NEWLINE . $func->text2html($row["signature"]);
 
 				$templ['mastercomment']['show']['row']['info']['number'] 	= $i++;
 				$templ['mastercomment']['show']['row']['info']['username'] 	= $row["username"];
@@ -106,11 +90,9 @@ class Mastercomment{
 				$templ['mastercomment']['show']['row']['info']['userid'] 	= $row["userid"];
 				$templ['mastercomment']['show']['row']['info']['commentid'] 	= $row["commentid"];
 
-				if($row["avatar_path"] != ""){
+				if($row["avatar_path"] != "")
 					$templ['mastercomment']['show']['row']['info']['avatar'] 	= "<img src=\"ext_inc/avatare/" . $row["avatar_path"] . "\"</img>";
-				}else{
-					$templ['mastercomment']['show']['row']['info']['avatar'] = "";
-				}
+				else $templ['mastercomment']['show']['row']['info']['avatar'] = "";
 
 				unset($templ['mastercomment']['show']['row']['control']['button_edit']);
 				unset($templ['mastercomment']['show']['row']['control']['button_delete']);
@@ -127,119 +109,88 @@ class Mastercomment{
 			$dsp->AddSingleRow($this->lang['mastercomment']['count_comments'] . " $count_entrys");
 		} else $dsp->AddSingleRow($this->lang['mastercomment']['no_comments']);
 		
-		if($_SESSION["auth"]["login"] != 0)
-		$dsp->AddDoubleRow($this->lang['mastercomment']['add_comment'], $dsp->FetchButton($this->config['working_link'] . "&mcact=add","add"));
+		#if($_SESSION["auth"]["login"] != 0)
+		#$dsp->AddDoubleRow($this->lang['mastercomment']['add_comment'], $dsp->FetchButton($this->config['working_link'] . "&mcact=add","add"));
+		$dsp->AddFieldsetEnd();
 		$dsp->AddContent();
-		
 	}
 	
 	function add_comment($action){
 		global $func,$db,$dsp,$config;
+
 		// Error Check
 		switch ($this->vars['mcstep']){
 			default:
 				if($action == "change"){
 					$comment = $db->query_first("SELECT commentid, text FROM {$config["tables"]["comments"]}
-							      							WHERE commentid='{$this->vars["commentid"]}'");	
-					
+					  WHERE commentid='{$this->vars["commentid"]}'");
 					$this->vars["comment_comment"] = $comment["text"];
 				}
-			
 			break;
+
 			case 2:
-			if(strlen($this->vars["comment_comment"]) > 1500) {
-
+			if (strlen($this->vars["comment_comment"]) > 1500) {
 				$error_text = $this->lang['mastercomment']['error_text'];
-
 				$this->vars['mcstep'] = 1;
-
 			}
 
-
-			if($this->vars["comment_comment"] == "") {
-
+			if ($this->vars["comment_comment"] == "") {
 				$error_text = $this->lang['mastercomment']['error_text2'];
-
 				$this->vars['mcstep'] = 1;
 			}
-		
 		}
-		
-		
+
 		switch ($this->vars['mcstep']){
-			
 			default:
-				$dsp->NewContent(str_replace("%ITEM%",$this->config['objectname'],$lang['mastercomment']['add_comment_capt']));
+				#$dsp->NewContent(str_replace("%ITEM%", $this->config['objectname'], $lang['mastercomment']['add_comment_capt']));
+				$dsp->AddFieldsetStart($this->lang['mastercomment']['comment']);
 				$dsp->SetForm($this->config['working_link'] . "&mcact=$action&mcstep=2&commentid={$this->vars["commentid"]}");
-				$dsp->AddTextAreaPlusRow("comment_comment",$this->lang['mastercomment']['comment'],$this->vars["comment_comment"],$error_text);
+				$dsp->AddTextAreaPlusRow("comment_comment", $this->lang['mastercomment']['comment'], $this->vars["comment_comment"], $error_text);
 				$dsp->AddFormSubmitRow("add");
+				$dsp->AddFieldsetEnd();
 				$dsp->AddBackButton($this->config['working_link']);
 				$dsp->AddContent();
 			break;
 			
 			case 2:
 			if ($_SESSION['add_comment_blocker'] != 1) {
-
 				$date = date("U");
 
 				$userid = $_SESSION['auth']['userid'];
-				if($action == "change"){
+				if ($action == "change"){
 					$db->query("UPDATE {$config["tables"]["comments"]} SET text = '{$this->vars["comment_comment"]}'
-										WHERE commentid = '{$this->vars["commentid"]}'");
-		
+					  WHERE commentid = '{$this->vars["commentid"]}'");
 					$func->confirmation($this->lang['mastercomment']['change_comment_ok'],$this->config['working_link']);
 					
-				}else{
+				} else {
 					$db->query("INSERT INTO {$config["tables"]["comments"]} SET relatedto_id = '{$this->config['objectid']}',
-												    relatedto_item = '{$this->config['modul']}',
-												    creatorid = '$userid',
-												    date = '$date',
-												    text = '{$this->vars["comment_comment"]}'");
-
+				    relatedto_item = '{$this->config['modul']}',
+				    creatorid = '$userid',
+				    date = '$date',
+				    text = '{$this->vars["comment_comment"]}'");
 					$func->confirmation($this->lang['mastercomment']['add_comment_ok'],$this->config['working_link']);
 				}
-
 				$_SESSION['add_comment_blocker'] = 1;
-
-			}
-
-			else $func->error("NO_REFRESH","");
-		
+			} else $func->error("NO_REFRESH","");
 		}
-		
 	}
-	
+
+
 	function delete_comment(){
 		global $func,$db,$config;
 		
 		switch($this->vars["mcstep"]) {
-
 			default:
-
-			$link_yes = ($this->config['working_link'] . "&commentid=" . $this->vars["commentid"] . "&mcstep=2&mcact=delete");
-
-			$func->question($this->lang['mastercomment']['del_comment_quest'],$link_yes,$this->config['working_link']);
-
+  			$link_yes = ($this->config['working_link'] . "&commentid=" . $this->vars["commentid"] . "&mcstep=2&mcact=delete");
+  			$func->question($this->lang['mastercomment']['del_comment_quest'],$link_yes,$this->config['working_link']);
 			break;
-
 
 			case 2:
-
-			$delete = $db->query("DELETE FROM {$config["tables"]["comments"]} WHERE commentid = '{$this->vars["commentid"]}'");
-
-			if($delete == 1) {
-
-				$func->confirmation($this->lang['mastercomment']['add_comment_del'],$this->config['working_link']);
-
-			}
-
-			else $func->error($this->lang['mastercomment']['comment_not_exist'],"");
-
+  			$delete = $db->query("DELETE FROM {$config["tables"]["comments"]} WHERE commentid = '{$this->vars["commentid"]}'");
+  			if ($delete == 1) $func->confirmation($this->lang['mastercomment']['add_comment_del'],$this->config['working_link']);
+  			else $func->error($this->lang['mastercomment']['comment_not_exist'],"");
 			break;
-
 		}
 	}
-	
-	
 }
 ?>
