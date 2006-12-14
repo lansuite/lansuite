a6536fb775fcab8ea07c45716fa70e03497ecd76
jochenjung <jochen.jung@gmail.com>
2006-12-14 09:36:27 +0000

    - Bugfix: Die Funktion preg_split ergab in neueren PHP-versionen einige Fehlermeldungen auf den Installations-Seiten (KnoX 14.12.2006)

--- inc/classes/class_xml.php
+++ inc/classes/class_xml.php
@@ -3,11 +3,11 @@
 class xml {
 	// This function gets master-tags in XML-files. It also combines closed and later re-opened tags
 	function get_tag_content_combine($tag_to_grab, $input) {
-		$tag_content = preg_split("/\<$tag_to_grab\>/i", $input, "", "PREG_SPLIT_NO_EMPTY");
+		$tag_content = preg_split("/\<$tag_to_grab\>/i", $input, 0, PREG_SPLIT_NO_EMPTY);
 		$i = "1";
 		while ($i < sizeof($tag_content)) {
 			if (strpos($tag_to_grab, " ") > 0) $tag_to_grab = substr($tag_to_grab, 0, strpos($tag_to_grab, " "));
-			$block = preg_split("/\<\/$tag_to_grab\>/i", $tag_content[$i], "", "PREG_SPLIT_NO_EMPTY");
+			$block = preg_split("/\<\/$tag_to_grab\>/i", $tag_content[$i], 0, PREG_SPLIT_NO_EMPTY);
 			$block[0] = str_replace("--lt--", "<", $block[0]);
 			$block[0] = str_replace("--gt--", ">", $block[0]);
 			$output .= $block[0];
@@ -19,11 +19,11 @@ class xml {
 
 	// This function gets master-tags in XML-files. It also combines closed and later re-opened tags, and writes them into arrays
 	function get_tag_content_array($tag_to_grab, $input) {
-		$tag_content = preg_split("/\<$tag_to_grab\>/i",$input,"","PREG_SPLIT_NO_EMPTY");
+		$tag_content = preg_split("/\<$tag_to_grab\>/i",$input, 0, PREG_SPLIT_NO_EMPTY);
 		$i = "1";
 		while ($i < sizeof($tag_content)) {
 			if (strpos($tag_to_grab, " ") > 0) $tag_to_grab = substr($tag_to_grab, 0, strpos($tag_to_grab, " "));
-			$block = preg_split("/\<\/$tag_to_grab\>/i", $tag_content[$i], "", "PREG_SPLIT_NO_EMPTY");
+			$block = preg_split("/\<\/$tag_to_grab\>/i", $tag_content[$i], 0, PREG_SPLIT_NO_EMPTY);
 #			$block[0] = str_replace("--lt--", "<", $block[0]);
 #			$block[0] = str_replace("--gt--", ">", $block[0]);
 			$output[] = $block[0];
@@ -36,9 +36,9 @@ class xml {
 	// This function grabs to contents of a single XML-Tag. It does not support combining!
 	function get_tag_content($tag_to_grab, $input, $replace_html = 1) {
 		// Grab header
-		$tag_content = preg_split("/\<$tag_to_grab\>/i", $input, "", "PREG_SPLIT_NO_EMPTY");
+		$tag_content = preg_split("/\<$tag_to_grab\>/i", $input, 0, PREG_SPLIT_NO_EMPTY);
 		if (strpos($tag_to_grab, " ") > 0) $tag_to_grab = substr($tag_to_grab, 0, strpos($tag_to_grab, " "));
-		$tag_content = preg_split("/\<\/$tag_to_grab\>/i", $tag_content[1], "", "PREG_SPLIT_NO_EMPTY");
+		$tag_content = preg_split("/\<\/$tag_to_grab\>/i", $tag_content[1], 0, PREG_SPLIT_NO_EMPTY);
 
 		if ($replace_html) {
 			$tag_content[0] = str_replace("--lt--", "<", $tag_content[0]);
