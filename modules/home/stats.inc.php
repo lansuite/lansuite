3e148661fe5e8b533051a842b7a77f54da4963dd
jochenjung <jochen.jung@gmail.com>
2006-12-20 22:03:11 +0000

    - Change: Partyverwaltung �berarbeitet und in eigenes Modul ausgelagert (KnoX 20.12.2006)

--- modules/home/stats.inc.php
+++ modules/home/stats.inc.php
@@ -1,19 +1,20 @@
 <?php
-
-$templ['home']['show']['item']['info']['caption'] = $lang["home"]["stats_caption"] . " " . $_SESSION['party_info']['name'];
-$templ['home']['show']['item']['control']['row'] = "";
-
-// With or without admins?
-if($cfg["guestlist_showorga"] == 0) { $querytype = "type = 1"; } else { $querytype = "type >= 1"; }
-
-$stat_infos = $stats->get_stat();
-$templ['home']['show']['item']['control']['row'] = '';
-
-// User paid / Checked In / Checked Out
-$templ['home']['show']['item']['control']['row'] .= $lang["home"]["stats_guests"] .': '. $stat_infos['user_paid'] .' / '. $stat_infos['user_checkin'] .' / '. $stat_infos['user_checkout'] . HTML_NEWLINE;
-
-// User overall / online
-$templ['home']['show']['item']['control']['row'] .= $lang["home"]["stats_user"] .": ". $stat_infos['user_visits'] .' / '. $stat_infos['user_online'] . HTML_NEWLINE;
+  $templ['home']['show']['item']['info']['caption'] = $lang["home"]["stats_caption"] . " " . $_SESSION['party_info']['name'];
+  $templ['home']['show']['item']['control']['row'] = "";
+  
+if ($party->count > 0) {
+  // With or without admins?
+  if($cfg["guestlist_showorga"] == 0) { $querytype = "type = 1"; } else { $querytype = "type >= 1"; }
+  
+  $stat_infos = $stats->get_stat();
+  $templ['home']['show']['item']['control']['row'] = '';
+  
+  // User paid / Checked In / Checked Out
+  $templ['home']['show']['item']['control']['row'] .= $lang["home"]["stats_guests"] .': '. $stat_infos['user_paid'] .' / '. $stat_infos['user_checkin'] .' / '. $stat_infos['user_checkout'] . HTML_NEWLINE;
+  
+  // User overall / online
+  $templ['home']['show']['item']['control']['row'] .= $lang["home"]["stats_user"] .": ". $stat_infos['user_visits'] .' / '. $stat_infos['user_online'] . HTML_NEWLINE;
+}
 
 // Additional Admin-Stats	
 if ($auth["type"] >= 2) {
