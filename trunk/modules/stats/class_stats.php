00b470ea523e20a2cf0fca736a4e85c2114d04f2
jochenjung <jochenjung@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2006-12-21 14:50:22 +0000

    - Bugfix: Statistiken lieferten teilweise unterschiedliche Werte. Ein Hit wurde auf einigen Systemen immer als neuer visit gez�hlt (KnoX 21.12.2006)

--- trunk/modules/stats/class_stats.php
+++ trunk/modules/stats/class_stats.php
@@ -10,34 +10,34 @@ class stats {
 
 		// Update usage stats
 		// Is the user known, or is it a new visit? - After 30min idle this counts as a new visit
-		if ($_SESSION['last_hit'] > (time() - 60 * 30)) {
-      // Existing session -> Only hit
-      
+
+    // Existing session -> Only hit
+		if ($_COOKIE['last_hit'] > (time() - 60 * 30)) {
 			$find = $db->query_first_rows("SELECT hits FROM {$config["tables"]["stats_usage"]} WHERE DATE_FORMAT(time, '%Y-%m-%d %H:00:00') = DATE_FORMAT(NOW(), '%Y-%m-%d %H:00:00')");
 			if ($find["number"] > 0) $db->query("UPDATE {$config["tables"]["stats_usage"]} SET hits = hits + 1 WHERE DATE_FORMAT(time, '%Y-%m-%d %H:00:00') = DATE_FORMAT(NOW(), '%Y-%m-%d %H:00:00')");
 			else $db->query("INSERT INTO {$config["tables"]["stats_usage"]} SET visits = 0, hits = 1, time = DATE_FORMAT(NOW(), '%Y-%m-%d %H:00:00')");
+
+    // New session -> Hit and visit
     } else {
-      // New session -> Hit and visit
 			$find = $db->query_first_rows("SELECT hits FROM {$config["tables"]["stats_usage"]} WHERE DATE_FORMAT(time, '%Y%m%d%H') = DATE_FORMAT(NOW(), '%Y%m%d%H')");
 			if ($find["number"] > 0) $db->query("UPDATE {$config["tables"]["stats_usage"]} SET visits = visits + 1, hits = hits + 1 WHERE DATE_FORMAT(time, '%Y-%m-%d %H:00:00') = DATE_FORMAT(NOW(), '%Y-%m-%d %H:00:00')");
 			else $db->query("INSERT INTO {$config["tables"]["stats_usage"]} SET visits = 1, hits = 1, time = DATE_FORMAT(NOW(), '%Y-%m-%d %H:00:00')");
-      
-  		$_SESSION['last_hit'] = time();
+
+      setcookie('last_hit', time(), time() + (30 * 60));
 		}
 
 
 		// Update search engine data
-		$search_engine = "";
-		if (strpos($_SERVER["http_referer"], "ttp://www.google.") > 0) $search_engine = "google";
-		if (strpos($_SERVER["http_referer"], ".yahoo.com/search") > 0) $search_engine = "yahoo";
-		if (strpos($_SERVER["http_referer"], ".altavista.com") > 0) $search_engine = "altavista";
-		if (strpos($_SERVER["http_referer"], "ttp://search.msn.") > 0) $search_engine = "msn";
-		if (strpos($_SERVER["http_referer"], ".aol.de/suche") > 0) $search_engine = "aol_de";
-		if (strpos($_SERVER["http_referer"], "search.aol.com/") > 0) $search_engine = "aol_com";
-		if (strpos($_SERVER["http_referer"], ".web.de/") > 0) $search_engine = "web_de";
+		$search_engine = '';
+		if (strpos($_SERVER['http_referer'], 'ttp://www.google.') > 0) $search_engine = 'google';
+		elseif (strpos($_SERVER['http_referer'], '.yahoo.com/search') > 0) $search_engine = 'yahoo';
+		elseif (strpos($_SERVER['http_referer'], '.altavista.com') > 0) $search_engine = 'altavista';
+		elseif (strpos($_SERVER['http_referer'], 'ttp://search.msn.') > 0) $search_engine = 'msn';
+		elseif (strpos($_SERVER['http_referer'], '.aol.de/suche') > 0) $search_engine = 'aol_de';
+		elseif (strpos($_SERVER['http_referer'], 'search.aol.com/') > 0) $search_engine = 'aol_com';
+		elseif (strpos($_SERVER['http_referer'], '.web.de/') > 0) $search_engine = 'web_de';
 
-		if ($search_engine != "") {
-
+		if ($search_engine != '') {
   		$query_var = array(
   			"google" => 'q',
   			"yahoo" => "p",
@@ -82,16 +82,14 @@ class stats {
 	
 	// Auslesen des Speichers
 	function mem_info() {
-		//version
+
 		$version = file("/proc/version");
 		$version = $version[0];
 		
 		$memory = file("/proc/meminfo");
 
+		//speicher auslesen aus Kernel 2.6 und 2.5
 		if (preg_match ("/\bLinux version 2\.5\b/i", $version) || preg_match ("/\bLinux version 2\.6\b/i", $version))  {
-
-			//speicher auslesen aus Kernel 2.6 und 2.5
-
 			$data['mem_total'] = explode(":",$memory[0]);    
 			$data['mem_total'] = explode("kB",$data['mem_total'][1]);  
 			$data['mem_total'] = round($data['mem_total'][0] / 1024,"2");
@@ -104,13 +102,10 @@ class stats {
 			$data['swap_free'] = explode(":",$memory[12]);  
 			$data['swap_free'] = explode("kB",$data['swap_free'][1]);   
 			$data['swap_free'] = round($data['swap_free'][0] / 1024,"2");
-
 		}
 
+		// Speicher auslesen für Kernel 2.4
 		elseif (preg_match ("/\bLinux version 2\.4\b/i", $version)) {
-
-			// Speicher auslesen für Kernel 2.4
-
 			$data['mem_total'] = explode(":",$memory[3]);    
 			$data['mem_total'] = explode("kB",$data['mem_total'][1]);  
 			$data['mem_total'] = round($data['mem_total'][0] / 1024,"2");
@@ -123,15 +118,13 @@ class stats {
 			$data['swap_free'] = explode(":",$memory[16]);  
 			$data['swap_free'] = explode("kB",$data['swap_free'] [1]);   
 			$data['swap_free'] = round($data['swap_free'] [0] / 1024,"2");
-
-
 		}
+
 		return $data;
 	}
 	
 	
 	function load_avg(){
-		//loadaverage
 		$loadavg = file ("/proc/loadavg");
 		$loadavg = split(" ",$loadavg[0]);
 		
@@ -158,32 +151,22 @@ class stats {
 		
 		$data['rxmbytes'] = round(($RX_bytes / 1024 / 1024),"1");
 		
-		if ($RX_bytes > (1024*1024)){
-			$data['RX'] = round(($RX_bytes / 1024 / 1024),"1") . " Mbyte";
-		}elseif ($RX_bytes > 1024){
-			$data['RX'] = round(($RX_bytes / 1024),"1") . " kbyte";
-		}else{
-			$data['RX'] = $RX_bytes . " Byte";
-
-		}
+		if ($RX_bytes > (1024*1024)) $data['RX'] = round(($RX_bytes / 1024 / 1024),"1") . " Mbyte";
+		elseif ($RX_bytes > 1024) $data['RX'] = round(($RX_bytes / 1024),"1") . " kbyte";
+		else $data['RX'] = $RX_bytes . " Byte";
 
 		$TX_bytes = explode("TX bytes:",$network_info);
 		$TX_bytes = explode(" ",$TX_bytes[1]); $TX_bytes = $TX_bytes[0];
 
 		$data['txmbytes'] = round(($TX_bytes / 1024 / 1024),"1");
 		
-		if ($TX_bytes > (1024*1024)){
-			$data['TX'] = round(($TX_bytes / 1024 / 1024),"1") . " Mbyte";
-		}elseif ($TX_bytes > 1024){
-			$data['TX'] = round(($TX_bytes / 1024),"1") . " kbyte";			
-		}else{
-			$data['TX'] = $TX_bytes . " Byte";
-
-		}
+		if ($TX_bytes > (1024*1024)) $data['TX'] = round(($TX_bytes / 1024 / 1024),"1") . " Mbyte";
+		elseif ($TX_bytes > 1024) $data['TX'] = round(($TX_bytes / 1024),"1") . " kbyte";			
+		else $data['TX'] = $TX_bytes . " Byte";
 
 		return $data;
-
 	}
+
 	function getExportData() {
 		global $cfg, $config;
 		
@@ -196,15 +179,13 @@ class stats {
 		$stats['end'] 		= $_SESSION['party_info']['partyend'];
 		$stats['mail'] 		= $cfg["sys_party_mail"];
 		$stats['version'] 	= $config['lansuite']['version'];
-		
-				
+
 		return $stats;		
-	} // export
+	}
 	
 	function export() {
-		
 		$stats = $this->getExportData();
-		
+
 		$stats_data  = 'name='.urlencode($stats["name"]);
 		$stats_data .= '&url='.urlencode($stats["url"]);
 		$stats_data .= '&plz='.urlencode($stats["plz"]);
@@ -214,54 +195,8 @@ class stats {
 		$stats_data .= '&end='.$stats["end"];
 		$stats_data .= '&mail='.$stats["mail"];
 		$stats_data .= '&version='.urlencode($stats['version']);
-		
-		
-		include("http://www.lansuite.de/report.php?".$stats_data);		
-		
-	}	
-	
-	function get_stat(){
-		global $db,$config,$party,$cfg;
 
-				
-		if (!isset($this->stat_data['user_paid']) and $party->party_id != ''){
-			if ($cfg["guestlist_showorga"] == 0) { $querytype = "type = 1"; } else { $querytype = "type >= 1"; }
-			
-			$timestamp = time()-600;
-			$row = $db->query_first("SELECT count(*) as n FROM {$config["tables"]["party_user"]} AS p
-        LEFT JOIN {$config["tables"]["user"]} ON user_id=userid
-        WHERE $querytype AND (p.paid > 0) AND p.party_id={$party->party_id}
-        ");
-			$row2 = $db->query_first("SELECT count(*) as n FROM {$config["tables"]["party_user"]} AS p
-        LEFT JOIN {$config["tables"]["user"]} ON user_id=userid
-        WHERE p.checkin>1 AND p.checkout=0 AND $querytype AND p.party_id={$party->party_id}
-        ");
-			$row3 = $db->query_first("SELECT count(*) as n FROM {$config["tables"]["party_user"]} AS p
-        LEFT JOIN {$config["tables"]["user"]} ON user_id=userid
-        WHERE p.checkout>1 AND $querytype AND p.party_id={$party->party_id}
-        ");
-			$row4  = $db->query_first("SELECT count(*) as n FROM {$config["tables"]["stats_auth"]}
-        WHERE lasthit>=$timestamp AND userid!='0' GROUP BY userid
-        ");
-			$row5 = $db->query_first("SELECT count(*) as n FROM {$config["tables"]["stats_auth"]}
-        WHERE lasthit>=$timestamp AND userid='0'
-        ");
-			$row6 = $db->query_first("SELECT count(*) as n FROM {$config["tables"]["user"]}
-        WHERE $querytype
-        ");
-			
-			$visits = $db->query_first("SELECT SUM(visits) AS visits, SUM(hits) AS hits FROM {$config['tables']['stats_usage']}");
-			
-			$this->stat_data['user_paid'] = $row['n'];
-			$this->stat_data['user_checkin'] = $row2['n'];
-			$this->stat_data['user_checkout'] = $row3['n'];
-			$this->stat_data['user_online'] = ($row4['n'] + $row5['n']);
-			$this->stat_data['user_registred'] = $row6['n'];
-			$this->stat_data['user_visits'] = $visits['visits'];
-					
-		}
-		return $this->stat_data;
-	}
-	
-} // class
+		include("http://www.lansuite.de/report.php?".$stats_data);		
+	}	
+}
 ?>
--- trunk/modules/stats/hits.php
+++ trunk/modules/stats/hits.php
@@ -2,7 +2,7 @@
 
 $dsp->NewContent($lang["stats"]["user_caption"], $lang["stats"]["user_subcaption"]);
 
-$visits = $db->query_first("SELECT COUNT(visits) AS insg FROM {$config["tables"]["stats_usage"]}");
+$visits = $db->query_first("SELECT SUM(visits) AS insg FROM {$config["tables"]["stats_usage"]}");
 $dsp->AddDoubleRow($lang["stats"]["user_visits_insg"], $visits["insg"]);
 
 $hits = $db->query_first("SELECT SUM(hits) AS insg FROM {$config["tables"]["stats_usage"]}");
@@ -10,7 +10,7 @@ $dsp->AddDoubleRow($lang["stats"]["user_hits_insg"], $hits["insg"]);
 $dsp->AddDoubleRow($lang["stats"]["user_hits_avg"], round($hits["insg"] / $visits["insg"], 2));
 
 $visit_timeout = time() - 60*60;
-$online = $db->query_first("SELECT COUNT(*) AS insg FROM {$config["tables"]["stats_auth"]} WHERE (lasthit > $visit_timeout)");
+$online = $db->query_first("SELECT SUM(visits) AS insg FROM {$config["tables"]["stats_auth"]} WHERE (lasthit > $visit_timeout)");
 $user_online = $db->query("SELECT user.username
 		FROM {$config["tables"]["stats_auth"]} AS auth
 		LEFT JOIN {$config["tables"]["user"]} AS user ON user.userid = auth.userid
