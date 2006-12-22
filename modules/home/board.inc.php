b9001b253148f2df8ac5112022cf63a1bcfd4bbb
jochenjung <jochen.jung@gmail.com>
2006-12-22 13:38:07 +0000

    Auf der Startseite werden nun die anzahl der Newskommentare und Boardposts im Thread gezeigt

--- modules/home/board.inc.php
+++ modules/home/board.inc.php
@@ -4,7 +4,7 @@ $templ['home']['show']['item']['info']['caption'] = $lang["home"]["board_caption
 $templ['home']['show']['item']['control']['row'] = "";
 
 $authtyp = $auth['type'] + 1;
-$query = $db->query("SELECT t.tid, p.pid, t.caption, MAX(p.date) AS LastPost
+$query = $db->query("SELECT t.tid, p.pid, t.caption, MAX(p.date) AS LastPost, COUNT(p.pid) AS posts
 	FROM {$config["tables"]["board_threads"]} AS t
 	LEFT JOIN {$config["tables"]["board_forums"]} AS f ON t.fid = f.fid
 	LEFT JOIN {$config["tables"]["board_posts"]} AS p ON p.tid = t.tid
@@ -14,7 +14,7 @@ $query = $db->query("SELECT t.tid, p.pid, t.caption, MAX(p.date) AS LastPost
 	LIMIT 0,5");
 if ($db->num_rows($query) > 0) while($row = $db->fetch_array($query)) {
   $templ['home']['show']['row']['control']['link']	= "index.php?mod=board&action=thread&tid={$row['tid']}&pid={$row['pid']}";
-  $templ['home']['show']['row']['info']['text']		= $row['caption'];
+  $templ['home']['show']['row']['info']['text']		= $row['caption'] .' ('.$row['posts'].')';
   $templ['home']['show']['item']['control']['row']	.= $dsp->FetchModTpl('home', 'show_row');
 } else {
 	$templ['home']['show']['row']['text']['info']['text'] = "<i>{$lang["home"]["board_noentry"]}</i>";
