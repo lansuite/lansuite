85da442d1ddd4e3b2f3d711627bc9308dc9b9cad
jochenjung <jochen.jung@gmail.com>
2007-01-02 18:33:13 +0000

    - Bugfix: #554 Beim Anmelden zur Party wurde keine eigene Mail mehr versendet (KnoX 02.01.2007)

--- modules/home/board.inc.php
+++ modules/home/board.inc.php
@@ -4,7 +4,7 @@ $templ['home']['show']['item']['info']['caption'] = $lang["home"]["board_caption
 $templ['home']['show']['item']['control']['row'] = "";
 
 $authtyp = $auth['type'] + 1;
-$query = $db->query("SELECT f.fid, t.tid, p.pid, t.caption, MAX(p.date) AS LastPost, COUNT(p.pid) AS posts
+$query = $db->query("SELECT f.fid, t.tid, MAX(p.pid) AS pid, t.caption, MAX(p.date) AS LastPost, COUNT(p.pid) AS posts
 	FROM {$config["tables"]["board_threads"]} AS t
 	LEFT JOIN {$config["tables"]["board_forums"]} AS f ON t.fid = f.fid
 	LEFT JOIN {$config["tables"]["board_posts"]} AS p ON p.tid = t.tid
@@ -13,7 +13,7 @@ $query = $db->query("SELECT f.fid, t.tid, p.pid, t.caption, MAX(p.date) AS LastP
 	ORDER BY LastPost DESC
 	LIMIT 0,5");
 if ($db->num_rows($query) > 0) while($row = $db->fetch_array($query)) {
-  $templ['home']['show']['row']['control']['link']	= "index.php?mod=board&action=thread&fid={$row['fid']}&tid={$row['tid']}";
+  $templ['home']['show']['row']['control']['link']	= "index.php?mod=board&action=thread&fid={$row['fid']}&tid={$row['tid']}&gotopid={$row['pid']}#pid{$row['pid']}";
   $templ['home']['show']['row']['info']['text']		= $row['caption'] .' ('.$row['posts'].')';
   $templ['home']['show']['item']['control']['row']	.= $dsp->FetchModTpl('home', 'show_row');
 } else {
