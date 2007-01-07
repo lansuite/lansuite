94f1cad2ac9402115439bdd3ed6e95da0627f0db
jochenjung <jochenjung@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2007-01-07 21:39:00 +0000

    - Add: LGZ ID in die Benutzeranmeldung aufgenommen. Liegen IDs werden nur noch angezeigt, wenn Turniermodul aktiv ist (KnoX 07.01.2007)
    Usermanager auf t() umgestellt

--- trunk/modules/usrmgr/search.inc.php
+++ trunk/modules/usrmgr/search.inc.php
@@ -2,7 +2,7 @@
 include_once('modules/usrmgr/search_main.inc.php');
 
 function PaidIconLink($paid){
-  global $dsp, $templ, $line, $party, $lang;
+  global $dsp, $templ, $line, $party;
   
   // Only link, if selected party = current party
   if ($_POST["search_dd_input"][1] == $party->party_id) $templ['ms2']['link'] = 'index.php?mod=usrmgr&action=changepaid&step=2&userid='. $line['userid'];
@@ -10,10 +10,10 @@ function PaidIconLink($paid){
   
   if ($paid) {
     $templ['ms2']['icon_name'] = 'paid';
-    $templ['ms2']['icon_title'] = $lang['usrmgr']['paid_yes'];
+    $templ['ms2']['icon_title'] = t('Bezahlt');
   } else {
     $templ['ms2']['icon_name'] = 'not_paid';
-    $templ['ms2']['icon_title'] = $lang['usrmgr']['paid_no'];
+    $templ['ms2']['icon_title'] = t('Nicht bezahlt');
   }
   $templ['ms2']['link_item'] = $dsp->FetchModTpl('mastersearch2', 'result_icon');
   if ($templ['ms2']['link']) $templ['ms2']['link_item'] = $dsp->FetchModTpl('mastersearch2', 'result_link');
@@ -46,7 +46,7 @@ function IfLowerOrEqualUserlevel($userid) {
 
 $ms2->AddTextSearchField('NGL/WWCL/LGZ-ID', array('u.nglid' => 'exact', 'u.nglclanid' => 'exact', 'u.wwclid' => 'exact', 'u.wwclclanid' => 'exact', 'u.lgzid' => 'exact', 'u.lgzclanid' => 'exact',));
 
-$ms2->AddTextSearchDropDown($lang['usrmgr']['add_type'], 'u.type', array('' => $lang['usrmgr']['all'], '1' => $lang['usrmgr']['details_guest'], '!1' => 'Nicht Gast', '<0' => $lang['usrmgr']['search_deactivated'], '2' => $lang['usrmgr']['add_type_admin'], '3' => $lang['usrmgr']['add_type_operator'], '2,3' => $lang['usrmgr']['search_orga']));
+$ms2->AddTextSearchDropDown(t('Benutzertyp'), 'u.type', array('' => t('Alle'), '1' => t('Gast'), '!1' => t('Nicht Gast'), '<0' => t('Deaktiviert'), '2' => t('Admin'), '3' => t('Operator'), '2,3' => t('Admin, oder Operator')));
 	
 $party_list = array('' => 'Alle');
 $row = $db->query("SELECT party_id, name FROM {$config['tables']['partys']}");
@@ -54,29 +54,29 @@ while($res = $db->fetch_array($row)) $party_list[$res['party_id']] = $res['name'
 $db->free_result($row);
 $ms2->AddTextSearchDropDown('Party', 'p.party_id', $party_list, $party->party_id);
 
-$ms2->AddTextSearchDropDown($lang['usrmgr']['add_paid'], 'p.paid', array('' => $lang['usrmgr']['all'], '0' => $lang['usrmgr']['add_paid_no'], '>1' => $lang['usrmgr']['details_paid'], '1' => 'Bezahlt per Vorverkauf', '2' => $lang['usrmgr']['search_paid_ak']));
-$ms2->AddTextSearchDropDown($lang['usrmgr']['checkin'], 'p.checkin', array('' => $lang['usrmgr']['all'], '0' => $lang['usrmgr']['checkin_no'], '>1' => $lang['usrmgr']['checkin']));
-$ms2->AddTextSearchDropDown($lang['usrmgr']['checkout'], 'p.checkout', array('' => $lang['usrmgr']['all'], '0' => $lang['usrmgr']['checkout_no'], '>1' => $lang['usrmgr']['checkout']));
-$ms2->AddTextSearchDropDown($lang['usrmgr']['add_gender'], 'u.sex', array('' => $lang['usrmgr']['all'], '0' => $lang['usrmgr']['search_unknown_sex'], '1' => $lang['usrmgr']['search_male'], '2' => $lang['usrmgr']['search_female']));
-$ms2->AddTextSearchDropDown($lang['usrmgr']['accounts'], 'u.locked', array('' => $lang['usrmgr']['all'], '0' => $lang['usrmgr']['unlocked'], '1' => $lang['usrmgr']['locked']));
+$ms2->AddTextSearchDropDown(t('Zahlstatus'), 'p.paid', array('' => t('Alle'), '0' => t('Nicht bezahlt'), '>1' => t('Bezahlt'), '1' => t('Bezahlt per Vorverkauf'), '2' => t('Bezahlt per Abendkasse')));
+$ms2->AddTextSearchDropDown(t('Eingecheckt'), 'p.checkin', array('' => t('Alle'), '0' => t('Nicht eingecheckt'), '>1' => t('Eingecheckt')));
+$ms2->AddTextSearchDropDown(t('Ausgecheckt'), 'p.checkout', array('' => t('Alle'), '0' => t('Nicht ausgecheckt'), '>1' => t('Ausgecheckt')));
+$ms2->AddTextSearchDropDown(t('Geschlecht'), 'u.sex', array('' => t('Alle'), '0' => t('Unbekannt'), '1' => t('Männlich'), '2' => t('Weblich')));
+$ms2->AddTextSearchDropDown(t('Accounts'), 'u.locked', array('' => t('Alle'), '0' => t('Nur freigegebene'), '1' => t('Nur gesperrte')));
 
 $ms2->AddSelect('c.url AS clanurl');
 $ms2->AddSelect('u.type');
-$ms2->AddResultField($lang['usrmgr']['details_clan'], 'c.name AS clan', 'ClanURLLink');
+$ms2->AddResultField(t('Clan'), 'c.name AS clan', 'ClanURLLink');
 // If Party selected
 if ($_POST["search_dd_input"][1] != '' or $_GET["search_dd_input"][1] != '') {
-  $ms2->AddResultField($lang['usrmgr']['paid'], 'p.paid', 'PaidIconLink');
-  $ms2->AddResultField('In', 'p.checkin', 'MS2GetDate');
-  $ms2->AddResultField('Out', 'p.checkout', 'MS2GetDate');
+  $ms2->AddResultField(t('Bezahlt'), 'p.paid', 'PaidIconLink');
+  $ms2->AddResultField(t('In'), 'p.checkin', 'MS2GetDate');
+  $ms2->AddResultField(t('Out'), 'p.checkout', 'MS2GetDate');
 }
 
-$ms2->AddIconField('details', 'index.php?mod=usrmgr&action=details&userid=', $lang['ms2']['details']);
-if ($party->count > 0) $ms2->AddIconField('signon', 'index.php?mod=usrmgr&action=party&user_id=', $lang['ms2']['signon']);
-$ms2->AddIconField('send_mail', 'index.php?mod=mail&action=newmail&step=2&userID=', $lang['ms2']['send_mail']);
-$ms2->AddIconField('change_pw', 'index.php?mod=usrmgr&action=newpwd&step=2&userid=', $lang['ms2']['change_pw'], 'IfLowerOrEqualUserlevel');
-if ($auth['type'] >= 2) $ms2->AddIconField('assign', 'index.php?mod=usrmgr&action=switch_user&step=10&userid=', $lang['ms2']['switch_user'], 'IfLowerUserlevel');
-if ($auth['type'] >= 2) $ms2->AddIconField('edit', 'index.php?mod=usrmgr&action=change&step=1&userid=', $lang['ms2']['edit']);
-if ($auth['type'] >= 3) $ms2->AddIconField('delete', 'index.php?mod=usrmgr&action=delete&step=2&userid=', $lang['ms2']['delete']);
+$ms2->AddIconField('details', 'index.php?mod=usrmgr&action=details&userid=', t('Details'));
+if ($party->count > 0) $ms2->AddIconField('signon', 'index.php?mod=usrmgr&action=party&user_id=', t('Partyanmeldung'));
+$ms2->AddIconField('send_mail', 'index.php?mod=mail&action=newmail&step=2&userID=', t('Mail senden'));
+$ms2->AddIconField('change_pw', 'index.php?mod=usrmgr&action=newpwd&step=2&userid=', t('Passwort ändern'), 'IfLowerOrEqualUserlevel');
+if ($auth['type'] >= 2) $ms2->AddIconField('assign', 'index.php?mod=usrmgr&action=switch_user&step=10&userid=', t('Benutzer wechseln'), 'IfLowerUserlevel');
+if ($auth['type'] >= 2) $ms2->AddIconField('edit', 'index.php?mod=usrmgr&action=change&step=1&userid=', t('Editieren'));
+if ($auth['type'] >= 3) $ms2->AddIconField('delete', 'index.php?mod=usrmgr&action=delete&step=2&userid=', t('Löschen'));
 
 
 if ($auth['type'] >= 2) {
@@ -87,9 +87,9 @@ if ($auth['type'] >= 2) {
   }
   $db->free_result($res);
 }
-if ($auth['type'] >= 3) $ms2->AddMultiSelectAction($lang['usrmgr']['unlock'], "index.php?mod=usrmgr&action=account_lock&step=11", 1);
-if ($auth['type'] >= 3) $ms2->AddMultiSelectAction($lang['usrmgr']['lock'], "index.php?mod=usrmgr&action=account_lock&step=10", 1);
-if ($auth['type'] >= 3) $ms2->AddMultiSelectAction($lang['ms2']['delete'], "index.php?mod=usrmgr&action=delete&step=10", 1);
+if ($auth['type'] >= 3) $ms2->AddMultiSelectAction(t('Freigeben'), "index.php?mod=usrmgr&action=account_lock&step=11", 1);
+if ($auth['type'] >= 3) $ms2->AddMultiSelectAction(t('Sperren'), "index.php?mod=usrmgr&action=account_lock&step=10", 1);
+if ($auth['type'] >= 3) $ms2->AddMultiSelectAction(t('Löschen'), "index.php?mod=usrmgr&action=delete&step=10", 1);
 
 $ms2->PrintSearch('index.php?mod=usrmgr&action=search', 'u.userid');
 ?>
