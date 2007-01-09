5d4b44587be232e76260b29307279955b68b25f6
jochenjung <jochenjung@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2007-01-09 15:34:00 +0000

    - Add: Modul Bugtracker (KnoX 09.01.2007) - Add: Mastersearch kann nun einzellne Zeilen entsprechend eines DB-Feldes einf�rben (KnoX 09.01.2007)

---
+++ trunk/modules/home/bugtracker.inc.php
@@ -0,0 +1,17 @@
+<?php
+
+$templ['home']['show']['item']['info']['caption'] = t('Neue Bugs und Feature Wünsche');
+$templ['home']['show']['item']['control']['row'] = "";
+
+$query = $db->query("SELECT * FROM {$config["tables"]["bugtracker"]} ORDER BY date LIMIT 0,8");
+if ($db->num_rows($query) > 0) while($row = $db->fetch_array($query)) {
+  $templ['home']['show']['row']['control']['link'] = "index.php?mod=bugtracker&bugid={$row['bugid']}";
+  if (strlen($row['caption']) > 40) $row['caption'] = substr($row['caption'], 0, 37).'...';
+  $templ['home']['show']['row']['info']['text']		= $row['caption'];
+  $templ['home']['show']['item']['control']['row']	.= $dsp->FetchModTpl('home', 'show_row');
+} else {
+	$templ['home']['show']['row']['text']['info']['text'] = "<i>". t('Keine Einträge vorhanden') ."</i>";
+	$templ['home']['show']['item']['control']['row'] .= $dsp->FetchModTpl('home', 'show_row_text');
+}
+
+?>
