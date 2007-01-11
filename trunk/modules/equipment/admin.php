49b24b58a8dd4a07031b8a4b03c215d8690b1150
jochenjung <jochenjung@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2007-01-11 22:38:20 +0000

    Funktionierende Module von addon ins Main Package verschoben

---
+++ trunk/modules/equipment/admin.php
@@ -0,0 +1,9 @@
+<?
+
+if (!$cfg["equipment_shopid"]) $func->error($lang["equipment"]["err_noid"], "");
+else {
+	$dsp->NewContent($lang["equipment"]["admin_caption"], $lang["equipment"]["admin_subcaption"]);
+	$dsp->AddSingleRow($lang["equipment"]["admin_text"]);
+	$dsp->AddContent();
+}
+?>
