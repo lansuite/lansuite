ed749e69255cfcf84391eff1517a8d91f952ae0a
jochenjung <jochen.jung@gmail.com>
2007-01-11 21:18:38 +0000

    - Designmanager erg�nzt. Jedoch nicht verlinkt, da er nicht wirklich funktioniert - Fehlende Current File eintr�ge erg�nzt

--- modules/guestlist/checking.php
+++ modules/guestlist/checking.php
@@ -2,5 +2,5 @@
 $link_target_yes = "?mod=guestlist&action=checked";
 $link_target_no = "?mod=guestlist&action=guestlist";
 
-$func->question(t("Soll das *Letzter Kontocheck Datum* auf das aktuelle Datum gesetzt werden?"), $link_target_yes, $link_target_no);
+$func->question(t("Soll das Datum <b>\"Letzter Kontocheck\"</b> auf das aktuelle Datum gesetzt werden?"), $link_target_yes, $link_target_no);
 ?>
