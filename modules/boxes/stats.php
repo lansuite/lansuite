f7685aecf7ec561453c05f6cfdc16c2177509dc3
jochenjung <jochen.jung@gmail.com>
2007-01-12 15:15:09 +0000

    Statistik in neue Zeile, da sonst evtl. zu lang Es werden nun 10, statt 5 eingeloggte gezeigt

--- modules/boxes/stats.php
+++ modules/boxes/stats.php
@@ -8,8 +8,10 @@ $total = $db->query_first("SELECT SUM(visits) AS visits, SUM(hits) AS hits FROM 
 $avg = $db->query_first("SELECT SUM(visits) AS visits, SUM(hits) AS hits FROM {$config["tables"]["stats_usage"]}
   WHERE DATE_FORMAT(time, '%Y-%m-%d %H:00:00') = DATE_FORMAT(DATE_SUB(NOW(), INTERVAL 1 HOUR), '%Y-%m-%d %H:00:00')
 	");
-$box->DotRow(t('Besucher').': <span title="'. t('insgesammt') .'">'. $total['visits'] .'</span> <span title="'. t('in der letzten Stunde') .'">('. $avg['visits'] .')</span>');
-$box->DotRow(t('Aufrufe').': <span title="'. t('insgesammt') .'">'. $total['hits'] .'</span> <span title="'. t('in der letzten Stunde') .'">('. $avg['hits'] .')</span>');
+$box->DotRow(t('Besucher').':');
+$box->EngangedRow('<span title="'. t('insgesammt') .'">'. $total['visits'] .'</span> <span title="'. t('in der letzten Stunde') .'">('. $avg['visits'] .')</span>');
+$box->DotRow(t('Aufrufe').':');
+$box->EngangedRow('<span title="'. t('insgesammt') .'">'. $total['hits'] .'</span> <span title="'. t('in der letzten Stunde') .'">('. $avg['hits'] .')</span>');
 
 // Get list of users currently online
 $user_online = $db->query("SELECT SQL_CALC_FOUND_ROWS user.username, user.userid
@@ -18,7 +20,7 @@ $user_online = $db->query("SELECT SQL_CALC_FOUND_ROWS user.username, user.userid
 	WHERE (auth.lasthit > ". (time() - 60 * 10) .") AND auth.login = '1' AND user.userid > 0
 	GROUP BY user.userid
 	ORDER BY auth.lasthit
-	LIMIT 5
+	LIMIT 10
 	");
 $online = $db->query_first('SELECT FOUND_ROWS() AS count');
 $box->DotRow(t('Eingeloggt') .': '. $online['count']);
