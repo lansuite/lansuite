5b1c4335b4a5b264e9e7980ad23128b245720947
jochenjung <jochen.jung@gmail.com>
2007-01-12 07:40:11 +0000

    Configeintr�ge �berarbeitet. Alte entfernt. Sprachauswahl ja/nein erg�nzt

--- modules/boxes/menu.php
+++ modules/boxes/menu.php
@@ -111,14 +111,15 @@ $db->free_result($res);
 $templ['box']['rows'] .= $menu_out;
 
 // Add Language select
-$cur_url = parse_url($_SERVER['REQUEST_URI']);
-$res = $db->query("SELECT cfg_value, cfg_display FROM {$config["tables"]["config_selections"]} WHERE cfg_key = 'language'");
-while ($row = $db->fetch_array($res)) {
-  if ($cur_url['query'] == '') $templ['box']['rows'] .= $dsp->FetchIcon($_SERVER['REQUEST_URI'] .'?language='. $row['cfg_value'], $row['cfg_value'], $row['cfg_display']).' ';
-  else $templ['box']['rows'] .= $dsp->FetchIcon($_SERVER['REQUEST_URI'] .'&language='. $row['cfg_value'], $row['cfg_value'], $row['cfg_display']).' ';
+if ($cfg['sys_show_langselect']) {
+  $cur_url = parse_url($_SERVER['REQUEST_URI']);
+  $res = $db->query("SELECT cfg_value, cfg_display FROM {$config["tables"]["config_selections"]} WHERE cfg_key = 'language'");
+  while ($row = $db->fetch_array($res)) {
+    if ($cur_url['query'] == '') $templ['box']['rows'] .= $dsp->FetchIcon($_SERVER['REQUEST_URI'] .'?language='. $row['cfg_value'], $row['cfg_value'], $row['cfg_display']).' ';
+    else $templ['box']['rows'] .= $dsp->FetchIcon($_SERVER['REQUEST_URI'] .'&language='. $row['cfg_value'], $row['cfg_value'], $row['cfg_display']).' ';
+  }
+  $db->free_result($res);
 }
-$db->free_result($res);
-
 
 // Callbacks
 $MenuCallbacks = Array('ShowSignon', 'ShowGuestMap');
