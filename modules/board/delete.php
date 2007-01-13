169d9c7159d0ad8e4cbb3a49838b0fcf5fe6d07f
jochenjung <jochen.jung@gmail.com>
2007-01-13 20:31:12 +0000

    - Change: Footer-Zeile neu gestaltet (KnoX 13.01.2007) - Change: Ajax-Loading wieder raus genommen (KnoX 13.01.2007) - Weitere kleine �nderungen - Change: Avatar-Pfad in der DB wird nun komplett angegeben, daher m�ssen Avatare neu hinzugef�gt werden (KnoX 13.01.2007) - MasterDelete fertig gestellt - Einlassassistent �berarbeitet - Add: Mails k�nnen nun aus dem Papierkorb gel�scht werden (KnoX 13.01.2007) - - Bugfix: Im SE-Modus wurde in Turnieren teilweise eine etwas kleinere Startzeit angezeigt (KnoX 13.01.2007)

--- modules/board/delete.php
+++ modules/board/delete.php
@@ -1,73 +1,39 @@
 <?php
-/*
+
 include_once('inc/classes/class_masterdelete.php');
 $md = new masterdelete();
 
-		$res = $db->query("SELECT tid FROM {$config["tables"]["board_threads"]} WHERE fid='{$_GET['fid']}'");
-		while ($row = $db->fetch_array($res)) {
-			$db->query("DELETE FROM {$config["tables"]["board_posts"]} WHERE tid='{$row['tid']}'");
-		}
-		
-$md->References['board_threads'] = '';
-#$md->References['board_posts'] = 'tid';
+// Delete post
+if ($_GET['pid']) {
+  $md->DeleteIfEmpty['board_threads'] = 'tid';
+  $md->Delete('board_posts', 'pid', $_GET['pid']);
 
-switch($_GET['step']) {
-	default:
-    include_once('modules/board/show.php');
-	break;
+// Delete thread
+} elseif ($_GET['tid']) {
+  $md->References['board_posts'] = '';
+  $md->References['board_read_state'] = '';
+  $md->References['board_bookmark'] = '';
+  $md->Delete('board_threads', 'tid', $_GET['tid']);
 
-	case 2:
-    $md->Delete('board_forums', 'fid', $_GET['fid']);
-  break;
+// Delete board
+} else {
+  $md->References['board_threads'] = '';
+  $md->SubReferences['board_posts'] = 'tid';
+  $md->SubReferences['board_read_state'] = 'tid';
+  $md->SubReferences['board_bookmark'] = 'tid';
 
-  case 10:
-    $md->MultiDelete('board_forums', 'fid');
-  break;
-}
-*/
+  switch($_GET['step']) {
+  	default:
+      include_once('modules/board/show.php');
+  	break;
 
-$LSCurFile = __FILE__;
+  	case 2:
+      $md->Delete('board_forums', 'fid', $_GET['fid']);
+    break;
 
-switch ($_GET['step']) {
-	default:
-    include_once('modules/board/show.php');
-	break;
-
-	case 2:
-		$text = t('Wollen Sie dieses Forum wirklich löschen?');
-
-		$link_target_yes = "index.php?mod=board&action=delete&step=3&fid={$_GET['fid']}";
-		$link_target_no = "index.php?mod=board&action=delete";
-		$func->question($text, $link_target_yes, $link_target_no);
-	break;
-
-	case 3:
-		$res = $db->query("SELECT tid FROM {$config["tables"]["board_threads"]} WHERE fid='{$_GET['fid']}'");
-		while ($row = $db->fetch_array($res)) {
-			$db->query("DELETE FROM {$config["tables"]["board_posts"]} WHERE tid='{$row['tid']}'");
-		}
-		$db->free_result($res);
-		$db->query("DELETE FROM {$config["tables"]["board_threads"]} WHERE fid='{$_GET['fid']}'");
-		$db->query("DELETE FROM {$config["tables"]["board_forums"]} WHERE fid='{$_GET['fid']}'");
-
-		$func->confirmation(t('Das Forum wurde erfolgreich gelöscht'), "index.php?mod=board&action=delete");
-	break;
-
-  // Post delete
-	case 10:
-    include_once('inc/classes/class_masterdelete.php');
-    $md = new masterdelete();
-    $md->Delete('board_posts', 'pid', $_GET['pid']);
-
-    // TODO: Check if last post in thread -> Delete thread!
-	break;
-
-  // Thread delete
-	case 11:
-    include_once('inc/classes/class_masterdelete.php');
-    $md = new masterdelete();
-    $md->References['board_posts'] = '';
-    $md->Delete('board_threads', 'tid', $_GET['tid']);
-	break;
+    case 10:
+      $md->MultiDelete('board_forums', 'fid');
+    break;
+  }
 }
 ?>
