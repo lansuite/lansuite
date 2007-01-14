4e030aa2b37e30b03eccc599c3112bc3d933b613
jochenjung <jochen.jung@gmail.com>
2007-01-14 12:34:45 +0000

    http://lansuite.orgapage.de/index.php?mod=bugtracker&bugid=44

--- modules/guestlist/checked.php
+++ modules/guestlist/checked.php
@@ -7,7 +7,7 @@ $minute = date("i" );
 $unixtimes = $func->date2unixstamp($year, $month, $day, $hour, $minute, 0);
 $link_target = "?mod=guestlist&action=guestlist";
 
-$db->query("UPDATE lansuite_partys SET checked = $unixtimes" );
+$db->query("UPDATE {$config["tables"]["partys"]} SET checked = $unixtimes WHERE party_id = ".(int)$party->party_id);
 $func->confirmation(t("Datum und Uhrzeit wurden erfolgreich eingetragen!"), $link_target) ;
 $dsp->AddContent();
 ?>
