bc491cfe547ba516ee1457257260b4b4337657c0
magic85 <magic85@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2007-01-22 16:00:02 +0000

    no message

---
+++ modules/mail/formular.php
@@ -0,0 +1,54 @@
+<?php
+
+if ($auth['login']) $senderemail = $auth['email'];
+	else $senderemail = $_POST["adress"];
+
+$receiver = "magic@lanrena.de";
+$headers .= 'From:' . $senderemail;
+
+switch($_GET["step"]) {
+	case 2:
+		if ($_POST["subject"] == ""){
+			$subject_error = $lang["mail"]["newsletter_err_nosubject"];
+			$_GET["step"] = 1;
+		}
+		if ($_POST["text"] == ""){
+			$text_error = $lang["mail"]["newsletter_err_notext"];
+			$_GET["step"] = 1;
+		}
+			if ($_POST["adress"] == ""){
+			$adress_error = $lang["mail"]["formular_err_noadress"];
+			$_GET["step"] = 1;
+		}
+		break;
+}
+
+switch($_GET["step"]) {
+	default:
+		$dsp->NewContent($lang["mail"]["formular_caption"], $lang["mail"]["formular_subcaption"]);
+		$dsp->SetForm("index.php?mod=mail&action=formular&step=2");
+
+		$dsp->AddHRuleRow();
+
+		$dsp->AddTextFieldRow("adress", $lang["mail"]["formular_adress"], $senderemail, $adress_error);
+		$dsp->AddTextFieldRow("subject", $lang["mail"]["newsletter_subject"], $_POST["subject"], $subject_error);
+		$dsp->AddTextAreaRow("text", $lang["mail"]["newsletter_text"], $_POST["text"], $text_error);
+
+		$dsp->AddFormSubmitRow("send");
+		$dsp->AddContent();
+	break;
+
+	case 2:
+
+		$success = "";
+		$fail = "";
+		$text = $_POST["text"];
+		
+			// Mail senden			
+		mail($receiver, $_POST['subject'], $text, $headers);
+
+		$func->confirmation($inet_success, "index.php?mod=mail&action=formular&step=1");
+	break;
+}
+
+?>
