0c5bc739f2297a0f7d4ae67128b4ee7360769c53
jochenjung <jochenjung@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2007-01-23 18:06:54 +0000

    Mailmodul �berholt

--- trunk/modules/mail/newmail.php
+++ trunk/modules/mail/newmail.php
@@ -1,86 +1,57 @@
 <?php
 
-$user_id = $_GET["userID"];
+$dsp->NewContent(t('Neue Mail verfassen'), '');
+$dsp->AddContent();
 
-// Error-Step
-switch($_GET["step"]) {
-	case 3:
-			
-		$body_i = strlen($_POST["msgbody_text"]);
-		$sub_i =  strlen($_POST["subject_text"]);
+function SendOnlineMail() {
+  global $db, $config, $mail;
 
-		if($body_i > 5000) {
-			$msgbody_error = $lang["mail"]["new_err_max5000"];
-			$_GET["step"] = 2;
-		}
+  // System-Mail: Insert will be done, by MF
+  if ($_POST['fromUserID'] and $_POST['type'] == 0) return true;
 
-		if($body_i == '') {
-		 $msgbody_error = $lang["mail"]["new_err_min1"];
-		 $_GET["step"] = 2;
-		}
+  // Inet-Mail
+  else {
+    $row = $db->query_first("SELECT name, firstname, email FROM {$config['tables']['user']} WHERE userid = ". (int)$_POST['toUserID']);
+    if ($_POST['fromUserID']) {
+      $row2 = $db->query_first("SELECT email FROM {$config['tables']['user']} WHERE userid = ". (int)$_POST['fromUserID']);
+      $_POST['SenderMail'] = $row2['email'];
+    }
 
-		if($sub_i == '') {
-			$subject_error = $lang["mail"]["new_err_subject"];
-			$_GET["step"] = 2;
-		}
-		if($sub_i > 100) {
-			$subject_error = $lang["mail"]["new_err_maxsubj"];
-			$_GET["step"] = 2;
-		}
-	break;
+    $mail->create_inet_mail($row['firstname'].' '.$row['name'], $row['email'], $_POST['Subject'], $_POST['msgbody'], $_POST['SenderMail']);
+    return false;
+  }
 }
 
+include_once('inc/classes/class_masterform.php');
+$mf = new masterform();
 
-// set pointer direkt to mastersearch selection
-if ($_GET["step"] == "") $_GET["step"]="userbyms";
+$selections = array();
+$res = $db->query("SELECT userid, username FROM {$config['tables']['user']} WHERE type > 0");
+while ($row = $db->fetch_array($res)) $selections[$row['userid']] = $row['username'];
+$db->free_result($res);
+$mf->AddField(t('EmpfÃ¤nger'), 'toUserID', IS_SELECTION, $selections, FIELD_OPTIONAL);
 
+if ($auth['userid']) {
+  $selections = array();
+  $selections[0] = t('Als System-Mail');
+  $selections[1] = t('An die Email-Adresse');
+  $mf->AddField(t('Mail-Typ'), 'type', IS_SELECTION, $selections, FIELD_OPTIONAL);
+} else {
+  $mf->AddField('', 'captcha', IS_CAPTCHA);
+	$mf->AddField(t('Absender'), 'SenderMail');
+}
 
-switch($_GET["step"]) {
-	default:
-		$questarr[0] = $lang["mail"]["new_select1"];
-		$questarr[1] = $lang["mail"]["new_select2"];
-		$questarr[2] = $lang["mail"]["new_select3"];
+$mf->AddField(t('Betreff'), 'Subject');
+$mf->AddField(t('Nachricht'), 'msgbody');
 
-		$linkarr[0]	 = "index.php?mod=mail&action=newmail&step=userbyms";
-		$linkarr[1]	 = "index.php?mod=mail&action=newmail&step=userbygroup";
-		$linkarr[2]	 = "index.php?mod=mail&action=newmail&step=userbyclan";				
+$mf->AddFix('mail_status', 'active');
+$mf->AddFix('src_status', 'send');
+$mf->AddFix('des_status', 'new');
+$mf->AddFix('fromUserID', $auth['userid']);
+$mf->AddFix('priority', 'normal');
+$mf->AddFix('tx_date', time());
 
-		$func->multiquestion($questarr, $linkarr, $lang["mail"]["new_toselect"]);
-	break;
-
-	// Übersicht ihrer Mails im Posteingang
-	case "userbyms":
-    $additional_where = 'u.type > 0';
-    $current_url = 'index.php?mod=mail&action=newmail&step=userbyms';
-    $target_url = 'index.php?mod=mail&action=newmail&step=2&userID=';
-    include_once('modules/usrmgr/search_basic_userselect.inc.php');
-	break;
-
-	case 2:
-		if ($user_id=='') $func->error($lang["mail"]["new_no_userid"], "index.php?mod=mail");
-
-		$get_username = $db->query_first("SELECT username FROM {$config["tables"]["user"]} WHERE userID = '$user_id'");
-
-		if($vars["replyto"]) {
-			$mail->get_mail($vars["replyto"]);
-			$_POST["subject_text"] = "Re: ".$mail->mail_subject["text"];
-		}
-
-        $dsp->NewContent($lang["mail"]["new_caption"], str_replace("%USERNAME%", $get_username["username"], $lang["mail"]["new_subcaption"]));
-        $dsp->SetForm("index.php?mod=mail&action=newmail&step=3&userID=$user_id&to_userid=$user_id&replyto=".$_GET["replyto"], "newmail");
-        $dsp->AddTextFieldRow("subject_text", $lang["mail"]["new_subject"], $_POST["subject_text"], $subject_error, 72);
-        $dsp->AddTextAreaPlusRow("msgbody_text", $lang["mail"]["new_text"], $_POST["msgbody_text"], $msgbody_error, 70, 20);
-        $dsp->AddFormSubmitRow("next");
-        $dsp->AddContent();
-	break;
-
-	// set staus "reply"
-	case 3:
-		if($mail->create_mail($auth["userid"], $_GET["to_userid"], $_POST["subject_text"], $_POST["msgbody_text"])) {
-			if ($_GET["replyto"]) $mail->set_status_reply($_GET["replyto"]);
-			$func->confirmation($lang["mail"]["new_success"], "index.php?mod=mail");
-		} 
-		else $func->error($lang["mail"]["new_send_error"] ." ". $mail->error, "index.php?mod=mail");
-	break;
+$mf->CheckBeforeInserFunction = 'SendOnlineMail';
+if ($mf->SendForm('index.php?mod=mail&action=newmail', 'mail_messages', 'mailID', '')) {
 }
 ?>
