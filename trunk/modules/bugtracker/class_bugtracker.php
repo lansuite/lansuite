c8199d2fc0381f197bdbd77c5e80ff68be75fccb
v0rt3x <v0rt3x@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2007-01-30 14:48:24 +0000

    Rechtschreibung korrigiert, Fettschrift - v0rt3x (30.01.2007)

--- trunk/modules/bugtracker/class_bugtracker.php
+++ trunk/modules/bugtracker/class_bugtracker.php
@@ -20,10 +20,10 @@ class Bugtracker {
     if ($auth['type'] <= 1) {
       $row = $db->query_first("SELECT caption, state FROM {$config['tables']['bugtracker']} WHERE bugid = ". (int)$bugid);
       if ($state > 2) {
-        $func->information(t('Der Status für des Eintrags "%1" konnte nicht geändert werden, da sie nur auf die Stati "Neu" und "Bestätigt wechseln dürfen"', array($row['caption'])));
+        $func->information(t('Der Status des Bugreports <b>"%1"</b> konnte nicht geändert werden, da Sie nur auf die Stati <b>"Neu"</b> und <b>"Bestätigt"</b> wechseln dürfen', array($row['caption'])));
         return;
       } elseif ($row['state'] == 6) {
-        $func->information(t('Der Status für des Eintrags "%1" konnte nicht geändert werden, da er bereits auf geschlossen steht"', array($row['caption'])));
+        $func->information(t('Der Status des Bugreports <b>"%1"</b> konnte nicht geändert werden, da er bereits auf <b>"Geschlossen"</b> steht', array($row['caption'])));
         return;
       }
     }
@@ -31,10 +31,10 @@ class Bugtracker {
     $row = $db->query_first("SELECT 1 AS found FROM {$config['tables']['bugtracker']} WHERE state = ". (int)$state ." AND bugid = ". (int)$bugid);
     if (!$row['found']) {
       $db->query("UPDATE {$config['tables']['bugtracker']} SET state = ". (int)$state .' WHERE bugid = '. (int)$bugid);
-      $func->log_event(t('Problem auf Status "%1" geändert', array($this->stati[$state])), 1, '', $bugid);
+      $func->log_event(t('Bugreport auf Status <b>"%1"</b> geändert', array($this->stati[$state])), 1, '', $bugid);
       if ($state == 3) {
         $row = $db->query_first("SELECT reporter, caption FROM {$config['tables']['bugtracker']} WHERE bugid = ". (int)$bugid);
-        $mail->create_sys_mail($row['reporter'], t('Feedback zu deinem Bugreport benötigt'), t('Der Status deines Bugreports %1 wurd auf Feedback benötigt gesetzt. Bitte schau dir den Report noch einmal an und helfe deine Angaben zu vervollständigen', array($row['caption'])));
+        $mail->create_sys_mail($row['reporter'], t('Feedback zu Ihrem Bugreport benötigt'), t('Der Status Ihres Bugreports <b>"%1"</b> wurde auf <b>"Feedback benötigt"</b> gesetzt. Bitte schauen Sie sich den Bugreport noch einmal an und helfen Sie, Ihre Angaben zu vervollständigen', array($row['caption'])));
         $func->log_event(t('Benachrichtigungsmail an Reporter versandt'), 1, '', $bugid);
       }
     }
@@ -50,7 +50,7 @@ class Bugtracker {
       if ($userid == 0) $func->log_event(t('Benutzerzuordnung gelöscht'), 1, '', $bugid);
       else {
         $row = $db->query_first("SELECT username FROM {$config['tables']['user']} WHERE userid = ". (int)$userid);
-        $func->log_event(t('Problem Benutzer "%1" zugeordnet', array($row['username'])), 1, '', $bugid);
+        $func->log_event(t('Bugreport Benutzer <b>"%1"</b> zugeordnet', array($row['username'])), 1, '', $bugid);
       }
     }
   }
