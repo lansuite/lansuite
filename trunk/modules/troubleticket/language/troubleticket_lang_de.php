fdc71e5e2c544a3d035bcf8f434d5134530f2241
v0rt3x <v0rt3x@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2007-03-01 15:15:35 +0000

    Schreibweise angepasst - v0rt3x (01.03.2007)

--- trunk/modules/troubleticket/language/troubleticket_lang_de.php
+++ trunk/modules/troubleticket/language/troubleticket_lang_de.php
@@ -8,35 +8,35 @@
  $lang['troubleticket']['modulname']     = 'troubleticket';     //necessary
 
  //Contend
- $lang['troubleticket']['headline']       = 'Troubleticket hinzuf&uuml;gen';
+ $lang['troubleticket']['headline']       = 'Troubleticket hinzufügen';
  $lang['troubleticket']['headl_edit']     = 'Troubleticket bearbeiten';
  $lang['troubleticket']['show']           = 'Troubleticket anzeigen';
  $lang['troubleticket']['description']    = 'Beschreibung';
- $lang['troubleticket']['priority']       = 'Priorit&auml;t';
- $lang['troubleticket']['head']           = '&Uuml;berschrift';
+ $lang['troubleticket']['priority']       = 'Priorität';
+ $lang['troubleticket']['head']           = 'Überschrift';
  $lang['troubleticket']['prob_descr']     = 'Problembeschreibung';
  $lang['troubleticket']['set_up_on']      = 'Eingetragen am/um';
  $lang['troubleticket']['from_user']      = 'Von Benutzer';
  $lang['troubleticket']['assign_orga']    = 'Bearbeitender Orga';
- $lang['troubleticket']['visible_4all']   = 'Sichtbar f&uuml;r Alle';
- $lang['troubleticket']['visible_4orga']  = 'Sichtbar nur f&uuml;r Orgas';
+ $lang['troubleticket']['visible_4all']   = 'Sichtbar für Alle';
+ $lang['troubleticket']['visible_4orga']  = 'Sichtbar nur für Orgas';
  $lang['troubleticket']['no_contend']     = 'Keine weiteren Informationen angegeben.';
  $lang['troubleticket']['add_confirm']    = 'Das Troubleticket wurde erfolgreich eingetragen';
- $lang['troubleticket']['change_confirm'] = 'Das Troubleticket wurde erfolgreich ge&auml;ndert';
- $lang['troubleticket']['unlink_confirm'] = 'Das ausgew&auml;hlte Ticket wurde gel&ouml;scht.';
+ $lang['troubleticket']['change_confirm'] = 'Das Troubleticket wurde erfolgreich geändert';
+ $lang['troubleticket']['unlink_confirm'] = 'Das ausgewählte Ticket wurde gelöscht.';
  $lang['troubleticket']['user_assign']    = 'Ihnen wurde das Troubleticket "<b>%TTCaption%</b>"zugewiesen. '; //endign with dot, followed by a space
- $lang['troubleticket']['assign_confirm'] = 'Das ausgew&auml;hlte Ticket wurde dem Orga zugewiesen.';
+ $lang['troubleticket']['assign_confirm'] = 'Das ausgewählte Ticket wurde dem Orga zugewiesen.';
  $lang['troubleticket']['show_info']      = 'Hier sehen Sie alle Informationen zu diesem Ticket';
  $lang['troubleticket']['no_hint']        = ' Kein Hinweis eingetragen';	//starts with a space
- $lang['troubleticket']['subline']        = ' Mit diesem Formular k&ouml;nnen Sie ein Troubleticket hinzuf&uuml;gen, falls Sie ein Problem haben'; //starts with a space
+ $lang['troubleticket']['subline']        = ' Mit diesem Formular können Sie ein Troubleticket hinzufügen, falls Sie ein Problem haben'; //starts with a space
 
  //Status
  $lang['troubleticket']['status']         = 'Ticketstatus';
- $lang['troubleticket']['status_choose']  = 'Status ausw&auml;hlen';
- $lang['troubleticket']['st_default']     = 'default: scriptfehler!';
- $lang['troubleticket']['st_new']         = 'Neu / Ungepr&uuml;ft';
- $lang['troubleticket']['st_acc']         = '&Uuml;berpr&uuml;ft / Akzeptiert';
- $lang['troubleticket']['st_checked']     = '&Uuml;berpr&uuml;ft am/um';
+ $lang['troubleticket']['status_choose']  = 'Status auswählen';
+ $lang['troubleticket']['st_default']     = 'default: Scriptfehler!';
+ $lang['troubleticket']['st_new']         = 'Neu / Ungeprüft';
+ $lang['troubleticket']['st_acc']         = 'Überprüft / Akzeptiert';
+ $lang['troubleticket']['st_checked']     = 'Überprüft am/um';
  $lang['troubleticket']['st_in_work']       = 'In Arbeit';
  $lang['troubleticket']['st_in_work_since'] = 'In Bearbeitung seit';
  $lang['troubleticket']['st_finished']      = 'Abgeschlossen';
@@ -46,9 +46,9 @@
 
  //Comment
  $lang['troubleticket']['com']			   = 'Kommentar';
- $lang['troubleticket']['com_4user']        = 'Kommentar f&uuml;r Benutzer';
- $lang['troubleticket']['com_4orga']        = 'Kommentar f&uuml;r Orgas';
- $lang['troubleticket']['com_fa4orga']      = 'Kommentar von und f&uuml;r Orgas';
+ $lang['troubleticket']['com_4user']        = 'Kommentar für Benutzer';
+ $lang['troubleticket']['com_4orga']        = 'Kommentar für Orgas';
+ $lang['troubleticket']['com_fa4orga']      = 'Kommentar von und für Orgas';
 
  //Search
  $lang['troubleticket']['ms_search_ticket'] = 'Ticket suchen';
@@ -57,9 +57,9 @@
  $lang['troubleticket']['ms_user_result']   = 'Benutzer: Ergebnis';
 
  //Change
- $lang['troubleticket']['option_nochange']  = ' Keine &Auml;nderung ';   // all options starts and ends with a space
- $lang['troubleticket']['option_back2poll'] = ' Problem nicht &uuml;bernehmen und zur&uuml;ckgeben ';
- $lang['troubleticket']['option_startwork'] = ' Problem &uuml;bernehmen und Bearbeitung beginnen ';
+ $lang['troubleticket']['option_nochange']  = ' Keine Änderung ';   // all options starts and ends with a space
+ $lang['troubleticket']['option_back2poll'] = ' Problem nicht übernehmen und zurückgeben ';
+ $lang['troubleticket']['option_startwork'] = ' Problem übernehmen und Bearbeitung beginnen ';
  $lang['troubleticket']['option_reopen']	   = ' Problem nochmal aufgreifen und bearbeiten ';  // this option is currently not in use
  $lang['troubleticket']['option_finished']  = ' Auf Erledigt setzen ';
  $lang['troubleticket']['option_refuse']    = ' Bearbeitung ablehnen ';
@@ -83,15 +83,15 @@
  $lang['troubleticket']['cat_no_err']		= 'Sie haben keine Kategorie zum ändern ausgewählt';
  
  //Questions
- $lang['troubleticket']['q_unlink'] = 'Wollen Sie das ausgew&auml;hlte Troubleticket wirklich l&ouml;schen?';
+ $lang['troubleticket']['q_unlink'] = 'Wollen Sie das ausgewählte Troubleticket wirklich löschen?';
 
  //Errors
  $lang['troubleticket']['err_max_size']  = 'Der Text darf nicht mehr als 5000 Zeichen enthalten';
- $lang['troubleticket']['err_no_head']   = 'Bitte geben Sie eine kurze Beschreibung / &Uuml;berschrift ein';
+ $lang['troubleticket']['err_no_head']   = 'Bitte geben Sie eine kurze Beschreibung / Überschrift ein';
  $lang['troubleticket']['err_assign']    = 'Das Troubleticket konnte nicht zugewiesen werden! Problem mit der Datenbank !';
  $lang['troubleticket']['err_no_reason'] = 'Bei einer direkten Ablehnung ist die Angabe eines Grundes notwendig.';
- $lang['troubleticket']['err_no_tt_id']  = 'Es wurde keine Troubleticket-ID &uuml;bergeben. Aufruf inkorrekt.';
- $lang['troubleticket']['err_unlink']    = 'Das Troubleticket konnte nicht gel&ouml;scht werden! Problem mit der Datenbank!';
- $lang['troubleticket']['err_no_cat']    = 'Bitte wählen sie eine Kategorie';
+ $lang['troubleticket']['err_no_tt_id']  = 'Es wurde keine Troubleticket-ID übergeben. Aufruf inkorrekt.';
+ $lang['troubleticket']['err_unlink']    = 'Das Troubleticket konnte nicht gelöscht werden! Problem mit der Datenbank!';
+ $lang['troubleticket']['err_no_cat']    = 'Bitte wählen Sie eine Kategorie';
  
 ?>
