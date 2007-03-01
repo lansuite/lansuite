b531a472d29eff772efd16fcd94bfc9b054bb013
v0rt3x <v0rt3x@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2007-03-01 15:21:24 +0000

    Schreibweise angepasst - v0rt3x (01.03.2007)

--- trunk/modules/party/language/party_lang_de.php
+++ trunk/modules/party/language/party_lang_de.php
@@ -8,31 +8,31 @@ $lang['signon']['translator'] = 'Jochen';
 
 // Partyverwaltung
 $lang['signon']['party_list_caption'] =		'Partyliste';
-$lang['signon']['party_list_subcaption'] =  'Hier sehen sie alle Partys';
-$lang['signon']['is_signon'] =				'Zu dieser Party bist du angemeldet';
-$lang['signon']['signon_start'] =			'Die Anmeldung &ouml;ffnet am:';
+$lang['signon']['party_list_subcaption'] =  'Hier sehen Sie alle Partys';
+$lang['signon']['is_signon'] =				'Zu dieser Party sind Sie angemeldet';
+$lang['signon']['signon_start'] =			'Die Anmeldung öffnet am:';
 $lang['signon']['signon_end'] =				'Die Anmeldung endete am:';
-$lang['signon']['signon_true'] =			'Zu dieser Party warst du angemeldet';
-$lang['signon']['signon_false'] =			'Zu dieser Party warst du nicht angemeldet';
+$lang['signon']['signon_true'] =			'Zu dieser Party waren Sie angemeldet';
+$lang['signon']['signon_false'] =			'Zu dieser Party waren Sie nicht angemeldet';
 $lang['signon']['signon_history'] =			'Party-History';
 
 
 // Error
 $lang['signon']['error_name'] = 			'Bitte einen Partynamen angeben';
 $lang['signon']['error_ort'] = 				'Bitte einen Ort angeben';
-$lang['signon']['error_max_guest'] = 		'Bitte Anzahl der Pl&auml;tze angeben';
+$lang['signon']['error_max_guest'] = 		'Bitte Anzahl der Plätze angeben';
 $lang['signon']['no_partys'] =				'Keine Party gefunden';
-$lang['signon']['entry_error'] =			'Konnte nich erstellt oder ge&auml;ndert werden';
-$lang['signon']['max_guest_error'] =		'Sie m&uuml;ssen die G&auml;steanzahl angeben';
+$lang['signon']['entry_error'] =			'Konnte nich erstellt oder geändert werden';
+$lang['signon']['max_guest_error'] =		'Sie müssen die Gästeanzahl angeben';
 $lang['signon']['error_group'] =			'Bitte geben Sie einen Gruppennamen ein';
 $lang['signon']['error_price_text'] =		'Bitte geben Sie einen Text ein';
 $lang['signon']['error_price'] =			'Bitte geben Sie einen Preis ein';
 
 // Party hinzufügen
-$lang['signon']['add_party_caption'] = 		'Party hinzuf&uuml;gen';
-$lang['signon']['add_party_subcaption'] =	'Bitte gib die Daten ein um eine Party hinzuzuf&uuml;gen';
+$lang['signon']['add_party_caption'] = 		'Party hinzufügen';
+$lang['signon']['add_party_subcaption'] =	'Bitte geben Sie die Daten ein um eine Party hinzuzufügen';
 $lang['signon']['partyname'] =				'Partyname';
-$lang['signon']['max_guest'] =				'Anzahl Pl&auml;tze';
+$lang['signon']['max_guest'] =				'Anzahl Plätze';
 $lang['signon']['plz'] =					'PLZ';
 $lang['signon']['ort'] =					'Ort';
 $lang['signon']['stime'] =					'Party startet am';
@@ -40,40 +40,40 @@ $lang['signon']['etime'] =					'Party endet am';
 $lang['signon']['sstime'] =					'Anmeldung startet am';
 $lang['signon']['setime'] =					'Anmeldung endet am';
 $lang['signon']['change_party'] =			'Standartparty festlegen:';
-$lang['signon']['change_party_id_ok'] =		'Die festeingestellte Party wurde ge&auml;ndert';
-$lang['signon']['change_party_id_fail'] =	'Die festeingestellte Party konnte nicht ge&auml;ndert werden';
-$lang['signon']['edit_party_caption'] =	    'Party &auml;ndern';
-$lang['signon']['edit_party_subcaption'] =	'Bitte &auml;ndern sie hier die Partydaten';
-$lang['signon']['update_ok'] =				'Die Party wurde ge&auml;ndert';
-$lang['signon']['add_ok'] =					'Die Party wurde hinzugef&uuml;gt';
+$lang['signon']['change_party_id_ok'] =		'Die festeingestellte Party wurde geändert';
+$lang['signon']['change_party_id_fail'] =	'Die festeingestellte Party konnte nicht geändert werden';
+$lang['signon']['edit_party_caption'] =	    'Party ändern';
+$lang['signon']['edit_party_subcaption'] =	'Bitte ändern Sie hier die Partydaten';
+$lang['signon']['update_ok'] =				'Die Party wurde geändert';
+$lang['signon']['add_ok'] =					'Die Party wurde hinzugefügt';
 $lang['signon']['show_party_caption'] =		'Party anzeigen';
-$lang['signon']['show_party_subcaption'] =	'Die Details der Partys';
+$lang['signon']['show_party_subcaption'] =	'Die Details der Partys.';
 $lang['signon']['show_list_caption'] =		'Partyliste';
-$lang['signon']['show_list_subcaption'] =	'Hier sehen sie eine Liste der Partys';
+$lang['signon']['show_list_subcaption'] =	'Hier sehen Sie eine Liste der Partys';
 
 // Preisverwaltung
-$lang['signon']['price_text'] =				'Text f&uuml;r Eintrittspreis';
+$lang['signon']['price_text'] =				'Text für Eintrittspreis';
 $lang['signon']['price'] =					'Preis';
-$lang['signon']['edit_price_caption'] =		'Preise &auml;ndern und hinzuf&uuml;gen';
-$lang['signon']['edit_price_subcaption'] =	'Hier k&ouml;nnen Preise und &auml;ndern';
-$lang['signon']['add_price_ok'] =			'Preis hinzugef&uuml;gt';
-$lang['signon']['edit_price_ok'] =			'Preis ge&auml;ndert';
+$lang['signon']['edit_price_caption'] =		'Preise ändern und hinzufügen';
+$lang['signon']['edit_price_subcaption'] =	'Hier können Preise und ändern';
+$lang['signon']['add_price_ok'] =			'Preis hinzugefügt';
+$lang['signon']['edit_price_ok'] =			'Preis geändert';
 $lang['signon']['depot_desc'] =				'Beschreibung eines Depots';
-$lang['signon']['depot_price'] =			'Depotpreis (Leer lassen wenn es kein Depot gibt.)';
-$lang['signon']['delete_price'] =			'Wollen sie den Preis %PRICE% wirklich l&ouml;schen?';
+$lang['signon']['depot_price'] =			'Depotpreis (Leer lassen, wenn es kein Depot gibt.)';
+$lang['signon']['delete_price'] =			'Wollen Sie den Preis %PRICE% wirklich löschen?';
 $lang['signon']['delete_price_capt'] =		'Preis zuweisen';
-$lang['signon']['delete_price_subcapt'] =	'Welchen Preis wollen sie Benutzern mit diesem Preis zuweisen.';
-$lang['signon']['delete_price_ok'] =		'Der Preis wurde gel&ouml;scht.';
+$lang['signon']['delete_price_subcapt'] =	'Welchen Preis wollen Sie Benutzern mit diesem Preis zuweisen.';
+$lang['signon']['delete_price_ok'] =		'Der Preis wurde gelöscht.';
 $lang['signon']['price_group_0'] =			'Der Preis wird keiner Gruppe zugewiesen da ein allgemeiner Preis existieren muss.';
 
 
 // Gruppenverwaltung
 $lang['signon']['edit_user_group_caption'] = 'Gruppenverwaltung';
-$lang['signon']['edit_user_group_subcaption'] = 'Erstellen sie Benutzergruppen um unterschiedliche Preise zu verlangen.';
+$lang['signon']['edit_user_group_subcaption'] = 'Erstellen Sie Benutzergruppen um unterschiedliche Preise zu verlangen.';
 $lang['signon']['group'] = 					'Gruppenname';
 $lang['signon']['group_desc'] =				'Gruppenbeschreibung';
-$lang['signon']['add_group_ok'] =			'Gruppe hinzugef&uuml;gt';
-$lang['signon']['edit_group_ok'] = 			'Gruppe ge&auml;ndert';
+$lang['signon']['add_group_ok'] =			'Gruppe hinzugefügt';
+$lang['signon']['edit_group_ok'] = 			'Gruppe geändert';
 
 
 ?>
