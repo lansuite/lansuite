656a2eb2b8f12a024b79a7dcf116991a4abe2f1a
v0rt3x <v0rt3x@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2007-03-01 14:58:57 +0000

    Schreibweise angepasst - v0rt3x (01.03.2007)

--- trunk/modules/seating/language/seating_lang_de.php
+++ trunk/modules/seating/language/seating_lang_de.php
@@ -16,7 +16,7 @@ $lang['seating']['blockname']     = 'Blockname';
 $lang['seating']['blockname']     = 'Blockname';
 $lang['seating']['blockname']     = 'Blockname';
 $lang['seating']['create_block']  = 'Sitzblock erstellen';
-$lang['seating']['cr_block_sub']  = ' Mit Hilfe des folgenden Formulars k&ouml;nnen Sie einen neuen Sitzblock erstellen. In einem folgenden zweiten Schritt k&ouml;nnen Sie dann Pl&auml;tze des Sitzblockes aktivieren bzw. deaktivieren um den Sitzblock Ihren Bed&uuml;rfnissen anzupassen..';
+$lang['seating']['cr_block_sub']  = ' Mit Hilfe des folgenden Formulars können Sie einen neuen Sitzblock erstellen. In einem folgenden zweiten Schritt können Sie dann Plätze des Sitzblockes aktivieren bzw. deaktivieren um den Sitzblock Ihren Bedürfnissen anzupassen.';
 $lang['seating']['reserve_seat']  = 'Diesen Sitzplatz für mich reservieren';
 $lang['seating']['mark_seat']     = 'Diesen Sitzplatz für einen Freund vormerken'. HTML_NEWLINE .'(Eine Vormekung kann von jedem überschrieben werden. Erst nach dem Bezahlen ist eine feste Reservierung möglich)';
 $lang['seating']['mark_my_seat']  = 'Diesen Sitzplatz für mich vormerken'. HTML_NEWLINE .'(Eine Vormekung kann von jedem überschrieben werden. Erst nach dem Bezahlen ist eine feste Reservierung möglich)';
@@ -29,12 +29,12 @@ $lang['seating']['u18_block']     = 'U18 Block';
 $lang['seating']['remark']        = 'Bemerkung';
 $lang['seating']['block_caption'] = 'Sitzblockbeschriftung';
 $lang['seating']['block_name']    = 'Sitzblockname';
-$lang['seating']['lenght_hori']   = 'L&auml;nge horizontal';
-$lang['seating']['lenght_vert']   = 'L&auml;nge vertikal';
-$lang['seating']['block_spacing'] = 'Sitzblock Zwischeng&auml;nge definieren';
-$lang['seating']['spacing_sub']   = ' Abst&auml;nde zwischen einzelnen Zeilen bzw. Reihen k&ouml;nnen mit den au&szlig;en angezeigten Pfeilen eingef&uuml;gt bzw. wieder gel&ouml;scht werden.';
+$lang['seating']['lenght_hori']   = 'Länge horizontal';
+$lang['seating']['lenght_vert']   = 'Länge vertikal';
+$lang['seating']['block_spacing'] = 'Sitzblock Zwischengänge definieren';
+$lang['seating']['spacing_sub']   = ' Abstände zwischen einzelnen Zeilen bzw. Reihen können mit den außen angezeigten Pfeilen eingefügt bzw. wieder gelöscht werden.';
 $lang['seating']['define_seat']   = 'Sitzblock Sitze definieren';
-$lang['seating']['def_seat_sub']  = 'Nun k&ouml;nnen Sie Pl&auml;tze des Sitzblockes aktivieren bzw. deaktivieren um den Sitzblock Ihren Bed&uuml;rfnissen anzupassen.<br /><br />Ganze Reihen bzw. Spalten von Pl&auml;tzen k&ouml;nnen aktiviert bzw. deaktiviert werden, indem Sie auf die Spalten- bzw. Reihen-Beschriftung  klicken.';
+$lang['seating']['def_seat_sub']  = 'Nun können Sie Plätze des Sitzblockes aktivieren bzw. deaktivieren um den Sitzblock Ihren Bedürfnissen anzupassen.<br /><br />Ganze Reihen bzw. Spalten von Plätzen können aktiviert bzw. deaktiviert werden, indem Sie auf die Spalten- bzw. Reihen-Beschriftung  klicken.';
 $lang['seating']['release_seat']  = 'Meinen Sitzplatz wieder freigeben';
 $lang['seating']['seat_info']     = 'Sitzplatz - Informationen';
 $lang['seating']['seat_info_sub'] = 'Fahren Sie mit der Maus über einen Sitzplatz, um weitere Informationen zu erhalten.';
@@ -44,28 +44,28 @@ $lang['seating']['clan']          = 'Clan';
 $lang['seating']['ip']            = 'IP';
 
 // Questions
-$lang['seating']['q_del_block']   = 'Wollen Sie diesen Sitzblock wirklich l&ouml;schen?';
-$lang['seating']['q_rel_seat']    = 'Sind Sie sicher, dass sie diesen Sitzplatz wieder freigeben m&ouml;chten?';
-$lang['seating']['q_res_however'] = 'Dennoch reservieren. %USERNAME% hat dadurch anschlie&szlig;end keinen Sitzplatz mehr';
-$lang['seating']['q_res_howev_2'] = 'Dennoch reservieren und %USERNAME% anschlie&szlig;end einen neuen Sitzplatz aussuchen';
+$lang['seating']['q_del_block']   = 'Wollen Sie diesen Sitzblock wirklich löschen?';
+$lang['seating']['q_rel_seat']    = 'Sind Sie sicher, dass Sie diesen Sitzplatz wieder freigeben möchten?';
+$lang['seating']['q_res_however'] = 'Dennoch reservieren. %USERNAME% hat dadurch anschließend keinen Sitzplatz mehr';
+$lang['seating']['q_res_howev_2'] = 'Dennoch reservieren und %USERNAME% anschließend einen neuen Sitzplatz aussuchen';
 $lang['seating']['q_cancel']      = 'Aktion abbrechen. Zurück zum Sitzplan';
 $lang['seating']['q_reserved_by'] = 'Dieser Sitzplatz ist aktuell belegt durch %USERNAME% (%FIRSTNAME% %NAME%)';
-$lang['seating']['q_change_seat'] = 'Du hast bereits einen Sitzplatz reserviert. M&ouml;chtest deinen Sitzplatz wieder frei geben und statt dessen diesen Platz reservieren?';
+$lang['seating']['q_change_seat'] = 'Sie haben bereits einen Sitzplatz reserviert. Möchten Sie Ihren Sitzplatz wieder frei geben und statt dessen diesen Platz reservieren?';
 $lang['seating']['q_reserve_mark']= 'Dieser Sitzplatz ist aktuell noch frei (bzw. nur markiert)'. HTML_NEWLINE .'Soll er fest reserviert oder nur markiert werden?';
 $lang['seating']['q_answ_reserve']= 'Sitzplatz reservieren'. HTML_NEWLINE .'(Ein evtl. zuvor für diesen Benutzer reservierter Platz wird freigegeben)';
 $lang['seating']['q_answ_mark']   = 'Sitzplatz markieren';
 
 
 // Informations
-$lang['seating']['i_choose_seat'] = 'Bitte w&auml;hlen Sie mindestens einen Sitzplatz aus!';
+$lang['seating']['i_choose_seat'] = 'Bitte wählen Sie mindestens einen Sitzplatz aus!';
 $lang['seating']['i_rel_seat']    = 'Der Sitzplatz wurde erfolgreich freigegeben';
-$lang['seating']['i_seat_locked'] = 'Sie haben bereits Ihren Sitzplatz gew&auml;hlt! Sollten Sie diesen &auml;ndern wollen, wenden sie sich bitte an die Orgas.';
-$lang['seating']['i_not_paid']    = 'Sie haben ihre Teilnahmegeb&uuml;hr noch nicht &uuml;berwiesen, oder Sie ist noch nicht bei uns eingetroffen!';
-$lang['seating']['i_not_paid2']   = 'Du musst zuerst für diese Party bezahlen, bevor du dir einen Sitzplatz reservieren darfst';
+$lang['seating']['i_seat_locked'] = 'Sie haben bereits Ihren Sitzplatz gewählt! Sollten Sie diesen ändern wollen, wenden Sie sich bitte an die Orgas.';
+$lang['seating']['i_not_paid']    = 'Sie haben ihre Teilnahmegebühr noch nicht &überwiesen oder Sie ist noch nicht bei uns eingetroffen!';
+$lang['seating']['i_not_paid2']   = 'Sie müssen zuerst für diese Party bezahlen, bevor Sie sich einen Sitzplatz reservieren dürfen.';
 $lang['seating']['i_signon_only'] = 'Nur zur Party angemeldete Benutzer dürfen Sitzplätze vormerken';
 
 // Confirmations
-$lang['seating']['c_del_block']   = 'Der Sitzblock wurde erfolgreich gel&ouml;scht';
+$lang['seating']['c_del_block']   = 'Der Sitzblock wurde erfolgreich gelöscht';
 $lang['seating']['c_plan_edit']   = 'Der Sitzplan wurde erfolgreich bearbeitet';
 $lang['seating']['c_seat_res']    = 'Der Sitzplatz wurde erfolgreich für %USERNAME% reserviert';
 $lang['seating']['c_seat_res2']   = 'Der Sitzplatz wurde erfolgreich reserviert';
@@ -85,15 +85,15 @@ $lang['seating']['checked_in']      = 'Besetzt (Eingecheckt)';
 $lang['seating']['checked_out']      = 'Frei (Ausgecheckt)';
 
 // Errors
-$lang['seating']['e_hori_lenght'] = 'Bitte geben Sie die horizontale L&auml;nge ein';
-$lang['seating']['e_vert_lenght'] = 'Bitte geben Sie die vertikale L&auml;nge ein';
-$lang['seating']['e_gt_zero']     = 'Bitte geben Sie eine Zahl, die gr&ouml;&szlig;er als 0 ist ein';
+$lang['seating']['e_hori_lenght'] = 'Bitte geben Sie die horizontale Länge ein';
+$lang['seating']['e_vert_lenght'] = 'Bitte geben Sie die vertikale Länge ein';
+$lang['seating']['e_gt_zero']     = 'Bitte geben Sie eine Zahl, die größer als 0 ist ein';
 $lang['seating']['e_lt_sixty']    = 'Bitte geben Sie eine kleinere Zahl als 60 ein';
 $lang['seating']['e_lt_hundred']  = 'Bitte geben Sie eine kleinere Zahl als 100 ein';
-$lang['seating']['e_gt_seats']    = 'Bitte geben Sie eine gr&ouml;&szlig;ere Zahl ein, da sonst Sitzpl&aumltze gel&ouml;scht werden. Um Trotzdem einen kleineren Sitzblock zu erzeugen, entfernen Sie bitte die betroffenen Benutzer.';
+$lang['seating']['e_gt_seats']    = 'Bitte geben Sie eine größere Zahl ein, da sonst Sitzplätze gelöscht werden. Um Trotzdem einen kleineren Sitzblock zu erzeugen, entfernen Sie bitte die betroffenen Benutzer.';
 $lang['seating']['e_max_chars']   = 'Bitte geben Sie weniger als 1500 Zeichen ein';
-$lang['seating']['e_choose_user'] = 'Es wurde kein Benutzer ausgew&auml;hlt';
-$lang['seating']['e_choose_seat'] = 'Es wurde kein Sitzblock ausgew&auml;hlt';
+$lang['seating']['e_choose_user'] = 'Es wurde kein Benutzer ausgewählt';
+$lang['seating']['e_choose_seat'] = 'Es wurde kein Sitzblock ausgewählt';
 $lang['seating']['e_no_seat']     = 'Dieser Sitzplatz existiert nicht';
 $lang['seating']['e_assigned']    = 'Dieser Sitzplatz ist bereits vergeben';
 $lang['seating']['e_max_marked']  = 'Sie haben bereits das Maximum an Sitzen reserviert';
