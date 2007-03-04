46c3cf446eec0145bc2f480767c486236aa1720f
jochenjung <jochenjung@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2007-03-04 23:25:19 +0000

    Mail-Verbesserungen

--- trunk/modules/mail/docu/de_help.php
+++ trunk/modules/mail/docu/de_help.php
@@ -8,5 +8,9 @@ $helplet['key'][1] = 'Sie haben Post!';
 $helplet['value'][1] = 'Immer dann wenn neue Nachrichten für Sie eintreffen, werden sie rechts in der Benutzerdaten-Box mit einem Brief-Icon darauf hingewiesen. Durch Anklicken dieses Icons gelangen Sie zu Ihrer neuen Nachricht';
 $helplet['key'][2] = 'Rundmail';
 $helplet['value'][2] = 'Über den gleichnamigen Link können Organisatoren eine Rundmail an alle, oder einen Teil der im System registrierten Benutzer rausschicken. Wir empfehlen die Option "Nur an Benutzer, die den Newsletter bei der Anmeldung bestellt haben" nur in dringenden Ausnahmefällen zu deaktivieren, da viele Benutzer die dieses Feld bei der Anmeldung deaktivieren keine Mails von Ihnen empfangen möchten. Über verschiedene Variablen, lässt sich der Newsletter individualisieren.';
+$helplet['key'][3] = 'Mails löschen';
+$helplet['value'][3] = 'Sie haben lediglich die Möglichkeit eine Mail aus dem Posteingang in den Papierkorb zu verschieben. Ein endgültiges Löschen einer Mail aus der Datenbank ist nicht möglich, denn das würde die Mail auch beim Sender löschen. Da dies zu Verwirrung führen kann und von diesem evtl. überhaupt nicht gewünscht ist können Mails daher nicht gelöscht werden';
+$helplet['key'][4] = 'Labels';
+$helplet['value'][4] = 'Aus dem gleichen Grund, wie beim Löschen, ist es nicht Möglich den Lese-Status wieder auf "Ungelesen" zu setzen. Statt dessen gibt es die Möglichekeit Mails im Posteingang mit 5 verschiedenfarbigen Labels zu markieren';
 
 ?>
