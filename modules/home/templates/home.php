282a251bd8b3d3a7ee1c128c84fcab645d33f105
jochenjung <jochen.jung@gmail.com>
2007-03-11 15:24:36 +0000

    Indizes gesetzt

--- modules/home/templates/home.php
+++ modules/home/templates/home.php
@@ -6,10 +6,10 @@ foreach($ModOverviews as $ModOverview) {
     if ($z != (count($ModOverviews) - 1)) echo '<li class="LineLeftHalf">';
     else echo '<li class="LineRight">';
   } else echo '<li class="LineRight">';
-  $st = round($sitetool->out_work(), 2);
+#  $st = round($sitetool->out_work(), 2);
   include('modules/home/'. $ModOverview .'.inc.php');
   echo $dsp->FetchModTpl('home', 'show_item');
-  echo round($sitetool->out_work() - $st, 2).'</li>';
+#  echo round($sitetool->out_work() - $st, 2).'</li>';
   if ($z % 2 == 1) echo '</ul>';
   $z++;
 }
