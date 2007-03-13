ff0a6e27ea978b5b57223b33333923afedf86331
v0rt3x <v0rt3x@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2007-03-13 14:52:40 +0000

    Design angepasst (simple/logo.gif --> osX/index_top_lansuite.gif) - v0rt3x (13.03.2007)

--- trunk/design/osX/templates/index.php
+++ trunk/design/osX/templates/index.php
@@ -9,7 +9,7 @@ if (!$_SESSION['lansuite']['fullscreen']) {
   $dsp->SetVar('BoxesLeft', $templ['index']['control']['boxes_letfside']);
   $dsp->SetVar('BoxesRight', $templ['index']['control']['boxes_rightside']);
   if ($_GET['design'] != 'base') {
-    $dsp->SetVar('Logo', '<img src="design/simple/images/logo.gif" alt="Logo" title="Lansuite" border="0" />');
+    $dsp->SetVar('Logo', '<img src="design/osX/images/index_top_lansuite.gif" alt="Logo" title="Lansuite" border="0" />');
     $dsp->SetVar('Debug', $func->ShowDebug());
   }
 } else $dsp->SetVar('Logo', '<a href="index.php?'. $URLQuery .'&fullscreen=no" class="menu"><img src="design/'. $auth['design'] .'/images/arrows_delete.gif" border="0" alt="" /></a> Lansuite - Vollbildmodus');
