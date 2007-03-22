ff5ec31ccc38a6f599b5fa3c5b107967a2707ab7
jochenjung <jochenjung@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2007-03-22 06:55:45 +0000

    no message

--- trunk/inc/classes/class_translation.php
+++ trunk/inc/classes/class_translation.php
@@ -17,14 +17,25 @@ function t($input, $parameters = NULL) {
   if ($input == '') return '';
   if (!$db->success) return ReplaceParameters($input, $parameters);
 
+  // Generate Mod-Name from FILE
+  if ($LSCurFile == 'DB') $mod = 'DB';
+  else {
+    $LSCurFile = str_replace('\\','/', $LSCurFile);
+    if (strpos($LSCurFile, 'modules/') !== false) {
+      $start = strpos($LSCurFile, 'modules/') + 8;
+      $mod = substr($LSCurFile, $start, strrpos($LSCurFile, '/') - $start);
+    } else $mod = 'System';
+  }
+
+  (strlen($input) > 255)? $long = '_long' : $long = '';
+
   // Load System, DB and current Mod on first call
   if ($TranslationFirstRun) {
-    $res = $db->query("SELECT id, org, $language FROM {$config['tables']['translation']} WHERE file = 'System' OR file = 'DB' or file = '$mod'");
+    $res = $db->qry('SELECT file, id, org, '. $language .' FROM %prefix%translation WHERE file = \'System\' OR file = \'DB\' OR file = %string%', $mod);
     while ($row = $db->fetch_array($res)) {
-      if ($row[$language]) $lang['initial'][$row['id']] = $row[$language];
-      else $lang['initial'][$row['id']] = $row['org'];
+      if ($row['file'] == 'System' or $row['file'] == 'DB') $db_mod = 'initial';
+      ($row[$language])? $lang[$db_mod][$row['id']] = $row[$language] : $lang[$db_mod][$row['id']] = $row['org'];
     }
-
     $TranslationFirstRun = 0;
   }
 
@@ -35,30 +46,16 @@ function t($input, $parameters = NULL) {
 
   // Read from DB
   else {
-
-    // Generate Mod-Name from FILE
-    if ($LSCurFile == 'DB') $mod = 'DB';
-    else {
-      $LSCurFile = str_replace('\\','/', $LSCurFile);
-      if (strpos($LSCurFile, 'modules/') !== false) {
-        $start = strpos($LSCurFile, 'modules/') + 8;
-        $mod = substr($LSCurFile, $start, strrpos($LSCurFile, '/') - $start);
-      } else $mod = 'System';
-    }
-
-    (strlen($input) > 255)? $long = '_long' : $long = '';
-
     // DB-Query
     $res = $db->qry('SELECT id, org, '. $language .' FROM %prefix%translation'. $long .' WHERE file = %string%', $mod);
     while ($row = $db->fetch_array($res)) {
-      if ($row[$language]) $lang[$mod][$row['id']] = $row[$language];
-      else $lang[$mod][$row['id']] = $row['org'];
+      ($row[$language])? $lang[$mod][$row['id']] = $row[$language] : $lang[$mod][$row['id']] = $row['org'];
     }
-    if ($lang[$mod][$key] != '') return ReplaceParameters($lang[$mod][$key], $parameters);
 
-    // Insert into DB
+    if ($lang[$mod][$key] != '') return ReplaceParameters($lang[$mod][$key], $parameters);
     else {
-      $db->qry('REPLACE INTO %prefix%translation'. $long .' SET id = %string%, file = %string%, org = %string%', $key, $mod, $input);
+      // Insert into DB
+      if ($key != '' and $mod != '' and $input != '') $db->qry('REPLACE INTO %prefix%translation'. $long .' SET id = %string%, file = %string%, org = %string%', $key, $mod, $input);
       return ReplaceParameters($input, $parameters);
     }
   }
