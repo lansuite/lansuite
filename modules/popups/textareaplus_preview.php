b9a1def779e8ffbd8a454760151301759df58b75
jochenjung <jochen.jung@gmail.com>
2007-04-06 15:51:57 +0000

    no message

--- modules/popups/textareaplus_preview.php
+++ modules/popups/textareaplus_preview.php
@@ -1,4 +1,5 @@
 <?
 $dsp->NewContent('Text-Vorschau');
-$dsp->AddSingleRow($func->text2html(str_replace('--NEWLINE--', "\n", $_GET['text'])));
+#$dsp->AddSingleRow($func->text2html(str_replace('--NEWLINE--', "\n", $_GET['text'])));
+$dsp->AddSingleRow($func->text2html(str_replace('--NEWLINE--', "\n", $_COOKIE['Preview'])));
 ?>
