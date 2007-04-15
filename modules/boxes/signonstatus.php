238e92daea7e3888c40f7a176c63694bd05716a8
jochenjung <jochen.jung@gmail.com>
2007-04-15 21:45:20 +0000

    no message

--- modules/boxes/signonstatus.php
+++ modules/boxes/signonstatus.php
@@ -57,7 +57,9 @@ if ($pixelcuruser > 0) $bar .= '<ul class="BarMarked" style="width:'. $pixelcuru
 if ($pixelges > 0) $bar .= '<ul class="BarFree" style="width:'. $pixelges .'px;" onmouseover="return overlib(\''. t('Frei') .': '. ($max - $cur)  .'\');" onmouseout="return nd();">&nbsp;</ul>';
 $bar .= '<ul class="BarClear">&nbsp;</ul>';
 
-$box->ItemRow("data", '<b>'. $_SESSION['party_info']['name'] .'</b>');
+if (strlen($_SESSION['party_info']['name']) > 16) $party_name = substr($_SESSION['party_info']['name'], 0, 14) .'...';
+else $party_name = $_SESSION['party_info']['name'];
+$box->ItemRow("data", '<b>'. $party_name .'</b>');
 $box->EngangedRow(date("d.m.y", $_SESSION['party_info']['partybegin']) .' - '. date("d.m.y", $_SESSION['party_info']['partyend']));
 
 $box->EngangedRow($bar);
