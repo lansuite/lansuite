e12d3679ccec75f08db5d36b9d315aff2b1936c2
denny1977 <denny1977@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2007-05-10 02:10:38 +0000

    Abfrage nach $_GET['sitereload'] hinzugef�gt. Wenn vorhanden wird der Wert genutzt um die Page per meta-Tag nach X Sekunden neu zu laden.

--- design/simple/templates/index.php
+++ design/simple/templates/index.php
@@ -3,6 +3,7 @@ $dsp->SetVar('title', $_SESSION['party_info']['name'] .' - lansuite '. $config['
 $dsp->SetVar('body_atr', $templ['index']['body']['js']);
 $dsp->SetVar('js', $templ['index']['control']['js']);
 $dsp->SetVar('DateLogout', $templ['index']['info']['current_date'] .' '. $templ['index']['info']['logout_link']);
+
 if ($_SESSION['lansuite']['fullscreen'] or $_GET['design'] == 'popup') $dsp->SetVar('ContentStyle', 'ContentFullscreen');
 else $dsp->SetVar('ContentStyle', 'Content');
 if (!$_SESSION['lansuite']['fullscreen']) {
@@ -21,7 +22,11 @@ if (!$_SESSION['lansuite']['fullscreen']) {
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="de" lang="de">
 <head>
 	<title><?$dsp->EchoVar('title')?></title>
-	<?$dsp->EchoTpl('design/templates/html_header.htm')?>
+	<?$dsp->EchoTpl('design/templates/html_header.htm');
+	  if( $_GET['sitereload'] ) {
+		echo '<meta http-equiv="refresh" content="'.$_GET['sitereload'].'; URL='.$_SERVER["PHP_SELF"].'?'.$_SERVER['QUERY_STRING'].'">';
+	  }
+	?>
   <link rel="stylesheet" type="text/css" href="ext_scripts/niftycube/niftyCorners.css" />
   <script type="text/javascript" src="ext_scripts/niftycube/niftycube.js"></script>
 </head>
