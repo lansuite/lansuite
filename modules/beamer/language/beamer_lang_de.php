3035df03202926f8b5ff7f75d5567f398cb74898
denny1977 <denny1977@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2007-05-14 10:54:07 +0000

    Erste Version von Dennys Beamermodul

---
+++ modules/beamer/language/beamer_lang_de.php
@@ -0,0 +1,31 @@
+<?php
+
+ if( !VALID_LS ) { die("Direct access not allowed!"); } // Direct-Call-Check
+
+ // The next 4 tags must be in every modul-language file exist !
+
+ // Specifications
+ $lang['beamer']['language_name'] 	= "Deutsch";
+ $lang['beamer']['version'] 		= 1;			// Integer !, Increment this Counter after you add, delete or rename of tags
+ $lang['beamer']['lastchange'] 		= "15.05.2007";	// String, must not have a strict format
+ $lang['beamer']['translator'] 		= "Denny Mleinek";		// String, the Realname of the translator
+
+ $lang['beamer']['beamer']		= "Beamer&uuml;bersicht";
+ $lang['beamer']['beamerstart']		= "Beamerinhalte pr&auml;sentieren";
+
+ $lang['beamer']['viewcontent']		= "Beamerfenster ";
+ $lang['beamer']['listcontent']		= "Auflistung der Inhalte";
+ $lang['beamer']['newcontent']      = "Inhalte hinzuf&uuml;gen";
+
+ $lang['beamer']['introtext']	= "Mit diesem Modul k&ouml;nnen Sie Texte und anderen Daten f&uuml;r eine Beamerpr&auml;sentation aufbereiten.";
+
+ $lang['beamer']['activecontent'] = "Aktive Inhalte: ";
+ $lang['beamer']['totalcontent'] =  "Inhalte gesamt: ";
+
+ 
+ /* HISTORY
+ *
+ *
+ */
+
+?>
