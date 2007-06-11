abccec0c63b0d23d61cb774dcebfaaefa4d96ca4
jochenjung <jochenjung@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2007-06-11 18:52:56 +0000

    - Change: GMaps sucht nun die Adressen in dem zur Party eingestellten Land (KnoX 11.06.2007)

--- trunk/modules/usrmgr/map.php
+++ trunk/modules/usrmgr/map.php
@@ -4,6 +4,19 @@ $dsp->NewContent(t('Benutzerkarte'), t('Auf dieser Karte finden Sie alle Benutze
 
 if (!$cfg['google_maps_api_key']) $func->information(t('Sie müssen sich zuerst unter http://www.google.com/apis/maps/signup.html einen Google-Maps API Key erzeugen und diesen auf der %1 eingeben.', array('<a href="index.php?mod=install&action=modules&step=10&module=install">'. t('AdminSeite in den Allgemeinen Einstellungen') .'</a>')));
 else {
+
+  switch($cfg['country']) {
+    case 'de': $GCountry = 'Germany' break;
+    case 'at': $GCountry = 'Austria' break;
+    case 'ch': $GCountry = 'Swiss' break;
+    case 'en': $GCountry = 'England' break;
+    case 'nl': $GCountry = 'Netherlands' break;
+    case 'es': $GCountry = 'Spain' break;
+    case 'it': $GCountry = 'Italy' break;
+    case 'fr': $GCountry = 'France' break;
+    default: $GCountry = 'Germany' break;
+  }
+
   $res = $db->query("SELECT u.*, s.avatar_path FROM {$config["tables"]["user"]} AS u
 		LEFT JOIN {$config["tables"]["usersettings"]} AS s ON u.userid = s.userid
 		WHERE u.plz > 0 AND u.type > 0
@@ -18,7 +31,7 @@ else {
 
     if ($row['avatar_path']) $text .= '<br>'. sprintf('<img src=\\"%s\\" alt=\\"%s\\" border=\\"0\\">', $row["avatar_path"], '');
 
-    $templ['guestmap']['adresses'] .= "showAddress('Germany', '{$row['city']}', '{$row['plz']}', '{$row['street']}', '{$row['hnr']}', '$text');\r\n";
+    $templ['guestmap']['adresses'] .= "showAddress('$GCountry', '{$row['city']}', '{$row['plz']}', '{$row['street']}', '{$row['hnr']}', '$text');\r\n";
 #      if ($row['street']) $templ['guestmap']['adresses'] .= "showAddress('{$row['street']}, {$row['plz']}, Germany', \"$text\");\r\n";
 #      elseif ($row['plz']) $templ['guestmap']['adresses'] .= "showAddress('{$row['plz']}, Germany', \"$text\");\r\n";
   }
