6f78d06cd24b0a74019db83ab906213b89d8c8d3
jochenjung <jochen.jung@gmail.com>
2007-06-11 18:52:56 +0000

    - Change: GMaps sucht nun die Adressen in dem zur Party eingestellten Land (KnoX 11.06.2007)

--- modules/guestlist/usermap.php
+++ modules/guestlist/usermap.php
@@ -6,6 +6,19 @@ $dsp->NewContent($lang["guestlist"]["map_caption"], $lang["guestlist"]["map_subc
 if ($cfg['guestlist_guestmap'] == 2) {
   if (!$cfg['google_maps_api_key']) $func->information(t('Sie müssen sich zuerst unter http://www.google.com/apis/maps/signup.html einen Google-Maps API Key erzeugen und diesen auf der %1 eingeben', array('<a href="index.php?mod=install&action=modules&step=10&module=install">'. t('AdminSeite in den Allgemeinen Einstellungen') .'</a>')));
   else {
+
+    switch($cfg['country']) {
+      case 'de': $GCountry = 'Germany' break;
+      case 'at': $GCountry = 'Austria' break;
+      case 'ch': $GCountry = 'Swiss' break;
+      case 'en': $GCountry = 'England' break;
+      case 'nl': $GCountry = 'Netherlands' break;
+      case 'es': $GCountry = 'Spain' break;
+      case 'it': $GCountry = 'Italy' break;
+      case 'fr': $GCountry = 'France' break;
+      default: $GCountry = 'Germany' break;
+    }
+
     $where_pid = '';
     if ($party->party_id) $where_pid = "AND (p.party_id = {$party->party_id})";
 
@@ -23,7 +36,7 @@ if ($cfg['guestlist_guestmap'] == 2) {
 
       if ($row['avatar_path']) $text .= '<br>'. sprintf('<img src=\\"%s\\" alt=\\"%s\\" border=\\"0\\">', $row["avatar_path"], '');
 
-      $templ['guestmap']['adresses'] .= "showAddress('Germany', '{$row['city']}', '{$row['plz']}', '{$row['street']}', '{$row['hnr']}', '$text');\r\n";
+      $templ['guestmap']['adresses'] .= "showAddress('$GCountry', '{$row['city']}', '{$row['plz']}', '{$row['street']}', '{$row['hnr']}', '$text');\r\n";
 #      if ($row['street']) $templ['guestmap']['adresses'] .= "showAddress('{$row['street']}, {$row['plz']}, Germany', \"$text\");\r\n";
 #      elseif ($row['plz']) $templ['guestmap']['adresses'] .= "showAddress('{$row['plz']}, Germany', \"$text\");\r\n";
     }
