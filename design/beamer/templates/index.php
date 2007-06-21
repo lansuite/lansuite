cecab682853b5179743b0cfe387916f131b184b2
andrekeller <andrekeller@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2007-06-21 08:52:33 +0000

    fixed DOCTYPE (there is no doctype //DE...)

--- design/beamer/templates/index.php
+++ design/beamer/templates/index.php
@@ -18,7 +18,7 @@ if (!$_SESSION['lansuite']['fullscreen']) {
 
 ?>
 <?php echo '<?xml version="1.0" encoding="UTF-8" ?>'."\n"; ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//DE" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="de" lang="de">
 <head>
 	<title><?$dsp->EchoVar('title')?></title>
