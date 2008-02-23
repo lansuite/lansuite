a381e6f8f73b39416c7d72f4f9fae900156c9e63
maztah <maztah@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2008-02-23 23:36:03 +0000

    Neu: Im Home per Icon ungelesene Mails kenntlich gemacht

--- trunk/modules/home/mail.inc.php
+++ trunk/modules/home/mail.inc.php
@@ -3,7 +3,7 @@
 $templ['home']['show']['item']['info']['caption'] = t('Neue Mails');
 $templ['home']['show']['item']['control']['row'] = "";
 
-$query = $db->qry('SELECT m.mailID, m.subject, u.username FROM %prefix%mail_messages AS m LEFT JOIN %prefix%user AS u ON m.FromUserID = u.userid
+$query = $db->qry('SELECT m.des_Status, m.mailID, m.subject, u.username FROM %prefix%mail_messages AS m LEFT JOIN %prefix%user AS u ON m.FromUserID = u.userid
   WHERE m.toUserID = %int% AND m.mail_status = \'active\'
   ORDER BY m.tx_date DESC
   LIMIT 0, %int%',
@@ -11,12 +11,16 @@ $query = $db->qry('SELECT m.mailID, m.subject, u.username FROM %prefix%mail_mess
 
 if ($db->num_rows($query) > 0) {
 	while ($row = $db->fetch_array($query)) {
-    $templ['home']['show']['row']['control']['link']	= 'index.php?mod=mail&action=showmail&ref=in&mailID='. $row['mailID'];
-    $templ['home']['show']['row']['info']['text']		= $row['subject'].' ['.$row['username'].']';
+   	 $templ['home']['show']['row']['control']['link']	= 'index.php?mod=mail&action=showmail&ref=in&mailID='. $row['mailID'];
+   	 $templ['home']['show']['row']['info']['text']		= $row['subject'].' ['.$row['username'].']';
+   	
+	if($row['des_Status'] == 'new')
+		$templ['home']['show']['item']['control']['row'] .= $dsp->FetchModTpl("home", "show_row_new");
+	else
 		$templ['home']['show']['item']['control']['row'] .= $dsp->FetchModTpl("home", "show_row");
 
-		$templ['home']['show']['row']['info']['text'] = '';
-		$templ['home']['show']['row']['info']['text2'] = '';
+	 $templ['home']['show']['row']['info']['text'] = '';
+	 $templ['home']['show']['row']['info']['text2'] = '';
 	}
 }
 else $templ['home']['show']['item']['control']['row'] = "<i>". t('Keine Mails bisher vorhanden') ."</i>";
