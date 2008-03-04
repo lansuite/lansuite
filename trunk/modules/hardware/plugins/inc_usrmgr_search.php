5afd0c29ae843e7266e9f15da3551c1efc161489
bytekilla <bytekilla@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2008-03-04 23:37:24 +0000

    BUGFIX : Hardware aus Usermanager entfernen http://lansuite.orgapage.de/index.php?mod=bugtracker&bugid=610 Bilder für Hardware Transparent gemacht und neu hochgeladen (waren fehlerhaft) Kleineren Fehler beim sortieren der Headermenues entfernt.

---
+++ trunk/modules/hardware/plugins/inc_usrmgr_search.php
@@ -0,0 +1,9 @@
+<?php
+// This File is a Part of the LS-Pluginsystem. It will be included in
+// modules/usrmgr/details.php to generate Modulspezific Headermenue for Userdetails
+
+// ADD HERE MODULSPECIFIC INCLUDES
+
+// ADD HERE MODULPUGINCODE
+
+?>
