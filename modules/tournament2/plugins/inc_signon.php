9a27885990bbd948bf1317169e755e7704069844
bytekilla <bytekilla@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2008-03-06 00:24:35 +0000

    FEATURE : tournament2 plugin integriert und vom usrmgr ausgelagert BUGFIX : Turnierdetails in USRMGR unklar bzw Fehler http://lansuite.orgapage.de/index.php?mod=bugtracker&bugid=612

---
+++ modules/tournament2/plugins/inc_signon.php
@@ -0,0 +1,9 @@
+<?php
+// This File is a Part of the LS-Pluginsystem. It will be included in 
+// ...
+
+// ADD HERE MODULSPECIFIC INCLUDES
+
+// ADD HERE MODULPUGINCODE
+
+?>
---
+++ modules/tournament2/plugins/plugin_cfg.php
@@ -0,0 +1,28 @@
+;<? /*
+; Position 0 = Plugin disabled
+
+[home]
+caption   = ""
+position  = "0"
+
+[usrmgr_main]
+caption   = ""
+position  = "0"
+
+[usrmgr_headermenue]
+caption   = "Turniere"
+position  = "1"
+
+[usrmgr_search]
+caption   = "-"
+position  = "0"
+
+[signon]
+caption   = ""
+position  = "0"
+
+[systemtest]
+caption   = ""
+position  = "0"
+
+;*/ ?>
