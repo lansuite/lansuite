4ffb8c3af267d1b5e28efdf03e4fb3ed4b35d90b
jochenjung <jochenjung@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2008-03-21 18:43:24 +0000

    Korrektur TimeLeft

--- trunk/modules/home/poll.inc.php
+++ trunk/modules/home/poll.inc.php
@@ -12,9 +12,10 @@ if ($db->num_rows($query) > 0) {
 	while($row = $db->fetch_array($query)) {
 		$templ['home']['show']['row']['control']['link']	= 'index.php?mod=poll&action=show&step=2&pollid='. $row['pollid'];
   if (strlen($row['caption']) > 42) $row['caption'] = substr($row['caption'], 0, 40) . '...';  
-		$templ['home']['show']['row']['info']['text']		= $row['caption'].$row["endtime"].'-'.time();
-		$templ['home']['show']['row']['info']['text2']		= '(Votes: '. $row['votes'] .')';
-  if ($row["endtime"] and $row["endtime"] < time()) $templ['home']['show']['row']['info']['text2'] .= ' <span onmouseover="return overlib(\''. t('Abstimmung wurde geschlossen') .'\');" onmouseout="return nd();"><img src="design/images/icon_locked.png" border="0" width="12" /></span>';
+		$templ['home']['show']['row']['info']['text']		= $row['caption'];
+		$templ['home']['show']['row']['info']['text2']		= '(Votes: '. $row['votes'] .') ';
+    if ($row["endtime"] and $row["endtime"] < time()) $templ['home']['show']['row']['info']['text2'] .= ' <span onmouseover="return overlib(\''. t('Abstimmung wurde geschlossen') .'\');" onmouseout="return nd();"><img src="design/images/icon_locked.png" border="0" width="12" /></span>';
+		elseif ($row["endtime"]) $templ['home']['show']['row']['info']['text2'] .= '['. ($row["endtime"] - time()) .' sec]';
 		$templ['home']['show']['item']['control']['row'] .= $dsp->FetchModTpl("home", "show_row");
 		$templ['home']['show']['row']['info']['text2']		= "";	// set var to NULL
 	}
