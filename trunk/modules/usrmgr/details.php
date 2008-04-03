f63f771c09f86eb8a535932e994b9ff277c2a9d9
maztah <maztah@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2008-04-03 14:31:18 +0000

    BUGFIX #640 http://lansuite.orgapage.de/index.php?mod=bugtracker&bugid=640

--- trunk/modules/usrmgr/details.php
+++ trunk/modules/usrmgr/details.php
@@ -279,11 +279,13 @@ break;
             // Threads
         $get_board_threads = $db->query("SELECT b.tid, b.date, t.caption FROM {$config['tables']['board_posts']} AS b
                 LEFT JOIN {$config['tables']['board_threads']} AS t ON b.tid = t.tid
-                WHERE b.userid = '{$_GET['userid']}'
+                LEFT JOIN {$config['tables']['board_forums']} AS f ON t.fid = f.fid
+   				WHERE b.userid = '{$_GET['userid']}' AND (f.need_type <= '{$auth['type']}' OR f.need_type = '1')
                 GROUP BY b.tid
                 ORDER BY b.date DESC
                 LIMIT 10
                 ");
+
             while($row_threads = $db->fetch_array($get_board_threads)) {
                 $threads .= $func->unixstamp2date($row_threads['date'], "datetime")." - <a href=\"index.php?mod=board&action=thread&tid={$row_threads['tid']}\">{$row_threads['caption']}</a>". HTML_NEWLINE;
             }
