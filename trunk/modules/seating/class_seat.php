c4d80bfac9bf53f1f12ae968ab6d50e8b296bea8
jochenjung <jochenjung@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2008-04-15 19:40:55 +0000

    Sitzplan konnte aus den userdetauils nicht geöffnet werden http://lansuite.orgapage.de/index.php?mod=bugtracker&bugid=650

--- trunk/modules/seating/class_seat.php
+++ trunk/modules/seating/class_seat.php
@@ -58,7 +58,7 @@ class seat2 {
       		
 		  $LinkText = $row['name'] .' - '. $this->CoordinateToName($x, $y, $row['orientation']);
 	    if ($LinkIt == 1) return "<a href=\"index.php?mod=seating&action=show&step=2&blockid=$blockid&col=$x&row=$y\">$LinkText</a>";
-	    if ($LinkIt == 2) return "<a href=\"#\" onclick=\"javascript:var w=window.open('index.php?mod=seating&action=popup&design=base&function=usrmgr&id=$blockid&userarray[]={$userid}&l=1','_blank','width=596,height=638,resizable=yes');\">$LinkText</a>";
+	    if ($LinkIt == 2) return "<a href=\"#\" onclick=\"javascript:var w=window.open('index.php?mod=seating&action=popup&design=popup&function=usrmgr&id=$blockid&userarray[]={$userid}&l=1','_blank','width=596,height=638,resizable=yes');\">$LinkText</a>";
 	    else return $LinkText;
 	  }
   }
