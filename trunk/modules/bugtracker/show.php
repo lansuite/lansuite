ab3d6bf27c7e0326c214b56f6da275eb5ef7da38
jochenjung <jochenjung@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2008-04-17 10:07:52 +0000

    Im Bugtracker können nun Spenden eingetragen werden

--- trunk/modules/bugtracker/show.php
+++ trunk/modules/bugtracker/show.php
@@ -54,7 +54,7 @@ function FetchType($type) {
   global $types, $line;
 
   $ret = $types[$type];
-  if ($line['price']) $ret .= '<br /><span style="white-space:nowrap;"'. (int)$line['price_payed'] .'&euro; / '. $line['price'] .'&euro; ['. (round((((int)$line['price_payed'] / (int)$line['price']) * 100), 1)) .'%]</span>';
+  if ($line['price']) $ret .= '<br /><span style="white-space:nowrap;">'. (int)$line['price_payed'] .'&euro; / '. $line['price'] .'&euro; ['. (round((((int)$line['price_payed'] / (int)$line['price']) * 100), 1)) .'%]</span>';
   return $ret;
 }
 
