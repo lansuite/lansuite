ac2d3155eba7484956257becccd84e42fcadef64
jochenjung <jochenjung@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2008-04-19 07:24:32 +0000

    Zufallsbild-Box: Code optimiert. U.a. war eine Endlosschleife darin, wenn kein Bild existiert hat

--- trunk/modules/boxes/random_pic.php
+++ trunk/modules/boxes/random_pic.php
@@ -1,36 +1,26 @@
 <?php
 $templ['box']['rows'] = "";
 
-$pics_row = $db->qry_first('SELECT COUNT(p.picid) AS activepics FROM %prefix%picgallery AS p');
-		
-if($pics_row["activepics"] > 0) 
-{	
-	$rowexisting = false;
-	while($rowexisting == false)
-	{
-		$zufall = rand(1,$pics_row["activepics"]);		
-		$pics_res = $db->query_first("SELECT p.picid, p.name, p.caption FROM {$config["tables"]["picgallery"]} AS p 
-					    WHERE p.picid = $zufall");
-	if($pics_res['name'] != "") $rowexisting = true;
-	}
+$pics_res = $db->qry_first('SELECT picid, name, caption FROM %prefix%picgallery WHERE name != \'\' ORDER BY RAND()');
+
+if ($pics_res['picid']) {
+  $pics_comrow = $db->qry_first('SELECT COUNT(commentid) AS comments FROM %prefix%comments WHERE relatedto_item = \'Picgallery\' AND relatedto_id = %int% ', $pics_res[picid]);
+  $pics_comments= $pics_comrow['comments']." Kommentare";
+  
+  $pic_array = explode("/", $pics_res['name']);
+  $pic_name = $pic_array[sizeof($pic_array)-1];
+  $pic_thumbname = "lsthumb_".$pic_name;
+  
+  $pic_neuname = str_replace($pic_name, $pic_thumbname, $pics_res['name']);
+  
+  $pic_namelink ='<img src="ext_inc/picgallery/'.$pic_neuname.'" />';
+  
+  $box->DotRow($pics_res['caption']);
+  $box->EmptyRow();
+  $templ['randompic']['tag'] = $pics_res['caption'];
+  $templ['randompic']['thumblink'] = $pic_neuname;
+  $templ['randompic']['link'] = $pics_res['name'];
+  $box->AddTemplate("box_randompic");
+  $box->EngangedRow($pics_comments);
 }
-
-$pics_comrow = $db->qry_first('SELECT COUNT(commentid) AS comments FROM %prefix%comments WHERE relatedto_id = %int% ', $pics_res[picid]);
-$pics_comments= $pics_comrow['comments']." Kommentare";
-
-$pic_array = explode("/", $pics_res['name']);
-$pic_name = $pic_array[sizeof($pic_array)-1];
-$pic_thumbname = "lsthumb_".$pic_name;
-
-$pic_neuname = str_replace($pic_name, $pic_thumbname, $pics_res['name']);
-
-$pic_namelink ='<img src="ext_inc/picgallery/'.$pic_neuname.'">';
-
-$box->DotRow($pics_res['caption']);
-$box->EmptyRow();
-$templ['randompic']['tag'] = $pics_res['caption'];
-$templ['randompic']['thumblink'] = $pic_neuname;
-$templ['randompic']['link'] = $pics_res['name'];
-$box->AddTemplate("box_randompic");
-$box->EngangedRow($pics_comments);
 ?>
