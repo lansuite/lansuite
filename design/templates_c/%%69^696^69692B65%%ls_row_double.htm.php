5453d09c3b46a4e81c1e728e4c2e14b3cb7dd712
jochenjung <jochen.jung@gmail.com>
2008-04-22 07:57:49 +0000

    Smarty-Umstellung

---
+++ design/templates_c/%%69^696^69692B65%%ls_row_double.htm.php
@@ -0,0 +1,7 @@
+<?php /* Smarty version 2.6.19, created on 2008-04-22 09:24:09
+         compiled from ls_row_double.htm */ ?>
+<li class="LineLeft"><?php echo $this->_tpl_vars['key']; ?>
+</li>
+<li class="LineRight" id="<?php echo $this->_tpl_vars['id']; ?>
+"><?php echo $this->_tpl_vars['value']; ?>
+</li>
---
+++ design/templates_c/%%7C^7C3^7C360EBB%%ls_row_firstline.htm.php
@@ -0,0 +1,4 @@
+<?php /* Smarty version 2.6.19, created on 2008-04-22 09:46:05
+         compiled from design/templates/ls_row_firstline.htm */ ?>
+<ul class="LineFirst"><?php echo $this->_tpl_vars['content']; ?>
+</ul>
