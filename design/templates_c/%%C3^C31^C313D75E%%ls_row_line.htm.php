78721c17f92eaf75fc198413fa8277c0d1a68e92
bytekilla <bytekilla@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2008-06-27 21:20:01 +0000

    PHP-Cachefiles aus dem Repository genommen. Achtung... nicht mehr mitübertragen.

--- design/templates_c/%%C3^C31^C313D75E%%ls_row_line.htm.php
+++
@@ -1,4 +0,0 @@
-<?php /* Smarty version 2.6.19, created on 2008-04-22 09:46:05
-         compiled from design/templates/ls_row_line.htm */ ?>
-<ul class="Line"><?php echo $this->_tpl_vars['content']; ?>
-</ul>
