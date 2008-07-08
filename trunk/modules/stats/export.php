ba945617a5a2c60c10a77acda71de163d25edd30
bytekilla <bytekilla@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2008-07-08 14:32:15 +0000

    BUGFIX : #719 Feedback fehlerhaft http://lansuite.orgapage.de/index.php?mod=bugtracker&bugid=719 Bei der automatischen t() ersetzung wurden " in ein ' eingebaut

--- trunk/modules/stats/export.php
+++ trunk/modules/stats/export.php
@@ -3,30 +3,30 @@
 $action = $_GET['action'];
 
 switch($action) {
-	case "exportinfo":
-		$templ['stats_data'] = $stats->getExportData();
-		if($templ['stats_data']['start'] == 0 ) 	$templ['stats_data']['date'] = '<font color="red"><i>".t('nicht eingetragen')."</i></font>'; 
-		else 						$templ['stats_data']['date'] = strftime("%d.%m.%Y",$templ['stats_data']['start']); 
+    case "exportinfo":
+        $templ['stats_data'] = $stats->getExportData();
+        if($templ['stats_data']['start'] == 0 )     $templ['stats_data']['date'] = '<font color="red"><i>'.t('nicht eingetragen').'</i></font>';
+        else                        $templ['stats_data']['date'] = strftime("%d.%m.%Y",$templ['stats_data']['start']); 
 
-		$templ['stats_data']['date'] .= ' - ';
-												
-		if($templ['stats_data']['end'] == 0) 		$templ['stats_data']['date'] .= '<font color="red"><i>".t('nicht eingetragen')."</i></font>';  
-		else 						$templ['stats_data']['date'] .= strftime("%d.%m.%Y",$templ['stats_data']['end']);  
-		
-		if($templ['stats_data']['name'] == 'LanParty with LANsuite') 		$templ['stats_data']['name'] = '<font color="red"><i>".t('nicht eingetragen')."</i></font>';
-		if($templ['stats_data']['url'] == 'http://www.one-network.org') 	$templ['stats_data']['url'] = '<font color="red"><i>".t('nicht eingetragen')."</i></font>';
-		
-		if($templ['stats_data']['plz'] == 0) 	$templ['stats_data']['plz'] = '<font color="red"><i>".t('nicht eingetragen')."</i></font>';
-		if($templ['stats_data']['mail'] == '') 	$templ['stats_data']['mail'] = '<font color="red"><i>".t('nicht eingetragen')."</i></font>';
+        $templ['stats_data']['date'] .= ' - ';
+                                                
+        if($templ['stats_data']['end'] == 0)        $templ['stats_data']['date'] .= '<font color="red"><i>'.t('nicht eingetragen').'</i></font>';
+        else                        $templ['stats_data']['date'] .= strftime("%d.%m.%Y",$templ['stats_data']['end']);  
+        
+        if($templ['stats_data']['name'] == 'LanParty with LANsuite')        $templ['stats_data']['name'] = '<font color="red"><i>'.t('nicht eingetragen').'</i></font>';
+        if($templ['stats_data']['url'] == 'http://www.one-network.org')     $templ['stats_data']['url'] = '<font color="red"><i>'.t('nicht eingetragen').'</i></font>';
+        
+        if($templ['stats_data']['plz'] == 0)    $templ['stats_data']['plz'] = '<font color="red"><i>'.t('nicht eingetragen').'</i></font>';
+        if($templ['stats_data']['mail'] == '')  $templ['stats_data']['mail'] = '<font color="red"><i>'.t('nicht eingetragen').'</i></font>';
 
     $gd->CreateButton('send');
-		$dsp->AddSingleRow($dsp->FetchModTpl('misc', 'stats_export'));
-		$dsp->AddContent();
-	break;
+        $dsp->AddSingleRow($dsp->FetchModTpl('misc', 'stats_export'));
+        $dsp->AddContent();
+    break;
 
-	default:
-		$stats->export();
-		$func->confirmation(t('Die Daten wurden erfolgreich &uuml;bermittelt'), "");	
-	break;
-}		
+    default:
+        $stats->export();
+        $func->confirmation(t('Die Daten wurden erfolgreich &uuml;bermittelt'), "");    
+    break;
+}       
 ?>
