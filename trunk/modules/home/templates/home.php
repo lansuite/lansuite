d46d867bd20f846f8ae868c7d3739d26441c294d
bytekilla <bytekilla@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2008-07-11 10:56:57 +0000

    Test für Designproblem bei Spalten Teil 2 http://lansuite.orgapage.de/index.php?mod=board&action=thread&tid=793

--- trunk/modules/home/templates/home.php
+++ trunk/modules/home/templates/home.php
@@ -4,8 +4,8 @@ foreach($ModOverviews as $ModOverview) {
   if ($z % 2 == 0) {
     $MainContent .= '<ul class="Line">';
     if ($z != (count($ModOverviews) - 1)) $MainContent .= '<li class="LineLeftHalf">';
-    else $MainContent .= '<li class="LineRight">';
-  } else $MainContent .= '<li class="LineRight">';
+    else $MainContent .= '<li class="LineRightHalf">';
+  } else $MainContent .= '<li class="LineRightHalf">';
 #  $st = round($sitetool->out_work(), 2);
   include('modules/home/'. $ModOverview .'.inc.php');
   $MainContent .= $dsp->FetchModTpl('home', 'show_item');
