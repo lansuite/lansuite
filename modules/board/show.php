d962d928e3afddd762542dfc41961d2774644fa3
jochenjung <jochen.jung@gmail.com>
2008-08-21 19:41:26 +0000

    http://lansuite.orgapage.de/index.php?mod=bugtracker&bugid=742

--- modules/board/show.php
+++ modules/board/show.php
@@ -28,7 +28,7 @@ $ms2 = new mastersearch2();
 $ms2->query['from'] = "{$config['tables']['board_forums']} AS f
     LEFT JOIN {$config['tables']['board_threads']} AS t ON f.fid = t.fid
     LEFT JOIN {$config['tables']['board_posts']} AS p ON t.tid = p.tid
-    LEFT JOIN {$config['tables']['user']} AS u ON f.need_group = u.group_id";
+    LEFT JOIN {$config['tables']['user']} AS u ON f.need_group > 0 AND f.need_group = u.group_id";
 $ms2->query['where'] = 'f.need_type <= '. ((int)($auth['type'] + 1) .' AND (!f.need_group OR u.userid = '. ((int)$auth['userid']) .')');
 $ms2->query['default_order_by'] = 'f.pos';
 
