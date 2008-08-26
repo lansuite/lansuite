a7e1e74306535b6f0c090413ccb9d0c149a5b65a
jochenjung <jochen.jung@gmail.com>
2008-08-26 13:06:16 +0000

    Modul CodeCheck Code-Konventionen prüfen http://lansuite.orgapage.de/index.php?mod=bugtracker&bugid=728

---
+++ modules/codecheck/show.php
@@ -0,0 +1,39 @@
+<?php
+
+$dsp->NewContent('Codecheck', '');
+
+$menunames[1] = t('ShortOpenTags');
+$menunames[2] = t('$db->query');
+$menunames[3] = t('Tabs');
+$dsp->AddHeaderMenu($menunames, 'index.php?mod=codecheck', $_GET['headermenuitem']);
+
+switch ($_GET['headermenuitem']) {
+  case 1: $dsp->AddSingleRow(t('Es sollte immer <?php statt <? verwendet werden. Anders funktioniert LS beispielsweise im IIS nicht')); break;
+  case 2: $dsp->AddSingleRow(t('$db->query sollte durch das neue $qb->qry ausgetauscht werden. Dieses kann SQL-Injections sehr zuverlässig verhindern')); break;
+  case 3: $dsp->AddSingleRow(t('Es sollten keine Tabs im Code verwendet werden. Statt dessen am Besten 2 Leerzeichen')); break;
+}
+
+$modules = opendir('modules');
+while ($mod = readdir($modules)) if (is_dir("modules/$mod") and $mod != '.' and $mod != '..' and $mod != '.svn') {
+  $out = '';
+
+  $files = opendir("modules/$mod");
+  while ($file = readdir($files)) if (is_file("modules/$mod/$file") and substr($file, strlen($file) - 4, 4) == '.php') {
+
+    $fh = fopen("modules/$mod/$file", 'r');
+    $content = fread($fh, filesize("modules/$mod/$file"));
+    fclose($fh);
+
+    switch ($_GET['headermenuitem']) {
+      case 1: if (preg_match("#<\\?[^a-z].#sUi", $content)) $out .= "modules/$mod/$file<br />"; break;
+      case 2: if (preg_match("#\\\$db->query#sUi", $content)) $out .= "modules/$mod/$file<br />"; break;
+      case 3: if (preg_match("#\t#sUi", $content)) $out .= "modules/$mod/$file<br />"; break;
+    }
+  }
+  closedir($files);
+
+  $dsp->AddDoubleRow($mod, $out);
+}
+closedir($modules);
+
+?>
