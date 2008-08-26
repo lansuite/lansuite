f8a3d8d237e06fd537cbcd2c9dc7837bd191046b
jochenjung <jochenjung@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2008-08-26 14:37:49 +0000

    Erster Entwurf Masterrate

---
+++ trunk/inc/classes/class_masterrate.php
@@ -0,0 +1,35 @@
+<?php
+
+class masterrate {
+
+	// Construktor
+	function masterrate($mod, $id) {
+	  global $auth;
+	  
+	  include_once('inc/classes/class_masterform.php');
+    $mf = new masterform();
+    $mf->LogID = $id;
+
+    $selections = array();
+    $selections[''] = t('Bitte auswählen');
+    $selections['1'] = '1 '. t('Sehr schlecht');
+    $selections['2'] = '2';
+    $selections['3'] = '3';
+    $selections['4'] = '4';
+    $selections['5'] = '5';
+    $selections['6'] = '6';
+    $selections['7'] = '7';
+    $selections['8'] = '8';
+    $selections['9'] = '9';
+    $selections['10'] = '10 '. t('Sehr gut');
+    $mf->AddField(t('Bewertung'), 'score', IS_SELECTION, $selections);
+
+    if (!$auth['login']) $mf->AddField('', 'captcha', IS_CAPTCHA);
+    $mf->AddFix('ref_name', $mod);
+    $mf->AddFix('ref_id', $id);
+    $mf->AddFix('date', 'NOW()');
+    $mf->AddFix('creatorid', $auth['userid']);
+    $mf->SendForm('', 'ratings', 'ratingid', $_GET['ratingid']);
+	}
+}
+?>
