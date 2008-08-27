7c466db63ef1a2ce7663d1a822aeebaed3debb9d
JochenJung <JochenJung@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2008-08-27 15:11:05 +0000

    Fehler in Verification Mail behoben Mail kann bei fehlerhaftem Login erneut versendet werden http://lansuite.orgapage.de/index.php?mod=bugtracker&bugid=520

--- trunk/modules/usrmgr/verify_email.php
+++ trunk/modules/usrmgr/verify_email.php
@@ -1,11 +1,21 @@
 <?php
 
-$dsp->NewContent(t('Email-Adresse verifizieren'), '');
+switch($_GET['step']) {
+  default:
+    $dsp->NewContent(t('Email-Adresse verifizieren'), '');
 
-$user_data = $db->qry_first('SELECT fcode FROM %prefix%user WHERE fcode = %string%', $_GET['verification_code']);
-if ($user_data['fcode'] and $_GET['verification_code'] != '') {
-  $db->qry('UPDATE %prefix%user SET email_verified = 1, fcode = '' WHERE fcode = %string%');
+    $user_data = $db->qry_first('SELECT 1 AS found FROM %prefix%user WHERE fcode = %string%', $_GET['verification_code']);
+    if ($user_data['found'] and $_GET['verification_code'] != '') {
+      $db->qry('UPDATE %prefix%user SET email_verified = 1, fcode = \'\' WHERE fcode = %string%', $_GET['verification_code']);
   
-	$func->confirmation(t('Ihre Email-Adresse wurde erfolgreich verifiziert. Vielen Dank!'), 'index.php');
-} else $func->error(t('Der von Ihnen Übermittelte Verifizierungscode ist inkorrekt! Bitte prüfen Sie, ob Sie die URL komplett aus der Benachrichtigungs-Mail kopiert haben.'));
+	   $func->confirmation(t('Ihre Email-Adresse wurde erfolgreich verifiziert. Vielen Dank!'), 'index.php');
+    } else $func->error(t('Der von Ihnen Übermittelte Verifizierungscode ist inkorrekt! Bitte prüfen Sie, ob Sie die URL komplett aus der Benachrichtigungs-Mail kopiert haben.'));
+  break;
+  
+  case 2:
+    include_once("modules/usrmgr/class_usrmgr.php");
+    $usrmgr = new UsrMgr();   
+    if ($usrmgr->SendVerificationEmail($_GET['userid'])) $func->confirmation(t('Die Verifikations-Email ist versandt worden.'));            
+  break;
+}
 ?>
