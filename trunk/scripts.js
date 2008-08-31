bcd9a11932bb64828f0eb194411099d99b82fd13
jochenjung <jochenjung@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2008-08-31 10:18:43 +0000

    http://lansuite.orgapage.de/index.php?mod=bugtracker&bugid=424

--- trunk/scripts.js
+++ trunk/scripts.js
@@ -278,8 +278,13 @@ function markieren_permanent(EintragSpalte) {
   else if (typeof(EintragSpalte.cells) != 'undefined') var Spalten = EintragSpalte.cells;
   else return false;
 
-  for (var c = 0; c < Spalten.length; c++) if (Spalten[c].className != 'row_value_highlighted') Spalten[c].className = 'row_value_highlighted';
-  else Spalten[c].className = 'row_value';
+  for (var c = 0; c < Spalten.length; c++) if (Spalten[c].className != 'row_value_highlighted') {
+    Spalten[c].className = 'row_value_highlighted';
+    if (c == 0 && typeof(Spalten[c].getElementsByTagName('input')[0]) != 'undefined') Spalten[c].getElementsByTagName('input')[0].checked = true;
+  } else {
+    Spalten[c].className = 'row_value';
+    if (c == 0 && typeof(Spalten[c].getElementsByTagName('input'[0])) != 'undefined') Spalten[c].getElementsByTagName('input')[0].checked = false;
+  }
 
   return true;
 }
@@ -312,177 +317,6 @@ function SubmitDropDown(FormObj, DropDownObj) {
 }
 
 
-//// Seating ////
-
-// Define marker for images
-var setdata;
-
-// Define Mouseflag
-var mouseflag = 0;
-
-// Functions for preload images
-function MM_preloadImages() {
-	var d=document;
-	if (d.images) {
-		if (!d.MM_p) d.MM_p = new Array();
-		var i, j = d.MM_p.length, a = MM_preloadImages.arguments;
-		for (i = 0; i < a.length; i++) if (a[i].indexOf("#")!=0) {
-			d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];
-		}
-	}
-}
-
-// Functions for find object
-function MM_findObj(n, d) {
-	var p, i, x;
-	if (!d) d = document; 
-	if ((p = n.indexOf("?")) > 0 && parent.frames.length) {
-		d = parent.frames[n.substring(p + 1)].document;
-		n = n.substring(0, p);
-	}
-	if (!(x = d[n]) && d.all) x = d.all[n];
-	for (i = 0; !x && i < d.forms.length; i++) x = d.forms[i][n];
-	for (i = 0; !x && d.layers && i < d.layers.length; i++) x = MM_findObj(n, d.layers[i].document);
-	if (!x && document.getElementById) x = document.getElementById(n);
-	return x;
-}
-
-// Functions for swap image 
-function MM_swapImage() {
-	MM_swapImgRestore();
-	var i, j = 0, x, a = MM_swapImage.arguments;
-	document.MM_sr = new Array;
-	for (i = 0; i < (a.length - 2); i += 3)
-	if ((x = MM_findObj(a[i])) != null) {
-		document.MM_sr[j++] = x;
-		if(!x.oSrc) x.oSrc = x.src;
-		x.src = a[i + 2];
-	}
-}
-
-// Functions for restore image 
-function MM_swapImgRestore() {
-  	var i, x, a = document.MM_sr;
-	for (i = 0; a && i < a.length && (x = a[i]) && x.oSrc; i++) x.src = x.oSrc;
-}
-
-// Functions to Change Image
-function changeImage(id){
-	var i = 0;
-	if (mouseflag == 1){
-    var StartPos = 0; 
-		//img = CurrentPicture.substring(0, CurrentPicture.length - 4);
-		if (CurrentPicture.indexOf("lsthumb_") > 0) StartPos = CurrentPicture.indexOf("lsthumb_") + 8;
-		else StartPos = CurrentPicture.lastIndexOf("/") + 1;
-		img = CurrentPicture.substring(StartPos, CurrentPicture.length - 4);
-		if (id != 'null' && document.getElementById(id.id.substring(1, id.id.length)).value != img){
-			//id.style.backgroundImage = "url("+ image[img].src +")";
-			id.style.backgroundImage = "url("+ CurrentPicture +")";
-			document.getElementById(id.id.substring(1,id.id.length)).value = img;
-		}
-	}
-}
-
-
-var CurrentPicture = '';
-function UpdateCurrentPicture(file) {
-  CurrentPicture = file;
-  document.images["PreviewIcon"].src = file;
-}
-
-
-function AllselectH(rowid) {
-  mouseflag = 1;
-	for (var x = 0; x < cols; x++) {
-		var zahl = x * 100 + rowid;
-    changeImage(document.getElementById("fcell"+zahl));
-	}
-  mouseflag = 0;
-}
-
-
-function AllselectV(colid) {
-  mouseflag = 1;
-	for (var x = 0; x < rows; x++) {
-		var zahl = colid * 100 + x;
-    changeImage(document.getElementById("fcell"+zahl));
-	}
-  mouseflag = 0;
-}
-
-
-// Functions for details an swap
-function showseat(id) {
-	if (id != 'null' && setdata != id) {
-		// Split seatdata 
-		data_array = seat[id].split(",");
-
-		// Swap image
-		MM_swapImgRestore();
-
-		// Write userdata
-		document.getElementById("seating").firstChild.nodeValue = 'Block: ' + data_array[4];
-		document.getElementById("name").firstChild.nodeValue = data_array[0];
-		if (data_array[1] || data_array[2]) document.getElementById("name").firstChild.nodeValue += ' (' + data_array[1] + ' ' + data_array[2] + ')';
-		document.getElementById("clan").firstChild.nodeValue = data_array[3];
-		document.getElementById("ip").firstChild.nodeValue = data_array[7];
-
-		switch (data_array[6]) {
-			case "1":
-					document.getElementById("seating").firstChild.nodeValue += ' [Frei]';
-					MM_swapImage(id,'','ext_inc/auto_images/{default_design}/seat/seat_free_onclick.png',1);
-			break;
-			case "2": 
-					document.getElementById("seating").firstChild.nodeValue += ' [Belegt]';
-					MM_swapImage(id,'','ext_inc/auto_images/{default_design}/seat/seat_reserved_onclick.png',1);
-			break;
-			case "3": 
-					document.getElementById("seating").firstChild.nodeValue += ' [Vorgemerkt]';
-					MM_swapImage(id,'','ext_inc/auto_images/{default_design}/seat/seat_marked_onclick.png',1);
-			break;
-			case "8":
-					document.getElementById("seating").firstChild.nodeValue += ' [Belegt8]';
-					MM_swapImage(id,'','ext_inc/auto_images/{default_design}/seat/seat_myselfe_onclick.png',1);
-			break;
-			case "9":
-					document.getElementById("seating").firstChild.nodeValue += ' [Belegt9]';
-					MM_swapImage(id,'','ext_inc/auto_images/{default_design}/seat/seat_clanmate_onclick.png',1);
-			break;
-			default:
-					document.getElementById("seating").firstChild.nodeValue += ' [Umgebung]';
-			break;
-		}
-
-		setdata = id;
-	}
-}
-
-function InitSeating() {
-  MM_preloadImages('ext_inc/auto_images/{default_design}/seat/seat_free_onclick.png'); 
-  MM_preloadImages('ext_inc/auto_images/{default_design}/seat/seat_reserved_onclick.png'); 
-  MM_preloadImages('ext_inc/auto_images/{default_design}/seat/seat_marked_onclick.png'); 
-  MM_preloadImages('ext_inc/auto_images/{default_design}/seat/seat_myselfe_onclick.png'); 
-  MM_preloadImages('ext_inc/auto_images/{default_design}/seat/seat_clanmate_onclick.png');
-  
-  var flagV = new Array();
-  var flagH = new Array();
-  
-  for (i = 0; i <= cols; i++) {
-  	flagV[i] = false;
-  }
-  
-  for (i = 0; i <= rows; i++) {
-  	flagH[i] = false;
-  } 
-}
-
-function setFlag(Ereignis){
-	mouseflag = 1;
-}
-
-function resetFlag(Ereignis){
-	mouseflag = 0;
-}
 
 // Tooltip windows
 function TX_getScrollPos() { 
