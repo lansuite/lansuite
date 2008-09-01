a695865e028772cc9532df1f62e5a6ca15e17bbd
jochenjung <jochenjung@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2008-09-01 21:03:52 +0000

    Alte Funktionen entfernt

--- trunk/modules/boxes/login.php
+++ trunk/modules/boxes/login.php
@@ -12,8 +12,6 @@ else $templ['login']['action'] = "index.php?mod=auth&action=login";
 $templ['login']['buttons_add'] = $dsp->FetchIcon('index.php?mod=signon', 'add_user');
 $templ['login']['buttons_pw'] = $dsp->FetchIcon('index.php?mod=usrmgr&amp;action=pwrecover', 'pw_forgot');
 
-$gd->CreateButton('login');
-#$templ['login']['buttons_login'] = '<input type="image" name="login" src="ext_inc/auto_images/'. $auth['design'] .'/'. $language .'/button_login.png" border="0" alt="Login" title="Login" />';
 $templ['login']['buttons_login'] = '<input type="submit" class="Button" name="login" value="Einloggen" />';
 $box->AddTemplate("box_login_content");
 
