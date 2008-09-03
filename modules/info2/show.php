6385822090a933cb5e5c78423e461a32061ec283
jochenjung <jochen.jung@gmail.com>
2008-09-03 18:58:45 +0000

    - Info2 ist nun auf MF umgestellt - Multilanguage-Support ist auch drin http://lansuite.orgapage.de/index.php?mod=bugtracker&bugid=708

--- modules/info2/show.php
+++ modules/info2/show.php
@@ -1,18 +1,21 @@
 <?php
 
+if ($language == 'de') $val = '';
+else $val = '_'. $language;
+
 if (($_GET["submod"] != "")||($_GET["id"]>=1)) {
 	
 	if ($_GET["submod"]) { 
 		// FIX : Remove on next Version, SUBMOD is only for compartiblity
-		$info = $db->query_first("SELECT active, text, caption FROM {$config['tables']['info']} WHERE caption = '{$_GET["submod"]}'");
+		$info = $db->query_first("SELECT active, text$val, shorttext$val, caption$val FROM {$config['tables']['info']} WHERE caption = '{$_GET["submod"]}'");
 	} else {
-		$info = $db->query_first("SELECT active, text, caption FROM {$config['tables']['info']} WHERE infoID = '{$_GET["id"]}'");
+		$info = $db->query_first("SELECT active, text$val, shorttext$val, caption$val FROM {$config['tables']['info']} WHERE infoID = '{$_GET["id"]}'");
 	}
 
-	$dsp->NewContent(t('Seite').": {$info["caption"]}", $info["shorttext"]);
+	$dsp->NewContent(t('Seite').": {$info["caption$val"]}", $info["shorttext$val"]);
 
   if ($info['active'] == 1) {
-	 $dsp->AddSingleRow($func->AllowHTML($info["text"]));
+	 $dsp->AddSingleRow($func->AllowHTML($info["text$val"]));
 	} else $func->error(t('Diese Info-Seite ist nicht aktiviert. Ein Admin muss sie zuerst im Info-Modul aktivieren'), "");
 	
 	// Show edit/aktivate Buttons
