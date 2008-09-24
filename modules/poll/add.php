ac0d9822a904d2bf5b40593081227a69ff952d64
jochenjung <jochen.jung@gmail.com>
2008-09-24 12:49:51 +0000

    - Fehler nach Änderung der DB-Struktur vom poll-Modul. http://lansuite.orgapage.de/index.php?mod=bugtracker&bugid=760

--- modules/poll/add.php
+++ modules/poll/add.php
@@ -4,8 +4,10 @@ function Update($id) {
   global $db;
 
   if ($_POST['poll_reset'] or !$_GET['pollid']) {
+			$res = $db->qry('SELECT polloptionid FROM %prefix%polloptions WHERE pollid = %int%', $id);
+			while ($row = $db->fetch_array($res)) $db->qry('DELETE FROM %prefix%pollvotes WHERE polloptionid = %int%', $row['polloptionid']);
+			$db->free_result($res);
 			$db->qry('DELETE FROM %prefix%polloptions WHERE pollid = %int%', $id);
-			$db->qry('DELETE FROM %prefix%pollvotes WHERE pollid = %int%', $id);
       if ($_POST['poll_option']) foreach ($_POST['poll_option'] as $key => $val) {
       if (trim($val) != '') $db->qry('INSERT INTO %prefix%polloptions SET caption = %string%, pollid = %int%', $val, $id);
     }
