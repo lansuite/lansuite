31904e8393cf9839adafd5502202d168fba31081
jochenjung <jochen.jung@gmail.com>
2008-09-25 10:30:53 +0000

    FCK-Editor Update

--- ext_scripts/FCKeditor/editor/filemanager/connectors/php/config.php
+++ ext_scripts/FCKeditor/editor/filemanager/connectors/php/config.php
@@ -28,16 +28,16 @@ global $Config ;
 // WARNING: don't just set "$Config['Enabled'] = true ;", you must be sure that only
 //		authenticated users can access this file or use some kind of session checking.
 $Config['Enabled'] = true ;
-$path = substr($_SERVER['REQUEST_URI'], 0, strpos($_SERVER['REQUEST_URI'], '/ext_scripts/'));
 
 // Path to user files relative to the document root.
-$Config['UserFilesPath'] = $path. '/ext_inc/UserFiles/' ;
+#$path = substr($_SERVER['REQUEST_URI'], 0, strpos($_SERVER['REQUEST_URI'], '/ext_scripts/'));
+$Config['UserFilesPath'] = '/ext_inc/UserFiles/';
 
 // Fill the following value it you prefer to specify the absolute path for the
 // user files directory. Useful if you are using a virtual directory, symbolic
 // link or alias. Examples: 'C:\\MySite\\userfiles\\' or '/root/mysite/userfiles/'.
 // Attention: The above 'UserFilesPath' must point to the same directory.
-$Config['UserFilesAbsolutePath'] = '' ;
+$Config['UserFilesAbsolutePath'] = '';
 
 // Due to security issues with Apache modules, it is recommended to leave the
 // following setting enabled.
