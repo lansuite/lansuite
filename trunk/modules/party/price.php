e7ee9544fbbd343dcf4e834a1bb0c3c29102d602
jochenjung <jochenjung@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2008-11-11 11:25:22 +0000

    http://lansuite.orgapage.de/index.php?mod=bugtracker&bugid=807

--- trunk/modules/party/price.php
+++ trunk/modules/party/price.php
@@ -1,5 +1,8 @@
 <?php
 
+if (!$_GET['party_id']) $_GET['party_id'] = $party->party_id;
+$dsp->AddDoubleRow('Party', $party->data['name']);
+
 switch ($_GET['step']) {
   case 11:
     $db->qry('UPDATE %prefix%partys SET evening_price_id = %int% WHERE party_id = %int%', $_GET['evening_price_id'], $_GET['party_id']);
@@ -30,24 +33,28 @@ $ms2->query['where'] = "party.party_id = ". (int)$_GET['party_id'];
 
 $ms2->config['EntriesPerPage'] = 20;
 
+/*
 $party_list = array('' => 'Alle');
 $row = $db->qry("SELECT party_id, name FROM %prefix%partys");
 while($res = $db->fetch_array($row)) $party_list[$res['party_id']] = $res['name'];
 $db->free_result($row);
 $ms2->AddTextSearchDropDown('Party', 'p.party_id', $party_list, $party->party_id);
+*/
 
 $ms2->AddResultField(t('Text für Eintrittspreis'), 'p.price_text');
 $ms2->AddResultField(t('Preis'), 'p.price');
 $ms2->AddResultField(t('Abendkasse-Preis?'), 'party.evening_price_id', 'eveningPriceIdLink');
 $ms2->AddResultField('Party', 'party.name');
 
-if ($auth['type'] >= 2) $ms2->AddIconField('edit', 'index.php?mod=party&action=price_edit&price_id=', t('Editieren'));
+if ($auth['type'] >= 2) $ms2->AddIconField('edit', 'index.php?mod=party&action=price_edit&party_id='. $_GET['party_id'] .'&price_id=', t('Editieren'));
 
-if ($auth['type'] >= 3) $ms2->AddMultiSelectAction(t('Löschen'), 'index.php?mod=party&action=price_del', 1);
+if ($auth['type'] >= 3) $ms2->AddMultiSelectAction(t('Löschen'), 'index.php?mod=party&action=price_del&party_id='. $_GET['party_id'], 1);
 
-$ms2->PrintSearch('index.php?mod=party&action=price', 'p.price_id');
+$ms2->PrintSearch('index.php?mod=party&action=price&party_id='. $_GET['party_id'], 'p.price_id');
 
 $dsp->AddSingleRow($dsp->FetchButton('index.php?mod=party&action=price_edit', 'add'));
+
+$dsp->AddBackButton('index.php?mod=party');
 $dsp->AddContent();
 
 ?>
