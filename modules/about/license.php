90eda5be28191e89c1366f920d8f0d8d64048243
jochenjung <jochen.jung@gmail.com>
2008-11-12 16:29:30 +0000

    Install-Modul auf Smarty umgestellt

--- modules/about/license.php
+++ modules/about/license.php
@@ -1,6 +1,6 @@
 <?php
 $dsp->NewContent($config['lansuite']['version'], 'A web based lanparty administration tool');
-$dsp->AddModTpl("about", "license");
+$dsp->AddSmartyTpl('license', 'about');
 $dsp->AddBackButton("index.php?mod=about", "about/license");
 $dsp->AddContent();
 ?>
