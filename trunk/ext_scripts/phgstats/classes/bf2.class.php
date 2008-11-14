c707578d7d232cf27a3ba8542bb925c3c2a72ca8
jochenjung <jochenjung@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2008-11-14 17:40:12 +0000

    Neue phgstats maps + klassen

--- trunk/ext_scripts/phgstats/classes/bf2.class.php
+++ trunk/ext_scripts/phgstats/classes/bf2.class.php
@@ -38,12 +38,12 @@ class bf2
     var $port     = false;
     var $socket   = false;
     var $write    = "\xFE\xFD\x09\x10\x20\x30\x40";
-    var $write2	  = "\xFE\xFD\x00\x10\x20\x30\x40";
+    var $write2          = "\xFE\xFD\x00\x10\x20\x30\x40";
     var $split    = "\xFF\xFF\xFF\x01";
     var $s_info   = false;
     var $g_info   = false;
     var $response = false;
-      
+
     function getvalue($srv_value, $srv_data)
     {
         // search the value of selected rule and return it
@@ -63,125 +63,125 @@ class bf2
 
     function splitdata()
     {
-	$g_end  = strpos($this->s_info, 'player_');
-	$g_info = substr($this->s_info, 0, $g_end);
-	$this->g_info = explode("\x00", $g_info);
-	
-	// now get the player data
-	$p_end  = strlen($this->s_info);
-	$p_info = substr($this->s_info, $g_end, $p_end);
-	
-	$this->p_info = $p_info;
+        $g_end  = strpos($this->s_info, 'player_');
+        $g_info = substr($this->s_info, 0, $g_end);
+        $this->g_info = explode("\x00", $g_info);
+
+        // now get the player data
+        $p_end  = strlen($this->s_info);
+        $p_info = substr($this->s_info, $g_end, $p_end);
+
+        $this->p_info = $p_info;
     }
 
     function microtime_float()
     {
         list($usec, $sec) = explode(" ", microtime());
-        
+
         return ((float)$usec + (float)$sec);
     }
 
     function connect()
     {
         if (($this->socket = fsockopen('udp://'. $this->host, $this->port, $errno, $errstr, 30)))
-	{
-	    return true;
-	}
-	
-	return false;
+        {
+            return true;
+        }
+
+        return false;
     }
 
     function disconnect()
     {
         if (fclose($this->socket))
-	{
-	    return true;
-	}
+        {
+            return true;
+        }
 
-	return false;
+        return false;
     }
 
     function get_split($info)
     {
         $end = strpos($info, "num") +3;
-	$split = substr($info, $end+1, 2);
-	$number = ord($split);
-	
-	return $number;
+        $split = substr($info, $end+1, 2);
+        $number = ord($split);
+
+        return $number;
     }
 
     function rm_split($info)
     {
         $info = substr($info, 15);
-	
-	return $info;
+
+        return $info;
     }
-    
+
     function rm_end($info)
     {
         $reverse = '';
-	$cache   = '';
-	
+        $cache   = '';
+
         $cache = substr($info, 0, -1);
-	$end  = strlen($cache);
-        
-	while ($end != -1)
+        $end  = strlen($cache);
+
+        while ($end != -1)
         {
-	    if ($end--)
-	    {
+            if ($end--)
+            {
                 $reverse .= $cache[$end];
-	    }
-	}
-	
-	$cut = substr($reverse, strpos($reverse, "\x00"));
-        $end = strlen($cut);
-        
-	$info = '';
-        
-	while ($end != -1)
-        {
-	    if ($end--)
-	    {
-                $info .= $cut[$end];
-	    }
+            }
         }
-	
-	return $info;
+
+        $cut = substr($reverse, strpos($reverse, "\x00"));
+        $end = strlen($cut);
+
+        $info = '';
+
+        while ($end != -1)
+        {
+            if ($end--)
+            {
+                $info .= $cut[$end];
+            }
+        }
+
+        return $info;
     }
-    
+
     function get_status()
     {
-	$timeout = false;
-	$ready   = 0;
-        
-	$data[0] = false;
-	$data[1] = false;
-	$data[2] = false;
+        $timeout = false;
+        $ready   = 0;
 
-	$packets[0]   = false;
-	$packets[1]   = false;
-	$packets[128] = false;
-	$packets[129] = false;
-	$packets[130] = false;
-	
+        $data[0] = false;
+        $data[1] = false;
+        $data[2] = false;
+
+        $packets[0]   = false;
+        $packets[1]   = false;
+        $packets[128] = false;
+        $packets[129] = false;
+        $packets[130] = false;
+
         if ($this->connect() === false)
-	{
-	    return false;
-	}
-        
-	socket_set_timeout($this->socket, 3);
-	
-	$time_begin = $this->microtime_float();
-	fwrite($this->socket, $this->write);
+        {
+            return false;
+        }
 
-	$info = fread($this->socket, 20);
-	$time_end = $this->microtime_float();
-	$this->response = $time_end - $time_begin;
+        socket_set_timeout($this->socket, 3);
 
-	$cache = substr($info, 5, -1);
-	$cache = pack("H*", dechex($cache));
+        $time_begin = $this->microtime_float();
+        fwrite($this->socket, $this->write);
 
-	fwrite($this->socket, $this->write2 . $cache . $this->split);
+        $info = fread($this->socket, 20);
+        $time_end = $this->microtime_float();
+        $this->response = $time_end - $time_begin;
+
+        $cache = substr($info, 5, -1);
+        $cache = pack("H*", dechex($cache));
+
+        fwrite($this->socket, $this->write2 . $cache . $this->split);
 
         while ($ready == false && $timeout == false)
         {
@@ -221,8 +221,8 @@ class bf2
             {
                 $info = $this->rm_split($info);
                 $info = substr($info, strpos($info, "\x00")+1);
-                
-		$data[1] = $this->rm_end($info);
+
+                $data[1] = $this->rm_end($info);
 
                 $packets[1] = true;
 
@@ -289,20 +289,20 @@ class bf2
         $info = $data[0] . $data[1] . $data[2];
 
         if (empty($info) || $info > 100)
-	{
-	    return false;
-	}
+        {
+            return false;
+        }
 
-	// response time
-	$this->response = ($this->response * 1000);
-	$this->response = (int)$this->response;
-        
-	if ($this->disconnect() === false)
-	{
-	    return false;
-	}
+        // response time
+        $this->response = ($this->response * 1000);
+        $this->response = (int)$this->response;
 
-	return $info;
+        if ($this->disconnect() === false)
+        {
+            return false;
+        }
+
+        return $info;
     }
 
    function getstream($host, $port, $queryport)
@@ -315,22 +315,22 @@ class bf2
         {
             $this->port = $queryport;
         }
-        
+
         $this->host = $host;
 
         // get the infostream from server
-	$this->s_info = $this->get_status();
-        
-	if ($this->s_info)
-	{
-	    $this->splitdata();
-        	
-	    return true;
-	}
-	else
-	{
-	    return false;
-	}
+        $this->s_info = $this->get_status();
+
+        if ($this->s_info)
+        {
+            $this->splitdata();
+
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
     function check_color($text)
@@ -373,63 +373,76 @@ class bf2
 
             return $ctext;
     }
-    
+
     function getrules($phgdir)
     {
         $srv_rules['sets'] = false;
-	
+
         // response time
-	$srv_rules['response'] = $this->response . ' ms';
-	
+        $srv_rules['response'] = $this->response . ' ms';
+
         // bfv setting pics
-	$sets['pb']      = '<img src="' . $phgdir . 'privileges/pb.gif" alt="pb">';
-	$sets['pass']    = '<img src="' . $phgdir . 'privileges/pass.gif" alt="pw">';
-        
-	// get the info strings from server info stream
-	$srv_rules['hostname']    = $this->getvalue('hostname',      $this->g_info);
-	$srv_rules['gametype']    = $this->getvalue('gametype',      $this->g_info);
-	$srv_rules['gamevariant'] = $this->getvalue('gamevariant',   $this->g_info);
-	$srv_rules['version']     = $this->getvalue('gamever',       $this->g_info);
-	$srv_rules['mapname']     = $this->getvalue('mapname',       $this->g_info);
-	$srv_rules['maxplayers']  = $this->getvalue('maxplayers',    $this->g_info);
-	$srv_rules['anticheat']   = $this->getvalue('bf2_anticheat', $this->g_info);
-	$srv_rules['needpass']    = $this->getvalue('password',      $this->g_info);
-        
-	// path to map picture and default info picture
-	if ($srv_rules['gamevariant'] == 'bf2')
-	{
-	    $srv_rules['map_path'] = 'maps/bf2';
-	    $srv_rules['gamename'] = 'Battlefield 2';
-	}
-	else
-	{
-	    $srv_rules['map_path'] = 'maps/bf2142';
-	    $srv_rules['gamename'] = 'Battlefield 2142';
-	}
+        $sets['pb']      = '<img src="' . $phgdir . 'privileges/pb.gif" alt="pb">';
+        $sets['pass']    = '<img src="' . $phgdir . 'privileges/pass.gif" alt="pw">';
 
-	$srv_rules['map_default'] = 'default.jpg';
-	
-	// get the connected player
-	$srv_rules['nowplayers'] = $this->getvalue('numplayers', $this->g_info);
-        
-	// complete the gamename
-	$srv_rules['gamename'] .= '<br>(' . $srv_rules['version'] .')';
-	
-	// server privileges
-	if ($srv_rules['needpass'] == 1)
-	{
-	    $srv_rules['sets'] .= $sets['pass'];
-	}
-	if ($srv_rules['anticheat'] == 1)
-	{
+        // get the info strings from server info stream
+        $srv_rules['hostname']    = $this->getvalue('hostname',      $this->g_info);
+        $srv_rules['gametype']    = $this->getvalue('gametype',      $this->g_info);
+        $srv_rules['gamevariant'] = $this->getvalue('gamevariant',   $this->g_info);
+        $srv_rules['version']     = $this->getvalue('gamever',       $this->g_info);
+        $srv_rules['mapname']     = $this->getvalue('mapname',       $this->g_info);
+        $srv_rules['maxplayers']  = $this->getvalue('maxplayers',    $this->g_info);
+        $srv_rules['anticheat']   = $this->getvalue('bf2_anticheat', $this->g_info);
+        $srv_rules['needpass']    = $this->getvalue('password',      $this->g_info);
+
+//  echo $srv_rules['gamevariant'];
+        // path to map picture and default info picture
+        if ($srv_rules['gamevariant'] == 'bf2')
+        {
+            $srv_rules['map_path'] = 'maps/bf2';
+            $srv_rules['gamename'] = 'Battlefield 2';
+        }
+        elseif ($srv_rules['gamevariant'] == 'bf2142')
+        {
+            $srv_rules['map_path'] = 'maps/bf2142';
+            $srv_rules['gamename'] = 'Battlefield 2142';
+        }
+        elseif ($srv_rules['gamevariant'] == 'airmaps' || $srv_rules['gamevariant'] == 'bfa' || $srv_rules['gamevariant'] == 'bfp2' || $srv_rules['gamevariant'] == 'bgf' || $srv_rules['gamevariant'] == 'bgf105' || $srv_rules['gamevariant'] == 'fallen_times' || $srv_rules['gamevariant'] == 'LAN_FUN_MOD' || $srv_rules['gamevariant'] == 'naw' || $srv_rules['gamevariant'] == 'sandbox' || $srv_rules['gamevariant'] == 'SR2' || $srv_rules['gamevariant'] == 'xpack' || $srv_rules['gamevariant'] == 'poe2' || $srv_rules['gamevariant'] == 'fh2')
+        {
+            $srv_rules['map_path'] = 'maps/bf2mod';
+            $srv_rules['gamename'] = 'Battlefield 2 Mod';
+        }
+        else
+        {
+            $srv_rules['map_path'] = 'maps/bf2mod';
+            $srv_rules['gamename'] = 'Battlefield - unbekannter Mod:';
+        }
+
+        $srv_rules['map_default'] = 'default.jpg';
+
+        // get the connected player
+        $srv_rules['nowplayers'] = $this->getvalue('numplayers', $this->g_info);
+
+        // complete the gamename
+         if ($srv_rules['gamevariant'] == 'bf2' || $srv_rules['gamevariant'] == 'bf2142')
+        {$srv_rules['gamename'] .= '<br>(' . $srv_rules['version'] .')';}
+         else {$srv_rules['gamename'] .= ' '.strtoupper($srv_rules['gamevariant']).'<br>('.$srv_rules['version'] .')';}
+
+        // server privileges
+        if ($srv_rules['needpass'] == 1)
+        {
+            $srv_rules['sets'] .= $sets['pass'];
+        }
+        if ($srv_rules['anticheat'] == 1)
+        {
             $srv_rules['sets'] .= $sets['pb'];
-	}
-	if ($srv_rules['sets'] === false)
-	{
-	    $srv_rules['sets'] = '-';
-	}
+        }
+        if ($srv_rules['sets'] === false)
+        {
+            $srv_rules['sets'] = '-';
+        }
 
-	// server general info (added by balgo 05.01.2006 only for test)
+        // server general info (added by balgo 05.01.2006 only for test)
         $srv_rules['htmlinfo'] =
         '<tr><td align="left">Mapname:</td><td align="left">'
         . $srv_rules['mapname']
@@ -466,121 +479,121 @@ class bf2
         . '</td></tr>' . "\n";
 
         // return all server rules
-	return $srv_rules;	    
-    }    
-    
+        return $srv_rules;
+    }
+
     function getplayers()
     {
         $players = array();
-	$clients = 0;
-	
-	// set html thead for playerlist without teams
-	$thead = '<tr><th>Rank</th>' .
-	         '<th>Name</th>'     .
-		 '<th>Score</th>'    .
-		 '<th>Team</th>'     .
-		 '<th>Death</th>'    .
-		 '<th>Skill</th>'    .
-		 '<th>Ping</th></tr>';
-	
+        $clients = 0;
+
+        // set html thead for playerlist without teams
+        $thead = '<tr><th>Rank</th>' .
+                 '<th>Name</th>'     .
+                 '<th>Score</th>'    .
+                 '<th>Team</th>'     .
+                 '<th>Death</th>'    .
+                 '<th>Skill</th>'    .
+                 '<th>Ping</th></tr>';
+
         // how many players must search
         $nowplayers = $this->getvalue('numplayers', $this->g_info);
-	if ($nowplayers == 0)
-	{
-	    $thead .=
-	    '<tr align=center><td>--</td>' .
-	    '<td>--</td>' .
-	    '<td>--</td>' .
-	    '<td>--</td>' .
-	    '<td>--</td>' .
-	    '<td>--</td>' .
-	    '<td>--</td></tr>' . "\n";	
-	}
-	else
-	{
-	    $nowplayers = $nowplayers - 1;
-        
-	    // sort the data to can scan it better
-	    $cache    = explode ("\x00\x00", $this->p_info);
+        if ($nowplayers == 0)
+        {
+            $thead .=
+            '<tr align=center><td>--</td>' .
+            '<td>--</td>' .
+            '<td>--</td>' .
+            '<td>--</td>' .
+            '<td>--</td>' .
+            '<td>--</td>' .
+            '<td>--</td></tr>' . "\n";
+        }
+        else
+        {
+            $nowplayers = $nowplayers - 1;
+
+            // sort the data to can scan it better
+            $cache    = explode ("\x00\x00", $this->p_info);
 
             $c_pl     = explode("\x00", $cache[1]);
-	    $c_score  = explode("\x00", $cache[3]);
-	    $c_ping   = explode("\x00", $cache[5]);
-	    $c_team   = explode("\x00", $cache[7]);
-	    $c_deaths = explode("\x00", $cache[9]);
-	    $c_pid    = explode("\x00", $cache[11]);
-	    $c_skill  = explode("\x00", $cache[13]);
-	
+            $c_score  = explode("\x00", $cache[3]);
+            $c_ping   = explode("\x00", $cache[5]);
+            $c_team   = explode("\x00", $cache[7]);
+            $c_deaths = explode("\x00", $cache[9]);
+            $c_pid    = explode("\x00", $cache[11]);
+            $c_skill  = explode("\x00", $cache[13]);
+
             // get the data of each player and add the team status
             while ($nowplayers != -1)
-            {   
-	        $pl        = $c_pl[$nowplayers];
-	        $pl_score  = $c_score[$nowplayers];
-	        $pl_ping   = $c_ping[$nowplayers];
-	        $pl_team   = $c_team[$nowplayers];
-	        $pl_deaths = $c_deaths[$nowplayers];
-	        $pl_pid    = $c_pid[$nowplayers];
-	        $pl_skill  = $c_skill[$nowplayers];
-	    
-	        // no player data
-	        if ($pl == '')
-	        {
-	            $pl        = '-';
-		    $pl_score  = '-';
-		    $pl_ping   = '-';
-		    $pl_team   = '-';
-		    $pl_deaths = '-';
-		    $pl_pid    = '-';
-		    $pl_skill  = '-';
-	        }
-	        else
-	        {
-	            $players[$clients] =
-		        $pl_score  . ' ' . 
-	                $pl_team   . ' ' .
-		        $pl_deaths . ' ' .
-		        $pl_skill  . ' ' .
-		        $pl_ping   . ' ' .
-		        "\"$pl\"";
+            {
+                $pl        = $c_pl[$nowplayers];
+                $pl_score  = $c_score[$nowplayers];
+                $pl_ping   = $c_ping[$nowplayers];
+                $pl_team   = $c_team[$nowplayers];
+                $pl_deaths = $c_deaths[$nowplayers];
+                $pl_pid    = $c_pid[$nowplayers];
+                $pl_skill  = $c_skill[$nowplayers];
+
+                // no player data
+                if ($pl == '')
+                {
+                    $pl        = '-';
+                    $pl_score  = '-';
+                    $pl_ping   = '-';
+                    $pl_team   = '-';
+                    $pl_deaths = '-';
+                    $pl_pid    = '-';
+                    $pl_skill  = '-';
                 }
-		
-	        $nowplayers--;
-	        $clients++;
-	    }
-	    sort($players, SORT_NUMERIC);
-	}
+                else
+                {
+                    $players[$clients] =
+                        $pl_score  . ' ' .
+                        $pl_team   . ' ' .
+                        $pl_deaths . ' ' .
+                        $pl_skill  . ' ' .
+                        $pl_ping   . ' ' .
+                        "\"$pl\"";
+                }
 
-	// store the html table line to the info array
-	$srv_player = $thead;
-        
-	// manage the player data in the following code
-	$index = 1;
+                $nowplayers--;
+                $clients++;
+            }
+            sort($players, SORT_NUMERIC);
+        }
 
-	while ($clients != 0)
-	{
-	     $clients--;
-	     
-	     list ($cache[$index], $player[$index]) = split ('\"', $players[$clients]);
-	     list ($score[$index],
-		   $team[$index],
-		   $deaths[$index],
-		   $skill[$index],
-		   $ping[$index])  = split (' ',  $cache[$index]);
-             
-	     $player[$index] = htmlentities($player[$index]);
-	     $ping[$index]   = $this->check_color($ping[$index]);
-	     
-	     $tdata = "<tr align=center><td>$index.</td>" .
-	              "<td>$player[$index]</td>" .
-	              "<td>$score[$index]</td>"  .
-		      "<td>$team[$index]</td>"  .
-		      "<td>$deaths[$index]</td>"    .
-		      "<td>$skill[$index]</td>"  .
-	              "<td>$ping[$index]</td></tr>\n";
-	                  
-	     $srv_player = $srv_player . $tdata;
-	     $index++;
-	}
+        // store the html table line to the info array
+        $srv_player = $thead;
+
+        // manage the player data in the following code
+        $index = 1;
+
+        while ($clients != 0)
+        {
+             $clients--;
+
+             list ($cache[$index], $player[$index]) = split ('\"', $players[$clients]);
+             list ($score[$index],
+                   $team[$index],
+                   $deaths[$index],
+                   $skill[$index],
+                   $ping[$index])  = split (' ',  $cache[$index]);
+
+             $player[$index] = htmlentities($player[$index]);
+             $ping[$index]   = $this->check_color($ping[$index]);
+
+             $tdata = "<tr align=center><td>$index.</td>" .
+                      "<td>$player[$index]</td>" .
+                      "<td>$score[$index]</td>"  .
+                      "<td>$team[$index]</td>"  .
+                      "<td>$deaths[$index]</td>"    .
+                      "<td>$skill[$index]</td>"  .
+                      "<td>$ping[$index]</td></tr>\n";
+
+             $srv_player = $srv_player . $tdata;
+             $index++;
+        }
 
         return $srv_player;
     }
