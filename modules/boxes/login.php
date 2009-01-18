b7c6c25dfa1ec374a9c622bcd7e306ac00ec44d4
maztah <maztah@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2009-01-18 17:46:03 +0000

    title gesetzt

--- modules/boxes/login.php
+++ modules/boxes/login.php
@@ -9,8 +9,8 @@
 
 if ($_GET['mod'] == 'logout') $smarty->assign('action', 'index.php');
 else $smarty->assign('action', 'index.php?mod=auth&action=login');
-$smarty->assign('buttons_add', $dsp->FetchIcon('index.php?mod=signon', 'add_user'));
-$smarty->assign('buttons_pw', $dsp->FetchIcon('index.php?mod=usrmgr&amp;action=pwrecover', 'pw_forgot'));
+$smarty->assign('buttons_add', $dsp->FetchIcon('index.php?mod=signon', 'add_user', t('Registrieren')));
+$smarty->assign('buttons_pw', $dsp->FetchIcon('index.php?mod=usrmgr&amp;action=pwrecover', 'pw_forgot',t('Passwort vergessen')));
 $smarty->assign('buttons_login', '<input type="submit" class="Button" name="login" value="Einloggen" />');
 
 $box->AddTemplate($smarty->fetch('modules/boxes/templates/box_login_content.htm'));
