427a3e668c2c6230217b4892dd17980dc1aa24c9
Jochen.Jung <Jochen.Jung@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2009-02-04 20:01:03 +0000

    http://lansuite.orgapage.de/index.php?mod=bugtracker&bugid=857

--- trunk/modules/stats/database.php
+++ trunk/modules/stats/database.php
@@ -14,15 +14,12 @@
 *
 **************************************************************************/
 
-$status = explode(' ', $db->stat());
 $hostname = explode(' ', $db->get_host_info());
-$uptime = explode(".", round($status[1] / 60 / 60, 2));
 
 $dsp->NewContent(t('Datenbank Statistik'), t('Auf dieser Seite erhalten Sie Informationen &uuml;ber die Datenbank und aktuelle Leistungsdaten.'));
 
 $dsp->AddDoubleRow(t('Servername'), $hostname[0]);
 $dsp->AddDoubleRow(t('Verbindungstyp'), $hostname[2]);
-$dsp->AddDoubleRow(t('L&auml;uft seit'), $uptime[0] . " " . t('Stunde(n)') . " " . round($uptime[1]*0.6) . " " . t('Minute(n)'));
 $dsp->AddDoubleRow(t('Abfragen Insg.'), $status[7]);
 $dsp->AddDoubleRow(t('Abfragen / Sekunde'), round($status[7] / $status[1], 3)); // $status[27]
 $dsp->AddDoubleRow(t('Offene Tabellen'), $status[22]);
