8f3c028dfbd13eb10666d7fb4fd8d87117039f59
webmaster@kiddiboy.de <webmaster@kiddiboy.de@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2009-02-17 02:04:42 +0000

    Clanverwaltung einfacher http://lansuite.orgapage.de/index.php?mod=bugtracker&bugid=870 Neue Funktionen: - Clans erstellen aus dem Menü und Clanübersicht heraus (nur wenn man aktuell keinem Clan angehört) - Clanlogo kann hochgeladen werden - Clans können kommentiert werden - Clans kann nun auch auf der Clandetailansicht beigetreten werden - Clans verlassen auch auf der Clandetailansicht - Clans editieren auch auf der Clandetailansicht (für Clan-Admins) - Wenn Clan vorhanden, wird ein Link unter "Meine Daten" angezeigt

--- trunk/modules/boxes/userdata.php
+++ trunk/modules/boxes/userdata.php
@@ -52,6 +52,9 @@ if (isset($_POST['login']) and isset($_POST['password'])) {
 
 // Show other links
 $box->DotRow(t('Meine Einstellungen'), "index.php?mod=usrmgr&amp;action=settings", '', "menu");
+// Show Clan
+if(($auth['clanid'] != NULL and $auth['clanid'] > 0) and in_array('clanmgr', $ActiveModules))
+$box->DotRow(t('Mein Clan'), "index.php?mod=clanmgr&amp;step=2&clanid=".$auth['clanid'], '', "menu");
 
 // New-Mail Notice
 if (in_array('mail', $ActiveModules)) {
