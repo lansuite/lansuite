3a5af923cc88a33795ad38d6b0caa031f196f860
webmaster@kiddiboy.de <webmaster@kiddiboy.de@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2009-02-26 16:02:06 +0000

    Bildgröße in der Signatur auf max 468px (Breite eines Standart Banners) von 450py geändert.
    Zukünftig sollte die max-width und max-height Anweisung in die Stylesheet Datei ausgelagert werden.

--- inc/classes/class_func.php
+++ inc/classes/class_func.php
@@ -383,7 +383,7 @@ class func {
           $img_start2 = '<img src="ext_inc/smilies/';
           $img_end   = '" border="0" alt="" />';
   
-          $string = preg_replace('#\\[img\\]([^[]*)\\[/img\\]#sUi', '<img src="\1" border="0" class="img" alt="" style="max-width:450px; max-height:450px; overflow:hidden;" />', $string);
+          $string = preg_replace('#\\[img\\]([^[]*)\\[/img\\]#sUi', '<img src="\1" border="0" class="img" alt="" style="max-width:468px; max-height:450px; overflow:hidden;" />', $string);
           $string = preg_replace('#\\[url=(index\.php\?[^\\]]*)\\]([^[]*)\\[/url\\]#sUi', '<a href="\\1" rel="nofollow">\\2</a>', $string);
           $string = preg_replace('#\\[url=([^\\]]*)\\]([^[]*)\\[/url\\]#sUi', '<a target="_blank" href="\\1" rel="nofollow">\\2</a>', $string);
     
