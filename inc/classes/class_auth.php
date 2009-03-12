61f02c7448ca747491964a448d7b14d8c2318240
webmaster@kiddiboy.de <webmaster@kiddiboy.de@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2009-03-12 16:02:55 +0000

    Errormeldung board2 während der installation

--- inc/classes/class_auth.php
+++ inc/classes/class_auth.php
@@ -273,11 +273,14 @@ class auth {
     		$userid = $this->auth['userid'];
 
 		// The User will be logged in on the phpBB Board if the modul is available, configured and active.
-        if (in_array('board2', $ActiveModules) and $config["board2"]["configured"]) {
-			include_once ('./modules/board2/class_board2.php');
-			$board2 = new Board2();
-			$board2->loginPhpBB($userid);
-		}
+        if ($config['environment']['configured'])
+        {
+	        if (in_array('board2', $ActiveModules) and $config["board2"]["configured"]) {
+				include_once ('./modules/board2/class_board2.php');
+				$board2 = new Board2();
+				$board2->loginPhpBB($userid);
+			}
+        }
     }
     
 	/**
