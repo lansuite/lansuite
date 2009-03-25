d9050e91cf01a0048dcc946b218a4bb200b195d4
webmaster@kiddiboy.de <webmaster@kiddiboy.de@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2009-03-25 14:50:08 +0000

    Feature Wunsch: Mails entgültig löschen: - zwei neue deleted Flags - Mails können aus dem Postausgang und dem Papierkorb gelöscht werden - Wenn Empfänger und Sender die Mail löschen, wird sie komplett aus der DB entfernt
    http://lansuite.orgapage.de/index.php?mod=bugtracker&bugid=70

--- trunk/modules/mail/outbox.php
+++ trunk/modules/mail/outbox.php
@@ -5,11 +5,30 @@ $mail_read_total = $db->qry_first("SELECT count(*) as n FROM %prefix%mail_messag
 $dsp->NewContent(t('Postausgang'), t('Sie haben <b>%1</b> Mail(s) versendet. Davon wurde(n) <b>%2</b> gelesen.',$mail_send_total["n"],$mail_read_total["n"]));
 $dsp->AddContent();
 
+
+if ($auth['userid']) {
+  switch($_GET['step']) {
+    // check if it can delete from Database and delete
+    case 20:
+      if (!$_POST['action'] and $_GET['mailid']) $_POST['action'][$_GET['mailid']] = 1;
+      foreach ($_POST['action'] as $key => $val){
+      	$rx_status = $db->qry_first("SELECT rx_deleted FROM %prefix%mail_messages WHERE mailID = %int%", $key);
+      	
+      	// Ist eMail vom Sender gelöscht? JA: Lösche aus DB, NEIN: Setze rx flag
+      	if($rx_status['rx_deleted'])
+      		$db->qry("DELETE FROM %prefix%mail_messages WHERE mailID = %int%", $key);
+      	else
+      		$db->qry("UPDATE %prefix%mail_messages SET tx_deleted = 1 WHERE mailID = %int%", $key);
+      }
+    break;
+  }
+}
+
 include_once('modules/mastersearch2/class_mastersearch2.php');
 $ms2 = new mastersearch2();
 
 $ms2->query['from'] = "{$config["tables"]["mail_messages"]} AS m LEFT JOIN {$config["tables"]["user"]} AS u ON m.ToUserID = u.userid";
-$ms2->query['where'] = "m.FromUserID = '{$auth['userid']}' AND m.mail_status != 'disabled'";
+$ms2->query['where'] = "m.FromUserID = '{$auth['userid']}' AND m.mail_status != 'disabled' AND tx_deleted = 0";
 $ms2->query['default_order_by'] = 'm.tx_date';
 $ms2->query['default_order_dir'] = 'DESC';
 
@@ -25,6 +44,11 @@ $ms2->AddResultField(t('Gesendet'), 'UNIX_TIMESTAMP(m.tx_date) AS tx_date', 'MS2
 $ms2->AddResultField(t('Gelesen'), 'UNIX_TIMESTAMP(m.rx_date) AS rx_date', 'MS2GetDate','',45);
 
 $ms2->AddIconField('details', 'index.php?mod=mail&action=showmail&ref=out&mailID=', t('Details'));
+$ms2->AddIconField('delete', 'index.php?mod=mail&action=outbox&step=20&mailid=', t('Entgültig löschen'));
+
+
+$ms2->AddMultiSelectAction(t('Entgültig löschen'), 'index.php?mod=mail&action=outbox&step=20', 1, 'delete');
+
 
 $ms2->PrintSearch('index.php?mod=mail&action=outbox', 'm.mailid');
 ?>
