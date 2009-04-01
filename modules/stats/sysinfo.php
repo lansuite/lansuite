6d635c8b98ac39b585d09cd49ab6776ff81715a7
schmitt-marc@t-online.de <schmitt-marc@t-online.de@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2009-04-01 01:38:26 +0000

    Messenger-Umbau: CSS-Styles und Grafiken für Tabsfunktionen (Tabs derzeit nur statisch nutzbar)

--- modules/stats/sysinfo.php
+++ modules/stats/sysinfo.php
@@ -1,8 +1,8 @@
 <?php
-$sysinfo_ramusage=66;
-$sysinfo_ramtotal=535740416;
-$sysinfo_ramfree=181866496;
+$sysinfo_ramusage=90;
+$sysinfo_ramtotal=1073201152;
+$sysinfo_ramfree=103960576;
 $sysinfo_virtualtotal=2147352576;
-$sysinfo_virtualfree=2120859648;
-$sysinfo_cpuusage=28;
+$sysinfo_virtualfree=2123341824;
+$sysinfo_cpuusage=100;
 ?>
