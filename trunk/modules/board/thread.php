b34ac7055740964b287d64c4e387d99aed4f695a
webmaster@kiddiboy.de <webmaster@kiddiboy.de@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2009-07-16 22:04:18 +0000

    Nochmal Bugfix Board Spam: Thread Erstellen Maske konnte durch übergabe einer tid trotz limits angezeigt werden

--- trunk/modules/board/thread.php
+++ trunk/modules/board/thread.php
@@ -52,13 +52,19 @@ if ($auth['type'] >= 2) switch ($_GET['step']) {
 
 $tid = (int)$_GET["tid"];
 $list_type = $auth['type'] + 1;
-$thread = $db->qry_first("SELECT t.fid, t.caption, t.closed, f.name AS ForumName, f.need_type, f.need_group FROM %prefix%board_threads AS t
-  LEFT JOIN %prefix%board_forums AS f ON t.fid = f.fid
-  WHERE t.tid=%int% AND f.need_type <= %string% AND (!f.need_group OR f.need_group = %int%)", $tid, $list_type, $auth['group_id']);
 
-//$forum = $db->qry_first("SELECT need_type, need_group FROM %prefix%board_forums WHERE fid = %int%", $_GET['fid']);
+//Show Thread or create new
+if($tid) {
+	$thread = $db->qry_first("SELECT t.fid, t.caption, t.closed, f.name AS ForumName, f.need_type, f.need_group FROM %prefix%board_threads AS t
+	  LEFT JOIN %prefix%board_forums AS f ON t.fid = f.fid
+	  WHERE t.tid=%int% AND f.need_type <= %string% AND (!f.need_group OR f.need_group = %int%)", $tid, $list_type, $auth['group_id']);
+	  
+	  if ($_GET['pid'] != '') $current_post = $db->qry_first("SELECT userid FROM %prefix%board_posts WHERE pid = %int%", $_GET['pid']);
+}
+else
+	$thread = $db->qry_first("SELECT need_type, need_group FROM %prefix%board_forums WHERE fid = %int%", $_GET['fid']);
 
-if ($thread['caption'] == '' and $tid) $func->information(t('Keine Beiträge vorhanden'), '');
+if (!$thread and $tid) $func->information(t('Keine Beiträge vorhanden'), '');
 elseif ($thread['caption'] != '') {
 
 	$fid = $thread["fid"];
@@ -163,8 +169,6 @@ elseif ($thread['caption'] != '') {
 	$dsp->AddContent();
 }
 
-if ($_GET['pid'] != '') $current_post = $db->qry_first("SELECT userid FROM %prefix%board_posts WHERE pid = %int%", $_GET['pid']);
-
 if ($thread['closed']) $func->information(t('Dieser Thread wurde geschlossen. Es können keine Antworten mehr geschrieben werden'), NO_LINK);
 elseif ($thread['need_type'] >= 1 and !$auth['login'] and !$_GET['tid']) $func->information(t('Sie müssen sich zuerst einloggen, um einen Thread in diesem Forum starten zu können'), NO_LINK);
 elseif ($thread['need_type'] >= 1 and !$auth['login'] and $_GET['tid']) $func->information(t('Um in diesem Board zu posten zu antworten, loggen Sie sich bitte zuerst ein.'), NO_LINK);
@@ -172,7 +176,7 @@ elseif ($thread['need_type'] > (int)($auth['type'] + 1)) $func->information(t('U
 elseif ($thread['need_group'] and $auth['group_id'] != $thread['need_group'] and $_GET['tid']) $func->information(t('Sie gehören nicht der richtigen Gruppe an, um auf diese Beiträge zu antworten.'), NO_LINK); 
 elseif ($thread['need_group'] and $auth['group_id'] != $thread['need_group'] and !$_GET['tid']) $new_thread = t('Sie gehören nicht der richtigen Gruppe an, um einen Thread in diesem Forum starten zu können');
 elseif ($_GET['pid'] != '' and $auth['type'] <= 1 and $current_post['userid'] != $auth['userid']) $func->error('Sie dürfen nur Ihre eigenen Beiträge editieren!', NO_LINK);
-else {
+elseif ($thread) {
 	//Topic erstellen oder auf Topic antworten
   if($_GET['tid']) $dsp->AddFieldsetStart(t('Antworten - Der Beitrag kann anschließend noch editiert werden'));
   else	$dsp->AddFieldsetStart(t('Thread erstellen'));
