b7d725e88c48b2eeac7b7969ff5f745c2aafb600
jochen.jung <jochen.jung@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2009-11-06 13:28:20 +0000

    http://lansuite.orgapage.de/index.php?mod=bugtracker&bugid=921

--- trunk/modules/stats/database.php
+++ trunk/modules/stats/database.php
@@ -17,13 +17,17 @@
 $hostname = explode(' ', $db->get_host_info());
 
 $dsp->NewContent(t('Datenbank Statistik'), t('Auf dieser Seite erhalten Sie Informationen &uuml;ber die Datenbank und aktuelle Leistungsdaten.'));
-
 $dsp->AddDoubleRow(t('Servername'), $hostname[0]);
 $dsp->AddDoubleRow(t('Verbindungstyp'), $hostname[2]);
-$dsp->AddDoubleRow(t('Abfragen Insg.'), $status[7]);
-$dsp->AddDoubleRow(t('Abfragen / Sekunde'), round($status[7] / $status[1], 3)); // $status[27]
-$dsp->AddDoubleRow(t('Offene Tabellen'), $status[22]);
-$dsp->AddDoubleRow(t('Threads'), $status[4]);
+
+$res = $db->qry('SHOW status');
+while ($row = $db->fetch_array($res)) $dsp->AddDoubleRow($row[0], $row[1]);
+$db->free_result($res);
+
+#$dsp->AddDoubleRow(t('Abfragen Insg.'), $status[7]);
+#$dsp->AddDoubleRow(t('Abfragen / Sekunde'), round($status[7] / $status[1], 3)); // $status[27]
+#$dsp->AddDoubleRow(t('Offene Tabellen'), $status[22]);
+#$dsp->AddDoubleRow(t('Threads'), $status[4]);
 
 $dsp->AddBackButton("index.php?mod=stats", "stats/db");
 $dsp->AddContent();
