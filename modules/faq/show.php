3341e1e4fc18c373970bcfee1cbbd9abd7d2745f
jochen.jung <jochen.jung@gmail.com>
2009-11-06 18:48:02 +0000

    Kleine Schönheitskorrekturen FAQ-Modul

--- modules/faq/show.php
+++ modules/faq/show.php
@@ -8,15 +8,15 @@ else {
 
 	while ($row = $db->fetch_array($get_cat)) {
 
-	  if ($auth['type'] > 1) $admin_link = $dsp->FetchIcon('index.php?mod=faq&object=cat&action=change_cat&catid='. $row["catid"] .'&step=2', 'edit');
-	  if ($auth['type'] > 2) $admin_link .= $dsp->FetchIcon('index.php?mod=faq&object=item&action=delete_cat&catid='. $row["catid"] .'&step=2', 'delete');
-    $dsp->AddFieldsetStart($row["name"] . $admin_link);
+	  if ($auth['type'] > 2) $admin_link = $dsp->FetchIcon('index.php?mod=faq&object=item&action=delete_cat&catid='. $row["catid"] .'&step=2', 'delete');
+	  if ($auth['type'] > 1) $admin_link .= $dsp->FetchIcon('index.php?mod=faq&object=cat&action=change_cat&catid='. $row["catid"] .'&step=2', 'edit');
+    $dsp->AddFieldsetStart($admin_link . $row["name"]);
 
   	$get_item = $db->qry("SELECT caption,itemid FROM %prefix%faq_item WHERE catid = %int% ORDER BY caption", $row['catid']);
 		while ($row = $db->fetch_array($get_item)) {
-		  if ($auth['type'] > 1) $admin_link = $dsp->FetchIcon('index.php?mod=faq&object=cat&action=change_item&itemid='. $row["itemid"] .'&step=2', 'edit');
-		  if ($auth['type'] > 2) $admin_link .= $dsp->FetchIcon('index.php?mod=faq&object=item&action=delete_item&itemid='. $row["itemid"] .'&step=2', 'delete');
-    	$dsp->AddSingleRow($dsp->FetchLink($func->text2html($row["caption"]), 'index.php?mod=faq&action=comment&itemid='. $row["itemid"]) . $admin_link);
+		  if ($auth['type'] > 2) $admin_link = $dsp->FetchIcon('index.php?mod=faq&object=item&action=delete_item&itemid='. $row["itemid"] .'&step=2', 'delete');
+		  if ($auth['type'] > 1) $admin_link .= $dsp->FetchIcon('index.php?mod=faq&object=cat&action=change_item&itemid='. $row["itemid"] .'&step=2', 'edit');
+    	$dsp->AddSingleRow($admin_link . $dsp->FetchLink($func->text2html($row["caption"]), 'index.php?mod=faq&action=comment&itemid='. $row["itemid"]));
 		}
 		$dsp->AddFieldsetEnd();
 	}
