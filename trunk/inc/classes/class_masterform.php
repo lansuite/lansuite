64f47654207a651ca84f7ad28cb7d33d23bf28dd
jochen.jung <jochen.jung@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2009-11-06 13:11:46 +0000

    http://lansuite.orgapage.de/index.php?mod=bugtracker&bugid=880

--- trunk/inc/classes/class_masterform.php
+++ trunk/inc/classes/class_masterform.php
@@ -513,7 +513,7 @@ class masterform {
               default: // Normal Textfield
                 ($field['type'] == IS_NOT_CHANGEABLE)? $not_changeable = 1 : $not_changeable = 0;
                 $maxlength = $this->get_fieldlenght($field['type']);
-                ($maxlength < 70)? $length = $maxlength + (5-($maxlength % 5)) : $length = 70;
+                ($maxlength > 0 and $maxlength < 70)? $length = $maxlength + (5-($maxlength % 5)) : $length = 70;
                 $dsp->AddTextFieldRow($field['name'], $field['caption'], $_POST[$field['name']], $this->error[$field['name']], $length, $field['optional'], $not_changeable, $maxlength);
               break;
             }
