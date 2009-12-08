2c7f30e71bdd42c0b4cdf6946cb22b51d1db910a
Jochen.Jung <jochen.jung@gmail.com>
2009-12-08 19:50:12 +0000

    Poll zeigt nur noch nicht geschlossene Polls

--- modules/home/poll.inc.php
+++ modules/home/poll.inc.php
@@ -5,11 +5,11 @@ $content = "";
 $query = $db->qry('SELECT UNIX_TIMESTAMP(p.endtime) AS endtime, p.pollid, p.caption, COUNT(v.polloptionid) AS votes, MAX(UNIX_TIMESTAMP(p.changedate)) AS changedate FROM %prefix%polls AS p
   LEFT JOIN %prefix%polloptions AS o on p.pollid = o.pollid
   LEFT JOIN %prefix%pollvotes AS v on o.polloptionid = v.polloptionid
-  WHERE !p.group_id OR p.group_id = %int%
+  WHERE (!p.group_id OR p.group_id = %int%) AND (p.endtime <= \'1970-01-01 01:00:00\' OR p.endtime > %string%)
   GROUP BY p.pollid
   ORDER BY changedate DESC
   LIMIT 0, %int%
-  ', $auth['group_id'], $cfg['home_item_cnt_poll']);
+  ', $auth['group_id'], date("Y-m-d 00:00:00"), $cfg['home_item_cnt_poll']);
 
 if ($db->num_rows($query) > 0) while($row = $db->fetch_array($query)) {
   $smarty->assign('link', 'index.php?mod=poll&action=show&step=2&pollid='. $row['pollid']);
