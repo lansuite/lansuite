98793b8479ec48491138aa059cee5f8d337d1c03
Jochen.Jung <jochen.jung@gmail.com>
2010-01-02 20:06:33 +0000

    - Such-Box gefixt - Forum-Springen über Dropdown verbessert

--- modules/board/forum.php
+++ modules/board/forum.php
@@ -220,7 +220,7 @@ if ($_GET['fid'] and $auth['login']) {
 
 // Generate Boardlist-Dropdown
 $foren_liste = $db->qry("SELECT fid, name FROM %prefix%board_forums WHERE need_type <= %int% AND (!need_group OR need_group = %int%)", ($auth['type'] + 1), $auth['group_id']);
-while ($forum = $db->fetch_array($foren_liste)) $goto .= "<option value=\"index.php?mod=board&action=forum&fid={$forum["fid"]}\">{$forum["name"]}</option>";
+while ($forum = $db->fetch_array($foren_liste)) $goto .= "<option value=\"{$forum["fid"]}\">{$forum["name"]}</option>";
 $smarty->assign('goto', $goto);
 $smarty->assign('forum_choise', t('Bitte auswählen'));
 $dsp->AddDoubleRow(t('Gehe zu Forum'), $smarty->fetch('modules/board/templates/forum_dropdown.htm'));
--- modules/install/boxes/search.php
+++ modules/install/boxes/search.php
@@ -10,23 +10,23 @@
 $smarty->assign('value', $_POST['search_input'][0]);
 
 ($_GET['mod'] == 'usrmgr')? $s = 'selected' : $s = '';
-$module = "<option value=\"index.php?mod=guestlist&amp;action=guestlist\" $s>".t('Benutzer')."</option>\n";
+$module = "<option value=\"guestlist|guestlist\" $s>".t('Benutzer')."</option>\n";
 
 if ($func->isModActive('news')){
 	($_GET['mod'] == 'news')? $s = 'selected' : $s = '';
-	$module .= "<option value=\"index.php?mod=news&amp;action=search\" $s>".t('News')."</option>\n";
+	$module .= "<option value=\"news|search\" $s>".t('News')."</option>\n";
 }
 if ($func->isModActive('server')){
 	($_GET['mod'] == 'server')? $s = 'selected' : $s = '';
-	$module .= "<option value=\"index.php?mod=server\" $s>".t('server')."</option>\n";
+	$module .= "<option value=\"server\" $s>".t('server')."</option>\n";
 }
 if ($func->isModActive('board')){
 	($_GET['mod'] == 'board')? $s = 'selected' : $s = '';
-	$module .= "<option value=\"index.php?mod=board&amp;action=forum\" $s>".t('Thread')."</option>\n";
+	$module .= "<option value=\"board|forum\" $s>".t('Thread')."</option>\n";
 }
 if ($func->isModActive('troubleticket')){
 	($_GET['mod'] == 'troubleticket')? $s = 'selected' : $s = '';
-	$module .= "<option value=\"index.php?mod=troubleticket\" $s>".t('Troubletickets')."</option>\n";
+	$module .= "<option value=\"troubleticket\" $s>".t('Troubletickets')."</option>\n";
 }
 
 $smarty->assign('module', $module);
