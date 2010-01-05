5b0628ec51d118e1afd115c3c126e940f30d8e97
Jochen.Jung <Jochen.Jung@5e92b3a1-d83c-0410-a81a-451d556f9d80>
2010-01-05 12:28:01 +0000

    Nicht gefundene Module erzeugen nur noch eine information() und der Besucher wird zur startseite geleitet

--- trunk/index_module.inc.php
+++ trunk/index_module.inc.php
@@ -3,92 +3,88 @@
 // Info Seite blockiert
 if ($cfg['sys_blocksite'] == 1) $func->information($cfg['sys_blocksite_text'], "index.php?mod=install");
 
-// Check, if all required user data fields, are known and force user to add them, if not.
 $missing_fields = 0;
-if (func::admin_exists() and $auth['login'] and $auth['userid'] and $_GET["mod"] != 'install')
-    include_once ('modules/usrmgr/missing_fields.php');
+if ($func->admin_exists() and $_GET["mod"] != 'install') {
+  // Check, if all required user data fields, are known and force user to add them, if not.
+  if ($auth['login'] and $auth['userid']) include_once ('modules/usrmgr/missing_fields.php');
 
-// Set Mod = 'Home', if none selected
-if ($_GET['mod'] == '' or !$func->check_var($_GET['mod'], 'string', 0, 50))
-    $mod = 'home'; #($_GET['templ'] == 'install')? $mod = 'install' : $mod = 'home';
-else
-    $mod = $_GET['mod'];
+  // Reset $auth['type'], if no permission to Mod
+  if ($auth['type'] > 1) {
 
-// Reset $auth['type'], if no permission to Mod
-if (func::admin_exists() and $auth['type'] > 1 and $_GET["mod"] != 'install') {
+      // Has at least someone (with rights equal or above) access to this mod?
+      $permission = $db->qry_first("SELECT 1 AS found FROM %prefix%user_permissions AS p
+        LEFT JOIN %prefix%user AS u on p.userid = u.userid
+        WHERE p.module = %string% AND u.type >= %int%", $_GET['mod'], $auth['type']);
 
-    // Has at least someone (with rights equal or above) access to this mod?
-    $permission = $db->qry_first("SELECT 1 AS found FROM %prefix%user_permissions AS p
-      LEFT JOIN %prefix%user AS u on p.userid = u.userid
-      WHERE p.module = %string% AND u.type >= %int%", $mod, $auth['type']);
+      // If so: Has the current user access to this mod?
+      if ($permission['found']) {
+          $permission = $db->qry_first("SELECT 1 AS found FROM %prefix%user_permissions WHERE module = %string% AND userid = %int%", $_GET['mod'], $auth['userid']);
 
-    // If so: Has the current user access to this mod?
-    if ($permission['found']) {
-        $permission = $db->qry_first("SELECT 1 AS found FROM %prefix%user_permissions WHERE module = %string% AND userid = %int%", $mod, $auth['userid']);
-
-        // If not: Set his rights to user-rights
-        if (!$permission['found']) {
-            $auth['type'] = 1;
-            $_SESSION['auth']['type'] = 1;
-            $authentication->auth['type'] = 1;
-        }
-    }
+          // If not: Set his rights to user-rights
+          if (!$permission['found']) {
+              $auth['type'] = 1;
+              $_SESSION['auth']['type'] = 1;
+              $authentication->auth['type'] = 1;
+          }
+      }
+  }
 }
+    
+
 
 if ($cfg['sys_blocksite'] == 1 and $auth['type'] < 2 and $_GET['mod'] != 'info2') $siteblock = true;
 
 if (!$missing_fields and !$siteblock) {
-    switch ($mod) {
+    switch ($_GET['mod']) {
         case 'logout':
             $func->confirmation(t('Sie wurden erfolgreich ausgeloggt.'), '');
-            break;
+        break;
 
         case 'auth':
             $_GET['mod'] = 'home';
-            //$func->confirmation(t('auth.'), '');
-            break;
+        break;
 
         case 'install':
-            if ($IsAboutToInstall) {
-                include_once ('modules/install/wizard.php');
-                break;
-            }
+            if ($IsAboutToInstall) include_once ('modules/install/wizard.php');
+        break;
 
         default:
-            // If module is deactivated display error message
-            if (!$func->isModActive($mod)) $func->error(t('Das Modul %1 wurde deaktiviert und steht somit nicht zur Verfügung.', $mod));
+            // If module is deactivated display information message and redirect to home-mod
+            if (!$func->isModActive($_GET['mod'])) {
+              $row = $db->qry_first('SELECT caption FROM %prefix%modules WHERE name = %string%', $_GET['mod']);
+              if ($row['caption']) $func->information(t('Das Modul %1 wurde deaktiviert und steht somit nicht zur Verfügung. Sie wurden zur Startseite weitergeleitet', $row['caption']), NO_LINK);
+              else $func->information(t('Das Modul %1 existiert nicht. Überprüfen Sie, ob Sie die Adresse korrekt eingegeben haben. Sie wurden zur Startseite weitergeleitet', $_GET['mod']), NO_LINK);
+              $_GET['mod'] = 'home';
+            }
 
             //// Load Mod-Config
-            else {
-                // 1) Search $_GET['action'] in DB (field "action")
-                $menu = $db->qry_first("SELECT file, requirement FROM %prefix%menu WHERE (module = %string%) and (action = %string%)", $mod, $_GET['action']);
+            // 1) Search $_GET['action'] in DB (field "action")
+            $menu = $db->qry_first("SELECT file, requirement FROM %prefix%menu WHERE (module = %string%) and (action = %string%)", $_GET['mod'], $_GET['action']);
+            if ($menu['file'] != '') {
+                if ($authentication->authorized($menu['requirement']))
+                    include_once ("modules/{$_GET['mod']}/{$menu['file']}.php");
+
+            // 2) Search $_GET['action'] in DB (field "file")
+            } else {
+                $menu = $db->qry_first("SELECT file, requirement FROM %prefix%menu WHERE (module = %string%) and (file = %string%)", $_GET['mod'], $_GET['action']);
                 if ($menu['file'] != '') {
                     if ($authentication->authorized($menu['requirement']))
-                        include_once ("modules/{$mod}/{$menu['file']}.php");
+                        include_once ("modules/{$_GET['mod']}/{$menu['file']}.php");
 
-                    // 2) Search $_GET['action'] in DB (field "file")
+                // 3) Search file named $_GET['action'] in the Mod-Directory
+                } elseif (file_exists("modules/{$_GET['mod']}/{$_GET['action']}.php")) {
+                    if ($authentication->authorized($menu['requirement']))
+                        include_once ("modules/{$_GET['mod']}/{$_GET['action']}.php");
+
+                // 4) Search 'default'-Entry in DB
                 } else {
-                    $menu = $db->qry_first("SELECT file, requirement FROM %prefix%menu WHERE (module = %string%) and (file = %string%)", $mod, $_GET['action']);
+                    $menu = $db->qry_first("SELECT file, requirement FROM %prefix%menu WHERE (module = %string%) and (action = 'default')",$_GET['mod']);
                     if ($menu['file'] != '') {
                         if ($authentication->authorized($menu['requirement']))
-                            include_once ("modules/{$mod}/{$menu['file']}.php");
+                            include_once ("modules/{$_GET['mod']}/{$menu['file']}.php");
 
-                        // 3) Search file named $_GET['action'] in the Mod-Directory
-                    } elseif (file_exists("modules/$mod/{$_GET['action']}.php")) {
-                        if ($authentication->authorized($menu['requirement']))
-                            include_once ("modules/{$mod}/{$_GET['action']}.php");
-
-                        // 4) Search 'default'-Entry in DB
-                    } else {
-                        $menu = $db->qry_first("SELECT file, requirement FROM %prefix%menu WHERE (module = %string%) and (action = 'default')",$mod);
-                        if ($menu['file'] != '') {
-                            if ($authentication->authorized($menu['requirement']))
-                                include_once ("modules/{$mod}/{$menu['file']}.php");
-
-                            // 4) Error: 'Not Found'
-                        } else
-                            $func->error(t('Leider ist die von Ihnen aufgerufene Seite auf diesem Server nicht vorhanden.<br/>Um Fehler zu vermeiden, sollten Sie die URL nicht manuell ändern, sondern die Links benutzen. Wenn Sie die Adresse manuell eingegeben haben überprüfen Sie bitte die URL.'));
-                    }
+                    // 4) Error: 'Not Found'
+                    } else $func->error(t('Leider ist die von Ihnen aufgerufene Seite auf diesem Server nicht vorhanden.<br/>Um Fehler zu vermeiden, sollten Sie die URL nicht manuell ändern, sondern die Links benutzen. Wenn Sie die Adresse manuell eingegeben haben überprüfen Sie bitte die URL.'));
                 }
             }
             break;
