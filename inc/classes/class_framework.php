f4715ee7f5d664b9618540653441f4c1a9bbe591
Jochen.Jung <jochen.jung@gmail.com>
2010-06-16 08:18:39 +0000

    Switched to asynchronous GoogleAnalytcis code and increased image cache time to two months, to increase page load speed

--- inc/classes/class_framework.php
+++ inc/classes/class_framework.php
@@ -245,13 +245,15 @@ class framework {
 
     $EndJS = '';
     if ($cfg['google_analytics_id']) $EndJS = '<script type="text/javascript">
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src=\'" + gaJsHost + "google-analytics.com/ga.js\' type=\'text/javascript\'%3E%3C/script%3E"));
-</script>
-<script type="text/javascript">
-var pageTracker = _gat._getTracker("'. $cfg['google_analytics_id'] .'");
-pageTracker._trackPageview();
-</script>';
+      var _gaq = _gaq || [];
+      _gaq.push([\'_setAccount\', \''. $cfg['google_analytics_id'] .'\']);
+      _gaq.push([\'_trackPageview\']);
+      (function() {
+        var ga = document.createElement(\'script\'); ga.type = \'text/javascript\'; ga.async = true;
+        ga.src = (\'https:\' == document.location.protocol ? \'https://ssl\' : \'http://www\') + \'.google-analytics.com/ga.js\';
+        var s = document.getElementsByTagName(\'script\')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+      </script>';
     $smarty->assign('EndJS', $EndJS);
 
     ### Switch Displaymodus (popup, base, print, normal, beamer)                    
