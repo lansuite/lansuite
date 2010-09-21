ad24bf021ae5295d75df13c6670bfe705db20352
Jochen.Jung <jochen.jung@gmail.com>
2010-09-21 10:27:02 +0000

    http://lansuite.orgapage.de/index.php?mod=bugtracker&bugid=996&ms_page=0

--- inc/classes/class_display.php
+++ inc/classes/class_display.php
@@ -430,8 +430,8 @@ class display {
     $this->AddDoubleRow($key, $value);
   }
 
-  function AddFormSubmitRow($text, $close = true) {
-    $this->AddDoubleRow('&nbsp;', '<input type="submit" class="Button" name="imageField" value="'. $text .'" />');
+  function AddFormSubmitRow($text, $close = true, $name = "imageField") {
+    $this->AddDoubleRow('&nbsp;', '<input type="submit" class="Button" name="'. $name .'" value="'. $text .'" />');
     if ($this->form_open and $close) $this->CloseForm();
   }
 
