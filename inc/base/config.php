aa18a6b5d5dcdc939ceab6a6043b2d4a4fedeb1b
root <root@intra.s.vpn>
2015-04-29 12:13:01 +0200

    git ignore for permissions

--- inc/base/config.php
+++ inc/base/config.php
@@ -1,20 +1,20 @@
 ;<?php /*
 
 [lansuite]
-version         = "SVN"
-default_design  = "simple"
-chmod_dir       = "777"
-chmod_file      = "666"
-debugmode       = "0"
+version				= "SVN"
+default_design			= "simple"
+chmod_dir			= "777"
+chmod_file			= "666"
+debugmode			= "0"
 
 [database]
-server    = "localhost"
-user      = "root"
-passwd    = ""
-database  = "lansuite"
-prefix    = "ls_"
+server				= "localhost"
+user				= "ls"
+passwd				= "hnxls"
+database			= "lansuite"
+prefix				= "ls_"
 
 [environment]
-configured  = "0"
+configured			= "1"
 
 ;*/ ?>
