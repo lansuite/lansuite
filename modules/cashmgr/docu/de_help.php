219a3b96c5d41fb75f935e503417cc48b5d44d88
Andy Grunwald <andygrunwald@gmail.com>
2017-04-23 20:27:49 +0200

    #6: Apply coding style guide PSR 1 and PSR 2 to ./modules/cashmgr (with php-cs-fixer)

--- modules/cashmgr/docu/de_help.php
+++ modules/cashmgr/docu/de_help.php
@@ -8,4 +8,3 @@ $helplet['key'][1] = 'Betrag Buchen - positve und negative Beträge';
 $helplet['value'][1] = 'Positive und Negative Beträge: Das Modul handelt aus Sicht der Lan. D.h ein positiver Betrag dient als Einnahme fuer die LAN. Ein negativer als Ausgabe. ';
 $helplet['key'][2] = 'Betrag Buchen - Fixbeträge';
 $helplet['value'][2] = 'Sind Betträge, die nicht veränderbar sind. Wird kein Benutzer angegeben, den die Zahlung betrifft, wird der Betrag automatisch auf Fix gesetzt (sinvoll fuer Hallenmiete (negativer Betrag) oder fuer Sponsorenverträge (positiver Betrag). Wird ein Benutzer angegeben und das Fix nicht gesetzt so wird dem User der Betrag gebucht und dem Bearbeiter (der diesen Betrag bucht) abgebucht. ';
-?>
