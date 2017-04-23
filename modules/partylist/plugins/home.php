14906e50b15a2f5dcbbe68dcf41bfbad9eed18e4
Andy Grunwald <andygrunwald@gmail.com>
2017-04-23 20:29:45 +0200

    #6: Apply coding style guide PSR 1 and PSR 2 to ./modules/partylist (with php-cs-fixer)

--- modules/partylist/plugins/home.php
+++ modules/partylist/plugins/home.php
@@ -9,11 +9,13 @@ $query = $db->qry("SELECT p.partyid, p.name, UNIX_TIMESTAMP(p.start) as start FR
   LIMIT 0,%int%
   ", $cfg['home_item_cnt_partylist']);
 
-if ($db->num_rows($query) > 0) while($row = $db->fetch_array($query)) {
-  $smarty->assign('link', "index.php?mod=partylist&partyid={$row['partyid']}");
-  $smarty->assign('text', $func->CutString($row['name'], 25));
-  $smarty->assign('text2', ' ['. $func->unixstamp2date($row['start'], 'date') .']');
-  $content .= $smarty->fetch('modules/home/templates/show_row.htm');
-} else $content = "<i>". t('Keine Einträge vorhanden') ."</i>";
-
-?>
+if ($db->num_rows($query) > 0) {
+    while ($row = $db->fetch_array($query)) {
+        $smarty->assign('link', "index.php?mod=partylist&partyid={$row['partyid']}");
+        $smarty->assign('text', $func->CutString($row['name'], 25));
+        $smarty->assign('text2', ' ['. $func->unixstamp2date($row['start'], 'date') .']');
+        $content .= $smarty->fetch('modules/home/templates/show_row.htm');
+    }
+} else {
+    $content = "<i>". t('Keine Einträge vorhanden') ."</i>";
+}
