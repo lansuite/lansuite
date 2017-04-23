d16cdf22bf9353e263569a6353fd9f939a69b472
Andy Grunwald <andygrunwald@gmail.com>
2017-04-23 20:31:48 +0200

    #6: Apply coding style guide PSR 1 and PSR 2 to ./modules/troubleticket (with php-cs-fixer)

--- modules/troubleticket/plugins/home.php
+++ modules/troubleticket/plugins/home.php
@@ -2,11 +2,9 @@
 $smarty->assign('caption', t('Troubletickets'));
 $content = '';
   
-// Additional Admin-Stats	
+// Additional Admin-Stats
 if ($auth["type"] >= 2) {
-  $row6 = $db->qry_first("SELECT count(*) as n FROM %prefix%troubleticket WHERE target_userid = '0'");
-  $row7 = $db->qry_first("SELECT count(*) as n FROM %prefix%troubleticket");
-  $content .= t('Troubletickets') .": ".$row6["n"]." / ".$row7["n"] . HTML_NEWLINE;
+    $row6 = $db->qry_first("SELECT count(*) as n FROM %prefix%troubleticket WHERE target_userid = '0'");
+    $row7 = $db->qry_first("SELECT count(*) as n FROM %prefix%troubleticket");
+    $content .= t('Troubletickets') .": ".$row6["n"]." / ".$row7["n"] . HTML_NEWLINE;
 }
-  
-?>
