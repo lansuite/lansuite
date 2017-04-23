e1a7d0b2df73ce374db6bcca44abbc34863c747d
Andy Grunwald <andygrunwald@gmail.com>
2017-04-23 20:28:28 +0200

    #6: Apply coding style guide PSR 1 and PSR 2 to ./modules/guestbook (with php-cs-fixer)

--- modules/guestbook/add.php
+++ modules/guestbook/add.php
@@ -3,16 +3,20 @@
 include_once('inc/classes/class_masterform.php');
 $mf = new masterform();
 
-if ($_POST['poster'] == '') $_POST['poster'] = $auth['username'];
+if ($_POST['poster'] == '') {
+    $_POST['poster'] = $auth['username'];
+}
 
 $mf->AddField(t('Autor'), 'poster', '', '');
-if (!$auth['login']) $mf->AddField('', 'captcha', IS_CAPTCHA);
+if (!$auth['login']) {
+    $mf->AddField('', 'captcha', IS_CAPTCHA);
+}
 $mf->AddField(t('Eintrag'), 'text', '', LSCODE_ALLOWED);
 $mf->AddGroup(t('Eintrag'));
 
 $mf->AddFix('date', time());
-if (!$_GET['guestbookid']) $mf->AddFix('userid', $auth['userid']);
+if (!$_GET['guestbookid']) {
+    $mf->AddFix('userid', $auth['userid']);
+}
 
 $mf->SendForm('index.php?mod=guestbook&action=add', 'guestbook', 'guestbookid', $_GET['guestbookid']);
-
-?>
--- modules/guestbook/delete.php
+++ modules/guestbook/delete.php
@@ -1,7 +1,6 @@
 <?php
 
 foreach ($_POST['action'] as $key => $val) {
-	$db->qry("DELETE FROM %prefix%guestbook WHERE guestbookid = %string%", $key);
+    $db->qry("DELETE FROM %prefix%guestbook WHERE guestbookid = %string%", $key);
 }
 $func->confirmation('Erfolgreich gelöscht', 'index.php?mod=guestbook');
-?>
