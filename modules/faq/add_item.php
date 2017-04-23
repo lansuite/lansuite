0898e53e8cbfc9023d0578d809219abfb539b347
Andy Grunwald <andygrunwald@gmail.com>
2017-04-23 20:27:59 +0200

    #6: Apply coding style guide PSR 1 and PSR 2 to ./modules/faq (with php-cs-fixer)

--- modules/faq/add_item.php
+++ modules/faq/add_item.php
@@ -14,113 +14,102 @@
 *
 **************************************************************************/
 
-switch($_GET["step"]) {
+switch ($_GET["step"]) {
 
-	case 2:
+    case 2:
 
-	//  ERRORS
-	$get_cat_names = $db->qry("SELECT name FROM %prefix%faq_cat");
+    //  ERRORS
+    $get_cat_names = $db->qry("SELECT name FROM %prefix%faq_cat");
 
-		while($row=$db->fetch_array($get_cat_names)) {
+        while ($row=$db->fetch_array($get_cat_names)) {
+            $name = $row["name"];
 
-			$name = $row["name"];
+            if ($name == $_POST["question_new_cat"] and $_POST["question_new_cat"] != "") {
+                $faq_error['question_cat'] = t('Dieser Kategoriename existiert bereits');
+                $_GET["step"] = 1;
+            }
+        }
 
-				if($name == $_POST["question_new_cat"] AND $_POST["question_new_cat"] != "" ) {
+    $i = strlen($_POST["question_text"]);
 
-					$faq_error['question_cat'] = t('Dieser Kategoriename existiert bereits');
-					$_GET["step"] = 1;
-
-				}
-		}
-
-	$i = strlen($_POST["question_text"]);
-
-		if($i > 5000) {
-
-			$faq_error['question_text'] = t('Die Antwort darf nicht mehr als 5000 Zeichen enthalten');
-			$_GET["step"] = 1;
-
-		}
+        if ($i > 5000) {
+            $faq_error['question_text'] = t('Die Antwort darf nicht mehr als 5000 Zeichen enthalten');
+            $_GET["step"] = 1;
+        }
 
 
-	if($_POST["question_new_cat"] == "" AND $_POST["question_cat"] == "new") {
+    if ($_POST["question_new_cat"] == "" and $_POST["question_cat"] == "new") {
+        $faq_error['cat_name']    = t('Bitte gib einen Namen für die neue Kategorie ein');
+        $_GET["step"] = 1;
+    }
 
-		$faq_error['cat_name']	= t('Bitte gib einen Namen für die neue Kategorie ein');
-		$_GET["step"] = 1;
-	}
-
-	if($_POST["question_caption"] == "") {
-
-		$faq_error['question_caption']	= t('Bitte gib eine Frage ein');
-		$_GET["step"] = 1;
-	}
+    if ($_POST["question_caption"] == "") {
+        $faq_error['question_caption']    = t('Bitte gib eine Frage ein');
+        $_GET["step"] = 1;
+    }
 
 
-	if($_POST["question_text"] == "") {
-		$faq_error['question_text']	= t('Bitte gib einen Text ein');
-		$_GET["step"] = 1;
-	}
+    if ($_POST["question_text"] == "") {
+        $faq_error['question_text']    = t('Bitte gib einen Text ein');
+        $_GET["step"] = 1;
+    }
 
-	if($_POST["question_cat"] == 0 AND $_POST["question_new_cat"] == "") {
+    if ($_POST["question_cat"] == 0 and $_POST["question_new_cat"] == "") {
+        $faq_error['question_cat']    = t('Bitte wähle eine Kategorie aus oder erstelle eine neue Kategorie');
+        $_GET["step"] = 1;
+    }
 
-		$faq_error['question_cat']	= t('Bitte wähle eine Kategorie aus oder erstelle eine neue Kategorie');
-		$_GET["step"] = 1;
-	}
-
-	if($_POST["question_cat"] != 0 AND $_POST["question_new_cat"] != "") {
-
-		$faq_error['question_cat']	= t('Bitte wähle eine Kategorie aus <b> ODER </b> erstelle eine neue Kategorie');
-		$_GET["step"] = 1;
-	}
+    if ($_POST["question_cat"] != 0 and $_POST["question_new_cat"] != "") {
+        $faq_error['question_cat']    = t('Bitte wähle eine Kategorie aus <b> ODER </b> erstelle eine neue Kategorie');
+        $_GET["step"] = 1;
+    }
 
 break;
 
 } // close switch
 
 
-switch($_GET["step"]) {
+switch ($_GET["step"]) {
 
-	default:
+    default:
 
-	unset($_SESSION['add_blocker_faqitem']);
+    unset($_SESSION['add_blocker_faqitem']);
 
-	$dsp->NewContent(t('Frage hinzufügen'),t(' Um eine Frage hinzuzufügen, fülle bitte das folgende Formular vollständig aus. Für das Feld Überschirft stehen 30 Zeichen, für das Feld Text 5000 Zeichen zur Verfügung. Im Feld Kategorie kannst du die Kategorie definieren, in der die Frage angezeigt werden soll.'));
-	$dsp->SetForm("index.php?mod=faq&object=item&action=add_item&step=2");
+    $dsp->NewContent(t('Frage hinzufügen'), t(' Um eine Frage hinzuzufügen, fülle bitte das folgende Formular vollständig aus. Für das Feld Überschirft stehen 30 Zeichen, für das Feld Text 5000 Zeichen zur Verfügung. Im Feld Kategorie kannst du die Kategorie definieren, in der die Frage angezeigt werden soll.'));
+    $dsp->SetForm("index.php?mod=faq&object=item&action=add_item&step=2");
 
-	$get_cats = $db->qry("SELECT name,catid FROM %prefix%faq_cat");
+    $get_cats = $db->qry("SELECT name,catid FROM %prefix%faq_cat");
 
-	$faq_cats[] = "<option selected value=\"0\"> ".t('Kategorie wählen')." </option>";
+    $faq_cats[] = "<option selected value=\"0\"> ".t('Kategorie wählen')." </option>";
 
-	while($row=$db->fetch_array($get_cats)) {
+    while ($row=$db->fetch_array($get_cats)) {
+        $faq_cats[] .= "<option value=" . $row["catid"] . "> " . $row["name"] . " </option>";
+    }
 
-		$faq_cats[] .= "<option value=" . $row["catid"] . "> " . $row["name"] . " </option>";
-	}
+    $dsp->AddTextFieldRow("question_caption", t('Frage / Überschrift'), $_POST['question_caption'], $faq_error['question_caption']);
+    $dsp->AddTextAreaPlusRow("question_text", t('Text'), $_POST['question_text'], $faq_error['question_text'], 70, 20);
+    $dsp->AddDropDownFieldRow("question_cat", t('Bestehende Kategorie'), $faq_cats, "");
+    $dsp->AddTextFieldRow("question_new_cat", t('Neue Kategorie'), $_POST['question_new_cat'], $faq_error['question_cat']);
+    $dsp->AddFormSubmitRow("add");
+    $dsp->AddContent();
 
-	$dsp->AddTextFieldRow("question_caption",t('Frage / Überschrift'),$_POST['question_caption'],$faq_error['question_caption']);
-	$dsp->AddTextAreaPlusRow("question_text",t('Text'),$_POST['question_text'],$faq_error['question_text'], 70, 20);
-	$dsp->AddDropDownFieldRow("question_cat",t('Bestehende Kategorie'),$faq_cats,"");
-	$dsp->AddTextFieldRow("question_new_cat",t('Neue Kategorie'),$_POST['question_new_cat'],$faq_error['question_cat']);
-	$dsp->AddFormSubmitRow("add");
-	$dsp->AddContent();
+    break; // BREAK DEFAULT
 
-	break; // BREAK DEFAULT
+    case 2:
 
-	case 2:
+    $courent_date = date("U");
 
-	$courent_date = date("U");
-
-	if($_POST["question_cat"] == 0 AND $_POST["question_new_cat"] != "" AND $_SESSION['add_blocker_faqitem'] != 1) {
-
-		$add_it = $db->qry("INSERT INTO %prefix%faq_cat SET
+    if ($_POST["question_cat"] == 0 and $_POST["question_new_cat"] != "" and $_SESSION['add_blocker_faqitem'] != 1) {
+        $add_it = $db->qry("INSERT INTO %prefix%faq_cat SET
 								name = %string%,
 								poster = %int%,
 								date = %string%
 								", $_POST["question_new_cat"], $_SESSION["auth"]["userid"], $courent_date);
 
-		$catid = $db->insert_id();
+        $catid = $db->insert_id();
 
 
-		$add_it = $db->qry("INSERT INTO %prefix%faq_item SET
+        $add_it = $db->qry("INSERT INTO %prefix%faq_item SET
 								caption = %string%,
 								text = %string%,
 								poster = %int%,
@@ -128,25 +117,16 @@ switch($_GET["step"]) {
 								catid = %int%
 								", $_POST["question_caption"], $_POST["question_text"], $_SESSION["auth"]["userid"], $courent_date, $catid);
 
-			if($add_it == 1) { $func->confirmation(t('Die Frage und die Kategorie wurden erfolgreich eingetragen'),"");
+        if ($add_it == 1) {
+            $func->confirmation(t('Die Frage und die Kategorie wurden erfolgreich eingetragen'), "");
 
-				$_SESSION['add_blocker_faqitem'] = 1;
-
-			}
-
-				else {
-
-					$func->error("NO_REFRESH");
-
-				}
-
-	}
-
-		else {
-
-			if($_SESSION["add_blocker_faqitem"] != 1) {
-
-				$add_it = $db->qry("INSERT INTO %prefix%faq_item SET
+            $_SESSION['add_blocker_faqitem'] = 1;
+        } else {
+            $func->error("NO_REFRESH");
+        }
+    } else {
+            if ($_SESSION["add_blocker_faqitem"] != 1) {
+                $add_it = $db->qry("INSERT INTO %prefix%faq_item SET
 								caption = %string%,
 								text = %string%,
 								poster = %int%,
@@ -154,23 +134,18 @@ switch($_GET["step"]) {
 								catid = %string%
 								", $_POST["question_caption"], $_POST["question_text"], $_SESSION["auth"]["userid"], $courent_date, $_POST["question_cat"]);
 
-					if($add_it == 1) { $func->confirmation(t('Die Frage wurde erfolgreich eingetragen'),"");
+                if ($add_it == 1) {
+                    $func->confirmation(t('Die Frage wurde erfolgreich eingetragen'), "");
 
-						$add_blocker_faqitem = 1;
+                    $add_blocker_faqitem = 1;
 
-						$_SESSION['add_blocker_faqitem'] = 1;
+                    $_SESSION['add_blocker_faqitem'] = 1;
+                }
+            } else {
+                $func->error("NO_REFRESH");
+            }
+        }
 
-
-					}
-			}
-
-				else
-
-					$func->error("NO_REFRESH");
-
-				}
-
-	break; // BREAK CASE 2
+    break; // BREAK CASE 2
 
 } // close switch step
-?>
--- modules/faq/show.php
+++ modules/faq/show.php
@@ -2,25 +2,32 @@
 $get_cat = $db->qry("SELECT catid, name FROM %prefix%faq_cat ORDER BY name");
 $count_cat = $db->num_rows($get_cat);
 
-if ($count_cat == 0) $func->information(t('Keine Einträge vorhanden.'), "index.php?mod=home");
-else {
-	$dsp->NewContent(t('FAQ'), t('Auf dieser Seite siehst du häufig gestellte Fragen und deren Antworten'));
+if ($count_cat == 0) {
+    $func->information(t('Keine Einträge vorhanden.'), "index.php?mod=home");
+} else {
+    $dsp->NewContent(t('FAQ'), t('Auf dieser Seite siehst du häufig gestellte Fragen und deren Antworten'));
 
-	while ($row = $db->fetch_array($get_cat)) {
+    while ($row = $db->fetch_array($get_cat)) {
+        if ($auth['type'] > 2) {
+            $admin_link = $dsp->FetchIcon('index.php?mod=faq&object=item&action=delete_cat&catid='. $row["catid"] .'&step=2', 'delete');
+        }
+        if ($auth['type'] > 1) {
+            $admin_link .= $dsp->FetchIcon('index.php?mod=faq&object=cat&action=change_cat&catid='. $row["catid"] .'&step=2', 'edit');
+        }
+        $dsp->AddFieldsetStart($admin_link . $row["name"]);
 
-	  if ($auth['type'] > 2) $admin_link = $dsp->FetchIcon('index.php?mod=faq&object=item&action=delete_cat&catid='. $row["catid"] .'&step=2', 'delete');
-	  if ($auth['type'] > 1) $admin_link .= $dsp->FetchIcon('index.php?mod=faq&object=cat&action=change_cat&catid='. $row["catid"] .'&step=2', 'edit');
-    $dsp->AddFieldsetStart($admin_link . $row["name"]);
-
-  	$get_item = $db->qry("SELECT caption,itemid FROM %prefix%faq_item WHERE catid = %int% ORDER BY caption", $row['catid']);
-		while ($row = $db->fetch_array($get_item)) {
-		  if ($auth['type'] > 2) $admin_link = $dsp->FetchIcon('index.php?mod=faq&object=item&action=delete_item&itemid='. $row["itemid"] .'&step=2', 'delete');
-		  if ($auth['type'] > 1) $admin_link .= $dsp->FetchIcon('index.php?mod=faq&object=cat&action=change_item&itemid='. $row["itemid"] .'&step=2', 'edit');
-    	$dsp->AddSingleRow($admin_link . $dsp->FetchLink($func->text2html($row["caption"]), 'index.php?mod=faq&action=comment&itemid='. $row["itemid"]));
-		}
-		$dsp->AddFieldsetEnd();
-	}
-	$dsp->AddSingleRow($faq_content, "class='menu'");
-	$dsp->AddContent();
+        $get_item = $db->qry("SELECT caption,itemid FROM %prefix%faq_item WHERE catid = %int% ORDER BY caption", $row['catid']);
+        while ($row = $db->fetch_array($get_item)) {
+            if ($auth['type'] > 2) {
+                $admin_link = $dsp->FetchIcon('index.php?mod=faq&object=item&action=delete_item&itemid='. $row["itemid"] .'&step=2', 'delete');
+            }
+            if ($auth['type'] > 1) {
+                $admin_link .= $dsp->FetchIcon('index.php?mod=faq&object=cat&action=change_item&itemid='. $row["itemid"] .'&step=2', 'edit');
+            }
+            $dsp->AddSingleRow($admin_link . $dsp->FetchLink($func->text2html($row["caption"]), 'index.php?mod=faq&action=comment&itemid='. $row["itemid"]));
+        }
+        $dsp->AddFieldsetEnd();
+    }
+    $dsp->AddSingleRow($faq_content, "class='menu'");
+    $dsp->AddContent();
 }
-?>
