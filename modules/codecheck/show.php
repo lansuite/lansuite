5cd460ceaf99d93da6ef95fd134c700411f2e3cf
Andy Grunwald <andygrunwald@gmail.com>
2017-04-23 20:27:53 +0200

    #6: Apply coding style guide PSR 1 and PSR 2 to ./modules/codecheck (with php-cs-fixer)

--- modules/codecheck/show.php
+++ modules/codecheck/show.php
@@ -15,27 +15,36 @@ switch ($_GET['headermenuitem']) {
   case 4: $dsp->AddSingleRow(t('Kein $ in erstem $db->qry Parameter. Statt dessen %string%, oder %int% verwenden. Zur Not %plain%')); break;
 }
 $modules = opendir('modules');
-while ($mod = readdir($modules)) if (is_dir("modules/$mod") and $mod != '.' and $mod != '..' and $mod != '.svn') {
-  $out = '';
+while ($mod = readdir($modules)) {
+    if (is_dir("modules/$mod") and $mod != '.' and $mod != '..' and $mod != '.svn') {
+        $out = '';
 
-  $files = opendir("modules/$mod");
-  while ($file = readdir($files)) if (is_file("modules/$mod/$file") and substr($file, strlen($file) - 4, 4) == '.php') {
+        $files = opendir("modules/$mod");
+        while ($file = readdir($files)) {
+            if (is_file("modules/$mod/$file") and substr($file, strlen($file) - 4, 4) == '.php') {
+                $fh = fopen("modules/$mod/$file", 'r');
+                $content = fread($fh, filesize("modules/$mod/$file"));
+                fclose($fh);
 
-    $fh = fopen("modules/$mod/$file", 'r');
-    $content = fread($fh, filesize("modules/$mod/$file"));
-    fclose($fh);
-
-    switch ($_GET['headermenuitem']) {
-      case 1: if (preg_match("#<\\?[^a-z].#sUi", $content)) $out .= "modules/$mod/$file<br />"; break;
-      case 2: if (preg_match("#\\\$db->query#sUi", $content)) $out .= "modules/$mod/$file<br />"; break;
-      case 3: if (preg_match("#\t#sUi", $content)) $out .= "modules/$mod/$file<br />"; break;
-      case 4: if (preg_match("#\\\$db->qry(.)*\\\$(.)*(\",|',|\"\\)|'\\))#", $content)) $out .= "modules/$mod/$file<br />"; break;
+                switch ($_GET['headermenuitem']) {
+      case 1: if (preg_match("#<\\?[^a-z].#sUi", $content)) {
+          $out .= "modules/$mod/$file<br />";
+      } break;
+      case 2: if (preg_match("#\\\$db->query#sUi", $content)) {
+          $out .= "modules/$mod/$file<br />";
+      } break;
+      case 3: if (preg_match("#\t#sUi", $content)) {
+          $out .= "modules/$mod/$file<br />";
+      } break;
+      case 4: if (preg_match("#\\\$db->qry(.)*\\\$(.)*(\",|',|\"\\)|'\\))#", $content)) {
+          $out .= "modules/$mod/$file<br />";
+      } break;
     }
-  }
-  closedir($files);
+            }
+        }
+        closedir($files);
 
-  $dsp->AddDoubleRow($mod, $out);
+        $dsp->AddDoubleRow($mod, $out);
+    }
 }
 closedir($modules);
-
-?>
