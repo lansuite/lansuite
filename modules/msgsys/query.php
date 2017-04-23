9a757fd2451435abb279f010b199eb56e9c9c308
Andy Grunwald <andygrunwald@gmail.com>
2017-04-23 20:29:28 +0200

    #6: Apply coding style guide PSR 1 and PSR 2 to ./modules/msgsys (with php-cs-fixer)

--- modules/msgsys/query.php
+++ modules/msgsys/query.php
@@ -1,13 +1,11 @@
 <?php
 
 if ($auth['login']) {
-	$row = $db->qry_first('SELECT username FROM	%prefix%user WHERE userid = %int%', $_GET['queryid']);
-  $smarty->assign ('username', $row['username']);
-	$index .= $smarty->fetch("design/templates/messenger_query_index.htm");
-	echo $index;
-
+    $row = $db->qry_first('SELECT username FROM	%prefix%user WHERE userid = %int%', $_GET['queryid']);
+    $smarty->assign('username', $row['username']);
+    $index .= $smarty->fetch("design/templates/messenger_query_index.htm");
+    echo $index;
 } else {
-	$func->information("NO_LOGIN");
-	echo $templ_index_content;
+    $func->information("NO_LOGIN");
+    echo $templ_index_content;
 }
-?>
--- modules/msgsys/query_messages.php
+++ modules/msgsys/query_messages.php
@@ -8,21 +8,21 @@ $query = $db->qry("SELECT text, timestamp, senderid FROM %prefix%messages
 
 $row2 = $db->qry_first("SELECT username FROM %prefix%user WHERE userid = %int%", $_GET[queryid]);
 
-while($row = $db->fetch_array($query)) {
-	$senderid	= $row["senderid"];
-	$timestamp	= $row["timestamp"];
-	$text		= $row["text"];
-			
-	$text = $func->text2html($text);
-	$date = $func->unixstamp2date($timestamp,"time");
-	
-	if($senderid == $auth['userid']) { 
-		$class	= "tbl_blue"; 
-		$msgs .= "<div class=\"$class\"><b>".$auth['username']." ($date):</b></div> $text".HTML_NEWLINE . HTML_NEWLINE;
-	} else {
-		$class	= "tbl_red"; 
-		$msgs .= "<div class=\"$class\"><b>".$row2['username']." ($date):</b></div> $text".HTML_NEWLINE . HTML_NEWLINE;
-	}
+while ($row = $db->fetch_array($query)) {
+    $senderid    = $row["senderid"];
+    $timestamp    = $row["timestamp"];
+    $text        = $row["text"];
+            
+    $text = $func->text2html($text);
+    $date = $func->unixstamp2date($timestamp, "time");
+    
+    if ($senderid == $auth['userid']) {
+        $class    = "tbl_blue";
+        $msgs .= "<div class=\"$class\"><b>".$auth['username']." ($date):</b></div> $text".HTML_NEWLINE . HTML_NEWLINE;
+    } else {
+        $class    = "tbl_red";
+        $msgs .= "<div class=\"$class\"><b>".$row2['username']." ($date):</b></div> $text".HTML_NEWLINE . HTML_NEWLINE;
+    }
 }
 
 $msgs .= "<a name=\"end\"></a>";
@@ -32,5 +32,3 @@ $smarty->assign('refreshtime', 15);
 $smarty->assign('queryid', $_GET['queryid']);
 $index .= $smarty->fetch("design/templates/messenger_query_messages.htm");
 echo $index;
-
-?>
