e6171bbaba043561bb4958353f510c3d06a28c63
Andy Grunwald <andygrunwald@gmail.com>
2017-04-23 20:27:53 +0200

    #6: Apply coding style guide PSR 1 and PSR 2 to ./modules/codecheck (with phpcbf)

--- modules/codecheck/show.php
+++ modules/codecheck/show.php
@@ -9,10 +9,18 @@ $menunames[4] = t('$ in $db->qry');
 $dsp->AddHeaderMenu($menunames, 'index.php?mod=codecheck', $_GET['headermenuitem']);
 
 switch ($_GET['headermenuitem']) {
-  case 1: $dsp->AddSingleRow(t('Es sollte immer <?php statt <? verwendet werden. Anders funktioniert LS beispielsweise im IIS nicht')); break;
-  case 2: $dsp->AddSingleRow(t('$db->query sollte durch das neue $qb->qry ausgetauscht werden. Dieses kann SQL-Injections sehr zuverlässig verhindern')); break;
-  case 3: $dsp->AddSingleRow(t('Es sollten keine Tabs im Code verwendet werden. Statt dessen am Besten 2 Leerzeichen')); break;
-  case 4: $dsp->AddSingleRow(t('Kein $ in erstem $db->qry Parameter. Statt dessen %string%, oder %int% verwenden. Zur Not %plain%')); break;
+    case 1:
+        $dsp->AddSingleRow(t('Es sollte immer <?php statt <? verwendet werden. Anders funktioniert LS beispielsweise im IIS nicht'));
+        break;
+    case 2:
+        $dsp->AddSingleRow(t('$db->query sollte durch das neue $qb->qry ausgetauscht werden. Dieses kann SQL-Injections sehr zuverlässig verhindern'));
+        break;
+    case 3:
+        $dsp->AddSingleRow(t('Es sollten keine Tabs im Code verwendet werden. Statt dessen am Besten 2 Leerzeichen'));
+        break;
+    case 4:
+        $dsp->AddSingleRow(t('Kein $ in erstem $db->qry Parameter. Statt dessen %string%, oder %int% verwenden. Zur Not %plain%'));
+        break;
 }
 $modules = opendir('modules');
 while ($mod = readdir($modules)) {
@@ -27,19 +35,27 @@ while ($mod = readdir($modules)) {
                 fclose($fh);
 
                 switch ($_GET['headermenuitem']) {
-      case 1: if (preg_match("#<\\?[^a-z].#sUi", $content)) {
-          $out .= "modules/$mod/$file<br />";
-      } break;
-      case 2: if (preg_match("#\\\$db->query#sUi", $content)) {
-          $out .= "modules/$mod/$file<br />";
-      } break;
-      case 3: if (preg_match("#\t#sUi", $content)) {
-          $out .= "modules/$mod/$file<br />";
-      } break;
-      case 4: if (preg_match("#\\\$db->qry(.)*\\\$(.)*(\",|',|\"\\)|'\\))#", $content)) {
-          $out .= "modules/$mod/$file<br />";
-      } break;
-    }
+                    case 1:
+                        if (preg_match("#<\\?[^a-z].#sUi", $content)) {
+                                      $out .= "modules/$mod/$file<br />";
+                        }
+                        break;
+                    case 2:
+                        if (preg_match("#\\\$db->query#sUi", $content)) {
+                                      $out .= "modules/$mod/$file<br />";
+                        }
+                        break;
+                    case 3:
+                        if (preg_match("#\t#sUi", $content)) {
+                                      $out .= "modules/$mod/$file<br />";
+                        }
+                        break;
+                    case 4:
+                        if (preg_match("#\\\$db->qry(.)*\\\$(.)*(\",|',|\"\\)|'\\))#", $content)) {
+                                      $out .= "modules/$mod/$file<br />";
+                        }
+                        break;
+                }
             }
         }
         closedir($files);
