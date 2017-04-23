89162535e74fb4aad286f6201a7dd9c5eac9fec6
Andy Grunwald <andygrunwald@gmail.com>
2017-04-23 20:31:09 +0200

    #6: Apply coding style guide PSR 1 and PSR 2 to ./modules/server2 (with php-cs-fixer)

--- modules/server2/admin.php
+++ modules/server2/admin.php
@@ -1,12 +1,11 @@
 <?php
 ob_start();
 
-define ('PHGDIR', 'ext_scripts/lgsl/');
+define('PHGDIR', 'ext_scripts/lgsl/');
 $use_file = 'index.php?mod=server2'; #basename(__FILE__);
 $use_bind = '&';#'?';
-require_once (PHGDIR . 'admin.php');
+require_once(PHGDIR . 'admin.php');
 
 $phg_content = ob_get_contents();
 ob_end_clean();
 $dsp->AddSingleRow($phg_content);
-?>
