97776da2722418ba76ad3e8ca69488a1c180ade6
Andy Grunwald <andygrunwald@gmail.com>
2017-04-23 20:31:08 +0200

    #6: Apply coding style guide PSR 1 and PSR 2 to ./modules/server (with php-cs-fixer)

--- modules/server/docu/de_help.php
+++ modules/server/docu/de_help.php
@@ -8,5 +8,3 @@ $helplet['key'][1] = 'Erreichbarkeitstest';
 $helplet['value'][1] = 'Ein Erreichbarkeistest ist integriert und informiert über den jeweiligen Status der Server. Hier wird zunächst per Ping die Erreichbarkeit der IP getestet (das hierfür verwendete Intervall liegt bei 60 Sekunden und lässt sich in der Modul Konfig definieren) und anschließend bei speziellen Services, wie FTP, HTTP, oder IRC versucht eine Verbindung zum Port aufzubauen. Entsprechend kann man aus der Liste auslesen, ob der komplette Serverrechner aus ist, oder nur der betreffende Service zur Zeit auf dem Server nicht läuft';
 $helplet['key'][2] = 'FTP-Debug';
 $helplet['value'][2] = 'Auf FTP-Servern erhält man weitere Informationen, falls der Erreichbarkeitstest fehlschlägt (z.B. ob es daran liegt, dass auf dem Server kein Home-Verzeichnis festgelegt ist, oder daran, dass das Downoad-Limit erreicht ist, etc.).';
-
-?>
