5b4b39587aeca549eafb5dbdb672a3758966a175
Andy Grunwald <andygrunwald@gmail.com>
2017-04-23 20:29:47 +0200

    #6: Apply coding style guide PSR 1 and PSR 2 to ./modules/paypal (with php-cs-fixer)

--- modules/paypal/paypal.php
+++ modules/paypal/paypal.php
@@ -1,40 +1,38 @@
 <?php
-	// GET FORM TO SEE WHAT YOU MUS PAY
-	
-if($auth['userid'] == 0 && $cfg['paypal_donation'] == 0){
-	$func->error(t('Du kannst nichts einzahlen wenn du nicht eingeloggt bist.'),"index.php?mod=home");
-}else{
-	$dsp->NewContent(t('Einzahlen'), t('Hier sehen sie was f&uuml;r Betr&auml;ge noch ausstehend sind. W&auml;hlen sie was sie bezahlen m&ouml;chten.'));
-	$dsp->AddSmartyTpl('javascript', 'paypal');
-	$dsp->SetForm("index.php?mod=paypal&action=paying&design=base\" target=\"PopWnd\" onsubmit=\"submitpaypal(); return false;","paypal");
+    // GET FORM TO SEE WHAT YOU MUS PAY
+    
+if ($auth['userid'] == 0 && $cfg['paypal_donation'] == 0) {
+    $func->error(t('Du kannst nichts einzahlen wenn du nicht eingeloggt bist.'), "index.php?mod=home");
+} else {
+    $dsp->NewContent(t('Einzahlen'), t('Hier sehen sie was f&uuml;r Betr&auml;ge noch ausstehend sind. W&auml;hlen sie was sie bezahlen m&ouml;chten.'));
+    $dsp->AddSmartyTpl('javascript', 'paypal');
+    $dsp->SetForm("index.php?mod=paypal&action=paying&design=base\" target=\"PopWnd\" onsubmit=\"submitpaypal(); return false;", "paypal");
 
-	// LIST ALL PARTYS
-	if($auth['userid'] != 0){
-		$pay_partys = $db->qry("SELECT * FROM %prefix%party_user AS pu LEFT JOIN %prefix%partys AS p USING(party_id) LEFT JOIN %prefix%party_prices AS price ON price.price_id=pu.price_id WHERE user_id=%int% AND paid = '0'", $auth['userid']);
+    // LIST ALL PARTYS
+    if ($auth['userid'] != 0) {
+        $pay_partys = $db->qry("SELECT * FROM %prefix%party_user AS pu LEFT JOIN %prefix%partys AS p USING(party_id) LEFT JOIN %prefix%party_prices AS price ON price.price_id=pu.price_id WHERE user_id=%int% AND paid = '0'", $auth['userid']);
 
-		if($db->num_rows($pay_partys) > 0){
-			while($pay = $db->fetch_array($pay_partys)){
-				$dsp->AddCheckBoxRow("price[]",$pay['name'],$pay['price_text'] . " " . $pay['price'] . " " . $cfg['paypal_currency_code'],"",NULL,NULL,NULL,$pay['price_id']);
-				if($cfg['paypal_depot'] && $pay['depot_price'] > 0){
-					$dsp->AddCheckBoxRow("depot[]",$pay['name'],$pay['depot_text'] . " " . $pay['depot_price'] . " " . $cfg['paypal_currency_code'],"",NULL,NULL,NULL,$pay['price_id']);
-				}
-			}
-		}else{
-			$dsp->AddSingleRow(t('Alle Eintrittspreise bezahlt.'));
-		}
-		
-			if($cfg['paypal_catering']){
-				$dsp->AddTextFieldRow("catering",t('Einzahlung f&uuml;r Catering'),0,"");
-			}
-	}
-	
-	if($cfg['paypal_donation']){
-		$dsp->AddTextFieldRow("donation",t('Spende f&uuml;r die Organisatoren'),0,"");
-	}
-	
-	$dsp->AddFormSubmitRow(t('Weiter'));
-	$db->free_result($pay_partys);
-	$dsp->AddContent();
+        if ($db->num_rows($pay_partys) > 0) {
+            while ($pay = $db->fetch_array($pay_partys)) {
+                $dsp->AddCheckBoxRow("price[]", $pay['name'], $pay['price_text'] . " " . $pay['price'] . " " . $cfg['paypal_currency_code'], "", null, null, null, $pay['price_id']);
+                if ($cfg['paypal_depot'] && $pay['depot_price'] > 0) {
+                    $dsp->AddCheckBoxRow("depot[]", $pay['name'], $pay['depot_text'] . " " . $pay['depot_price'] . " " . $cfg['paypal_currency_code'], "", null, null, null, $pay['price_id']);
+                }
+            }
+        } else {
+            $dsp->AddSingleRow(t('Alle Eintrittspreise bezahlt.'));
+        }
+        
+        if ($cfg['paypal_catering']) {
+            $dsp->AddTextFieldRow("catering", t('Einzahlung f&uuml;r Catering'), 0, "");
+        }
+    }
+    
+    if ($cfg['paypal_donation']) {
+        $dsp->AddTextFieldRow("donation", t('Spende f&uuml;r die Organisatoren'), 0, "");
+    }
+    
+    $dsp->AddFormSubmitRow(t('Weiter'));
+    $db->free_result($pay_partys);
+    $dsp->AddContent();
 }
-	
-?>
