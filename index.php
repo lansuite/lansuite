46f7f26740fde1cd100571af7748b652b86c2743
Andy Grunwald <andygrunwald@gmail.com>
2017-05-21 18:57:53 +0300

    Set error_reporting to E_ALL ^ E_NOTICE ^ E_DEPRECATED ^ E_STRICT

--- index.php
+++ index.php
@@ -1,17 +1,10 @@
 <?php
-### Set Error Reporting & INI-Settings
+// Set error_reporting properly
+// Right now we depend on it. This will change in future.
+error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED ^ E_STRICT);
 
-if (defined('E_STRICT')) {
-    error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED ^ E_STRICT);
-} // Will work for PHP >= 5.3
-elseif (defined('E_DEPRECATED')) {
-    error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED);
-} // Will work for PHP >= 5.3
-else {
-    error_reporting(E_ALL ^ E_NOTICE);
-} // For PHP < 5.3
 if (function_exists('ini_set')) {
-  // Disable SID in URL
+    // Disable SID in URL
     ini_set('url_rewriter.tags', '');
 }
 
