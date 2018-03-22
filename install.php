972961bd7f7df4033dc5173958054813a9a4d84e
M4LuZ <FUik,`l\'F6@v*Xg]iS>
2018-03-22 12:56:37 +0100

    Fix #149: Refactoring, reformattting and re-documenting as requested by reviewer (yet to be tested!)

--- install.php
+++ install.php
@@ -1,40 +1,18 @@
 <?php
 
-//raise error reporting - could be interesting and this is autmomatically reset when index.php is executed
+// Raise error reporting - could be interesting and this is automatically reset when index.php is executed
 error_reporting(E_ALL);
 
+// Include check function required for this
+require_once ('inc/Functions/DependencyPreCheck.php');
 
-/**
-* Check for common failures
-*
-* Small function to check against a number of dependencies that cause install.php to fail without showing any error details
-* @return bool check result. True if error found, false if all OK
-*/
-function checkdeps(){
-
-	$error =false;
-	$errmsg='The following issues were found while preparing to run the installation:<br/>'. PHP_EOL;
-
-	//check if composer was executed (folder ./vendor created)
-	if (!is_dir(__DIR__ . '/vendor')) {
-
-		$error = true;
-		$errmsg .= __DIR__ . '/vendor directory is missing. Did you run composer?<br/>' .PHP_EOL;
-	}
-
-	//check if smarty-directory is writable
-	if (!is_writable(__DIR__ . '/ext_inc/templates_c')) {
-		$error = true;
-		$errmsg .= __DIR__ . '/ext_inc/templates_c seems not to be writable by the PHP user. Please confirm that file and folder access rights are applied correctly<br/>' . PHP_EOL;
-	}
-
-	//check error state and return stored messages if problem found
-	if ($error) echo $errmsg;
-	return $error;
-}
-
-//execute install.php only if dependencies are met
-if (!checkdeps()){
+// Execute install.php only if dependencies are met
+$preCheckResult = dependencyPreCheck();
+if ($preCheckResult){
+	// Issue(s) found, show text to user
+	echo $preCheckResult;
+} else {
+	// No issues found, continue execution
 	$_GET["mod"] = "install";
 	include_once("index.php");
 }
