a7d3dfc3d1f7f7ec4699c76cade8517648b50eeb
Andy Grunwald <andygrunwald@gmail.com>
2018-04-14 13:05:03 +0200

    Fix #213: Rework of the module cashmgr (reformatting, removing dead code)
    - Code reformatting
    - Delete dead and unused Code
    - PHPDoc for function and methods

--- modules/cashmgr/account.php
+++ modules/cashmgr/account.php
@@ -2,6 +2,5 @@
 
 include_once("modules/cashmgr/class_accounting.php");
 
-
 $account = new accounting($auth['userid']);
 $account->getAccounting();
--- modules/cashmgr/fixbooking.php
+++ modules/cashmgr/fixbooking.php
@@ -7,19 +7,18 @@ $dsp->NewContent(t('Betrag Buchen'), t('Fixbetrag (z.B Miete oder Sponsoring) od
 $mf->AddField('Betreff', 'comment');
 $mf->AddField('Betrag (bei Negativen, minus davor)', 'movement');
 
-$party_list = array();
-    $row = $db->qry("SELECT party_id, name FROM %prefix%partys");
+$party_list = [];
+$row = $db->qry("SELECT party_id, name FROM %prefix%partys");
 while ($res = $db->fetch_array($row)) {
     $party_list[$res['party_id']] = $res['name'];
 }
     
-$group_list = array();
-    $row = $db->qry("SELECT id, caption FROM %prefix%cashmgr_group");
+$group_list = [];
+$row = $db->qry("SELECT id, caption FROM %prefix%cashmgr_group");
 while ($res = $db->fetch_array($row)) {
     $group_list[$res['id']] = $res['caption'];
 }
 
-    
 $mf->AddDropDownFromTable(t('Party'), 'partyid', 'party_id', 'name', 'partys');
 $mf->AddDropDownFromTable(t('Gruppe'), 'groupid', 'id', 'caption', 'cashmgr_group');
 $mf->AddFix('fix', '1');
