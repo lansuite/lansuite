5242f2cb0b33d8386eccaafc19fb6c8d8c977ed9
Andy Grunwald <andygrunwald@gmail.com>
2018-04-14 13:17:19 +0200

    Undefined constant testbooking1 should be handled as string

--- modules/cashmgr/accounting.php
+++ modules/cashmgr/accounting.php
@@ -3,4 +3,4 @@
 $dsp->NewContent(t('Kalkulation'), t('Zur aktuellen Lanparty zum derzeitigen Stand'));
 
 $account = new \LanSuite\Module\CashMgr\Accounting($party->party_id);
-$account->booking(3, testbooking1, 3);
+$account->booking(3, 'testbooking1', 3);
