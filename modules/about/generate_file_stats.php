b3f3167c9f6d199cf1bcf8fbaf57ec6b2f550413
Andy Grunwald <andygrunwald@gmail.com>
2018-05-04 22:46:36 +0200

    Module "about": Replaced deprecated functions eregi and eregi_replace
    Replaced
    - eregi => preg_match
    - eregi_replace => preg_replace

--- modules/about/generate_file_stats.php
+++ modules/about/generate_file_stats.php
@@ -5,12 +5,12 @@ $dir = ".";
 lets_open_the_dir($dir);
     
 foreach ($files as $file) {
-    if (eregi(".php", $file)) {
+    if (preg_match(".php", $file)) {
         $data = get_the_lines_and_chars($file);
         $php_lines += $data[0];
         $php_chars += $data[1];
     }
-    if (eregi(".htm", $file)) {
+    if (preg_match(".htm", $file)) {
         $data = get_the_lines_and_chars($file);
         $html_lines += $data[0];
         $html_chars += $data[1];
@@ -20,10 +20,10 @@ foreach ($files as $file) {
 $filecontents = file("modules/about/credits.php");
 $fo = fopen("modules/about/credits.php", "w");
 foreach ($filecontents as $filecontent) {
-    $filecontent = eregi_replace("<!--PHP-LINES-START-->(.*)<!--PHP-LINES-STOP-->", "<!--PHP-LINES-START-->$php_lines<!--PHP-LINES-STOP-->", $filecontent);
-    $filecontent = eregi_replace("<!--PHP-CHARS-START-->(.*)<!--PHP-CHARS-STOP-->", "<!--PHP-CHARS-START-->$php_chars<!--PHP-CHARS-STOP-->", $filecontent);
-    $filecontent = eregi_replace("<!--HTML-LINES-START-->(.*)<!--HTML-LINES-STOP-->", "<!--HTML-LINES-START-->$html_lines<!--HTML-LINES-STOP-->", $filecontent);
-    $filecontent = eregi_replace("<!--HTML-CHARS-START-->(.*)<!--HTML-CHARS-STOP-->", "<!--HTML-CHARS-START-->$html_chars<!--HTML-CHARS-STOP-->", $filecontent);
+    $filecontent = preg_replace("<!--PHP-LINES-START-->(.*)<!--PHP-LINES-STOP-->", "<!--PHP-LINES-START-->$php_lines<!--PHP-LINES-STOP-->", $filecontent);
+    $filecontent = preg_replace("<!--PHP-CHARS-START-->(.*)<!--PHP-CHARS-STOP-->", "<!--PHP-CHARS-START-->$php_chars<!--PHP-CHARS-STOP-->", $filecontent);
+    $filecontent = preg_replace("<!--HTML-LINES-START-->(.*)<!--HTML-LINES-STOP-->", "<!--HTML-LINES-START-->$html_lines<!--HTML-LINES-STOP-->", $filecontent);
+    $filecontent = preg_replace("<!--HTML-CHARS-START-->(.*)<!--HTML-CHARS-STOP-->", "<!--HTML-CHARS-START-->$html_chars<!--HTML-CHARS-STOP-->", $filecontent);
     fwrite($fo, $filecontent);
 }
 fclose($fo);
