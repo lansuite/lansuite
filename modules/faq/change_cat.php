525f47ff2a41ff03e932a30b21be7c8c26ba4bb0
Andy Grunwald <andygrunwald@gmail.com>
2018-05-10 14:25:41 -0700

    Module "faq": Code cleanup and added CGL checks to TravisCI

--- modules/faq/change_cat.php
+++ modules/faq/change_cat.php
@@ -1,46 +1,23 @@
 <?php
-/*************************************************************************
-*
-*	Lansuite - Webbased LAN-Party Management System
-*	-------------------------------------------------------------------
-*	Lansuite Version:	2.0
-*	File Version:		2.0
-*	Filename: 		change_cat.php
-*	Module: 		FAQ
-*	Main editor: 		Micheal@one-network.org
-*	Last change: 		01.04.2003 13:16
-*	Description: 		Changes FAQ Cats
-*	Remarks:
-*
-**************************************************************************/
 
 switch ($_GET["step"]) {
     case 3:
-    //  ERRORS
         $get_cat_names = $db->qry("SELECT name FROM %prefix%faq_cat");
-    
         while ($row=$db->fetch_array($get_cat_names)) {
             $name = $row["name"];
-        
             if ($name == $_POST["cat_caption"]) {
                 $faq_error['cat_caption'] = t('Dieser Kategoriename existiert bereits');
                 $_GET["step"] = 2;
             }
         }
-            
-            
+
         if ($_POST["cat_caption"] == "") {
             $faq_error['cat_caption']    = t('Bitte gib einen Namen für die neue Kategorie ein');
-            
             eval($error);
-            
             $_GET["step"] = 2;
         }
-            
-                
         break;
-} // close switch
-
+}
 
 switch ($_GET["step"]) {
     case 2:
@@ -55,12 +32,11 @@ switch ($_GET["step"]) {
             $dsp->AddTextFieldRow("cat_caption", t('Frage ändern'), $_POST['cat_caption'], $faq_error['cat_caption']);
             $dsp->AddFormSubmitRow("edit");
         } else {
-                $func->error(t('Diese Kategorie existiert nicht'));
+            $func->error(t('Diese Kategorie existiert nicht'));
         }
             
         break;
-        
-    
+
     case 3:
         if ($_SESSION["change_blocker_faq_cat"] != 1) {
             $get_data = $db->qry_first("SELECT name FROM %prefix%faq_cat WHERE catid = %int%", $_GET["catid"]);
@@ -79,12 +55,8 @@ switch ($_GET["step"]) {
             } else {
                 $func->error(t('Diese Kategorie existiert nicht'));
             }
-        } // close if blocker
-        
-        else {
+        } else {
             $func->error("NO_REFRESH");
         }
-                
-        
         break;
-} // close switch step
+}
