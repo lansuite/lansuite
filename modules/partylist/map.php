d32b1e13f7169830c3672befd4077ef3938d96cb
Andy Grunwald <andygrunwald@gmail.com>
2018-05-16 08:31:11 +0200

    Module "partylist": Added PHPDoc, moved functions to Functions/ folder, removed dead code
    - Removed dead code
    - Moved functions to Function/-folder
    - Reformatted code to match CGL
    - Added CGL check to TravisCI

--- modules/partylist/map.php
+++ modules/partylist/map.php
@@ -2,23 +2,20 @@
 
 $dsp->NewContent(t('Party-Karte'), t('Partys, die Lansuite verwenden'));
 
-#if (!$cfg['google_maps_api_key']) $func->information(t('Du musst dir zuerst unter http://www.google.com/apis/maps/signup.html einen Google-Maps API Key erzeugen und diesen auf der %1 eingeben', '<a href="index.php?mod=install&action=modules&step=10&module=install">'. t('AdminSeite in den Allgemeinen Einstellungen').'</a>'));
-#else {
-  $where_pid = '';
+$where_pid = '';
 if ($party->party_id) {
     $where_pid = "AND (p.party_id = {$party->party_id})";
 }
 
-  $res = $db->qry("SELECT *, UNIX_TIMESTAMP(start) AS start, UNIX_TIMESTAMP(end) AS end FROM %prefix%partylist AS p WHERE start > NOW()");
+$res = $db->qry("SELECT *, UNIX_TIMESTAMP(start) AS start, UNIX_TIMESTAMP(end) AS end FROM %prefix%partylist AS p WHERE start > NOW()");
 
-  $addresses = '';
+$addresses = '';
 while ($row = $db->fetch_array($res)) {
     $text = "<b>{$row['name']}</b><br />- {$row['motto']} -<br>". $func->unixstamp2date($row['start'], 'datetime') .' - '. $func->unixstamp2date($row['end'], 'datetime') ."<br>{$row['street']} {$row['hnr']}, {$row['plz']} {$row['city']}";
     $addresses .= "showAddress('Germany', '". addslashes($row['city']) ."', '". addslashes($row['plz']) ."', '". addslashes($row['street']) ."', '". addslashes($row['hnr']) ."', '". addslashes($text) ."');\r\n";
 }
-  $db->free_result($haus_data);
-  $smarty->assign('addresses', $addresses);
+$db->free_result($haus_data);
+$smarty->assign('addresses', $addresses);
 
-  $smarty->assign('apikey', $cfg['google_maps_api_key']);
-  $dsp->AddSingleRow($smarty->fetch('modules/guestlist/templates/googlemaps.htm'));
-#}
+$smarty->assign('apikey', $cfg['google_maps_api_key']);
+$dsp->AddSingleRow($smarty->fetch('modules/guestlist/templates/googlemaps.htm'));
--- modules/partylist/show.php
+++ modules/partylist/show.php
@@ -16,151 +16,6 @@ switch ($_GET['step']) {
     break;
 }
 
-function GetSite($url)
-{
-    global $HTTPHeader;
-
-    $url = @parse_url($url);
-    if (!$url['port']) {
-        $url['port'] = 80;
-    }
-    $url['host'] = trim($url['host']);
-    $url['path'] = trim($url['path']);
-    if (!$url['host'] or !$url['path']) {
-        $HTTPHeader = t('Hostname, oder Pfad fehlt');
-        return '';
-    }
-    try {
-        # $ip = gethostbyname($url['host']);
-        $fp = @fsockopen($url['host'], $url['port'], $errno, $errstr, 1);
-    # $fp = stream_set_timeout($fp, 1);
-    } catch (Exception $e) {
-        // Ignore connection errors
-    }
-
-    if (!$fp) {
-        $HTTPHeader = $errno.': '.$errstr;
-        return '';
-    } else {
-        $cont = '';
-
-        fputs($fp, "GET {$url['path']} HTTP/1.0\r\nHost: {$url['host']}\r\n\r\n");
-        while (!feof($fp)) {
-            $line = fgets($fp, 128);
-            if ($line == '') {
-                break;
-            }
-            $cont .= $line;
-        }
-        fclose($fp);
-
-        $HTTPHeader = substr($cont, 0, strpos($cont, "\r\n\r\n"));
-
-        $StatusCode = substr($HTTPHeader, strpos($HTTPHeader, ' ') + 1, 3);
-        if ($StatusCode != 200) {
-            return '';
-        }
-
-        return substr($cont, strpos($cont, "\r\n\r\n") + 4);
-    }
-}
-
-
-function AddSignonStatus($lsurl, $show_history = 0)
-{
-    global $xml, $dsp, $HTTPHeader, $func;
-
-    if (substr($lsurl, strlen($lsurl) - 1, 1) != '/') {
-        $lsurl .= '/';
-    }
-    if (substr($lsurl, 0, 7) != 'http://') {
-        $lsurl = 'http://'. $lsurl;
-    }
-    $lsurl .= 'ext_inc/party_infos/infos.xml';
-#  $lines = @file($lsurl);
-    $content = GetSite($lsurl);
-
-#  if (!$lines) return t('infos.xml fehlt');
-    if (!$content) {
-        return '<div class="infolink" style="display:inline">'. t('infos.xml fehlt') .'<span class="infobox">'. $lsurl .HTML_NEWLINE.HTML_NEWLINE. str_replace("'", "\\'", str_replace('"', "'", str_replace("\r\n", HTML_NEWLINE, $HTTPHeader))) .'</span></div>';
-    } else {
-        #    $content = '';
-    #    foreach ($lines as $line_num => $line) $content .= $line;
-
-        $system = $xml->get_tag_content_array('system', $content);
-        // Version 3.0 XML-File
-        if ($system) {
-            #    $name = $xml->get_tag_content('name', $system[0]);
-        #    $link = $xml->get_tag_content('link', $system[0]);
-        #    $language = $xml->get_tag_content('language', $system[0]);
-            $current_party = $xml->get_tag_content('current_party', $system[0]);
-        #    $users = $xml->get_tag_content('users', $system[0]);
-
-            $partys = $xml->get_tag_content_array('party', $content);
-            $ret = '';
-            if (!$partys) {
-                return t('Noch keine Party angelegt');
-            } else {
-                foreach ($partys as $p) {
-                    $partyid = $xml->get_tag_content('partyid', $p);
-                    $partyname = $xml->get_tag_content('name', $p);
-                    $max_guest = $xml->get_tag_content('max_guest', $p);
-                    $ort = $xml->get_tag_content('ort', $p);
-                    $plz = $xml->get_tag_content('plz', $p);
-      #     $startdate = $xml->get_tag_content('startdate', $p);
-      #     $enddate = $xml->get_tag_content('enddate', $p);
-      #     $sstartdate = $xml->get_tag_content('sstartdate', $p);
-      #     $senddate = $xml->get_tag_content('senddate', $p);
-                    $registered = $xml->get_tag_content('registered', $p);
-                    $paid = $xml->get_tag_content('paid', $p);
-
-            # Overview
-                    if (!$_GET['partyid'] and $current_party == $partyid) {
-                        $ret .= $func->CreateSignonBar($registered, $paid, $max_guest).'Max.: '.$max_guest;
-                    }
-
-            # Details
-                    if ($_GET['partyid']) {
-                        if (!$show_history and $current_party == $partyid) {
-                            $ret .= $func->CreateSignonBar($registered, $paid, $max_guest);
-                        } elseif ($show_history and $current_party != $partyid) {
-                            $dsp->AddDoubleRow($partyname .HTML_NEWLINE. $plz .' '. $ort, $func->CreateSignonBar($registered, $paid, $max_guest));
-                        }
-                    }
-                }
-            }
-              return $ret;
-
-          // Old version
-        } else {
-            $guests = $xml->get_tag_content('guests', $content);
-            $paid_guests = $xml->get_tag_content('paid_guests', $content);
-            $max_guests = $xml->get_tag_content('max_guests', $content);
-
-            return $func->CreateSignonBar($guests, $paid_guests, $max_guests);
-        }
-    }
-}
-
-function EditAllowed()
-{
-    global $line, $auth;
-
-    if ($line['userid'] == $auth['userid'] or $auth['type'] >= 2) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function NameAndMotto($name)
-{
-    global $line;
-
-    return $name .HTML_NEWLINE. $line['motto'];
-}
-
-
 if (!$_GET['partyid']) {
     if ($_GET['action'] == 'history') {
         $where = 'p.end < NOW()';
@@ -195,9 +50,16 @@ if (!$_GET['partyid']) {
 
     $ms2->PrintSearch('index.php?mod=partylist&action='. $_GET['action'], 'p.partyid');
 } else {
-    $row = $db->qry_first("SELECT u.username, p.*, UNIX_TIMESTAMP(p.start) AS start, UNIX_TIMESTAMP(p.end) AS end FROM %prefix%partylist AS p
-   LEFT JOIN %prefix%user AS u on p.userid = u.userid
- WHERE p.partyid = %int%", $_GET['partyid']);
+    $row = $db->qry_first("
+      SELECT
+        u.username,
+        p.*,
+        UNIX_TIMESTAMP(p.start) AS start,
+        UNIX_TIMESTAMP(p.end) AS end
+      FROM %prefix%partylist AS p
+      LEFT JOIN %prefix%user AS u on p.userid = u.userid
+      WHERE
+        p.partyid = %int%", $_GET['partyid']);
     $framework->AddToPageTitle($row["name"]);
 
     if (substr($row['url'], 0, 7) != 'http://') {
