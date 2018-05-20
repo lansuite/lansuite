4a82889c71c302026c50a2a3f86ae87f4b6762c4
Andy Grunwald <andygrunwald@gmail.com>
2018-05-20 22:35:36 +0200

    Module "sponsor": Rework of the module
    - Fixed wrong constant error
    - Applied CGL formatting
    - Added CGL check to TravisCI
    - Deleted dead code
    - Moved functions to Functions/-Folder

--- modules/sponsor/gotourl.php
+++ modules/sponsor/gotourl.php
@@ -1,18 +1,4 @@
 <?php
-/*************************************************************************
-*
-*	Lansuite - Webbased LAN-Party Management System
-*	-------------------------------------------------------------------
-*	Lansuite Version:	2.0
-*	File Version:		2.1
-*	Filename: 			show.php
-*	Module: 			Sponsorpresentation
-*	Main editor: 		jochen@one-network.org
-*	Last change: 		29.05.2004
-*	Description: 		show a list of sponsors
-*	Remarks:
-*
-**************************************************************************/
 
 $sponsoren = $db->qry("UPDATE %prefix%sponsor SET hits = hits + 1 WHERE url=%string%", $_GET["url"]);
 
