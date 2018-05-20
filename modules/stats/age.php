a2b1229f88c957062811a1bdc5e52d471d7fb518
Andy Grunwald <andygrunwald@gmail.com>
2018-05-20 23:03:19 +0200

    Module "stats": Rework of the module
    - Delete unused and dead code
    - Moved class Stats to Classes folder and applied namespace
    - Applied CGL formatting
    - Added CGL check to TravisCI

--- modules/stats/age.php
+++ modules/stats/age.php
@@ -2,8 +2,14 @@
 
 $dsp->NewContent(t('Benutzeralter'), '');
 
-$res = $db->qry('SELECT birthday, DATE_FORMAT(FROM_DAYS(TO_DAYS(NOW()) - TO_DAYS(birthday)), \'%Y\') + 0 AS age, COUNT(*) AS anz
-FROM %prefix%user GROUP BY age ORDER BY age');
+$res = $db->qry('
+  SELECT
+    birthday,
+    DATE_FORMAT(FROM_DAYS(TO_DAYS(NOW()) - TO_DAYS(birthday)), \'%Y\') + 0 AS age,
+    COUNT(*) AS anz
+  FROM %prefix%user
+  GROUP BY age
+  ORDER BY age');
 
 while ($row = $db->fetch_array($res)) {
     if ($row['birthday'] == '0000-00-00') {
