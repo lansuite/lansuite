fd6ba2e3e21cd8d874d88ab258056795cd97dc0d
M4LuZ <MaLuZ@gmx.net>
2018-05-25 10:27:32 +0200

    Added error messages to both map displays
    Now shows errors if prerequisites are not met.
    Found that the wrong config key was used in the partymap
    Also added a @todo to Func, as the IP check seems to not support IPv6 addresses (I guess we can replace that with native PHP functionality?)
    
    refs #301

--- modules/partylist/map.php
+++ modules/partylist/map.php
@@ -10,12 +10,18 @@ if ($party->party_id) {
 $res = $db->qry("SELECT *, UNIX_TIMESTAMP(start) AS start, UNIX_TIMESTAMP(end) AS end FROM %prefix%partylist AS p WHERE start > NOW()");
 
 $addresses = '';
-while ($row = $db->fetch_array($res)) {
-    $text = "<b>{$row['name']}</b><br />- {$row['motto']} -<br>". $func->unixstamp2date($row['start'], 'datetime') .' - '. $func->unixstamp2date($row['end'], 'datetime') ."<br>{$row['street']} {$row['hnr']}, {$row['plz']} {$row['city']}";
-    $addresses .= "showAddress('Germany', '". addslashes($row['city']) ."', '". addslashes($row['plz']) ."', '". addslashes($row['street']) ."', '". addslashes($row['hnr']) ."', '". addslashes($text) ."');\r\n";
+if ($res->num_rows > 0){
+    while ($row = $db->fetch_array($res)) {
+        $text = "<b>{$row['name']}</b><br />- {$row['motto']} -<br>". $func->unixstamp2date($row['start'], 'datetime') .' - '. $func->unixstamp2date($row['end'], 'datetime') ."<br>{$row['street']} {$row['hnr']}, {$row['plz']} {$row['city']}";
+        $addresses .= "showAddress('Germany', '". addslashes($row['city']) ."', '". addslashes($row['plz']) ."', '". addslashes($row['street']) ."', '". addslashes($row['hnr']) ."', '". addslashes($text) ."');\r\n";
+    }
+    $smarty->assign('addresses', $addresses);
+    if (!empty($cfg['google_analytics_id'])){
+        $smarty->assign('apikey', $cfg['google_analytics_id']);
+        $dsp->AddSingleRow($smarty->fetch('modules/guestlist/templates/googlemaps.htm'));
+    } else {
+        $func->error(t('Die Google Analytics ID wurde nicht konfiguriert, ist aber notwendig zur Benutzung von Google Maps-Diensten.'));
+    }
+} else {
+     $func->error(t('Es wurden keine Party-Einträge gefunden'));
 }
-$db->free_result($haus_data);
-$smarty->assign('addresses', $addresses);
-
-$smarty->assign('apikey', $cfg['google_maps_api_key']);
-$dsp->AddSingleRow($smarty->fetch('modules/guestlist/templates/googlemaps.htm'));
