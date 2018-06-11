e13a8d2bdbf244c0cc69bf837013d13bee5e6bf0
M4LuZ <MaLuZ@gmx.net>
2018-06-11 22:22:45 +0200

    restricting type field to only "sql" or "php"

--- modules/cron2/add.php
+++ modules/cron2/add.php
@@ -4,7 +4,8 @@ $dsp->NewContent(t('Cronjob hinzufügen'), '');
 $mf = new \LanSuite\MasterForm();
 
 $mf->AddField(t('Name'), 'name');
-$mf->AddField(t('Typ'), 'type');
+$selections = ['sql' => 'SQL-Befehl','php'=>'PHP-Datei'];
+$mf->AddField(t('Typ'), 'type', \LanSuite\MasterForm::IS_SELECTION, $selections);
 $mf->AddField(t('Statement(SQL)/Datei(PHP)'), 'function');
 $mf->AddField(t('Aktiv'), 'active');
 $mf->AddField(t('Ausführen täglich, um'), 'runat');
