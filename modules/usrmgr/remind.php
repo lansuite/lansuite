46cb296883db4b947886b127c58c9938a56d6665
M4LuZ <MaLuZ@gmx.net>
2018-09-05 23:21:46 +0200

    travis nitpicking

--- modules/usrmgr/remind.php
+++ modules/usrmgr/remind.php
@@ -13,8 +13,8 @@ if (!$cfg['sys_internet']) {
                 $func->information(t('Für den System-Account darf kein neues Passwort generiert werden'), "index.php?mod=usrmgr&action=pwrecover&step=1");
             } elseif ($user_data['username']) {
                 $fcode = '';
-                for ($x=0; $x<=24; $x++) {
-                    $fcode.=chr(mt_rand(65, 90));
+                for ($x = 0; $x <= 24; $x++) {
+                    $fcode .= chr(mt_rand(65, 90));
                 }
 
                 $db->qry("UPDATE %prefix%user SET fcode='$fcode' WHERE email = %string%", $_POST['pwr_mail']);
@@ -24,34 +24,32 @@ if (!$cfg['sys_internet']) {
                 $mail = new \LanSuite\Module\Mail\Mail();
                 //Build the Party-URL based on the settings or the client URL, whatever is available
                 //Try HTTPS first...
-                if (!empty($cfg['sys_partyurl_ssl'])){
+                if (!empty($cfg['sys_partyurl_ssl'])) {
                     $verification_link = $cfg['sys_partyurl_ssl'];
-                    if (substr($cfg['sys_partyurl_ssl'],-1,1) !='/') {$verification_link .= '/';} //make sure that it ends with a slash
-                    $verification_link .= "index.php?mod=usrmgr&action=pwrecover&step=3&fcode=$fcode"; 
+                    if (substr($cfg['sys_partyurl_ssl'], -1, 1) != '/') {
+                        $verification_link .= '/';
+                    } //make sure that it ends with a slash
+                    $verification_link .= "index.php?mod=usrmgr&action=pwrecover&step=3&fcode=$fcode";
                 } else { // fallback to old version, 
                     if (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) {
                         $proto = 'https://';
                     } else {
                         $proto = 'http://';
                     }
-                    $verification_link = $proto . $_SERVER['SERVER_NAME']. ":" . $_SERVER['SERVER_PORT'].$path . "index.php?mod=usrmgr&action=pwrecover&step=3&fcode=$fcode";
+                    $verification_link = $proto . $_SERVER['SERVER_NAME'] . ":" . $_SERVER['SERVER_PORT'] . $path . "index.php?mod=usrmgr&action=pwrecover&step=3&fcode=$fcode";
                 }
-            // Now build the email subject with text replacements...
-            $pwrecoverytxt = str_replace("%PATH%", $verification_link, $cfg['usrmgr_pwrecovery_text']); // Replace pw-reset link
-            $pwrecoverytxt = str_replace("%USERNAME%", $user_data['username'], $pwrecoverytxt); // replace username
-            $pwrecoverytxt = str_replace("%PARTYNAME%", $cfg['sys_page_title'], $pwrecoverytxt); //replace party name
-            
-            //now assemble it all into a mail
-            $mail->create_inet_mail(
-                $user_data['username'], 
-                $_POST['pwr_mail'], 
-                $cfg['usrmgr_pwrecovery_subject'], 
-                $pwrecoverytxt
-            );
-                
-                
-                
-                
+                // Now build the email subject with text replacements...
+                $pwrecoverytxt = str_replace("%PATH%", $verification_link, $cfg['usrmgr_pwrecovery_text']); // Replace pw-reset link
+                $pwrecoverytxt = str_replace("%USERNAME%", $user_data['username'], $pwrecoverytxt); // replace username
+                $pwrecoverytxt = str_replace("%PARTYNAME%", $cfg['sys_page_title'], $pwrecoverytxt); //replace party name
+                //now assemble it all into a mail
+                $mail->create_inet_mail(
+                        $user_data['username'], $_POST['pwr_mail'], $cfg['usrmgr_pwrecovery_subject'], $pwrecoverytxt
+                );
+
+
+
+
                 $func->confirmation(t('Dir wurde nun eine Freischalte-URL an die angegebene Emailadresse gesendet. Mit dem Aufruf dieser URL wird dir neues Passwort generiert werden.'), "index.php");
             } else {
                 $func->information(t('Die von dir eigegebene Email existiert nicht in der Datenbank'), "index.php?mod=usrmgr&action=pwrecover&step=1");
@@ -63,13 +61,13 @@ if (!$cfg['sys_internet']) {
             $user_data = $db->qry_first("SELECT fcode FROM %prefix%user WHERE fcode = %string%", $_GET['fcode']);
             if (($user_data['fcode']) && ($_GET['fcode'] != '')) {
                 $new_pwd = "";
-                for ($x=0; $x<=8; $x++) {
+                for ($x = 0; $x <= 8; $x++) {
                     $new_pwd .= chr(mt_rand(65, 90));
                 }
-        
+
                 $db->qry("UPDATE %prefix%user SET password = %string%, fcode = '' WHERE fcode = %string%", md5($new_pwd), $_GET['fcode']);
 
-                $func->confirmation(t('Das neue Kennwort wurde erfolgreich generiert.<br>Es lautet:') ."\"<b>$new_pwd</b>\"", "index.php");
+                $func->confirmation(t('Das neue Kennwort wurde erfolgreich generiert.<br>Es lautet:') . "\"<b>$new_pwd</b>\"", "index.php");
             } else {
                 $func->error(t('Der von dir übermittelte Freischaltecode ist inkorrekt! Es wurde kein neues Kennwort generiert. Bitte prüfe, ob du die URL komplett aus der Benachrichtigungs-Mail kopiert hast.'), "index.php?mod=usrmgr&action=pwrecover&step=1");
             }
