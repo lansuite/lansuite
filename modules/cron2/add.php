10901d6c33d9b8e7f916f5d8a69186abc0bb3a03
M4LuZ <MaLuZ@gmx.net>
2018-09-12 01:01:59 +0200

    even more of that

--- modules/cron2/add.php
+++ modules/cron2/add.php
@@ -6,7 +6,13 @@ $mf = new \LanSuite\MasterForm();
 $mf->AddField(t('Name'), 'name');
 $mf->AddField(t('Statement'), 'function');
 $mf->AddField(t('Aktiv'), 'active', '', '', \LanSuite\MasterForm::FIELD_OPTIONAL);
-$mf->AddField(t('Typ'), 'type', \LanSuite\MasterForm::IS_SELECTION, array('php'=>'php','sql'=>'sql'), \LanSuite\MasterForm::FIELD_OPTIONAL);
+$mf->AddField(
+    t('Typ'),
+    'type',
+    \LanSuite\MasterForm::IS_SELECTION,
+    array('php'=>'php','sql'=>'sql'),
+    \LanSuite\MasterForm::FIELD_OPTIONAL
+    );
 $mf->AddField(t('Ausführen täglich, um'), 'runat');
 
 $mf->SendForm('index.php?mod=cron2&action=add', 'cron', 'jobid', $_GET['jobid']);
