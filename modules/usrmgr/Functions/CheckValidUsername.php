6725db790bbcb647c747ff94bb68a83010a34319
M4LuZ <MaLuZ@gmx.net>
2018-09-22 23:41:39 +0200

    Fixed my horrible style...hopefully

--- modules/usrmgr/Functions/CheckValidUsername.php
+++ modules/usrmgr/Functions/CheckValidUsername.php
@@ -5,11 +5,12 @@
  * @param string $username The username to be checked
  * @return bool|mixed|string
  */
-function CheckValidUsername($username){
+function CheckValidUsername($username)
+{
     global $cfg, $db;
-    if ($cfg['signon_username_unique']){
+    if ($cfg['signon_username_unique']) {
         $row = $db->qry_first('SELECT * FROM %prefix%user WHERE username=%string%', $username);
-        if ($row){
+        if ($row) {
             return t('Der gewählte Username existiert bereits');
         }
     }
