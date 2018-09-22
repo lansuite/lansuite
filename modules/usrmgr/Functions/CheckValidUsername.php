12ae141e96f32fb7384c10e5b80054564f064a82
M4LuZ <MaLuZ@gmx.net>
2018-09-22 23:37:50 +0200

    Added option to signon module to check username uniqueness. All tested OK
    #fixes 72

---
+++ modules/usrmgr/Functions/CheckValidUsername.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Checks (at the moment only) if the selected username is already existing in the database
+ *
+ * @param string $username The username to be checked
+ * @return bool|mixed|string
+ */
+function CheckValidUsername($username){
+    global $cfg, $db;
+    if ($cfg['signon_username_unique']){
+        $row = $db->qry_first('SELECT * FROM %prefix%user WHERE username=%string%', $username);
+        if ($row){
+            return t('Der gewählte Username existiert bereits');
+        }
+    }
+    return false;
+}
