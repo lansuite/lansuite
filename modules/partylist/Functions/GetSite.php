d7d05f56c83f33f67288b9b16b05fd477fe8785c
Andreas Jakum <aj-gh@users.noreply.github.com>
2019-02-03 17:37:12 +0100

    Partylist: Make Travis happy..

--- modules/partylist/Functions/GetSite.php
+++ modules/partylist/Functions/GetSite.php
@@ -6,5 +6,5 @@
  */
 function GetSite($url)
 {
-    return @file_get_contents($url,false,stream_context_create(array('http' => array('timeout' => 10))));
+    return @file_get_contents($url, false, stream_context_create(array('http' => array('timeout' => 10))));
 }
