f0cb2f2625eb8ea81223d992687b311a4eb91b4b
M4LuZ <MaLuZ@gmx.net>
2022-12-29 18:51:20 +0100

    Fixed issues found by SonarCloud

--- modules/party/Classes/Party.php
+++ modules/party/Classes/Party.php
@@ -392,12 +392,14 @@ class Party
     */
     public function getGuestQty($party_id = NULL)
     {
+        global $cache;
+        
         if (empty($party_id)) {
             $party_id = $this->party_id;
         }
         
         $partyCache = $cache->getItem('party.guestcount.' . $party_id);
-        if (!$partyCache->isHit()){
+        if (!$partyCache->isHit()) {
             // Fetch in one query
             if ($cfg["guestlist_showorga"] == 0) {
                 $querytype = "type = 1";
