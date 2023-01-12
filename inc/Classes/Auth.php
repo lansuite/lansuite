794607ffbbe5bdd8f7f24bf17ae9e545ae04d1d7
Andreas Jakum <aj-gh@users.noreply.github.com>
2023-01-12 20:44:38 +0100

    Limit lasthiturl to 100 chars as we expect it to fit a varchar(100).

--- inc/Classes/Auth.php
+++ inc/Classes/Auth.php
@@ -609,7 +609,7 @@ class Auth
             // If a session loaded no page for over one hour, this counts as a new visit
             $db->qry('UPDATE %prefix%stats_auth SET visits = visits + 1 WHERE (sessid=%string%) AND (lasthit < %int%)', $this->auth["sessid"], $visit_timeout);
             // Update user-stats and lasthit, so the timeout is resetted
-            $db->qry('UPDATE %prefix%stats_auth SET lasthit=%int%, hits = hits + 1, ip=%string%, lasthiturl= %string% WHERE sessid=%string%', $this->timestamp, $this->auth["ip"], $_SERVER['REQUEST_URI'], $this->auth["sessid"]);
+            $db->qry('UPDATE %prefix%stats_auth SET lasthit=%int%, hits = hits + 1, ip=%string%, lasthiturl= %string% WHERE sessid=%string%', $this->timestamp, $this->auth["ip"], substr($_SERVER['REQUEST_URI'],0,100), $this->auth["sessid"]);
         }
 
         // Heartbeat
