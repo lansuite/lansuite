b5762b1604db1c4eca197c6a2906d4e85c3d7e60
mrflobow <30889461+mrflobow@users.noreply.github.com>
2023-01-17 10:40:47 +0100

    * Sonar Fixes

--- modules/mail/Classes/SMTPMail.php
+++ modules/mail/Classes/SMTPMail.php
@@ -6,7 +6,7 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-require 'vendor/autoload.php';
+require_once 'vendor/autoload.php';
 
 class SMTPMail
 {
