5c89cdb4d6a838aaa7560058e5599a60d4ff7538
Andy Grunwald <andygrunwald@gmail.com>
2023-05-26 18:43:29 +0200

    Authentication: Define authentication types

--- inc/base/define.php
+++ inc/base/define.php
@@ -10,3 +10,9 @@ define('VALID_LS', true);
 define('HTML_NEWLINE', '<br/>');
 define('HTML_FONT_ERROR', '<font class="error">');
 define('HTML_FONT_END', '</font>');
+
+// Authentication types
+define('LS_AUTH_TYPE_ANONYMOUS', 0);
+define('LS_AUTH_TYPE_USER', 1);
+define('LS_AUTH_TYPE_ADMIN', 2);
+define('LS_AUTH_TYPE_SUPERADMIN', 3);
