e127f66d3726c1a6c214352ecc1e993537389621
Andy Grunwald <andygrunwald@gmail.com>
2023-05-28 12:08:01 +0200

    Module "Sponsor": Fix PHP warnings when accessin `$banner`

--- modules/sponsor/banner.php
+++ modules/sponsor/banner.php
@@ -8,6 +8,7 @@ if ($httpsServer == 'on' || getenv('REMOTE_ADDR') == "62.67.200.4") {
 } else {
     $where = "rotation AND ((pic_path != '' AND pic_path != 'http://') OR pic_path_banner != '')";
 }
+
 $banner = $db->qry_first("
   SELECT
     sponsorid,
@@ -20,17 +21,19 @@ $banner = $db->qry_first("
 unset($where);
 
 $file_name = '';
-$old_file_name = 'ext_inc/banner/banner_'. substr($banner['pic_path_banner'], strrpos($banner["pic_path_banner"], 'ext_inc/banner/') + 15, strlen($banner['pic_path_banner']));
+$old_file_name = '';
+if ($banner) {
+    $old_file_name = 'ext_inc/banner/banner_'. substr($banner['pic_path_banner'], strrpos($banner["pic_path_banner"], 'ext_inc/banner/') + 15, strlen($banner['pic_path_banner']));
+}
 
-if ($banner['sponsorid']) {
+if ($banner && $banner['sponsorid']) {
     $db->qry("UPDATE %prefix%sponsor SET views_banner = views_banner + 1 WHERE sponsorid = %int%", $banner['sponsorid']);
 }
 
 // If no specific rotation banner is given, use the banner from the sponsor page
-if ($banner['pic_path_banner'] == '' and file_exists($old_file_name)) {
+$file_name = $banner['pic_path_banner'] ?? '';
+if ($banner && $banner['pic_path_banner'] == '' && file_exists($old_file_name)) {
     $file_name = $old_file_name;
-} else {
-    $file_name = $banner['pic_path_banner'];
 }
 
 // If entry is HTML-Code
@@ -42,10 +45,11 @@ if (str_starts_with($file_name, 'html-code://')) {
         $file_name = 'ext_inc/banner/one_network_banner.jpg';
     }
 
-    $code = '<img src="'. $file_name .'" border="1" width="468" height="60" class="img_border" title="'. $banner['name'] .'" alt="Sponsor Banner"/>';
+    $imgTitleTag = ($banner) ? 'title="'. $banner['name'] .'"': '';
+    $code = '<img src="'. $file_name .'" border="1" width="468" height="60" class="img_border" '. $imgTitleTag .' alt="Sponsor Banner"/>';
 
     // Link banner, if in online mode
-    if ($cfg['sys_internet'] and $banner["sponsorid"]) {
+    if ($cfg['sys_internet'] && $banner && $banner["sponsorid"]) {
         $code = '<a href="index.php?mod=sponsor&amp;action=bannerclick&amp;design=base&amp;type=banner&amp;sponsorid='. $banner["sponsorid"] .'" target="_blank">'. $code .'</a>';
     }
   
