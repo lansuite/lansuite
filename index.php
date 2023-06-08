7e9833e646f56bbcbc582b473665f317d487a026
Andy Grunwald <andygrunwald@gmail.com>
2023-06-08 12:19:07 +0200

    Index: Integrate new Database class and setup connections

--- index.php
+++ index.php
@@ -200,8 +200,27 @@ $ms_number = 0;
 // Display Functions (to load the lansuite-templates)
 $dsp = new \LanSuite\Display();
 
-// DB Functions (to work with the databse)
+// DB Functions (to work with the database)
+// TODO Remove the "old" database class, once everything is migrated to the new one (see below)
 $db = new \LanSuite\DB();
+
+// Loading the new database class with standard support for prepared statements.
+$database = new \LanSuite\Database(
+    $config['database']['server'],
+    $config['database']['dbport'] ?? 3306,
+    $config['database']['user'],
+    $config['database']['passwd'],
+    $config['database']['database'],
+    $config['database']['charset'] ?? 'utf8mb4'
+);
+try {
+    $database->connect();
+    $database->setTablePrefix($config['database']['prefix']);
+} catch(\mysqli_sql_exception $e) {
+    $databaseError = $e->getMessage() . ' (' . $e->getCode() . ')';
+    echo HTML_FONT_ERROR . t('Die Verbindung zur Datenbank ist fehlgeschlagen. LanSuite wird abgebrochen. Zurückgegebener MySQL-Fehler: ' . $databaseError) . HTML_FONT_END;
+    exit();
+}
 
 // Security Functions (to lock pages)
 $sec = new \LanSuite\Security();
