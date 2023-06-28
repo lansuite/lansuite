cdac5f2e411921874c1a3dee7b21e5b3c549bf4a
Andy Grunwald <andygrunwald@gmail.com>
2023-06-28 22:09:20 +0200

    Module "Mail": Fix PHP comment

--- modules/mail/Classes/Mail.php
+++ modules/mail/Classes/Mail.php
@@ -121,7 +121,7 @@ class Mail
                 $cfg["mail_smtp_user"],
                 $cfg["mail_smtp_pass"]
             );
-            //Either use explicit sender or use indivdiual user, depends on the SMTP configuration
+            // Either use explicit sender or use indivdiual user, depends on the SMTP configuration
             $from = empty($cfg["mail_smtp_send_from"]) ? $from : $cfg["mail_smtp_send_from"];
             return $smtpMail->sendMail($from, $to_user_email, $subject_text, $msgbody_text, $this->inet_headers);
 
