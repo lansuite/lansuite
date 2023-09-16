00d0c9570ca7ef5e24b6b97ef147731f0b3bbabb
Andy Grunwald <andygrunwald@gmail.com>
2023-09-16 16:36:09 +0200

    [Usermgr] Fix PHP Warnings when you want to signup for a party

--- inc/Classes/MasterForm.php
+++ inc/Classes/MasterForm.php
@@ -539,7 +539,7 @@ class MasterForm
                                             $err = false;
 
                                             // Copy WYSIWYG editor variable
-                                            if (($SQLFieldTypes[$field['name']] == 'text' || $SQLFieldTypes[$field['name']] == 'mediumtext' || $SQLFieldTypes[$field['name']] == 'longtext') && $field['selections'] == self::HTML_WYSIWYG) {
+                                            if (array_key_exists($field['name'], $SQLFieldTypes) && ($SQLFieldTypes[$field['name']] == 'text' || $SQLFieldTypes[$field['name']] == 'mediumtext' || $SQLFieldTypes[$field['name']] == 'longtext') && $field['selections'] == self::HTML_WYSIWYG) {
                                                 $this->FCKeditorID++;
                                                 $_POST[$field['name']] = $_POST['FCKeditor'. $this->FCKeditorID];
                                             }
@@ -548,14 +548,14 @@ class MasterForm
                                             if (!$this->DependOnStarted or $_POST[$this->DependOnField]) {
                                                 // -- Convertions --
                                                 // Convert Post-date to unix-timestap
-                                                if ($SQLFieldTypes[$field['name']] == 'datetime') {
+                                                if (array_key_exists($field['name'], $SQLFieldTypes) && $SQLFieldTypes[$field['name']] == 'datetime') {
                                                     //1997-12-31 23:59:59
                                                     $_POST[$field['name']] = $_POST[$field['name'].'_value_year'] .'-'. $_POST[$field['name'].'_value_month'] .'-'.
                                                     $_POST[$field['name'].'_value_day'] .' '. $_POST[$field['name'].'_value_hours'] .':'. $_POST[$field['name'].'_value_minutes'] .':00';
                                                     $__POST[$field['name']] = $_POST[$field['name']];
                                                 }
 
-                                                if ($SQLFieldTypes[$field['name']] == 'date') {
+                                                if (array_key_exists($field['name'], $SQLFieldTypes) && $SQLFieldTypes[$field['name']] == 'date') {
                                                     $_POST[$field['name']] = $_POST[$field['name'].'_value_year'] .'-'. $_POST[$field['name'].'_value_month'] .'-'. $_POST[$field['name'].'_value_day'];
                                                     $__POST[$field['name']] = $_POST[$field['name']];
                                                 }
@@ -590,11 +590,11 @@ class MasterForm
                                                       $this->error[$field['name']] = t('Bitte fülle dieses Pflichtfeld aus.');
 
                                                 // Check Int
-                                                } elseif (str_contains($SQLFieldTypes[$field['name']], 'int') && $SQLFieldTypes[$field['name']] != 'tinyint(1)' && $SQLFieldTypes[$field['name']] != "enum('0','1')" && $_POST[$field['name']] and (int)$_POST[$field['name']] == 0) {
+                                                } elseif (array_key_exists($field['name'], $SQLFieldTypes) && str_contains($SQLFieldTypes[$field['name']], 'int') && $SQLFieldTypes[$field['name']] != 'tinyint(1)' && $SQLFieldTypes[$field['name']] != "enum('0','1')" && array_key_exists($field['name'], $_POST) && $_POST[$field['name']] && (int) $_POST[$field['name']] == 0) {
                                                       $this->error[$field['name']] = t('Bitte gib eine Zahl ein.');
 
                                                 // Check date
-                                                } elseif (($SQLFieldTypes[$field['name']] == 'datetime' || $SQLFieldTypes[$field['name']] == 'date') && (!checkdate($_POST[$field['name'].'_value_month'], $_POST[$field['name'].'_value_day'], $_POST[$field['name'].'_value_year']) && !($_POST[$field['name'].'_value_month']=="00" && $_POST[$field['name'].'_value_day']=="00" && $_POST[$field['name'].'_value_year']=="0000"))) {
+                                                } elseif (array_key_exists($field['name'], $SQLFieldTypes) && ($SQLFieldTypes[$field['name']] == 'datetime' || $SQLFieldTypes[$field['name']] == 'date') && (!checkdate($_POST[$field['name'].'_value_month'], $_POST[$field['name'].'_value_day'], $_POST[$field['name'].'_value_year']) && !($_POST[$field['name'].'_value_month']=="00" && $_POST[$field['name'].'_value_day']=="00" && $_POST[$field['name'].'_value_year']=="0000"))) {
                                                       $this->error[$field['name']] = t('Das eingegebene Datum ist nicht korrekt.');
 
                                                 // Check new passwords
@@ -606,7 +606,7 @@ class MasterForm
                                                       $this->error['captcha'] = t('Captcha falsch wiedergegeben.');
 
                                                 // No \r \n \t \0 \x0B in Non-Multiline-Fields
-                                                } elseif ($field['type'] != 'text' && $field['type'] != 'mediumtext' && $field['type'] != 'longtext' && $SQLFieldTypes[$field['name']] != 'text' && $SQLFieldTypes[$field['name']] != 'mediumtext' && $SQLFieldTypes[$field['name']] != 'longtext' && !is_array($fieldValue) && ((str_contains($fieldValue, "\r")) || (str_contains($fieldValue, "\n")) || (str_contains($fieldValue, "\t")) || (str_contains($fieldValue, "\0")) || (str_contains($fieldValue, "\x0B")))) {
+                                                } elseif ($field['type'] != 'text' && $field['type'] != 'mediumtext' && $field['type'] != 'longtext' && array_key_exists($field['name'], $SQLFieldTypes) && $SQLFieldTypes[$field['name']] != 'text' && $SQLFieldTypes[$field['name']] != 'mediumtext' && $SQLFieldTypes[$field['name']] != 'longtext' && !is_array($fieldValue) && ((str_contains($fieldValue, "\r")) || (str_contains($fieldValue, "\n")) || (str_contains($fieldValue, "\t")) || (str_contains($fieldValue, "\0")) || (str_contains($fieldValue, "\x0B")))) {
                                                       $this->error[$field['name']] = t('Dieses Feld enthält nicht erlaubte Steuerungszeichen (z.B. einen Tab, oder Zeilenumbruch)');
 
                                                 // Callbacks
@@ -619,7 +619,7 @@ class MasterForm
 
                                                 // Check double uniques
                                                 // Neccessary in Multi Line Edit Mode? If so: Still to do
-                                                if ($SQLFieldUnique[$field['name']]) {
+                                                if (array_key_exists($field['name'], $SQLFieldTypes) && $SQLFieldUnique[$field['name']]) {
                                                     $check_double_where = '';
                                                     if ($this->isChange) {
                                                         $check_double_where = ' AND '. $idname .' != '. (int)$id;
@@ -1146,8 +1146,9 @@ class MasterForm
                                 }
                                 $db_query = substr($db_query, 0, strlen($db_query) - 2);
 
-                                // If the table entry should be created, or deleted wheter the control field is checked
-                                if ($this->AddInsertControllField != '' and !$_POST[$InsContName]) {
+                                // If the table entry should be created, or deleted whether the control field is checked
+                                $InsContNameParameter = $_POST[$InsContName] ?? 0;
+                                if ($this->AddInsertControllField != '' && !$InsContNameParameter) {
                                     $db->qry("DELETE FROM %prefix%%plain% WHERE %plain% %plain% = %int%", $table, $AddKey, $idname, $id);
 
                                 // Send query
