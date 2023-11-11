9ab8102d22d4da7dd2dac9f17039a9fa1877dca4
M4LuZ <MaLuZ@gmx.net>
2023-11-11 18:06:32 +0100

    Cleaned up duplicated checks for _ADMIN

--- modules/info2/change.php
+++ modules/info2/change.php
@@ -3,8 +3,8 @@
 if ($auth['type'] <= 1) {
     $ms2 = new \LanSuite\Module\MasterSearch2\MasterSearch2();
 
-    $ms2->query['from'] = "%prefix%info AS i";
-    $ms2->query['where'] = "i.active";
+    $ms2->query['from'] = '%prefix%info AS i';
+    $ms2->query['where'] = 'i.active';
 
     $ms2->config['EntriesPerPage'] = 50;
 
@@ -19,7 +19,7 @@ if ($auth['type'] <= 1) {
         $_POST['content'] = $_POST['FCKeditor1'] ?? '';
     }
 
-    $stepParameter = $_GET["step"] ?? 0;
+    $stepParameter = $_GET['step'] ?? 0;
     switch ($stepParameter) {
         default:
             $dsp->NewContent(t('Informationsseite - Bearbeiten'), t('Hier kannst du den Inhalt der Info-Seiten editieren.'));
@@ -27,7 +27,7 @@ if ($auth['type'] <= 1) {
 
             $ms2 = new \LanSuite\Module\MasterSearch2\MasterSearch2();
 
-            $ms2->query['from'] = "%prefix%info AS i";
+            $ms2->query['from'] = '%prefix%info AS i';
             $ms2->query['where'] = "i.link = ''";
 
             $ms2->config['EntriesPerPage'] = 50;
@@ -39,21 +39,13 @@ if ($auth['type'] <= 1) {
             $ms2->AddIconField('details', 'index.php?mod=info2&action=show_info2&id=', t('Details'));
             if ($auth['type'] >= \LS_AUTH_TYPE_ADMIN) {
                 $ms2->AddIconField('edit', 'index.php?mod=info2&action=change&step=2&infoID=', t('Editieren'));
-            }
-            if ($auth['type'] >= \LS_AUTH_TYPE_ADMIN) {
-                  $ms2->AddMultiSelectAction('Deaktivieren', 'index.php?mod=info2&action=change&step=20', 1);
-            }
-            if ($auth['type'] >= \LS_AUTH_TYPE_ADMIN) {
-                  $ms2->AddMultiSelectAction('Aktivieren (jedoch nicht verlinken)', 'index.php?mod=info2&action=change&step=21', 1);
-            }
-            if ($auth['type'] >= \LS_AUTH_TYPE_ADMIN) {
-                  $ms2->AddMultiSelectAction('Aktivieren und verlinken', 'index.php?mod=info2&action=change&step=22', 1);
-            }
-            if ($auth['type'] >= \LS_AUTH_TYPE_ADMIN) {
-                  $ms2->AddMultiSelectAction('Aktivieren und verlinken nur für Admins', 'index.php?mod=info2&action=change&step=23', 1);
+                $ms2->AddMultiSelectAction('Deaktivieren', 'index.php?mod=info2&action=change&step=20', 1);
+                $ms2->AddMultiSelectAction('Aktivieren (jedoch nicht verlinken)', 'index.php?mod=info2&action=change&step=21', 1);
+                $ms2->AddMultiSelectAction('Aktivieren und verlinken', 'index.php?mod=info2&action=change&step=22', 1);
+                $ms2->AddMultiSelectAction('Aktivieren und verlinken nur für Admins', 'index.php?mod=info2&action=change&step=23', 1);
             }
             if ($auth['type'] >= \LS_AUTH_TYPE_SUPERADMIN) {
-                  $ms2->AddMultiSelectAction('Löschen', 'index.php?mod=info2&action=change&step=10', 1);
+                $ms2->AddMultiSelectAction('Löschen', 'index.php?mod=info2&action=change&step=10', 1);
             }
 
             $ms2->PrintSearch('index.php?mod=info2', 'i.infoID');
@@ -61,7 +53,7 @@ if ($auth['type'] <= 1) {
             $dsp->AddSingleRow($dsp->FetchSpanButton('Externen Link erstellen', 'index.php?mod=info2&action=change&step=30'));
 
             $ms2 = new \LanSuite\Module\MasterSearch2\MasterSearch2();
-            $ms2->query['from'] = "%prefix%info AS i";
+            $ms2->query['from'] = '%prefix%info AS i';
             $ms2->query['where'] = "i.link != ''";
 
             $ms2->config['EntriesPerPage'] = 50;
@@ -73,17 +65,9 @@ if ($auth['type'] <= 1) {
 
             if ($auth['type'] >= \LS_AUTH_TYPE_ADMIN) {
                 $ms2->AddIconField('edit', 'index.php?mod=info2&action=change&step=30&infoID=', t('Editieren'));
-            }
-            if ($auth['type'] >= \LS_AUTH_TYPE_ADMIN) {
                 $ms2->AddMultiSelectAction('Deaktivieren', 'index.php?mod=info2&action=change&step=20', 1);
-            }
-            if ($auth['type'] >= \LS_AUTH_TYPE_ADMIN) {
                 $ms2->AddMultiSelectAction('Aktivieren (jedoch nicht verlinken)', 'index.php?mod=info2&action=change&step=21', 1);
-            }
-            if ($auth['type'] >= \LS_AUTH_TYPE_ADMIN) {
                 $ms2->AddMultiSelectAction('Aktivieren und verlinken', 'index.php?mod=info2&action=change&step=22', 1);
-            }
-            if ($auth['type'] >= \LS_AUTH_TYPE_ADMIN) {
                 $ms2->AddMultiSelectAction('Aktivieren und verlinken nur für Admins', 'index.php?mod=info2&action=change&step=23', 1);
             }
             if ($auth['type'] >= \LS_AUTH_TYPE_SUPERADMIN) {
@@ -92,7 +76,7 @@ if ($auth['type'] <= 1) {
 
             $ms2->PrintSearch('index.php?mod=info2', 'i.infoID');
             break;
-    
+
         // Generate Editform
         case 2:
             if ($_GET['infoID'] != '') {
@@ -103,7 +87,7 @@ if ($auth['type'] <= 1) {
                     %prefix%info AS i
                     LEFT JOIN %prefix%menu AS m ON i.caption = m.caption AND m.action = 'show_info2'
                   WHERE
-                    i.infoID = %int%", $_GET["infoID"]);
+                    i.infoID = %int%", $_GET['infoID']);
             }
 
             $dsp->NewContent(t('Informationsseite - Bearbeiten'), t('Hier kannst du den Inhalt der Seite editieren.'));
@@ -117,79 +101,79 @@ if ($auth['type'] <= 1) {
                     $valkey = '';
                     $optional = 0;
                 } else {
-                      $valkey = '_'. $val;
-                      $optional = \LanSuite\MasterForm::FIELD_OPTIONAL;
+                    $valkey = '_'.$val;
+                    $optional = \LanSuite\MasterForm::FIELD_OPTIONAL;
                 }
-                $mf->AddField(t('Seitentitel'), 'caption'. $valkey, '', '', $optional);
-                $mf->AddField(t('Untertitel'), 'shorttext'. $valkey, '', '', $optional);
+                $mf->AddField(t('Seitentitel'), 'caption'.$valkey, '', '', $optional);
+                $mf->AddField(t('Untertitel'), 'shorttext'.$valkey, '', '', $optional);
                 if ($cfg['info2_use_fckedit']) {
-                    $mf->AddField(t('Text'), 'text'. $valkey, '', \LanSuite\MasterForm::HTML_WYSIWYG, $optional);
+                    $mf->AddField(t('Text'), 'text'.$valkey, '', \LanSuite\MasterForm::HTML_WYSIWYG, $optional);
                 } else {
-                    $mf->AddField(t('Text'), 'text'. $valkey, '', '', $optional);
+                    $mf->AddField(t('Text'), 'text'.$valkey, '', '', $optional);
                 }
                 $mf->AddPage($translation->lang_names[$val]);
             }
             $mf->AdditionalDBUpdateFunction = 'UpdateInfo2';
             $mf->SendForm('index.php?mod=info2&action=change&step=2', 'info', 'infoID', $_GET['infoID']);
 
-            $dsp->AddBackButton("index.php?mod=info2&action=change", "info2/form");
+            $dsp->AddBackButton('index.php?mod=info2&action=change', 'info2/form');
             break;
 
         // Delete entry
         case 10:
-            foreach ($_POST["action"] as $item => $val) {
-                $menu_intem = $db->qry_first("SELECT caption FROM %prefix%info WHERE infoID = %string%", $item);
-                $db->qry("DELETE FROM %prefix%menu WHERE action = 'show_info2' AND caption = %string%", $menu_intem["caption"]);
-                $db->qry("DELETE FROM %prefix%info WHERE infoID = %string%", $item);
+            foreach ($_POST['action'] as $item => $val) {
+                $menu_intem = $db->qry_first('SELECT caption FROM %prefix%info WHERE infoID = %string%', $item);
+                $db->qry("DELETE FROM %prefix%menu WHERE action = 'show_info2' AND caption = %string%", $menu_intem['caption']);
+                $db->qry('DELETE FROM %prefix%info WHERE infoID = %string%', $item);
             }
 
-            $func->confirmation(t('Der Eintrag wurde gelöscht.'), "index.php?mod=info2&action=change");
+            $func->confirmation(t('Der Eintrag wurde gelöscht.'), 'index.php?mod=info2&action=change');
             break;
 
         // Deactivate
         case 20:
             if ($_GET['infoID']) {
-                $_POST["action"][$_GET['infoID']] = '1';
+                $_POST['action'][$_GET['infoID']] = '1';
             }
-            foreach ($_POST["action"] as $item => $val) {
-                $db->qry("UPDATE %prefix%info SET active = 0 WHERE infoID = %string%", $item);
-                $menu_intem = $db->qry_first("SELECT active, caption, shorttext FROM %prefix%info WHERE infoID = %string%", $item);
-                $db->qry("DELETE FROM %prefix%menu WHERE action = 'show_info2' AND caption = %string%", $menu_intem["caption"]);
+            foreach ($_POST['action'] as $item => $val) {
+                $db->qry('UPDATE %prefix%info SET active = 0 WHERE infoID = %string%', $item);
+                $menu_intem = $db->qry_first('SELECT active, caption, shorttext FROM %prefix%info WHERE infoID = %string%', $item);
+                $db->qry("DELETE FROM %prefix%menu WHERE action = 'show_info2' AND caption = %string%", $menu_intem['caption']);
             }
-            $func->confirmation(t('Eintrag deaktiviert'), "index.php?mod=info2&action=change");
+            $func->confirmation(t('Eintrag deaktiviert'), 'index.php?mod=info2&action=change');
             break;
-    
+
         // Activate
         case 21:
             if ($_GET['infoID']) {
-                $_POST["action"][$_GET['infoID']] = '1';
+                $_POST['action'][$_GET['infoID']] = '1';
             }
-            foreach ($_POST["action"] as $item => $val) {
-                $db->qry("UPDATE %prefix%info SET active = 1 WHERE infoID = %string%", $item);
+            foreach ($_POST['action'] as $item => $val) {
+                $db->qry('UPDATE %prefix%info SET active = 1 WHERE infoID = %string%', $item);
             }
-            $func->confirmation(t('Eintrag aktiviert'), "index.php?mod=info2&action=change");
+            $func->confirmation(t('Eintrag aktiviert'), 'index.php?mod=info2&action=change');
             break;
-    
+
         // Activate and link
         case 22:
             if ($_GET['infoID']) {
-                $_POST["action"][$_GET['infoID']] = '1';
+                $_POST['action'][$_GET['infoID']] = '1';
             }
-            foreach ($_POST["action"] as $item => $val) {
-                $menu_intem = $db->qry_first("SELECT active, caption, shorttext, link FROM %prefix%info WHERE infoID = %string%", $item);
+            foreach ($_POST['action'] as $item => $val) {
+                $menu_intem = $db->qry_first('SELECT active, caption, shorttext, link FROM %prefix%info WHERE infoID = %string%', $item);
                 $info_menu = $db->qry_first("SELECT pos FROM %prefix%menu WHERE module='info2'");
 
-                $db->qry("DELETE FROM %prefix%menu WHERE action = 'show_info2' AND caption = %string%", $menu_intem["caption"]);
+                $db->qry("DELETE FROM %prefix%menu WHERE action = 'show_info2' AND caption = %string%", $menu_intem['caption']);
 
-                ($cfg['info2_use_submenus'])? $level = 1 : $level = 0;
+                ($cfg['info2_use_submenus']) ? $level = 1 : $level = 0;
 
                 if ($menu_intem['link']) {
-                    $link = $menu_intem['link'] .'" target="_blank';
+                    $link = $menu_intem['link'].'" target="_blank';
                 } else {
-                    $link = 'index.php?mod=info2&action=show_info2&id='. $item;
+                    $link = 'index.php?mod=info2&action=show_info2&id='.$item;
                 }
 
-                $db->qry("UPDATE %prefix%info SET active = 1 WHERE infoID = %string%", $item);
+                $db->qry('UPDATE %prefix%info SET active = 1 WHERE infoID = %string%', $item);
                 $db->qry("
                   INSERT INTO %prefix%menu
                   SET
@@ -201,31 +185,31 @@ if ($auth['type'] <= 1) {
                     level = %string%,
                     pos = %string%,
                     action = 'show_info2',
-                    file = 'show'", $menu_intem["caption"], $menu_intem["shorttext"], $link, $level, $info_menu["pos"]);
+                    file = 'show'", $menu_intem['caption'], $menu_intem['shorttext'], $link, $level, $info_menu['pos']);
             }
-            $func->confirmation(t('Eintrag aktiviert'), "index.php?mod=info2&action=change");
+            $func->confirmation(t('Eintrag aktiviert'), 'index.php?mod=info2&action=change');
             break;
-    
+
         // Activate and link (admin only)
-        case 23:
+        case 23 :
             if ($_GET['id']) {
-                $_POST["action"][$_GET['id']] = '1';
+                $_POST['action'][$_GET['id']] = '1';
             }
-            foreach ($_POST["action"] as $item => $val) {
-                $menu_intem = $db->qry_first("SELECT active, caption, shorttext, link FROM %prefix%info WHERE infoID = %string%", $item);
+            foreach ($_POST['action'] as $item => $val) {
+                $menu_intem = $db->qry_first('SELECT active, caption, shorttext, link FROM %prefix%info WHERE infoID = %string%', $item);
                 $info_menu = $db->qry_first("SELECT pos FROM %prefix%menu WHERE module='info2'");
 
-                $db->qry("DELETE FROM %prefix%menu WHERE action = 'show_info2' AND caption = %string%", $menu_intem["caption"]);
+                $db->qry("DELETE FROM %prefix%menu WHERE action = 'show_info2' AND caption = %string%", $menu_intem['caption']);
 
-                ($cfg['info2_use_submenus'])? $level = 1 : $level = 0;
+                ($cfg['info2_use_submenus']) ? $level = 1 : $level = 0;
 
                 if ($menu_intem['link']) {
-                    $link = $menu_intem['link'] .'" target="_blank';
+                    $link = $menu_intem['link'].'" target="_blank';
                 } else {
-                    $link = 'index.php?mod=info2&action=show_info2&id='. $item;
+                    $link = 'index.php?mod=info2&action=show_info2&id='.$item;
                 }
 
-                $db->qry("UPDATE %prefix%info SET active = 1 WHERE infoID = %string%", $item);
+                $db->qry('UPDATE %prefix%info SET active = 1 WHERE infoID = %string%', $item);
                 $db->qry("
                   INSERT INTO %prefix%menu
                   SET
@@ -237,13 +221,13 @@ if ($auth['type'] <= 1) {
                     level = %string%,
                     pos = %string%,
                     action = 'show_info2',
-                    file = 'show'", $menu_intem["caption"], $menu_intem["shorttext"], $link, $level, $info_menu["pos"]);
+                    file = 'show'", $menu_intem['caption'], $menu_intem['shorttext'], $link, $level, $info_menu['pos']);
             }
-            $func->confirmation(t('Eintrag aktiviert'), "index.php?mod=info2&action=change");
+            $func->confirmation(t('Eintrag aktiviert'), 'index.php?mod=info2&action=change');
             break;
 
         // Define external link
-        case 30:
+        case 30 :
             $dsp->NewContent(t('Informationsseite - Bearbeiten'), t('Hier kannst du einen externen Link definieren.'));
 
             $mf = new \LanSuite\MasterForm();
@@ -254,15 +238,15 @@ if ($auth['type'] <= 1) {
                 if ($val == 'de') {
                     $val = '';
                 } else {
-                    $val = '_'. $val;
+                    $val = '_'.$val;
                 }
-                $mf->AddField(t('Seitentitel'), 'caption'. $val);
-                $mf->AddField(t('Popup-Infotext'), 'shorttext'. $val);
+                $mf->AddField(t('Seitentitel'), 'caption'.$val);
+                $mf->AddField(t('Popup-Infotext'), 'shorttext'.$val);
             }
             $mf->AdditionalDBUpdateFunction = 'UpdateInfo2';
             $mf->SendForm('index.php?mod=info2&action=change&step=30', 'info', 'infoID', $_GET['infoID']);
 
-            $dsp->AddBackButton("index.php?mod=info2&action=change", "info2/form");
+            $dsp->AddBackButton('index.php?mod=info2&action=change', 'info2/form');
             break;
     }
 }
