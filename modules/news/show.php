5b67e4515c872949b7af7e99ad12799cc4ce07ea
M4LuZ <MaLuZ@gmx.net>
2023-12-27 19:29:30 +0100

    Added visibility field

--- modules/news/show.php
+++ modules/news/show.php
@@ -1,6 +1,7 @@
 <?php
 
-$get_amount = $db->qry_first('SELECT count(*) as number FROM %prefix%news');
+
+$get_amount = $database->queryWithOnlyFirstRow('SELECT count(*) as number FROM %prefix%news');
 $overall_news = $get_amount["number"];
 
 if ($overall_news == 0) {
@@ -19,7 +20,7 @@ if ($overall_news == 0) {
         $newsCompletedConfig = $cfg['news_completed'] ?? 0;
         $pages = $func->page_split($newsPageParameter, $cfg["news_shorted_archiv"], $overall_news - ($cfg['news_shorted'] + $newsCompletedConfig), "index.php?mod=news&action=show&subaction=archive", "news_page");
 
-        $get_newsshorted = $db->qry("SELECT UNIX_TIMESTAMP(n.date) AS date, n.caption, n.text, u.username, n.newsid FROM %prefix%news AS n LEFT JOIN %prefix%user AS u ON u.userid = n.poster ORDER BY n.top DESC, n.date DESC %plain%", $pages["sql"]);
+        $get_newsshorted = $database->query("SELECT UNIX_TIMESTAMP(n.date) AS date, n.caption, n.text, u.username, n.newsid FROM %prefix%news AS n LEFT JOIN %prefix%user AS u ON u.userid = n.poster ORDER BY n.top DESC, n.date DESC " . $pages["sql"]);
         while ($row=$db->fetch_array($get_newsshorted)) {
             $tmpDate = $func->unixstamp2date($row['date'], 'daydate');
             $shortnews[$tmpDate][$row[1]]['caption'] = "<a href=\"index.php?mod=news&amp;action=comment&amp;newsid=" .$row['newsid'] ."\">" .$row['caption'] ."</a>";
