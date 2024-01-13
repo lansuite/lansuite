062ea3e8552b75c55c801fee2f56be4a78d5ab50
M4LuZ <MaLuZ@gmx.net>
2024-01-13 17:36:38 +0100

    added missing hyphen

--- modules/picgallery/download.php
+++ modules/picgallery/download.php
@@ -8,9 +8,9 @@ $requestPath = $request->query->get('picurl');
 if ($file->exists($requestPath)) {
     $fullPath = $file->getFullPath($requestPath);
     //set headers for download
-    header("Content-Type: application/octet-stream");
-    header("Content-Disposition: attachment; filename=" . basename($requestPath) . '"');
-    header("Content-Length: ".filesize($fullPath));   
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename="' . basename($requestPath) . '"');
+    header('Content-Length: '.filesize($fullPath));   
     //dump file content
     $file->outputFileContent($requestPath);
 } else {
