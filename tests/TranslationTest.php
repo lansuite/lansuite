5315daad9101483439337c09513bc3b0334213c0
M4LuZ <MaLuZ@gmx.net>
2024-02-21 21:30:20 +0100

    replaced id with hash to make test pass again

--- tests/TranslationTest.php
+++ tests/TranslationTest.php
@@ -19,7 +19,7 @@ class TranslationTest extends TestCase
             'show_translation_links' => true,
             'sys_language' => '',
         ];
-        $expected = 'This is Sparta foo not bar baz <a href=index.php?mod=misc&action=translation&step=40&id=22><img src=design/images/icon_translate.png height=10 width=10 border=0></a>';
+        $expected = 'This is Sparta foo not bar baz <a href=index.php?mod=misc&action=translation&step=40&id=d81cf8faf0a9b58c77ec6b8c83d75700><img src=design/images/icon_translate.png height=10 width=10 border=0></a>';
 
         $translation = new \LanSuite\Translation();
         $actual = $translation->ReplaceParameters($input, $parameters, $key);
@@ -40,7 +40,7 @@ class TranslationTest extends TestCase
             'show_translation_links' => true,
             'sys_language' => '',
         ];
-        $expected = 'This is Sparta foo not bar baz <a href=index.php?mod=misc&action=translation&step=40&id=5><img src=design/images/icon_translate.png height=10 width=10 border=0></a>';
+        $expected = 'This is Sparta foo not bar baz <a href=index.php?mod=misc&action=translation&step=40&id=c397dc81823c06c7a52a5c8fa32e340f><img src=design/images/icon_translate.png height=10 width=10 border=0></a>';
 
         $translation = new \LanSuite\Translation();
         $actual = $translation->ReplaceParameters($input, $parameters, $key);
