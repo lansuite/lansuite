885f52d141ede17e4701da54a5ae02b8983f223d
Andy Grunwald <andygrunwald@gmail.com>
2024-02-25 10:40:43 +0100

    [Discord] Reformat box code

--- modules/discord/boxes/discord.php
+++ modules/discord/boxes/discord.php
@@ -1,22 +1,26 @@
 <?php
 
-/**
-* Code to trigger Discord box generation and inclusion of the formatting CSS code
-*/
-
 $discord = new \LanSuite\Module\Discord\Discord();
 $discordServerData = $discord->fetchServerData();
-//Load either custom style definition or fall back to default one
-if (file_exists('design/' . $auth['design'] . '/discord.css')) {
-    $framework->add_css_path('design/' . $auth['design'] . '/discord.css');
+
+// Load either custom style definition or fall back to default one
+$customCssPath = 'design/' . $auth['design'] . '/discord.css';
+if (file_exists($customCssPath)) {
+    $framework->add_css_path($customCssPath);
+
 } else {
     $framework->add_css_path('modules/discord/boxes/default.css');
 }
+
 if (!$discordServerData) {
-    // Failed to fetch Discord status XML.
-    // Possible reasons: No connectivity, Discord server issues, Widget not enabled in Discord server settings
+    // Failed to fetch Discord server data.
+    // Possible reasons:
+    //  - No connectivity
+    //  - Discord server issues
+    //  - Widget not enabled in Discord server settings
     // TODO: Improve error reporting.
     $box->Row('<b>Error:</b> Unable to retrieve server data.');
+
 } else {
     $boxcontent = $discord->genBoxContent($discordServerData);
     $box->Row($boxcontent);
