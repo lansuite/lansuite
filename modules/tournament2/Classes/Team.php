bdbae34dc214345bd2bef3ab77159bce207365de
Andy Grunwald <andygrunwald@gmail.com>
2024-03-03 09:42:35 +0100

    [tournament2] Fix coin calculation if no coins are available

--- modules/tournament2/Classes/Team.php
+++ modules/tournament2/Classes/Team.php
@@ -161,6 +161,7 @@ class Team
             teams.leaderid = %int%
             AND t.party_id = %int%
           GROUP BY teams.leaderid", $userid, $party->party_id);
+        $sumTeamCoins = $team_coin["t_coins"] ?? 0;
 
         $member_coin = $db->qry_first("
           SELECT
@@ -171,6 +172,7 @@ class Team
             members.userid = %int%
             AND t.party_id = %int%
           GROUP BY members.userid", $userid, $party->party_id);
+        $sumMemberCoins = $member_coin["t_coins"] ?? 0;
 
         // Is the user already signed on to this tournament?
         if (is_array($team) && $team["found"]) {
@@ -197,7 +199,7 @@ class Team
             $func->information(t('%1 kann diesem Turnier nicht beitreten. In diesem Turnier dürfen nur Benutzer mitspielen, die <b>nicht</b> in einem Unter-18-Block sitzen', $user["username"]));
 
         // Are enough coins left to afford this tournament
-        } elseif (($cfg["t_coins"] - intval($team_coin["t_coins"]) - intval($member_coin["t_coins"]) - intval($t["coins"])) < 0) {
+        } elseif (($cfg["t_coins"] - intval($sumTeamCoins) - intval($sumMemberCoins) - intval($t["coins"])) < 0) {
             $func->information(t('%1 besitzt nicht genügend Coins um an diesem Turnier teilnehmen zu können!', $user["username"]));
 
         // Everything fine
