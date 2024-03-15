6f3c6a5917451d6728a2e033df80b14b8124ad21
Stephan Ruloff <stephan.ruloff@gmail.com>
2024-03-15 11:45:07 +0100

    Fixed broken end-date check

--- modules/party/Functions/CheckEndDate.php
+++ modules/party/Functions/CheckEndDate.php
@@ -7,7 +7,7 @@ function CheckEndDate($enddate): bool|string
 {
     global $func;
 
-    if ($func->str2time($enddate) < $func->str2time($_POST['startdate'])) {
+    if ($func->str2time($enddate) > $func->str2time($_POST['startdate'])) {
         return t('Der Endzeitpunkt muss nach dem Startzeitpunkt liegen');
     } else {
         return false;
