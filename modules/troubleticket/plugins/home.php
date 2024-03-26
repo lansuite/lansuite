9c3b2ce54d60cc7aa7a5f31ffb2a44cdabe81d98
Andy Grunwald <andygrunwald@gmail.com>
2024-03-26 08:33:25 +0100

    [Troubleticket] Switch SQL statements to Prepared Statements API

--- modules/troubleticket/plugins/home.php
+++ modules/troubleticket/plugins/home.php
@@ -5,7 +5,7 @@ $content = '';
   
 // Additional Admin-Stats
 if ($auth['type'] >= \LS_AUTH_TYPE_ADMIN) {
-    $row6 = $db->qry_first("SELECT COUNT(*) AS n FROM %prefix%troubleticket WHERE target_userid = '0'");
-    $row7 = $db->qry_first("SELECT COUNT(*) AS n FROM %prefix%troubleticket");
+    $row6 = $database->queryWithOnlyFirstRow("SELECT COUNT(*) AS n FROM %prefix%troubleticket WHERE target_userid = '0'");
+    $row7 = $database->queryWithOnlyFirstRow("SELECT COUNT(*) AS n FROM %prefix%troubleticket");
     $content .= t('Troubletickets') .": ".$row6["n"]." / ".$row7["n"] . HTML_NEWLINE;
 }
