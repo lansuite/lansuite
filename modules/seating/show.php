3b9245e9e7ee66715a8c5f196c1c0a89c281f3a8
Andy Grunwald <andygrunwald@gmail.com>
2024-05-21 09:00:07 +0200

    SQL quoted keyword `col`

--- modules/seating/show.php
+++ modules/seating/show.php
@@ -166,7 +166,7 @@ switch ($stepParameter) {
           WHERE
             blockid = ?
             AND `row` = ?
-            AND col = ?", [$_GET['blockid'], $_GET['row'], $_GET['col']]);
+            AND `col` = ?", [$_GET['blockid'], $_GET['row'], $_GET['col']]);
 
         // Check paid
         if (!$user_data['paid'] and $cfg['seating_paid_only']) {
