28eb62f694e4a13615530fee29fcdb2b886a8c21
M4LuZ <MaLuZ@gmx.net>
2024-05-22 14:44:06 +0200

    Various minor fixes & improvements (#1137)
    * increase PIN-strength to 6 numbers
    
    * faq: Removed icon duplication and moved icons to end of text
    
    * Added additional variables for replacement
    
    * Added documentation for additional info2 variables
    
    * party: Extended location field to cover a full post address
    
    * fix double use of position 6
    
    * add icon for counter-strike 2

--- modules/faq/show.php
+++ modules/faq/show.php
@@ -2,7 +2,7 @@
 $get_cat = $db->qry("SELECT catid, name FROM %prefix%faq_cat ORDER BY name");
 $count_cat = $db->num_rows($get_cat);
 
-$admin_link = '';
+
 
 if ($count_cat == 0) {
     $func->information(t('Keine Einträge vorhanden.'), "index.php?mod=home");
@@ -10,6 +10,8 @@ if ($count_cat == 0) {
     $dsp->NewContent(t('FAQ'), t('Auf dieser Seite siehst du häufig gestellte Fragen und deren Antworten'));
 
     while ($row = $db->fetch_array($get_cat)) {
+
+        $admin_link = '';
         if ($auth['type'] > \LS_AUTH_TYPE_ADMIN) {
             $admin_link .= $dsp->FetchIcon('delete', 'index.php?mod=faq&object=item&action=delete_cat&catid=' . $row["catid"] . '&step=2');
         }
@@ -17,20 +19,20 @@ if ($count_cat == 0) {
             $admin_link .= $dsp->FetchIcon('edit', 'index.php?mod=faq&object=cat&action=change_cat&catid=' . $row["catid"] . '&step=2');
         }
 
-        $dsp->AddFieldsetStart($admin_link . $row["name"]);
+        $dsp->AddFieldsetStart($row["name"] . $admin_link);
 
         $get_item = $db->qry("SELECT caption,itemid FROM %prefix%faq_item WHERE catid = %int% ORDER BY caption", $row['catid']);
-        $admin_link = '';
+
         while ($row = $db->fetch_array($get_item)) {
+            $admin_link = '';
             if ($auth['type'] > \LS_AUTH_TYPE_ADMIN) {
                 $admin_link .= $dsp->FetchIcon('delete', 'index.php?mod=faq&object=item&action=delete_item&itemid=' . $row["itemid"] . '&step=2');
             }
             if ($auth['type'] > \LS_AUTH_TYPE_USER) {
                 $admin_link .= $dsp->FetchIcon('edit', 'index.php?mod=faq&object=cat&action=change_item&itemid=' . $row["itemid"] . '&step=2');
             }
-            $dsp->AddSingleRow($admin_link . $dsp->FetchLink($func->text2html($row["caption"]), 'index.php?mod=faq&action=comment&itemid='. $row["itemid"]));
+            $dsp->AddSingleRow($dsp->FetchLink($func->text2html($row["caption"]), 'index.php?mod=faq&action=comment&itemid='. $row["itemid"]). $admin_link );
         }
         $dsp->AddFieldsetEnd();
     }
-    $dsp->AddSingleRow($faq_content, "class='menu'");
 }
