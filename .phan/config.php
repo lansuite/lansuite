638bf7c57479bc25f8cc652c39e2553cf7cc07ff
Andy Grunwald <andygrunwald@gmail.com>
2024-05-30 11:43:18 +0200

    Upgrade PHP minimum version from v8.1 to v8.2

--- .phan/config.php
+++ .phan/config.php
@@ -11,7 +11,7 @@ return [
     // If this is set to `null`,
     // then Phan assumes the PHP version which is closest to the minor version
     // of the php executable used to execute Phan.
-    "target_php_version" => 8.1,
+    "target_php_version" => 8.2,
 
     // A list of directories that should be parsed for class and
     // method information. After excluding the directories
